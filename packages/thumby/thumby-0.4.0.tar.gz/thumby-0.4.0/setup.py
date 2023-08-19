8518c0ce3758da1921958d3cd27390346500987c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:14 +0000

    Add package thumby/0.4.0/thumby-0.4.0.tar.gz

---
+++ packages/thumby/thumby-0.4.0.tar.gz/thumby-0.4.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as freadme:
+    long_description = freadme.read()
+
+setup(
+    name='thumby',
+    version='0.4.0',
+    url='https://github.com/ThunderFly-aerospace/thumby/',
+    author='Dusan Jansky',
+    description='Simple python library for inserting .png thumbnails into gcode files.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['thumby'],
+    package_dir={'':'src'},
+    license='MIT',
+    install_requires=['Pillow>=9.0.1'],
+    extras_require = {
+        'dev': [
+            'pytest-runner',
+            'pytest',
+        ],
+    },
+)
