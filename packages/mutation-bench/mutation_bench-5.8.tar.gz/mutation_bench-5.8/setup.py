e6ff47e2dfbf55fc01da761f2998142a2352d67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:31 +0000

    Add package mutation-bench/5.8/mutation_bench-5.8.tar.gz

---
+++ packages/mutation-bench/mutation_bench-5.8.tar.gz/mutation_bench-5.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from setuptools import  find_namespace_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='mutation_bench',
+    version='5.8',
+    description='A tool for Prediction of high-risk cancer patients using mutation profiles',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files = ('LICENSE.txt',),
+    url='https://github.com/raghavagps/mutation_bench', 
+    packages=find_namespace_packages(where="src"),
+    package_dir={'':'src'},
+    package_data={'mutation_bench.perl_scripts':['*']
+    },
+    entry_points={ 'console_scripts' : ['mutation_bench = mutation_bench.muthrp:main']},
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=[
+        'numpy', 'pandas==1.4.0', 'rpy2', 'xgboost','ranger','scikit-learn', 'tqdm',  # Add any Python dependencies here
+    ]
+)
