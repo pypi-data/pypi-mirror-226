d287d4695e62add530f25c427b73301043cc56f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:12 +0000

    Add package nvidia-monitor/0.0.1/nvidia-monitor-0.0.1.tar.gz

---
+++ packages/nvidia-monitor/nvidia-monitor-0.0.1.tar.gz/nvidia-monitor-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nvidia-monitor',
+    version='0.0.1',
+    description='NVIDIA GPU monitoring package written by KwangryeolPark',
+    author='pkr7098',
+    author_email='pkr7098@gmail.com',
+    url='https://github.com/KwangryeolPark/PyPI.nvidia-monitor.git',
+    install_requires=['tqdm'],
+    packages=find_packages(exclude=[]),
+    keywords=['nvidia-smi', 'gpu', 'monitor', 'nvidia', 'nvidia-monitor'],
+    python_requires='>=3.7',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
