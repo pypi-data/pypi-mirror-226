fe40bafaf9feb36caed4e5d6c3046c88f45a55c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:08 +0000

    Add package roguewave/0.2.19/roguewave-0.2.19-py3-none-any.whl

---
+++ packages/roguewave/roguewave-0.2.19-py3-none-any.whl/roguewave/spotter/__init__.py
@@ -0,0 +1,21 @@
+from .read_csv_data import (
+    read_spectra,
+    read_displacement,
+    read_gps,
+    read_raw_spectra,
+    read_baro,
+    read_raindb,
+    read_baro_raw,
+    read_sst,
+    read_gmn,
+    read_location,
+    read_and_concatenate_spotter_csv,
+)
+
+from .analysis import (
+    spectra_from_raw_gps,
+    spectra_from_displacement,
+    displacement_from_gps_doppler_velocities,
+    displacement_from_gps_positions,
+    spotter_frequency_response_correction,
+)
