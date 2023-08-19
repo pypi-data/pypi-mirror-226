d40bcd4d61867922759f650e84f76a03cd104b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:02 +0000

    Add package backend.ai-agent/23.3.11/backend.ai-agent-23.3.11.tar.gz

---
+++ packages/backend.ai-agent/backend.ai-agent-23.3.11.tar.gz/backend.ai-agent-23.3.11/ai/backend/agent/exception.py
@@ -0,0 +1,62 @@
+class InitializationError(Exception):
+    """
+    Errors during agent initialization and compute plugin setup
+    """
+
+    pass
+
+
+class ResourceError(ValueError):
+    pass
+
+
+class UnsupportedResource(ResourceError):
+    pass
+
+
+class InvalidResourceCombination(ResourceError):
+    pass
+
+
+class InvalidResourceArgument(ResourceError):
+    pass
+
+
+class NotMultipleOfQuantum(InvalidResourceArgument):
+    pass
+
+
+class InsufficientResource(ResourceError):
+    pass
+
+
+class UnsupportedBaseDistroError(RuntimeError):
+    pass
+
+
+class ContainerCreationError(Exception):
+    container_id: str
+
+    def __init__(self, container_id: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.container_id = container_id
+
+
+class K8sError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+
+class AgentError(RuntimeError):
+    """
+    A dummy exception class to distinguish agent-side errors passed via
+    aiozmq.rpc calls.
+
+    It carrise two args tuple: the exception type and exception arguments from
+    the agent.
+    """
+
+    def __init__(self, *args, exc_repr: str = None):
+        super().__init__(*args)
+        self.exc_repr = exc_repr
