c41340023cab53a9d49d0bb03f4398fde8d95733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:34 +0000

    Add package math-utilsvik/0.1/math-utilsvik-0.1.tar.gz

---
+++ packages/math-utilsvik/math-utilsvik-0.1.tar.gz/math-utilsvik-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-utilsvik',
+    version='0.1',
+    description='A simple math utilities library',
+    author='Vikram Shinde',
+    author_email='vik.shinde@example.com',
+    url='https://github.com/vikramshinde12/math-utils',
+    packages=find_packages(),
+)
