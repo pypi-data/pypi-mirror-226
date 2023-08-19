77a04beeb9aecf639e7f18873931095e44b36908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:06 +0000

    Add package cougar/0.5.12/cougar-0.5.12-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/cougar/cougar-0.5.12-cp39-cp39-macosx_11_0_x86_64.whl/cougar/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.5.12"
+
+from .rolling import (
+    rolling_argmax,
+    rolling_argmin,
+    rolling_kurt,
+    rolling_max,
+    rolling_mean,
+    rolling_median,
+    rolling_min,
+    rolling_quantile,
+    rolling_rank,
+    rolling_skew,
+    rolling_std,
+    rolling_sum,
+    rolling_var,
+)
