f92b64ace46e83e23ccaa9b08df8b945490cd828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:05 +0000

    Add package TPDNE-utils/0.0.10/TPDNE-utils-0.0.10.tar.gz

---
+++ packages/TPDNE-utils/TPDNE-utils-0.0.10.tar.gz/TPDNE-utils-0.0.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'TPDNE-utils',
+  packages = find_packages(exclude=[]),
+  version = '0.0.10',
+  license='MIT',
+  description = 'TPDNE',
+  include_package_data = True,
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/TPDNE',
+  keywords = [],
+  install_requires = [
+    'beartype',
+    'einops>=0.6',
+    'jinja2',
+    'numpy',
+    'pillow'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
