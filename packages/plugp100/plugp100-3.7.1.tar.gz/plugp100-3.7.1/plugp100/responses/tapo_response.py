cbb27efc2e15f6f269a247e565f37b2c858f6bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:35 +0000

    Add package plugp100/3.7.1/plugp100-3.7.1.tar.gz

---
+++ packages/plugp100/plugp100-3.7.1.tar.gz/plugp100-3.7.1/plugp100/responses/tapo_response.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import TypeVar, Generic, Optional, Any
+
+from plugp100.common.utils.json_utils import Json
+from plugp100.responses.tapo_exception import TapoException
+from plugp100.common.functional.either import Either, Right, Left
+
+T = TypeVar("T")
+
+
+@dataclass
+class TapoResponse(Generic[T]):
+    error_code: int
+    result: Optional[T]
+    msg: Optional[str]
+
+    @staticmethod
+    def try_from_json(json: dict[str, Any]) -> Either["TapoResponse[Json]", Exception]:
+        response = TapoResponse(
+            json.get("error_code", -1),
+            json.get("result", {}),
+            json.get("msg", "No message"),
+        )
+        if response.error_code == 0:
+            return Right(response)
+        else:
+            return Left(TapoException.from_error_code(response.error_code, response.msg))
