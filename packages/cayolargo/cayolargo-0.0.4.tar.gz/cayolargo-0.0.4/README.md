90f34cf1567ed74bd5153151307d8e56fc94fd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 14:05:09 +0000

    Add package cayolargo/0.0.4/cayolargo-0.0.4.tar.gz

---
+++ packages/cayolargo/cayolargo-0.0.4.tar.gz/cayolargo-0.0.4/README.md
@@ -0,0 +1,8 @@
+cayolargo
+=========
+
+Python library for Cryptocurrency Markets Analysis and Crypto Algo-Trading
+
+Creatded by Pawel Lachowicz, PhD
+
+QuantAtRisk.com
---
+++ packages/cayolargo/cayolargo-0.0.4.tar.gz/cayolargo-0.0.4/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.4'
+PACKAGE_NAME = 'cayolargo'
+AUTHOR = 'Pawel Lachowicz'
+AUTHOR_EMAIL = 'enquiries@quantatrisk.com'
+URL = ''
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Python library for Cryptocurrency Markets Analysis and Crypto Algo-Trading'
+LONG_DESCRIPTION = LONG_DESCRIPTION = (HERE / "README.md").read_text()
+
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'python-binance==1.0.17',
+      'matplotlib',
+      'requests'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
