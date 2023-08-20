95c4b5e1b9ca268fe203ebe01e362c53e740c4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:34 +0000

    Add package legend-pygeom-optics/0.4.0/legend_pygeom_optics-0.4.0-py3-none-any.whl

---
+++ packages/legend-pygeom-optics/legend_pygeom_optics-0.4.0-py3-none-any.whl/legendoptics/copper.py
@@ -0,0 +1,33 @@
+"""Copper."""
+
+from __future__ import annotations
+
+import logging
+
+import pint
+from pint import Quantity
+
+from legendoptics.utils import readdatafile
+
+log = logging.getLogger(__name__)
+u = pint.get_application_registry()
+
+
+def copper_reflectivity() -> tuple[Quantity, Quantity]:
+    """Measurements from [Wegmann2017]_.
+
+    .. optics-plot::
+    """
+    return readdatafile("cu_reflectivity.dat")
+
+
+def pyg4_copper_attach_reflectivity(mat, reg) -> None:
+    """Attach the optical reflectivity to the given copper material instance.
+
+    See Also
+    --------
+    .copper_reflectivity
+    """
+    λ, refl = copper_reflectivity()
+    with u.context("sp"):
+        mat.addVecPropertyPint("REFLECTIVITY", λ.to("eV"), refl)
