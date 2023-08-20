b4a8370f78b55075d10f2c347e3beae4e819c9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:26 +0000

    Add package ffun/0.4.0/ffun-0.4.0-py3-none-any.whl

---
+++ packages/ffun/ffun-0.4.0-py3-none-any.whl/ffun/cli/cli.py
@@ -0,0 +1,8 @@
+from .application import app  # noqa: F401
+from .commands import configs  # noqa: F401
+from .commands import supertokens  # noqa: F401
+from .commands import workers  # noqa: F401
+from .commands import yoyo  # noqa: F401
+
+
+__all__ = ["app"]
