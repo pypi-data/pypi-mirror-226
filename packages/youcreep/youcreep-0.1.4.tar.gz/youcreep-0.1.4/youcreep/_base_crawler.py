be75429709711877605db5311915b888457f24c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:33 +0000

    Add package youcreep/0.1.4/youcreep-0.1.4.tar.gz

---
+++ packages/youcreep/youcreep-0.1.4.tar.gz/youcreep-0.1.4/youcreep/_base_crawler.py
@@ -0,0 +1,39 @@
+import abc
+import pathlib
+from typing import Union
+
+from gembox.debug_utils import Debugger
+
+from youcreep.page_parser import YoutubePageParser
+from youcreep.browser_agent import YoutubeBrowserAgent
+
+
+class YoutubeBaseCrawler:
+    def __init__(self, headless=False, debug_tool: Debugger = None, interactor_config_path: Union[str, pathlib.Path] = None):
+        self._browser_agent = YoutubeBrowserAgent(headless=headless, debug_tool=debug_tool, interactor_config_path=interactor_config_path)
+        self._page_parser = YoutubePageParser(browser_agent=self._browser_agent)
+
+    @property
+    async def is_running(self):
+        page = await self._browser_agent.browser_manager.get_page()
+        return self._browser_agent.is_running and page is not None
+
+    async def start(self) -> None:
+        await self._browser_agent.start()
+
+    async def stop(self) -> None:
+        await self._browser_agent.stop()
+
+    @abc.abstractmethod
+    def crawl(self, *args, **kwargs):
+        raise NotImplementedError
+
+    async def __aenter__(self):
+        await self.start()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.stop()
+
+
+__all__ = ["YoutubeBaseCrawler"]
