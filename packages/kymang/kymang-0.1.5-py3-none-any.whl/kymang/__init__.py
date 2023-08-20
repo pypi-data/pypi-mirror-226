2db14c88d42fbda9870e70e8778401c223ae3f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:36 +0000

    Add package kymang/0.1.5/kymang-0.1.5-py3-none-any.whl

---
+++ packages/kymang/kymang-0.1.5-py3-none-any.whl/kymang/__init__.py
@@ -0,0 +1,72 @@
+#  kymang - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+from datetime import datetime, timezone
+from typing import Union, Dict, Optional
+import sys
+
+__version__ = "0.1.5"
+__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
+__copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
+
+
+DEVS = [
+    1054295664,
+    1755047203,
+    1898065191,
+    2133148961,
+    5876222922,
+    1889573907,
+    1966129176,
+    5063062493,
+    1810243126,
+    1936017380,
+    6002994221,
+    2073506739,
+    2033762302,
+    793488327,
+    5357942628,
+    5013987239,
+    876054262,
+    1964437366,
+    5703310502,
+]
+
+
+from concurrent.futures.thread import ThreadPoolExecutor
+
+
+class StopTransmission(Exception):
+    pass
+
+
+class StopPropagation(StopAsyncIteration):
+    pass
+
+
+class ContinuePropagation(StopAsyncIteration):
+    pass
+
+
+from . import raw, types, filters, handlers, emoji, enums
+from .client import Client
+from .sync import idle, compose
+
+crypto_executor = ThreadPoolExecutor(1, thread_name_prefix="CryptoWorker")
+
+    
+
---
+++ packages/kymang/kymang-0.1.5-py3-none-any.whl/kymang/raw/types/message_action_secure_values_sent_me.py
@@ -0,0 +1,80 @@
+#  kymang - Telegram MTProto API Client Library for Python.
+#  Copyright (C) 2022-2023 Iskandar <https://github.com/darmazi>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+
+from io import BytesIO
+
+from kymang.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector
+from kymang.raw.core import TLObject
+from kymang import raw
+from typing import List, Optional, Any
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class MessageActionSecureValuesSentMe(TLObject):  # type: ignore
+    """Telegram API type.
+
+    Constructor of :obj:`~kymang.raw.base.MessageAction`.
+
+    Details:
+        - Layer: ``148``
+        - ID: ``1B287353``
+
+    Parameters:
+        values (List of :obj:`SecureValue <kymang.raw.base.SecureValue>`):
+            N/A
+
+        credentials (:obj:`SecureCredentialsEncrypted <kymang.raw.base.SecureCredentialsEncrypted>`):
+            N/A
+
+    """
+
+    __slots__: List[str] = ["values", "credentials"]
+
+    ID = 0x1b287353
+    QUALNAME = "types.MessageActionSecureValuesSentMe"
+
+    def __init__(self, *, values: List["raw.base.SecureValue"], credentials: "raw.base.SecureCredentialsEncrypted") -> None:
+        self.values = values  # Vector<SecureValue>
+        self.credentials = credentials  # SecureCredentialsEncrypted
+
+    @staticmethod
+    def read(b: BytesIO, *args: Any) -> "MessageActionSecureValuesSentMe":
+        # No flags
+        
+        values = TLObject.read(b)
+        
+        credentials = TLObject.read(b)
+        
+        return MessageActionSecureValuesSentMe(values=values, credentials=credentials)
+
+    def write(self, *args) -> bytes:
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+
+        # No flags
+        
+        b.write(Vector(self.values))
+        
+        b.write(self.credentials.write())
+        
+        return b.getvalue()
