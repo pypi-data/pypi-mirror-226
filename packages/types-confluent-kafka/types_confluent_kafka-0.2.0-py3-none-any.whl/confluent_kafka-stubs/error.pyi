005beb1c89a8ec8d23fb92bfb7aa4a08c6054e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:43 +0000

    Add package types-confluent-kafka/0.2.0/types_confluent_kafka-0.2.0-py3-none-any.whl

---
+++ packages/types-confluent-kafka/types_confluent_kafka-0.2.0-py3-none-any.whl/confluent_kafka-stubs/error.pyi
@@ -0,0 +1,7 @@
+"""
+types-confluent-kafka: A package providing type hints for the confluent-kafka Python package.
+This package is licensed under the Apache 2.0 License.
+"""
+
+from .cimpl import KafkaError as KafkaError
+from .cimpl import KafkaException as KafkaException
