6ea341302c8f94d1f6f15aaea5d3b5fe7c37bf19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:50 +0000

    Add package fusion-engine/1.1.3/fusion-engine-1.1.3.tar.gz

---
+++ packages/fusion-engine/fusion-engine-1.1.3.tar.gz/fusion-engine-1.1.3/release.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+import os
+
+# Get a list of all .py files in the src/fusionengine directory
+py_files = []
+for root, dirs, files in os.walk("src/fusionengine"):
+    for file in files:
+        if file.endswith(".py"):
+            module_path = os.path.join(root, file)
+            module_name = module_path.replace(os.path.sep, ".")[:-3]
+            py_files.append((module_name, module_path))
+
+# Create a list of Extension objects
+extensions = [
+    Extension(module_name, sources=[module_path]) for module_name, module_path in py_files
+]
+
+setup(ext_modules=extensions, include_package_data=True)
