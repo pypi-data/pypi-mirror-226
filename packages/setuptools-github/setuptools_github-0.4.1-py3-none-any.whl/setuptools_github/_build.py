8b3edf57ce1613d26f1dbce4ec385ceed0eaae4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:33 +0000

    Add package setuptools-github/0.4.1/setuptools_github-0.4.1-py3-none-any.whl

---
+++ packages/setuptools-github/setuptools_github-0.4.1-py3-none-any.whl/setuptools_github/_build.py
@@ -0,0 +1,9 @@
+# autogenerate build file
+branch = 'refs/tags/release/0.4.1'
+build = '25'
+current = '0.4.1'
+ref = 'refs/tags/release/0.4.1'
+runid = '5910593981'
+sha = 'f2f50ee7a15f52dfdf8f8d71cc4a42fea5a7a92e'
+version = '0.4.1'
+workflow = 'tags'
