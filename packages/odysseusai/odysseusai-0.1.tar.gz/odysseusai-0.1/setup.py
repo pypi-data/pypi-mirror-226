9a70b7adaeb91d33192d19e69d80926703f9c7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:31 +0000

    Add package odysseusai/0.1/odysseusai-0.1.tar.gz

---
+++ packages/odysseusai/odysseusai-0.1.tar.gz/odysseusai-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odysseusai",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Christos Ziakas",
+    author_email="chziakas@gmail.com",
+    description="A package to log LLM models",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chziakas/odysseus_ai",
+)
