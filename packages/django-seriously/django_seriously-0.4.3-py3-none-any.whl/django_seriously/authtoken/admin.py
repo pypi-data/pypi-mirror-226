dc02defb53fdaf98c4eccb999f19303099901384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:46 +0000

    Add package django-seriously/0.4.3/django_seriously-0.4.3-py3-none-any.whl

---
+++ packages/django-seriously/django_seriously-0.4.3-py3-none-any.whl/django_seriously/authtoken/admin.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.contrib import admin, messages
+from django.contrib.admin import ModelAdmin
+from django.utils.translation import gettext_lazy as _
+
+from django_seriously.authtoken.forms import TokenChangeForm
+from django_seriously.authtoken.models import Token
+from django_seriously.authtoken.utils import generate_token
+
+
+class TokenAdmin(ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "name",
+        "scopes",
+    )
+    form = TokenChangeForm
+
+    def save_model(self, request, obj: Token, form, change):
+        if not change:
+            token = generate_token()
+            self.message_user(
+                request=request,
+                message=_(
+                    'New bearer token is "{}". This can only be viewed once!'
+                ).format(token.encoded_bearer),
+                level=messages.WARNING,
+            )
+            obj.id = token.id
+            obj.key = token.key
+        return super().save_model(request, obj, form, change)
+
+    def get_readonly_fields(self, request, obj=None):
+        return "id", "created_at", "updated_at", "last_seen_at"
+
+
+if "django_seriously.authtoken" in settings.INSTALLED_APPS:
+    admin.site.register(Token, TokenAdmin)
---
+++ packages/django-seriously/django_seriously-0.4.3-py3-none-any.whl/django_seriously/authtoken/models.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy as _
+
+from django_seriously.settings import seriously_settings
+from django_seriously.utils.models import BaseModel
+
+
+class Token(BaseModel):
+    name = models.CharField(max_length=25, blank=True)
+    key = models.CharField(_("Key"), max_length=128)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="auth_tokens",
+        on_delete=models.CASCADE,
+    )
+    scopes = models.CharField(
+        blank=True,
+        max_length=50,
+        help_text=(
+            f"comma-separated list of scopes. choices are "
+            f"{','.join(seriously_settings.AUTH_TOKEN_SCOPES) or 'n/a'}."
+        ),
+    )
+    last_seen_at = models.DateTimeField(blank=True, null=True)
+
+    @cached_property
+    def scope_list(self):
+        return self.scopes.split(",")
+
+    def clean(self) -> None:
+        super().clean()
+        if not self.scopes:
+            scopes = []
+        elif isinstance(self.scopes, (list, tuple)):
+            scopes = self.scopes
+        elif isinstance(self.scopes, str):
+            scopes = self.scopes.split(",")
+        else:
+            raise ValidationError({"scopes": "invalid scopes input"})
+
+        valid_scopes = seriously_settings.AUTH_TOKEN_SCOPES
+        if valid_scopes and not all([s in valid_scopes for s in scopes]):
+            raise ValidationError(
+                {"scopes": f"invalid scope choices. valid choices are: {valid_scopes}"}
+            )
+        self.scopes = ",".join(scopes)
+
+    def __str__(self):
+        return f"{self.name} ({self.user})"
+
+    class Meta:
+        abstract = "django_seriously.authtoken" not in settings.INSTALLED_APPS
