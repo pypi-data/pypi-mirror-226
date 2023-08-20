ddd4e7bb40c79d7463f939601b87a6c306ad2ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:42 +0000

    Add package kymang/0.0.3/kymang-0.0.3.tar.gz

---
+++ packages/kymang/kymang-0.0.3.tar.gz/kymang-0.0.3/kymang/methods/calls/leave_group_call.py
@@ -0,0 +1,39 @@
+from typing import Union
+
+import kymang
+from kymang import raw
+
+
+
+class LeaveGroupCall:
+    async def leave_group_call(
+        self: "kymang.Client",
+        chat_id: Union[int, str]
+    ) -> "kymang.raw.base.Updates":
+        """ Leave group call
+        """
+        group_call = await self.get_group_call(chat_id)
+
+        if group_call is None:
+            return None
+
+        call = group_call.call
+        participants = group_call.participants
+        source = None
+
+        for user in participants:
+            if user and user.is_self:
+                source = user.source
+
+        if source is None:
+            return None
+
+        return await self.invoke(
+            raw.functions.phone.LeaveGroupCall(
+                call=raw.types.InputGroupCall(
+                    id=call.id,
+                    access_hash=call.access_hash
+                ),
+                source=source
+            )
+        )
