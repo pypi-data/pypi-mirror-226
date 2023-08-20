461dcd1945d482608084150a51c3563cbe56afc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:31 +0000

    Add package pynear/0.1.0/pynear-0.1.0-cp37-cp37m-win_amd64.whl

---
+++ packages/pynear/pynear-0.1.0-cp37-cp37m-win_amd64.whl/pynear/benchmark/generate_datasets.py
@@ -0,0 +1,20 @@
+import glob
+import os
+
+import numpy as np
+from PIL import Image
+
+import pynear
+from pynear.benchmark.dataset import BenchmarkDataset
+
+
+def main():
+    datasets = BenchmarkDataset.available_datasets()
+    for dataset in datasets:
+        print(">>>>>>>>>", dataset.name(), dataset.dimension(), dataset.size())
+        # force memory release
+        dataset.unload_data()
+
+
+if __name__ == "__main__":
+    main()
