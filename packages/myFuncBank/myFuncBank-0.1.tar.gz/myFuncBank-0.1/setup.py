ea152788e1c6d800cf583e570e29e51d5c239b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:59 +0000

    Add package myFuncBank/0.1/myFuncBank-0.1.tar.gz

---
+++ packages/myFuncBank/myFuncBank-0.1.tar.gz/myFuncBank-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myFuncBank",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # 添加你的依赖项
+        "pandas",
+        "os",
+        "warnings"
+    ],
+    author="Alex Han",
+    author_email="Mr.Alex.usa@gmail.com",
+    description="This is my private function bank package",
+    url="https://github.com/yourusername/mypackage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
