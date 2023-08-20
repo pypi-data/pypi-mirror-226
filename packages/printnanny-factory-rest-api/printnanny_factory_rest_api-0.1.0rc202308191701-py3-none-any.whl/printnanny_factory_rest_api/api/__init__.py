d40059b655670df2b2c369911bbef35eb283416c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:49 +0000

    Add package printnanny-factory-rest-api/0.1.0rc202308191701/printnanny_factory_rest_api-0.1.0rc202308191701-py3-none-any.whl

---
+++ packages/printnanny-factory-rest-api/printnanny_factory_rest_api-0.1.0rc202308191701-py3-none-any.whl/printnanny_factory_rest_api/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from printnanny_factory_rest_api.api.auth_api import AuthApi
+from printnanny_factory_rest_api.api.printers_api import PrintersApi
+from printnanny_factory_rest_api.api.reservations_api import ReservationsApi
+from printnanny_factory_rest_api.api.schema_api import SchemaApi
+from printnanny_factory_rest_api.api.users_api import UsersApi
+from printnanny_factory_rest_api.api.workspaces_api import WorkspacesApi
+
