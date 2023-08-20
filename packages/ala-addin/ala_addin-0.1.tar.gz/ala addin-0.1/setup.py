2230dff48fcfe4ed0e7e0b4194f0ccf8ba4392d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:12 +0000

    Add package ala-addin/0.1/ala_addin-0.1.tar.gz

---
+++ packages/ala-addin/ala_addin-0.1.tar.gz/ala addin-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+        name='ala addin',
+        version='0.1',
+        description='libery in python ',
+        author='ala addin',
+        packages=setuptools.find_packages()
+)
