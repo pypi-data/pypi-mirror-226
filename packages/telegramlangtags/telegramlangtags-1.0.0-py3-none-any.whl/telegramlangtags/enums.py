6e121fe697279992ff1cdc356dc3ee7dee434347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:46 +0000

    Add package telegramlangtags/1.0.0/telegramlangtags-1.0.0-py3-none-any.whl

---
+++ packages/telegramlangtags/telegramlangtags-1.0.0-py3-none-any.whl/telegramlangtags/enums.py
@@ -0,0 +1,554 @@
+from enum import StrEnum
+
+
+class LanguageCode(StrEnum):
+    """Telegram language codes Enum ISO-639-1 without region"""
+
+    AB = "ab"
+    """Abkhazian language"""
+
+    AA = "aa"
+    """Afar language"""
+
+    AF = "af"
+    """Afrikaans language"""
+
+    AK = "ak"
+    """Akan language"""
+
+    SQ = "sq"
+    """Albanian language"""
+
+    AM = "am"
+    """Amharic language"""
+
+    AR = "ar"
+    """Arabic language"""
+
+    AN = "an"
+    """Aragonese language"""
+
+    HY = "hy"
+    """Armenian language"""
+
+    AS = "as"
+    """Assamese language"""
+
+    AV = "av"
+    """Avaric language"""
+
+    AE = "ae"
+    """Avestan language"""
+
+    AY = "ay"
+    """Aymara language"""
+
+    AZ = "az"
+    """Azerbaijani language"""
+
+    BM = "bm"
+    """Bambara language"""
+
+    BA = "ba"
+    """Bashkir language"""
+
+    EU = "eu"
+    """Basque language"""
+
+    BE = "be"
+    """Belarusian language"""
+
+    BN = "bn"
+    """Bengali language"""
+
+    BI = "bi"
+    """Bislama language"""
+
+    BS = "bs"
+    """Bosnian language"""
+
+    BR = "br"
+    """Breton language"""
+
+    BG = "bg"
+    """Bulgarian language"""
+
+    MY = "my"
+    """Burmese language"""
+
+    CA = "ca"
+    """Catalan, Valencian languages"""
+
+    CH = "ch"
+    """Chamorro language"""
+
+    CE = "ce"
+    """Chechen language"""
+
+    NY = "NY"
+    """Chichewa, Chewa, Nyanja languages"""
+
+    ZH = "zh"
+    """Chinese language"""
+
+    CU = "cu"
+    """Church Slavonic, Old Slavonic, Old Church Slavonic languages"""
+
+    CV = "cv"
+    """Chuvash language"""
+
+    KW = "kw"
+    """Cornish language"""
+
+    CO = "co"
+    """Corsican language"""
+
+    CR = "cr"
+    """Cree language"""
+
+    HR = "hr"
+    """Croatian language"""
+
+    CS = "cs"
+    """Czech language"""
+
+    DA = "da"
+    """Danish language"""
+
+    DV = "dv"
+    """Divehi, Dhivehi, Maldivian languages"""
+
+    NL = "nl"
+    """Dutch, Flemish languages"""
+
+    DZ = "dz"
+    """Dzongkha language"""
+
+    EN = "en"
+    """English language"""
+
+    EO = "eo"
+    """Esperanto language"""
+
+    ET = "et"
+    """Estonian language"""
+
+    EE = "ee"
+    """Ewe language"""
+
+    FO = "fo"
+    """Faroese language"""
+
+    FJ = "fj"
+    """Fijian language"""
+
+    FI = "fi"
+    """Finnish language"""
+
+    FR = "fr"
+    """French language"""
+
+    FY = "fy"
+    """Western Frisian language"""
+
+    FF = "ff"
+    """Fulah language"""
+
+    GD = "gd"
+    """Gaelic, Scottish Gaelic languages"""
+
+    GL = "gl"
+    """Galician language"""
+
+    LG = "lg"
+    """Ganda language"""
+
+    KA = "ka"
+    """Georgian language"""
+
+    DE = "de"
+    """German language"""
+
+    EL = "el"
+    """Greek, Modern (1453–) languag"""
+
+    KL = "kl"
+    """Kalaallisut, Greenlandic languages"""
+
+    GN = "gn"
+    """Guarani language"""
+
+    GU = "gu"
+    """Gujarati language"""
+
+    HT = "ht"
+    """Haitian, Haitian Creole languages"""
+
+    HA = "ha"
+    """Hausa language"""
+
+    HE = "he"
+    """Hebrew language"""
+
+    HZ = "hz"
+    """Herero language"""
+
+    HI = "hi"
+    """Hindi language"""
+
+    HO = "ho"
+    """Hiri Motu language"""
+
+    HU = "hu"
+    """Hungarian language"""
+
+    IS = "is"
+    """Icelandic"""
+
+    IO = "io"
+    """Ido"""
+
+    IG = "ig"
+    """Igbo"""
+
+    ID = "id"
+    """Indonesian"""
+
+    IA = "ia"
+    """Interlingua"""
+
+    IE = "ie"
+    """Interlingue, Occidental"""
+
+    IU = "iu"
+    """Inuktitut"""
+
+    IK = "ik"
+    """Inupiaq"""
+
+    GA = "ga"
+    """Irish"""
+
+    IT = "it"
+    """Italian"""
+
+    JA = "ja"
+    """Japanese"""
+
+    JV = "jv"
+    """Javanese"""
+
+    KN = "kn"
+    """Kannada"""
+
+    KR = "kr"
+    """Kanuri"""
+
+    KS = "ks"
+    """Kashmiri"""
+
+    KK = "kk"
+    """Kazakh"""
+
+    KM = "km"
+    """Central Khmer"""
+
+    KI = "ki"
+    """Kikuyu, Gikuyu"""
+
+    RW = "rw"
+    """Kinyarwanda"""
+
+    KY = "ky"
+    """Kirghiz, Kyrgyz"""
+
+    KV = "kv"
+    """Komi"""
+
+    KG = "kg"
+    """Kongo"""
+
+    KO = "ko"
+    """Korean"""
+
+    KJ = "kj"
+    """Kuanyama, Kwanyama"""
+
+    KU = "ku"
+    """Kurdish"""
+
+    LO = "lo"
+    """Lao"""
+
+    LA = "la"
+    """Latin"""
+
+    LV = "lv"
+    """Latvian"""
+
+    LI = "li"
+    """Limburgan"""
+
+    LN = "ln"
+    """Lingala"""
+
+    LT = "lt"
+    """Lithuanian"""
+
+    LU = "lu"
+    """Luba-Katanga"""
+
+    LB = "lb"
+    """Luxembourgish"""
+
+    MK = "mk"
+    """Macedonian"""
+
+    MG = "mg"
+    """Malagasy"""
+
+    MS = "ms"
+    """Malay"""
+
+    ML = "ml"
+    """Malayalam"""
+
+    MT = "mt"
+    """Maltese"""
+
+    GV = "gv"
+    """Manx"""
+
+    MI = "mi"
+    """Maori"""
+
+    MR = "mr"
+    """Marathi"""
+
+    MH = "MH"
+    """Marshallese"""
+
+    MN = "mn"
+    """Mongolian"""
+
+    NA = "na"
+    """Nauru"""
+
+    NV = "nv"
+    """Navajo"""
+
+    ND = "nd"
+    """North Ndebele"""
+
+    NR = "nr"
+    """South Ndebele"""
+
+    NG = "ng"
+    """Ndonga"""
+
+    NE = "ne"
+    """Nepali"""
+
+    NO = "no"
+    """Norwegian"""
+
+    NB = "nb"
+    """Norwegian Bokmål"""
+
+    NN = "nn"
+    """Norwegian Nynorsk"""
+
+    II = "ii"
+    """Sichuan Yi, Nuosu"""
+
+    OC = "oc"
+    """Occitan"""
+
+    OJ = "oj"
+    """Ojibwa"""
+
+    OR = "or"
+    """Oriya"""
+
+    OM = "om"
+    """Oromo"""
+
+    OS = "os"
+    """Ossetian, Ossetic"""
+
+    PI = "pi"
+    """Pali"""
+
+    PS = "ps"
+    """Pashto"""
+
+    FA = "fa"
+    """Persian"""
+
+    PL = "pl"
+    """Polish"""
+
+    PT = "pt"
+    """Portuguese"""
+
+    PA = "pa"
+    """Punjabi"""
+
+    QU = "qu"
+    """Quechua"""
+
+    RO = "ro"
+    """Romanian, Moldavian, Moldovan"""
+
+    RM = "rm"
+    """Romansh"""
+
+    RN = "rn"
+    """Rundi"""
+
+    RU = "ru"
+    """russian"""
+
+    SE = "se"
+    """Northern Sami"""
+
+    SM = "sm"
+    """Samoan"""
+
+    SG = "sg"
+    """Sango"""
+
+    SA = "sa"
+    """Sanskrit"""
+
+    SC = "sc"
+    """Sardinian"""
+
+    SR = "sr"
+    """Serbian"""
+
+    SN = "sn"
+    """Shona"""
+
+    SD = "sd"
+    """Sindhi"""
+
+    SI = "si"
+    """Sinhala"""
+
+    SK = "sk"
+    """Slovak"""
+
+    SL = "sl"
+    """Slovenian"""
+
+    SO = "so"
+    """Somali"""
+
+    ST = "st"
+    """Southern Sotho"""
+
+    ES = "es"
+    """Spanish, Castilian"""
+
+    SU = "su"
+    """Sundanese"""
+
+    SW = "sw"
+    """Swahili"""
+
+    SS = "ss"
+    """Swati"""
+
+    SV = "sv"
+    """Swedish"""
+
+    TL = "tl"
+    """Tagalog"""
+
+    TY = "ty"
+    """Tahitian"""
+
+    TG = "tg"
+    """Tajik"""
+
+    TA = "ta"
+    """Tamil"""
+
+    TT = "tt"
+    """Tatar"""
+
+    TE = "te"
+    """Telugu"""
+
+    TH = "th"
+    """Thai"""
+
+    BO = "bo"
+    """Tibetan"""
+
+    TI = "ti"
+    """Tigrinya"""
+
+    TO = "to"
+    """Tonga (Tonga Islands)"""
+
+    TS = "ts"
+    """Tsonga"""
+
+    TN = "tn"
+    """Tswana"""
+
+    TR = "tr"
+    """Turkish"""
+
+    TK = "tk"
+    """Turkmen"""
+
+    TW = "tw"
+    """Twi"""
+
+    UG = "ug"
+    """Uighur, Uyghur"""
+
+    UK = "uk"
+    """Ukrainian"""
+
+    UR = "ur"
+    """Urdu"""
+
+    UZ = "uz"
+    """Uzbek"""
+
+    VE = "ve"
+    """Venda"""
+
+    VI = "vi"
+    """Vietnamese"""
+
+    VO = "vo"
+    """Volapük"""
+
+    WA = "wa"
+    """Walloon"""
+
+    CY = "cy"
+    """Welsh"""
+
+    WO = "wo"
+    """Wolof"""
+
+    XH = "xh"
+    """Xhosa"""
+
+    YI = "yi"
+    """Yiddish"""
+
+    YO = "yo"
+    """Yoruba"""
+
+    ZA = "za"
+    """Zhuang"""
+
+    ZU = "zu"
+    """Zulu"""
