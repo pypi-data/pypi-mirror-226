678075b5b2cc741f0c0c88252bc279403a368a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:39 +0000

    Add package syntaxlight/0.1.14/syntaxlight-0.1.14.tar.gz

---
+++ packages/syntaxlight/syntaxlight-0.1.14.tar.gz/syntaxlight-0.1.14/syntaxlight/parsers/__init__.py
@@ -0,0 +1,12 @@
+from .json_parser import JsonParser
+from .toml_parser import TomlParser
+from .parser import Parser
+from .c_parser import CParser
+from .xml_parser import XmlParser
+from .shell_parser import ShellParser, ShellCSS
+from .bnf_parser import BNFParser
+from .lua_parser import LuaParser
+from .asm_parser import X86AssemblyParser, RISCVAssmemblyParser
+from .css_parser import CSSParser
+from .makefile_parser import MakefileParser
+from .dot_parser import DotParser
