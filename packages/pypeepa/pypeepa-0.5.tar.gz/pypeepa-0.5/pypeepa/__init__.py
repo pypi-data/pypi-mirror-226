5bb555cd5c7c70c59298d8e02ea11353157b6b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:11 +0000

    Add package pypeepa/0.5/pypeepa-0.5.tar.gz

---
+++ packages/pypeepa/pypeepa-0.5.tar.gz/pypeepa-0.5/pypeepa/__init__.py
@@ -0,0 +1,37 @@
+from .fileInteraction import (
+    getFilePath,
+    asyncListDir,
+    asyncReadJSON,
+    createDirectory,
+    readData,
+    writeFile,
+)
+from .optimisations import processCSVInChunks
+from .userInteraction import (
+    askYNQuestion,
+    getInputFilePath,
+    printArray,
+    selectOptionQuestion,
+    signature,
+    progressBarIterator,
+)
+
+__author__ = "Ishfaq Ahmed"
+__email__ = "ishfaqahmed0837@gmail.com"
+__version__ = "0.5"
+__description__ = ("Custom built utilities for general use",)
+__all__ = (
+    "processCSVInChunks",
+    "getFilePath",
+    "askYNQuestion",
+    "getInputFilePath",
+    "printArray",
+    "selectOptionQuestion",
+    "signature",
+    "asyncListDir",
+    "asyncReadJSON",
+    "createDirectory",
+    "readData",
+    "writeFile",
+    "progressBarIterator",
+)
