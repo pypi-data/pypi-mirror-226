13efddf6f89da371fd16fde0f1fbe39905dcb4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:05 +0000

    Add package zrb/0.0.88/zrb-0.0.88-py3-none-any.whl

---
+++ packages/zrb/zrb-0.0.88-py3-none-any.whl/zrb/builtin/__init__.py
@@ -0,0 +1,27 @@
+from . import group
+from . import base64
+from . import env
+from . import eval
+from . import git
+from . import md5
+from . import explain
+from . import project
+from . import ubuntu
+from . import update
+from . import version
+from . import devtool
+from . import generator
+
+assert group
+assert base64
+assert env
+assert eval
+assert git
+assert md5
+assert explain
+assert project
+assert ubuntu
+assert update
+assert version
+assert devtool
+assert generator
