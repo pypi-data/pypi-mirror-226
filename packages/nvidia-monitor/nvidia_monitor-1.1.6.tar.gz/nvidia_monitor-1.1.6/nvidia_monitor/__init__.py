d8a48e737d9cb51a9f6f8bfdb8f0291f8acede8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:39 +0000

    Add package nvidia-monitor/1.1.6/nvidia_monitor-1.1.6.tar.gz

---
+++ packages/nvidia-monitor/nvidia_monitor-1.1.6.tar.gz/nvidia_monitor-1.1.6/nvidia_monitor/__init__.py
@@ -0,0 +1,7 @@
+from .queries import STATUS_QUERY 
+from .queries import COMPUTE_QUERY
+from .process import Process
+from .status import Status
+
+__version__ = '1.1.6'
+
---
+++ packages/nvidia-monitor/nvidia_monitor-1.1.6.tar.gz/nvidia_monitor-1.1.6/nvidia_monitor/queries.py
@@ -0,0 +1,33 @@
+STATUS_QUERY = [
+    "index",
+    "timestamp",
+    "driver_version",
+    "count",
+    "name",
+    "serial",
+    "uuid",
+    "display_mode",
+    "persistence_mode",
+    "fan.speed",
+    "pstate",
+    "memory.total",
+    "memory.used",
+    "memory.free",
+    "utilization.gpu",
+    "temperature.gpu",
+    "power.draw",
+    "power.draw.average",
+    "power.limit",
+    "power.max_limit",
+]
+
+COMPUTE_QUERY = [
+    "timestamp",
+    "gpu_name",
+    "gpu_bus_id",
+    "gpu_serial",
+    "gpu_uuid",
+    "pid",
+    "process_name",
+    "used_gpu_memory",
+]
