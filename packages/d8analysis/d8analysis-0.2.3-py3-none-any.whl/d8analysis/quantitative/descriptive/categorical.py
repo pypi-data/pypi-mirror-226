3780def7d61bee0067902af15f0fd6a2c6b71ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:44 +0000

    Add package d8analysis/0.2.3/d8analysis-0.2.3-py3-none-any.whl

---
+++ packages/d8analysis/d8analysis-0.2.3-py3-none-any.whl/d8analysis/quantitative/descriptive/categorical.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Exploratory Data Analysis Framework                                                 #
+# Version    : 0.1.19                                                                              #
+# Python     : 3.10.10                                                                             #
+# Filename   : /d8analysis/quantitative/descriptive/categorical.py                                 #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/d8analysis                                         #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Thursday June 8th 2023 02:56:56 am                                                  #
+# Modified   : Saturday August 19th 2023 06:56:16 pm                                               #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
+from dataclasses import dataclass
+import statistics
+from typing import Union
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from dependency_injector.wiring import inject, Provide
+
+from d8analysis.quantitative.descriptive.base import DescriptiveOne
+from d8analysis.container import D8AnalysisContainer
+from d8analysis.visual.base import Canvas
+
+
+# ------------------------------------------------------------------------------------------------ #
+@dataclass
+class CategoricalStats(DescriptiveOne):
+    name: str  # Name of variable
+    length: int  # total  length of variable
+    count: int  # number of non-null values
+    size: float  # total number of bytes
+    mode: Union[int, str]
+    unique: int
+
+    @classmethod
+    def describe(cls, x: Union[pd.Series, np.ndarray], name: str = None) -> None:
+        name = name or cls.get_name(x=x)
+        return cls(
+            name=name,
+            length=len(x),
+            count=len(list(filter(None, x))),
+            size=x.__sizeof__(),
+            mode=statistics.mode(x),
+            unique=len(np.unique(x)),
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class CategoricalFreqDistribution:
+    """Computes the frequency distribution of a categorical random variable
+
+    data (Union[pd.DataFrame, pd.Series]): DataFrame or Series that has a value_counts method.
+    x (str): A key in the DataFrame or Series.
+    caps (bool): Whether to capitalize first letter of variables. Default = True
+    canvas (Canvas): Configuration for the plot method
+
+    """
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, pd.Series],
+        x: str,
+        caps: bool = True,
+        canvas: Canvas = Provide[D8AnalysisContainer.canvas.seaborn],
+    ) -> None:
+        self._data = data
+        self._x = x
+        self._caps = caps
+        self._canvas = canvas()
+        self._freq = None
+
+    def compute(self) -> pd.DataFrame:
+        abs = (
+            self._data[self._x].value_counts(normalize=False, sort=False, ascending=True).to_frame()
+        )
+        rel = (
+            self._data[self._x].value_counts(normalize=True, sort=False, ascending=True).to_frame()
+        )
+        self._freq = abs.join(rel, on=self._x)
+        self._freq.loc["Total"] = self._freq.sum()
+        self._freq["cumulative"] = self._freq["proportion"].cumsum()
+
+        if self._caps:
+            self._x = self._x.capitalize()
+            self._freq.columns = [col.capitalize() for col in self._freq.columns]
+
+        self._freq.loc[self._freq.index[-1], self._freq.columns[-1]] = " "
+        return self._freq
+
+    def plot(self, title: str = None, ax: plt.Axes = None) -> None:  # pragma: no cover
+        if ax is None:
+            _, ax = self._canvas.get_figaxes()
+
+        if title is None:
+            title = "Distribution of " + self._x
+
+        sns.set_palette(palette=self._canvas.palette)
+        sns.set_style(self._canvas.style)
+
+        sns.countplot(data=self._data, x=self._x)
+
+        ax.set_title(label=title)
---
+++ packages/d8analysis/d8analysis-0.2.3-py3-none-any.whl/d8analysis/visual/seaborn/grid.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Exploratory Data Analysis Framework                                                 #
+# Version    : 0.1.19                                                                              #
+# Python     : 3.10.12                                                                             #
+# Filename   : /d8analysis/visual/seaborn/grid.py                                                  #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/d8analysis                                         #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Tuesday August 15th 2023 04:04:08 pm                                                #
+# Modified   : Saturday August 19th 2023 07:25:59 pm                                               #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
+import math
+
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import seaborn as sns
+from dependency_injector.wiring import inject, Provide
+
+from d8analysis.visual.seaborn.base import SeabornVisual
+from d8analysis.visual.seaborn.config import SeabornCanvas
+from d8analysis.container import D8AnalysisContainer
+
+
+# ------------------------------------------------------------------------------------------------ #
+class GridPlot:
+    """Encapsulates a single or multi-plot visualization
+
+    Args:
+        canvas (SeabornCanvas): A dataclass containing the configuration of the canvas
+            for the visualization. Optional.
+
+    """
+
+    @inject
+    def __init__(
+        self,
+        title: str = None,
+        canvas: type[SeabornCanvas] = Provide[D8AnalysisContainer.canvas.seaborn],
+    ) -> None:
+        self._title = title
+        self._canvas = canvas
+        self._plots = []
+
+    def add_plot(self, plot: SeabornVisual) -> None:
+        """Adds a plot object to the visualization
+
+        Args:
+            plot (Plot): A Plot object
+        """
+        self._plots.append(plot)
+
+    def plot(self) -> None:
+        sns.set_style(self._canvas.style)
+        sns.set_palette(self._canvas.palette)
+
+        axes = self._set_axes()
+        for ax, plot in zip(axes, self._plots):
+            plot.plot(ax=ax)
+
+    def _set_axes(self) -> None:
+        """Sets the axis object on each designated plot."""
+        nplots = len(self._plots)
+        nrows = math.ceil(nplots / self._canvas.maxcols)
+        ncols = min(self._canvas.maxcols, nplots)
+
+        fig = plt.figure(
+            layout="constrained", figsize=(self._canvas.width, self._canvas.height * nrows)
+        )
+
+        if self._title is not None:
+            fig.suptitle(self._title)
+
+        gs = GridSpec(nrows=nrows, ncols=ncols, figure=fig)
+
+        axes = []
+
+        for idx, plot in enumerate(self._plots):
+            row = int(idx / ncols)
+            col = idx % ncols
+
+            # If last plot, take all remaining columns.
+            if idx < nplots - 1:
+                ax = fig.add_subplot(gs[row, col])
+            else:
+                ax = fig.add_subplot(gs[row, col:])
+            axes.append(ax)
+        return axes
