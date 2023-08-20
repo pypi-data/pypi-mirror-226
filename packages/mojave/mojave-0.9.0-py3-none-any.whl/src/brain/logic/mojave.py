75379a486c7801515247170620a70767adfd7d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:58 +0000

    Add package mojave/0.9.0/mojave-0.9.0-py3-none-any.whl

---
+++ packages/mojave/mojave-0.9.0-py3-none-any.whl/src/brain/logic/mojave.py
@@ -0,0 +1,29 @@
+import openai
+import os
+import IPython.display
+from IPython.display import Markdown, display
+import read
+from read import augmented_query
+
+augmented_query = read.augmented_query
+# print(augmented_query)
+
+# Set the OpenAI API key
+openai.api_key = os.environ["OPENAI"]
+
+# system message to prime the model
+primer = f"""You are Q&A bot. A highly intelligent system that answers human questions based on the information provded by the user above each question.
+If the information can not be found in the information provided by the user, come up with a plausible answer.
+"""
+
+res = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": primer},
+        {"role": "user", "content": augmented_query},
+    ],
+)
+
+print(res["choices"][0]["message"]["content"])
+# response = display(Markdown(res["choices"][0]["message"]["content"]))
+# print(response)
