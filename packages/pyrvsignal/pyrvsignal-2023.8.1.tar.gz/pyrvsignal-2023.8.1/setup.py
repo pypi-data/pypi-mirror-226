5d025bb0bdf76d60abfbdb7f2b47630a17e57219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:15 +0000

    Add package pyrvsignal/2023.8.1/pyrvsignal-2023.8.1.tar.gz

---
+++ packages/pyrvsignal/pyrvsignal-2023.8.1.tar.gz/pyrvsignal-2023.8.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="pyrvsignal",
+    author="Ravikirana B",
+    version="2023.08.1",
+    author_email="ravikiranb36@gmail.com",
+    description="Signal for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ravikiranb36/pyrvsignal.github.io.git",
+    license="MIT",
+    packages=setuptools.find_packages(exclude=("tests", "venv")),
+    include_package_data=True,
+    package_data={},
+    keywords='pyrvsignal signal thread_safe',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
+)
