ebba0f8d110a4e80f5ff33a9a16f2ef2c055c10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:22 +0000

    Add package kuttle/0.1.0/kuttle-0.1.0.tar.gz

---
+++ packages/kuttle/kuttle-0.1.0.tar.gz/kuttle-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kuttle',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here if needed in the future
+    ],
+    author='Dima Solodukha',
+    author_email='ds@ktl.ai',
+    description='A simple deployment package for ML.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kuttleio/kuttle',  # Replace with your repo URL
+)
