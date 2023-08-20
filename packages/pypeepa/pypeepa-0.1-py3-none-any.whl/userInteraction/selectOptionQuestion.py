461ee3bf33b2e6ad58e16a2a67461bf85e66929e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:14 +0000

    Add package pypeepa/0.1/pypeepa-0.1-py3-none-any.whl

---
+++ packages/pypeepa/pypeepa-0.1-py3-none-any.whl/userInteraction/selectOptionQuestion.py
@@ -0,0 +1,26 @@
+async def selectOptionQuestion(
+    question: str, min: int, max: int, nullPossible: bool = False
+):
+    invalid_input = True
+    while invalid_input:
+        user_input = input(f"{question} ")
+        if user_input == "" and nullPossible:
+            invalid_input = False
+        elif inputIsInt(user_input):
+            if min <= int(user_input) <= max:
+                invalid_input = False
+        else:
+            print(f"\x1B[31mInvalid input! Please input within\x1B[37m {min}-{max}")
+    return user_input
+
+
+def inputIsInt(input):
+    try:
+        val = int(input)
+        return True
+    except ValueError:
+        try:
+            val = float(input)
+            return False
+        except ValueError:
+            return False
