08607276d84e4f5df0bda2cc08fcdf5b1d19f057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:14 +0000

    Add package swiftly-sys/0.0.25/swiftly_sys-0.0.25-py3-none-any.whl

---
+++ packages/swiftly-sys/swiftly_sys-0.0.25-py3-none-any.whl/swiftly/core/main.py
@@ -0,0 +1,88 @@
+import os
+import subprocess
+
+import questionary
+
+from swiftly.utils.loader import Loader
+from swiftly.utils.get import get_config
+from swiftly.utils.cli import clireturn
+from swiftly.utils.do import add_to_config
+from swiftly.utils.check import is_swiftly, is_online, is_using_git
+from swiftly.core.config import SWIFTLY_PROJECT_LOCATION_VAR, SWIFTLY_PROJECT_NAME_VAR
+
+"""
+ACTIVATE:
+"""
+
+def check_swiftly():
+    """check if it's swiftly project, and tell what to do next"""
+    if not is_swiftly():
+        answer = questionary.confirm("This is not a swiftly project. Do you want to convert it to a swiftly project?").ask()
+        if answer:
+            clireturn("init")
+        else:
+            clireturn("exit")
+    else:
+        clireturn("continue")
+
+        
+def update_swiftly():
+    """Update and install the latest version of swiftly-sys."""
+    if not is_online():
+        return
+    
+    loader = Loader()
+    
+    # Determine the appropriate python command based on the platform
+    python_command = "python3" if os.name == "posix" else "python"
+    
+    # Start the loader with a message
+    loader.start("Checking swiftly")
+    
+    try:
+        # Execute pip install to update swiftly-sys and check for errors
+        subprocess.run([python_command, "-m", "pip", "install", "swiftly-sys", "--upgrade", "--break-system-packages"], check=True, stdout=subprocess.PIPE)
+        loader.end("Checked swiftly")
+        
+    except subprocess.CalledProcessError:
+        loader.end("Failed to update swiftly", failed=True)
+
+
+"""
+INIT
+"""
+
+def init():
+    """Initialize the project."""
+    # Check if it's a swiftly project
+    pass
+        
+def makeapp():
+    """Create a new app."""
+    # Logic for creating a new app goes here
+    pass
+
+def install():
+    """Install the necessary dependencies."""
+    # Logic for installing dependencies goes here
+    pass
+
+def uninstall():
+    """Uninstall the project."""
+    # Logic for uninstalling the project goes here
+    pass
+
+def run():
+    """Run the project."""
+    # Logic for running the project goes here
+    pass
+
+def add_framework():
+    """Add a new framework to the project."""
+    # Logic for adding a new framework goes here
+    pass
+
+def custom():
+    """Handle custom operations."""
+    # Logic for custom operations goes here
+    pass
