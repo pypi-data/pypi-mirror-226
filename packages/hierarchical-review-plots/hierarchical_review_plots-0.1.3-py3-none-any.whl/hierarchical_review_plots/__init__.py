0c232879b77c8b9072d12a8ce601a59b542ac7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:28 +0000

    Add package hierarchical-review-plots/0.1.3/hierarchical_review_plots-0.1.3-py3-none-any.whl

---
+++ packages/hierarchical-review-plots/hierarchical_review_plots-0.1.3-py3-none-any.whl/hierarchical_review_plots/__init__.py
@@ -0,0 +1,7 @@
+"""Module of hierarchical review plots"""
+
+__version__ = "0.1.3"
+from .transformations import add_offset  # noqa
+from .polar_bar_plotly import *  # noqa
+from .heat_map_consistent_altair import *  # noqa
+from .weighted_xg_plotly import *  # noqa
