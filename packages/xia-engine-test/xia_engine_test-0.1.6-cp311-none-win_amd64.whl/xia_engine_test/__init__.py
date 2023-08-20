07b59b9dae8ec52007efdcf067b14f04599c5b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:22 +0000

    Add package xia-engine-test/0.1.6/xia_engine_test-0.1.6-cp311-none-win_amd64.whl

---
+++ packages/xia-engine-test/xia_engine_test-0.1.6-cp311-none-win_amd64.whl/xia_engine_test/__init__.py
@@ -0,0 +1,12 @@
+from xia_engine_test.field_test import FieldTest
+from xia_engine_test.document_example import MessageHello, DocumentSimple, External
+from xia_engine_test.models import DocumentBasic
+
+__all__ = [
+    "FieldTest",
+    "MessageHello", "DocumentSimple", "External",
+    "DocumentBasic"
+]
+
+
+__version__ = "0.1.6"
