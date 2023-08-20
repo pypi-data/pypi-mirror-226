78e52b2ac1f915b0f366f0f3c007484780306540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:07 +0000

    Add package Shortners/1.0.5/Shortners-1.0.5-py3-none-any.whl

---
+++ packages/Shortners/Shortners-1.0.5-py3-none-any.whl/Shortners/module.py
@@ -0,0 +1,25 @@
+import aiohttp
+#========================================================================
+
+class Shortners:
+
+    def __init__(self, api, site):
+        self.apis = api
+        self.site = site
+        self.seon = aiohttp.ClientSession
+
+#========================================================================
+
+    async def shortlink(self, recived):
+        async with self.seon() as seion:
+            param = {'api': self.apis, 'url': recived}
+            async with seion.get(self.site, params=param) as res:
+                if res.status == "success" or res.status == 200:
+                    dataes = await res.json()
+                    reurns = dataes["shortenedUrl"]
+                    ruerns = reurns if reurns else None
+                    return ruerns
+                else:
+                    return None
+
+#========================================================================
