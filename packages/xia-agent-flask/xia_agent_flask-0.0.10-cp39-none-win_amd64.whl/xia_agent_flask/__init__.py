dffefc454a5d2283f4e152e4ce1fd25ceedc29c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:54 +0000

    Add package xia-agent-flask/0.0.10/xia_agent_flask-0.0.10-cp39-none-win_amd64.whl

---
+++ packages/xia-agent-flask/xia_agent_flask-0.0.10-cp39-none-win_amd64.whl/xia_agent_flask/__init__.py
@@ -0,0 +1,10 @@
+from xia_agent_flask.api import AgentFunctionApi
+from xia_agent_flask.api import FlaskRequestParser, GcpLogParser, PubsubLogParser
+
+
+__all__ = [
+   "AgentFunctionApi",
+   "FlaskRequestParser", "GcpLogParser", "PubsubLogParser"
+]
+
+__version__ = "0.0.10"
