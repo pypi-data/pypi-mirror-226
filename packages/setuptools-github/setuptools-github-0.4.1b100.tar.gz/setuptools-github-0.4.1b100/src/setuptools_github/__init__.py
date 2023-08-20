690cb7b78f05d4d60decc360ed2fc764aad6c3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:28 +0000

    Add package setuptools-github/0.4.1b100/setuptools-github-0.4.1b100.tar.gz

---
+++ packages/setuptools-github/setuptools-github-0.4.1b100.tar.gz/setuptools-github-0.4.1b100/src/setuptools_github/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.4.1b100"
+__hash__ = "ba8a6dc"
---
+++ packages/setuptools-github/setuptools-github-0.4.1b100.tar.gz/setuptools-github-0.4.1b100/src/setuptools_github/_build.py
@@ -0,0 +1,9 @@
+# autogenerate build file
+branch = 'beta/0.4.1'
+build = '100'
+current = '0.4.1b100'
+ref = 'refs/heads/beta/0.4.1'
+runid = '5910520256'
+sha = 'ba8a6dc8a9550813ded7371ec28114093ebf4fb6'
+version = '0.4.1b100'
+workflow = 'beta'
