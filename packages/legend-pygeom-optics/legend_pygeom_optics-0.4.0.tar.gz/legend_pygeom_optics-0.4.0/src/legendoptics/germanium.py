c0541b30a3a323e5fa017c26208ee8acc5711140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:34 +0000

    Add package legend-pygeom-optics/0.4.0/legend_pygeom_optics-0.4.0.tar.gz

---
+++ packages/legend-pygeom-optics/legend_pygeom_optics-0.4.0.tar.gz/legend_pygeom_optics-0.4.0/src/legendoptics/germanium.py
@@ -0,0 +1,38 @@
+"""Germanium.
+
+.. [Wegmann2017] A. Wegmann “Characterization of the liquid argon veto of the GERDA
+    experiment and its application for the measurement of the 76Ge half-life”
+    (PhD thesis). https://www.mpi-hd.mpg.de/gerda/public/2017/phd2017-anneWegmann.pdf
+"""
+
+from __future__ import annotations
+
+import logging
+
+import pint
+from pint import Quantity
+
+from legendoptics.utils import readdatafile
+
+log = logging.getLogger(__name__)
+u = pint.get_application_registry()
+
+
+def germanium_reflectivity() -> tuple[Quantity, Quantity]:
+    """Measurements from [Wegmann2017]_ (with GERDA dead-layer Li-doped germanium, at room temperature).
+
+    .. optics-plot::
+    """
+    return readdatafile("ge_reflectivity.dat")
+
+
+def pyg4_germanium_attach_reflectivity(mat, reg) -> None:
+    """Attach the optical reflectivity to the given germanium material instance.
+
+    See Also
+    --------
+    .germanium_reflectivity
+    """
+    λ, refl = germanium_reflectivity()
+    with u.context("sp"):
+        mat.addVecPropertyPint("REFLECTIVITY", λ.to("eV"), refl)
---
+++ packages/legend-pygeom-optics/legend_pygeom_optics-0.4.0.tar.gz/legend_pygeom_optics-0.4.0/tests/test_import.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pint
+
+u = pint.get_application_registry()
+
+
+def test_import():
+    import legendoptics  # noqa: F401
+    import legendoptics.copper
+    import legendoptics.fibers
+    import legendoptics.germanium
+    import legendoptics.lar
+    import legendoptics.nylon
+    import legendoptics.silicon
+    import legendoptics.tetratex
+    import legendoptics.tpb
+
+    legendoptics.lar.lar_fano_factor()
+    legendoptics.lar.lar_emission_spectrum()
+    wvl = np.arange(110, 400, 20) * u.nm
+    legendoptics.lar.lar_dielectric_constant_bideau_mehu(wvl)
+    legendoptics.lar.lar_dielectric_constant_cern2020(wvl)
+    legendoptics.lar.lar_dielectric_constant(wvl)
+    legendoptics.lar.lar_refractive_index(wvl)
+    legendoptics.lar.lar_rayleigh(wvl, 87 * u.K)
+    legendoptics.lar.lar_abs_length(wvl)
+    legendoptics.lar.lar_peak_attenuation_length()
+    legendoptics.lar.lar_lifetimes()
+    legendoptics.lar.lar_scintillation_params()
+
+    legendoptics.tpb.tpb_quantum_efficiency()
+    legendoptics.tpb.tpb_refractive_index()
+    legendoptics.tpb.tpb_wls_timeconstant()
+    legendoptics.tpb.tpb_wls_emission()
+    legendoptics.tpb.tpb_wls_absorption()
+
+    legendoptics.fibers.fiber_cladding2_refractive_index()
+    legendoptics.fibers.fiber_cladding1_refractive_index()
+    legendoptics.fibers.fiber_core_refractive_index()
+    legendoptics.fibers.fiber_wls_absorption()
+    legendoptics.fibers.fiber_wls_emission()
+    legendoptics.fibers.fiber_wls_timeconstant()
+    legendoptics.fibers.fiber_absorption_length()
+    legendoptics.fibers.fiber_absorption_path_length()
+
+    legendoptics.tetratex.tetratex_reflectivity()
+
+    legendoptics.germanium.germanium_reflectivity()
+
+    legendoptics.copper.copper_reflectivity()
+
+    legendoptics.silicon.silicon_complex_rindex()
+
+    legendoptics.nylon.nylon_refractive_index()
+    legendoptics.nylon.nylon_absorption()
