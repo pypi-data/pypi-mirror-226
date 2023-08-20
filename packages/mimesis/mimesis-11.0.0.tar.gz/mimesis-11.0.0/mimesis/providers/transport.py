ed0e524f6078cd46ca176f5a0b2a88f8209b303e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:02 +0000

    Add package mimesis/11.0.0/mimesis-11.0.0.tar.gz

---
+++ packages/mimesis/mimesis-11.0.0.tar.gz/mimesis-11.0.0/mimesis/providers/transport.py
@@ -0,0 +1,63 @@
+"""Provides data related to transports."""
+
+import typing as t
+
+from mimesis.data import (
+    AIRPLANES,
+    AUTO_MANUFACTURERS,
+    CARS,
+    VR_CODES,
+    VRC_BY_LOCALES,
+)
+from mimesis.locales import Locale
+from mimesis.providers.base import BaseProvider
+
+__all__ = ["Transport"]
+
+
+class Transport(BaseProvider):
+    """Class for generating data related to transports."""
+
+    class Meta:
+        name = "transport"
+
+    def manufacturer(self) -> str:
+        """Get a random car manufacturer.
+
+        :return: A car manufacturer
+
+        :Example:
+            Tesla.
+        """
+        return self.random.choice(AUTO_MANUFACTURERS)
+
+    def car(self) -> str:
+        """Get a random vehicle.
+
+        :return: A vehicle.
+
+        :Example:
+            Tesla Model S.
+        """
+        return self.random.choice(CARS)
+
+    def airplane(self) -> str:
+        """Generate a dummy airplane model.
+
+        :return: Airplane model.
+
+        :Example:
+            Boeing 727.
+        """
+        return self.random.choice(AIRPLANES)
+
+    def vehicle_registration_code(self, locale: t.Optional[Locale] = None) -> str:
+        """Get vehicle registration code of country.
+
+        :param locale: Registration code for locale (country).
+        :return: Vehicle registration code.
+        """
+        if locale:
+            return VRC_BY_LOCALES[locale.value]
+
+        return self.random.choice(VR_CODES)
