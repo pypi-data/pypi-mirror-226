afc37b2a7fe077106d1a5e239226f843f7969a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:37 +0000

    Add package cwhois/1.0.3/cwhois-1.0.3.tar.gz

---
+++ packages/cwhois/cwhois-1.0.3.tar.gz/cwhois-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools.extension import Extension
+
+def main():
+    setup(name="cwhois",
+          version="1.0.3",
+          description="Python interface for executing rfc1036 whois",
+          author="Mariusz Krzyzok, Marco d'Itri",
+          url="http://github.com/damemay/cwhois/",
+          license="GPL-2.0",
+          ext_modules=[Extension(
+              "cwhois",
+              sources=['utils.c', 'whois.c'],
+              )],
+          keywords=["whois", "tld", "domain", "registrar",],
+          python_requires=">=3.5",
+          platforms="All")
+
+if __name__ == "__main__":
+    main()
