537f991b19f475a27539348bfc339b53fa1debab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:55 +0000

    Add package kymang/0.0.8/kymang-0.0.8-py3-none-any.whl

---
+++ packages/kymang/kymang-0.0.8-py3-none-any.whl/kymang/raw/base/upload/file.py
@@ -0,0 +1,64 @@
+#  kymang - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+from typing import Union
+from kymang import raw
+from kymang.raw.core import TLObject
+
+File = Union[raw.types.upload.File, raw.types.upload.FileCdnRedirect]
+
+
+# noinspection PyRedeclaration
+class File:  # type: ignore
+    """Telegram API base type.
+
+    Constructors:
+        This base type has 2 constructors available.
+
+        .. currentmodule:: kymang.raw.types
+
+        .. autosummary::
+            :nosignatures:
+
+            upload.File
+            upload.FileCdnRedirect
+
+    Functions:
+        This object can be returned by 1 function.
+
+        .. currentmodule:: kymang.raw.functions
+
+        .. autosummary::
+            :nosignatures:
+
+            upload.GetFile
+    """
+
+    QUALNAME = "kymang.raw.base.upload.File"
+
+    def __init__(self):
+        raise TypeError("Base types can only be used for type checking purposes: "
+                        "you tried to use a base type instance as argument, "
+                        "but you need to instantiate one of its constructors instead. "
+                        "More info: https://docs.kymang.org/telegram/base/file")
