4cebfccf47411aedbfb7f6fbfd245858fd0bc914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:54 +0000

    Add package ifnepitope2/1.2/ifnepitope2-1.2.tar.gz

---
+++ packages/ifnepitope2/ifnepitope2-1.2.tar.gz/ifnepitope2-1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from setuptools import  find_namespace_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='ifnepitope2',
+    version='1.2',
+    description='A tool to predict ifn inducing epitopes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files = ('LICENSE.txt',),
+    url='https://github.com/raghavagps/ifnepitope2', 
+    packages=find_namespace_packages(where="src"),
+    package_dir={'':'src'},
+    package_data={'ifnepitope2.blast_binaries':['**/*'], 
+    'ifnepitope2.blast_db':['**/*'],
+    'ifnepitope2.model':['*']},
+    entry_points={ 'console_scripts' : ['ifnepitope2 = ifnepitope2.python_scripts.ifnepitope2:main']},
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy', 'pandas==1.4.0', 'tqdm', 'scikit-learn==1.2.1',  # Add any Python dependencies here
+    ]
+)
