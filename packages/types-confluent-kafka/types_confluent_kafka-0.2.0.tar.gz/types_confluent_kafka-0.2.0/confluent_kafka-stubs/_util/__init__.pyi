9b5c8d37a423142f701bbb612381c7ea5eb0881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:04:57 +0000

    Add package types-confluent-kafka/0.2.0/types_confluent_kafka-0.2.0.tar.gz

---
+++ packages/types-confluent-kafka/types_confluent_kafka-0.2.0.tar.gz/types_confluent_kafka-0.2.0/confluent_kafka-stubs/_util/__init__.pyi
@@ -0,0 +1,7 @@
+"""
+types-confluent-kafka: A package providing type hints for the confluent-kafka Python package.
+This package is licensed under the Apache 2.0 License.
+"""
+
+from .conversion_util import ConversionUtil as ConversionUtil
+from .validation_util import ValidationUtil as ValidationUtil
