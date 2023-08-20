2c2fdde1e1a050f4cf2ce0900e2d911c8f95c7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:27 +0000

    Add package erscipcard/1.24/erscipcard-1.24.tar.gz

---
+++ packages/erscipcard/erscipcard-1.24.tar.gz/erscipcard-1.24/erscipcard/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import User1
+from .widget import AvatarFileUploadInput
+from .widget import JalaliDateWidget
+from django.conf import settings
+
+class User1form(forms.ModelForm):
+    class Meta:
+        model = User1
+        #fields = "__all__"
+        widgets = { "etebar": JalaliDateWidget , "pic": AvatarFileUploadInput  }
+        try:
+            exclude = settings.ERSCIPCARD_EXCLUDE if hasattr(settings, 'ERSCIPCARD_EXCLUDE') and settings.ERSCIPCARD_EXCLUDE else ['address','reserv1','reserv2','reserv3','reserv4',]
+            labels = settings.ERSCIPCARD_LABELS if hasattr(settings, 'ERSCIPCARD_LABELS') and settings.ERSCIPCARD_LABELS else {}
+        except:
+            pass
+        
+
+
