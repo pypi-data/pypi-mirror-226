4ab3087d4fde146bd7a875e5857bb1ad98ec33d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:03 +0000

    Add package qsketchmetric/1.6.7/qsketchmetric-1.6.7.tar.gz

---
+++ packages/qsketchmetric/qsketchmetric-1.6.7.tar.gz/qsketchmetric-1.6.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages  # type: ignore
+
+VERSION = '1.6.7'
+DESCRIPTION = 'Parametric 2D CAD'
+LONG_DESCRIPTION = 'Parametric 2D drawings renderer using QCAD Professional'
+
+
+setup(
+    name="qsketchmetric",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/MadScrewdriver/qsketchmetric",
+    author="Franciszek Łajszczak",
+    author_email="franciszek@lajszczak.dev",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='CAD, QCAD, 2D, parametric, drawing, renderer, python renderer, python CAD, python 2d CAD, p'
+             'python 2d drawing, python parametric drawing, python parametric CAD, python QCAD, QCAD python, '
+             'parametric QCAD python, parametric QCAD, QCAD parametric, QCAD python parametric, QCAD python 2d,',
+    python_requires=">=3.9",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+    ]
+)
