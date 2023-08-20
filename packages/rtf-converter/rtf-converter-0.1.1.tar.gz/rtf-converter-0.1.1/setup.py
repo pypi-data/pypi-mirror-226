96f08fd18ae7232908e5184db0b07be69dbc5fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:23 +0000

    Add package rtf-converter/0.1.1/rtf-converter-0.1.1.tar.gz

---
+++ packages/rtf-converter/rtf-converter-0.1.1.tar.gz/rtf-converter-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Read the content of the README.md file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rtf-converter',
+    version='0.1.1',
+    packages=find_packages(),
+    
+    description='Easily convert RTF to plain text format - Supports Javascript, Python, CSharp and PHP.',
+    
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+)
