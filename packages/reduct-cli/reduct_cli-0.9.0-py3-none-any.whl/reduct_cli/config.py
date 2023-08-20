999518c3c520cd69c6da318f088b08f913e1d33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:48 +0000

    Add package reduct-cli/0.9.0/reduct_cli-0.9.0-py3-none-any.whl

---
+++ packages/reduct-cli/reduct_cli-0.9.0-py3-none-any.whl/reduct_cli/config.py
@@ -0,0 +1,34 @@
+"""Configuration"""
+import os
+from pathlib import Path
+from typing import Dict
+
+import tomlkit as toml
+from pydantic import HttpUrl, BaseModel
+
+
+class Alias(BaseModel):
+    """Alias of storage instance"""
+
+    url: HttpUrl
+    token: str
+
+
+class Config(BaseModel):
+    """Configuration as a dict"""
+
+    aliases: Dict[str, Alias]
+
+
+def write_config(path: Path, config: Config):
+    """Write config to TOML file"""
+    if not Path.exists(path):
+        os.makedirs(path.parent, exist_ok=True)
+    with open(path, "w", encoding="utf8") as config_file:
+        toml.dump(config.dict(), config_file)
+
+
+def read_config(path: Path) -> Config:
+    """Read config from TOML file"""
+    with open(path, "r", encoding="utf8") as config_file:
+        return Config.parse_obj(toml.load(config_file))
