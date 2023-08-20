b72571c993b2d31fd987d8ceb35b56ed36e263d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:12 +0000

    Add package kymang/0.1.2/kymang-0.1.2-py3-none-any.whl

---
+++ packages/kymang/kymang-0.1.2-py3-none-any.whl/kymang/raw/types/recent_me_url_user.py
@@ -0,0 +1,80 @@
+#  kymang - Telegram MTProto API Client Library for Python.
+#  Copyright (C) 2022-2023 Iskandar <https://github.com/darmazi>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+
+from io import BytesIO
+
+from kymang.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector
+from kymang.raw.core import TLObject
+from kymang import raw
+from typing import List, Optional, Any
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class RecentMeUrlUser(TLObject):  # type: ignore
+    """Telegram API type.
+
+    Constructor of :obj:`~kymang.raw.base.RecentMeUrl`.
+
+    Details:
+        - Layer: ``148``
+        - ID: ``B92C09E2``
+
+    Parameters:
+        url (``str``):
+            N/A
+
+        user_id (``int`` ``64-bit``):
+            N/A
+
+    """
+
+    __slots__: List[str] = ["url", "user_id"]
+
+    ID = 0xb92c09e2
+    QUALNAME = "types.RecentMeUrlUser"
+
+    def __init__(self, *, url: str, user_id: int) -> None:
+        self.url = url  # string
+        self.user_id = user_id  # long
+
+    @staticmethod
+    def read(b: BytesIO, *args: Any) -> "RecentMeUrlUser":
+        # No flags
+        
+        url = String.read(b)
+        
+        user_id = Long.read(b)
+        
+        return RecentMeUrlUser(url=url, user_id=user_id)
+
+    def write(self, *args) -> bytes:
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+
+        # No flags
+        
+        b.write(String(self.url))
+        
+        b.write(Long(self.user_id))
+        
+        return b.getvalue()
