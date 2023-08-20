a085eb0f4f957e90929ff51fc7b7a4af2a022837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:48 +0000

    Add package kymang/0.0.6/kymang-0.0.6-py3-none-any.whl

---
+++ packages/kymang/kymang-0.0.6-py3-none-any.whl/kymang/raw/base/help/terms_of_service_update.py
@@ -0,0 +1,64 @@
+#  kymang - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+from typing import Union
+from kymang import raw
+from kymang.raw.core import TLObject
+
+TermsOfServiceUpdate = Union[raw.types.help.TermsOfServiceUpdate, raw.types.help.TermsOfServiceUpdateEmpty]
+
+
+# noinspection PyRedeclaration
+class TermsOfServiceUpdate:  # type: ignore
+    """Telegram API base type.
+
+    Constructors:
+        This base type has 2 constructors available.
+
+        .. currentmodule:: kymang.raw.types
+
+        .. autosummary::
+            :nosignatures:
+
+            help.TermsOfServiceUpdate
+            help.TermsOfServiceUpdateEmpty
+
+    Functions:
+        This object can be returned by 1 function.
+
+        .. currentmodule:: kymang.raw.functions
+
+        .. autosummary::
+            :nosignatures:
+
+            help.GetTermsOfServiceUpdate
+    """
+
+    QUALNAME = "kymang.raw.base.help.TermsOfServiceUpdate"
+
+    def __init__(self):
+        raise TypeError("Base types can only be used for type checking purposes: "
+                        "you tried to use a base type instance as argument, "
+                        "but you need to instantiate one of its constructors instead. "
+                        "More info: https://docs.kymang.org/telegram/base/terms-of-service-update")
---
+++ packages/kymang/kymang-0.0.6-py3-none-any.whl/kymang/raw/base/secure_plain_data.py
@@ -0,0 +1,54 @@
+#  kymang - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+from typing import Union
+from kymang import raw
+from kymang.raw.core import TLObject
+
+SecurePlainData = Union[raw.types.SecurePlainEmail, raw.types.SecurePlainPhone]
+
+
+# noinspection PyRedeclaration
+class SecurePlainData:  # type: ignore
+    """Telegram API base type.
+
+    Constructors:
+        This base type has 2 constructors available.
+
+        .. currentmodule:: kymang.raw.types
+
+        .. autosummary::
+            :nosignatures:
+
+            SecurePlainEmail
+            SecurePlainPhone
+    """
+
+    QUALNAME = "kymang.raw.base.SecurePlainData"
+
+    def __init__(self):
+        raise TypeError("Base types can only be used for type checking purposes: "
+                        "you tried to use a base type instance as argument, "
+                        "but you need to instantiate one of its constructors instead. "
+                        "More info: https://docs.kymang.org/telegram/base/secure-plain-data")
