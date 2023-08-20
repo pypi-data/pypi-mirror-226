c5484ed613cea915d918692451c12f824a68122b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:44 +0000

    Add package wikiscraper/1.1.8/wikiscraper-1.1.8-py3-none-any.whl

---
+++ packages/wikiscraper/wikiscraper-1.1.8-py3-none-any.whl/wikiscraper/__init__.py
@@ -0,0 +1,190 @@
+#┌----------------------------------------------┐#
+#| Wikiscraper								    |#
+#| Developed by Alexandre MEYER                 |#
+#| License CC BY 4.0                            |#
+#| https://github.com/Alexandre333/wikiscraper  |#
+#| 2021 - 2023                                  |#
+#└----------------------------------------------┘#
+
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+# default language
+LANG_ISO6391 = "en"
+
+def lang(iso):
+	global LANG_ISO6391
+	LANG_ISO6391 = iso
+
+class searchBySlug:
+
+	def __init__(self, slug):
+		self.slug = slug
+		r = requests.get('https://'+LANG_ISO6391+'.wikipedia.org/wiki/'+self.slug)
+		self.misoSoup = BeautifulSoup(r.text, 'html.parser')
+
+		for s in self.misoSoup.select('sup'):
+			s.extract()
+
+	def getTitle(self):
+		title = self.misoSoup.find(id="firstHeading")
+
+		return title.text
+
+	def getURL(self):
+		url = 'https://'+LANG_ISO6391+'.wikipedia.org/wiki/'+self.slug
+
+		return url
+
+	def getAbstract(self, limit=1000):
+		dataAbstract = []
+
+		if self.misoSoup.find('div', attrs={'class': "noarticletext"}):
+			dataAbstract.append("Unable to find the requested query: please check the spelling of the slug")
+		else:
+			if self.misoSoup.find(class_="infobox_v3"):
+				info_class_name = "infobox_v3"
+			elif self.misoSoup.find(class_="infobox_v2"):
+				info_class_name = "infobox_v2"
+			else:
+				info_class_name = "infobox"
+
+			tag = self.misoSoup.find(class_=info_class_name).find_next_sibling('p')
+	
+			# Thanks salmanwahed
+			# https://stackoverflow.com/questions/34585206/beautiful-soup-find-all-p-until-form
+			for i in range(limit):
+				if isinstance(tag, bs4.element.Tag):
+					if tag.name == 'div':
+						break
+					else:
+						dataAbstract.append(tag.text.replace(u'\n', u'').replace(u'\xa0', u' '))
+						tag = tag.nextSibling
+				else:
+					tag = tag.nextSibling
+
+		return dataAbstract
+
+	def getSideInfo(self, label):
+		value_label = []
+		
+		if self.misoSoup.find('div', attrs={'class': "noarticletext"}):
+			value_label.append("Unable to find the requested query: please check the spelling of the slug")
+		else:
+			if self.misoSoup.find(class_="infobox_v3"):
+				info_class_name = "infobox_v3"
+			elif self.misoSoup.find(class_="infobox_v2"):
+				info_class_name = "infobox_v2"
+			else:
+				info_class_name = "infobox"
+
+			all_tr = self.misoSoup.find(class_=info_class_name).findAll('tr')
+
+			for tr in all_tr:
+				if tr.find('th'):
+					if tr.find('th').find('a'):
+						row_label = tr.find('th').find('a').text
+					else:
+						row_label = tr.find('th').text
+						
+					if row_label == label:
+						if tr.find('td'):
+							if tr.find('td').text:
+								row_value = tr.find('td').text
+							else:
+								row_value = tr.find('td').find('a', attrs={'class': None}).text
+
+							row_value = row_value.replace(u'\xa0', u' ').replace(u'\n', u'')
+							value_label.append(row_value)
+
+		return value_label
+
+	def getImage(self):
+		images = []
+
+		if self.misoSoup.find('div', attrs={'class': "noarticletext"}):
+			images.append("Unable to find the requested query: please check the spelling of the slug")
+		else:
+			thumbs = self.misoSoup.findAll('a', class_="mw-file-description")
+
+			for thumb in thumbs:
+				image = "https:"+thumb.find('img').attrs['src']
+				images.append(image)
+		
+		return images
+
+	def getSection(self, h2_name=None, h3_name=None, h4_name=None, limit=1000):
+		content = []
+
+		if self.misoSoup.find('div', attrs={'class': "noarticletext"}):
+			content.append("Unable to find the requested query: please check the spelling of the slug")
+		else:
+
+			if h2_name is None:
+				sections = self.misoSoup.findAll("h2")
+				for section in sections:
+					h2_title = section.find(class_="mw-headline")
+					if h2_title is not None:
+						content.append(h2_title.text)
+			else:
+				
+				if h4_name is not None:
+					h4_name = h4_name.replace(" ", "_")				
+					section = self.misoSoup.find(id=h4_name).findNext('p')
+				elif h3_name is not None:
+					h3_name = h3_name.replace(" ", "_")				
+					section = self.misoSoup.find(id=h3_name).findNext('p')
+				else:
+					h2_name = h2_name.replace(" ", "_")				
+					section = self.misoSoup.find(id=h2_name).findNext('p')
+
+				for i in range(limit):
+					if isinstance(section, bs4.element.Tag):
+						if section.name == 'h2' or section.name == 'h3' or section.name == 'h4':
+							break
+						else:
+							content.append(section.text)
+							section = section.nextSibling
+					else:
+						section = section.nextSibling
+
+		return content
+
+	def getContentsTable(self, subcontents=False):
+		if not subcontents:
+			content = []
+		else:
+			content = {}
+
+		if self.misoSoup.find('div', attrs={'class': "noarticletext"}):
+			content.append("Unable to find the requested query: please check the spelling of the slug")
+		else:
+			if self.misoSoup.find(class_="toctitle"):
+				all_li = self.misoSoup.find(class_="toctitle").findNext('ul')
+				class_name_content = "toctext"
+			else:
+				all_li = self.misoSoup.find(class_="vector-toc").findNext('ul')
+				class_name_content = "vector-toc-text"
+
+			for li in all_li:
+				if not li.find('a') == -1:
+					li_value = li.find('a').find(class_=class_name_content).text
+					
+					if not subcontents:
+						li_value = li_value.replace(u'\xa0', u' ').replace(u'\n', u'')
+						content.append(li_value)
+					else:
+						if li.find('ul'):
+							all_li_of_ul = li.find('a').findNext('ul')
+							list_li = []
+							for li_of_ul in all_li_of_ul:
+								if not li_of_ul.find('a') == -1:
+									li_of_ul = li_of_ul.find('a').find(class_=class_name_content).text
+									li_of_ul = li_of_ul.replace(u'\xa0', u' ').replace(u'\n', u'')
+									list_li.append(li_of_ul)
+								
+								content[li_value] = list_li
+						else:
+							content[li_value] = []
+		return content
---
+++ packages/wikiscraper/wikiscraper-1.1.8-py3-none-any.whl/wikiscraper/examples.py
@@ -0,0 +1,45 @@
+import wikiscraper as ws
+
+# Set the language page in Wikipedia for the query
+# ISO 639-1 & by default "en" for English
+ws.lang("fr")
+
+# Search and get content by the URL slug of the article
+# Exemple : https://fr.wikipedia.org/wiki/Paris
+result = ws.searchBySlug("Paris")
+
+# Get article's title
+print(result.getTitle())
+
+# Get article's URL
+print(result.getURL())
+
+### Sidebar ###
+# Get value of the sidebar information label
+print(result.getSideInfo("Gentilé"))
+
+### Abstract ###
+# Get all paragraphs of abstract
+print(result.getAbstract())
+# Get the second paragraph of abstract
+print(result.getAbstract()[1])
+# Optional : Get the x paragraphs, starting from the beginning
+print(result.getAbstract(2))
+
+### Images ###
+# Get all illustration images
+img = result.getImage()
+# Get a specific image thanks to its position in the page
+print(img[0]) # Main image
+
+### Sections ###
+# Get table of contents
+# Only first headlines
+print(result.getContentsTable())
+# All headelines (first and second levels)
+print(result.getContentsTable(subcontents=True))
+
+# Get paragraphs from a specific section thanks to the parents' header title
+# All optional args in order : .getSection(h2Title, h3Title, h4Title)
+# Exemple : https://fr.wikipedia.org/wiki/Paris#Politique_et_administration
+print(result.getSection('Politique et administration', 'Statut et organisation administrative', 'Historique')[0])
