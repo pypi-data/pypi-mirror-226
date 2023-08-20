5727279c16d7fb8477e118d85b5ce9019629272d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:27 +0000

    Add package crypzo/0.0.0/crypzo-0.0.0.tar.gz

---
+++ packages/crypzo/crypzo-0.0.0.tar.gz/crypzo-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+# :)
+VERSION = '1.0.0'
+
+# Setting up
+setup(
+    name='crypzo', 
+    author="",
+    author_email="",
+    packages=find_packages(),
+)
