a0fd4f5af66974b09b1e219839c14fa7ed371931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:13 +0000

    Add package ezpyqt/1.0.3/ezpyqt-1.0.3.tar.gz

---
+++ packages/ezpyqt/ezpyqt-1.0.3.tar.gz/ezpyqt-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0.3"
+
+setup(
+    name="ezpyqt",
+    version=__version__,
+    description="To make the use of PyQt easier, Hence the name ezpyqt!",
+    author="Saeid Alizadeh",
+    author_email="saeidalz96@gmail.com",
+    url="https://github.com/saeidalz13/ezpyqt",
+    packages=find_packages(),
+    install_requires=[
+        "PySide6",
+    ],
+)
