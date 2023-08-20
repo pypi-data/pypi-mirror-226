114cde0fecb0ee9b6074bd5bc1a6a2f102ca912c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:52 +0000

    Add package xia-service-cloudflare/0.0.7/xia_service_cloudflare-0.0.7-cp310-none-win_amd64.whl

---
+++ packages/xia-service-cloudflare/xia_service_cloudflare-0.0.7-cp310-none-win_amd64.whl/xia_service_cloudflare/__init__.py
@@ -0,0 +1,11 @@
+from xia_service_cloudflare.engine import CfServiceAdmEngine, CfServiceAdmParam, CfServiceAdmClient
+from xia_service_cloudflare.service import CloudflareService
+
+
+__all__ = [
+    "CfServiceAdmEngine", "CfServiceAdmParam", "CfServiceAdmClient",
+    "CloudflareService"
+]
+
+
+__version__ = "0.0.7"
