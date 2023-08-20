3cccb0d3e2f13ff3b8401bad16f8d2f9d221765d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:26 +0000

    Add package volworld-word-book-library-se-test/0.1.3/volworld_word_book_library_se_test-0.1.3.tar.gz

---
+++ packages/volworld-word-book-library-se-test/volworld_word_book_library_se_test-0.1.3.tar.gz/volworld_word_book_library_se_test-0.1.3/src/volworld_word_book_library_se_test/pg7e2_add_word_list_dialog.py
@@ -0,0 +1,30 @@
+
+import pyperclip
+from selenium.webdriver import Keys
+from behave import *
+from api.A import A
+from volworld_aws_api_common.test.behave.selenium_utils import w__get_element_by_shown_dom_id, w__click_element_by_dom_id, \
+    click_element, scroll_to_bottom
+from volworld_aws_api_common.test.behave.row_utils import assert_tag_icon_class_of_list
+from selenium.webdriver.common.by import By
+from volworld_common.test.behave.BehaveUtil import BehaveUtil
+
+from volworld_word_book_library_se_test.nav_utils import open_nav_more_actions_drawer
+from test.behave.CotA import CotA
+from test.behave.PageGroup import PageGroup
+
+
+@when('"{mentor}" paste in word list of [add word list dialog] as "{word_list}"')
+def clear_word_list(c, mentor: str, word_list: str):
+    pyperclip.copy(BehaveUtil.clear_string(word_list))
+    input_elm = w__get_element_by_shown_dom_id(c, [A.Word, A.List, A.Dialog, A.Input])
+    input_elm.click()
+    input_elm.send_keys(Keys.CONTROL, 'v')
+
+
+@then('[save button] of [add word list dialog] is "{active}"')
+def check_add_word_list_dialog_btn(c, active: str):
+    btn = w__get_element_by_shown_dom_id(c, [A.Word, A.List, A.Dialog, A.Save, A.Button])
+    assert btn is not None
+    enabled = BehaveUtil.clear_string(active).lower() == 'enabled'
+    assert btn.is_enabled() == enabled
---
+++ packages/volworld-word-book-library-se-test/volworld_word_book_library_se_test-0.1.3.tar.gz/volworld_word_book_library_se_test-0.1.3/src/volworld_word_book_library_se_test/pg9f0_my_book_list_page__pagination.py
@@ -0,0 +1,49 @@
+import base64
+
+from volworld_aws_api_common.api.FrontEndUrl import FrontEndUrl
+from volworld_common.test.behave.BehaveUtil import BehaveUtil
+from api.A import A
+from src.volworld_word_book_library_se_test.book_row_utils import assert_book_count_and_total_page
+from behave import *
+from volworld_aws_api_common.test.behave.selenium_utils import (
+    w__get_element_by_shown_dom_id, w__click_element_by_dom_id, w__assert_element_existing, \
+    w__key_in_element_by_dom_id, w__get_element_by_presence_dom_id, w__assert_element_not_existing)
+
+from volworld_aws_api_common.test.ProjectMode import ProjectMode
+from test.behave.CotA import CotA
+from test.behave.Page import Page
+
+
+@then('there are {count_str} showing books and total {total_page_count_str} pages with total {total_count_str} books')
+def then_book_count_page_total_count(c, count_str: str, total_page_count_str: str, total_count_str: str):
+    target_page = getattr(c, CotA.TargetPage)
+    if target_page == Page.Pg9F0_My_Book_List_Page:
+        assert_book_count_and_total_page(c, count_str, total_page_count_str, total_count_str)
+    else:
+        assert False, f"Unknown target page {target_page}"
+
+
+@then('there are {book_count_str} showing books and total {total_page_count_str} pages')
+def then_showing_books_and_total_pages(c, book_count_str: str, total_page_count_str: str):
+    target_page = getattr(c, CotA.TargetPage)
+    if target_page == Page.Pg9F0_My_Book_List_Page:
+        assert_book_count_and_total_page(c, book_count_str, total_page_count_str)
+    else:
+        assert False, f"Unknown target page {target_page}"
+
+
+@when('{mentor} open target testing page with search keyword {keywords} and item per page is {item_per_page_str}')
+def when_open_page_with_keywords_and_item_per_page(c, mentor: str, keywords: str, item_per_page_str: str):
+    target_page = getattr(c, CotA.TargetPage)
+    assert target_page == Page.Pg9F0_My_Book_List_Page
+    ks = BehaveUtil.clear_string(keywords).split(" ")
+    kwb64 = list()
+    for k in ks:
+        kwb64.append(base64.b64encode(bytes(k, "utf-8")).decode('utf-8'))
+    sch = '%'.join(kwb64)
+    print(f'sch = [{sch}]')
+
+    url = f"{FrontEndUrl.Root}/#/{ProjectMode.testUrlPrefix}{A.Book}/{A.List}?sch={sch}&impp={BehaveUtil.clear_int(item_per_page_str)}"
+
+    print(f"open default [9F0_My-Book-List-Page] url={url}")
+    c.browser.get(url)
