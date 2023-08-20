eeb28e1af453a434b74bd08ff8b8499c4fdac7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 04:07:57 +0000

    Add package rlgym-api/2.0.0a0/rlgym-api-2.0.0a0.tar.gz

---
+++ packages/rlgym-api/rlgym-api-2.0.0a0.tar.gz/rlgym-api-2.0.0a0/rlgym/api/engine/renderer.py
@@ -0,0 +1,18 @@
+"""
+An RLGym renderer.
+"""
+from abc import abstractmethod
+from typing import Any, Generic
+from rlgym.api.typing import StateType
+
+
+class Renderer(Generic[StateType]):
+    #TODO docs
+
+    @abstractmethod
+    def render(self, state: StateType) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def close(self):
+        raise NotImplementedError
