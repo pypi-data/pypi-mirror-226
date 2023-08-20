f545fd47b054d433af13c4380bfbdcf6c8f57fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:21 +0000

    Add package graiax-fastapi/0.4.1/graiax_fastapi-0.4.1-py3-none-any.whl

---
+++ packages/graiax-fastapi/graiax_fastapi-0.4.1-py3-none-any.whl/graiax/fastapi/service.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from fastapi import FastAPI
+from launart import Launart, Service
+
+from .interface import FastAPIProvider
+
+
+class FastAPIService(Service):
+    id = "http.server/graiax.fastapi"
+    required: set[str] = {"asgi.service/uvicorn"}
+    stages: set[str] = {"blocking"}
+    supported_interface_types: set = {FastAPIProvider}
+
+    def __init__(self, fastapi: FastAPI | None = None) -> None:
+        self.fastapi = fastapi or FastAPI()
+        super().__init__()
+
+    def get_interface(self, _: type[FastAPIProvider]):
+        return FastAPIProvider(self, self.fastapi)
+
+    async def launch(self, manager: Launart):
+        async with self.stage("blocking"):
+            await manager.status.wait_for_sigexit()
