32398e8ebdd1b8bad00b0868740f7a683638f2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:03 +0000

    Add package dynrender-skia-opt/0.3.4/dynrender_skia_opt-0.3.4-py3-none-any.whl

---
+++ packages/dynrender-skia-opt/dynrender_skia_opt-0.3.4-py3-none-any.whl/dynrender_skia/DynStyle.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+@File    :   DynStyle.py
+@Time    :   2023/07/16 20:42:46
+@Author  :   Polyisoprene
+@Version :   1.0
+@Desc    :   None
+"""
+
+from pathlib import Path
+from typing import Any, Union
+
+from pydantic import BaseModel, validator
+
+
+class FontSize(BaseModel):
+    text: int
+    name: int
+    time: int
+    title: int
+    sub_title: int
+
+
+class FontColor(BaseModel):
+    text: tuple
+    title: tuple
+    name_big_vip: tuple
+    name_small_vip: tuple
+    rich_text: tuple
+    sub_title: tuple
+    white: tuple
+
+
+class BackgroundColor(BaseModel):
+    normal: tuple
+    repost: tuple
+    border: tuple
+
+
+class FontCfg(BaseModel):
+    font_family: str
+    emoji_font_family: str
+    font_style: Any
+    font_size: FontSize
+
+    @validator("font_family", "emoji_font_family", always=True, pre=True)
+    def _change_path_to_str(cls, v: Union[str, Path]):
+        v_ = Path(v)
+        if v_.exists() and v_.is_file():
+            return v_.as_uri()
+        else:
+            return v
+
+
+class ColorCfg(BaseModel):
+    font_color: FontColor
+    background: BackgroundColor
+
+
+class PolyStyle(BaseModel):
+    font: FontCfg
+    color: ColorCfg
