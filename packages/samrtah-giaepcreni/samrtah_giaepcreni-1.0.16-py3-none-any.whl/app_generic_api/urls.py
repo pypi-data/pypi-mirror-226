cf12d749bc85e84b6d503eb46aec6180ca028c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:14 +0000

    Add package samrtah-giaepcreni/1.0.16/samrtah_giaepcreni-1.0.16-py3-none-any.whl

---
+++ packages/samrtah-giaepcreni/samrtah_giaepcreni-1.0.16-py3-none-any.whl/app_generic_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from app_generic_api.views import GenericApiView
+
+app_name = "app_generic_api"
+
+urlpatterns = [
+    # path("", GenericApiView.as_view(), name="generic_api"),
+]
