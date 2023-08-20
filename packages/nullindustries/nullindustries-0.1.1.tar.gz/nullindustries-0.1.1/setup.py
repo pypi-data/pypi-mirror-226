b8d5e926c3b37bedfce82ab8bd4eb277c5be121a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:06 +0000

    Add package nullindustries/0.1.1/nullindustries-0.1.1.tar.gz

---
+++ packages/nullindustries/nullindustries-0.1.1.tar.gz/nullindustries-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nullindustries',
+    version='0.1.1',
+    description='Null Industries Utility Package',
+    author='Null Industries (Nothsa)',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+    ],
+)
+
