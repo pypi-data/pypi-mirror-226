8f650bb08079159b59ea913af25165a23406164d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:07:55 +0000

    Add package setuptools-github/0.4.2/setuptools-github-0.4.2.tar.gz

---
+++ packages/setuptools-github/setuptools-github-0.4.2.tar.gz/setuptools-github-0.4.2/src/setuptools_github/_build.py
@@ -0,0 +1,9 @@
+# autogenerate build file
+branch = 'release/0.4.2'
+build = '26'
+current = '0.4.2'
+ref = 'refs/tags/release/0.4.2'
+runid = '5910677038'
+sha = 'e4225c99371d5479f05998e033e721f2dd01657e'
+version = '0.4.2'
+workflow = 'tags'
