5e14186402944dcb37905bac7e7049693387a0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:40 +0000

    Add package kymang/0.1.7/kymang-0.1.7-py3-none-any.whl

---
+++ packages/kymang/kymang-0.1.7-py3-none-any.whl/kymang/kymang/cmd.py
@@ -0,0 +1,59 @@
+from kymang import filters
+
+
+from ubot.config import KYNAN, OWNER_ID, USER_ID
+from ubot import bot, ubot
+
+class FILTERS:
+    ME = filters.me
+    GROUP = filters.group
+    PRIVATE = filters.private
+    OWNER = filters.user(OWNER_ID)
+    GUA = filters.user(KYNAN)
+    ME_GROUP = filters.me & filters.group
+    YA_GUA = filters.me & filters.user(KYNAN)
+    ME_OWNER = filters.me & filters.user(OWNER_ID)
+    ME_USER = filters.me & filters.user(USER_ID)
+    
+
+class PY:
+    def BOT(command, filter=FILTERS.PRIVATE):
+        def wrapper(func):
+            @bot.on_message(filters.command(command) & filter)
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def UBOT(command, filter=FILTERS.ME):
+        def wrapper(func):
+            @ubot.on_message(filters.command(command, "^") & filters.user(OWNER_ID))
+            @ubot.on_message(anjay(command) & filter)
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def INLINE(command):
+        def wrapper(func):
+            @bot.on_inline_query(filters.regex(command))
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def CALLBACK(command):
+        def wrapper(func):
+            @bot.on_callback_query(filters.regex(command))
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
