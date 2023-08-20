428d013acf2d271950f2ea0d16e6c4b8ced5afaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 14:05:14 +0000

    Add package kymang/0.1.4/kymang-0.1.4-py3-none-any.whl

---
+++ packages/kymang/kymang-0.1.4-py3-none-any.whl/kymang/raw/base/messages/affected_messages.py
@@ -0,0 +1,66 @@
+#  kymang - Telegram MTProto API Client Library for Python.
+#  Copyright (C) 2022-2023 Iskandar <https://github.com/darmazi>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+from typing import Union
+from kymang import raw
+from kymang.raw.core import TLObject
+
+AffectedMessages = Union[raw.types.messages.AffectedMessages]
+
+
+# noinspection PyRedeclaration
+class AffectedMessages:  # type: ignore
+    """Telegram API base type.
+
+    Constructors:
+        This base type has 1 constructor available.
+
+        .. currentmodule:: kymang.raw.types
+
+        .. autosummary::
+            :nosignatures:
+
+            messages.AffectedMessages
+
+    Functions:
+        This object can be returned by 4 functions.
+
+        .. currentmodule:: kymang.raw.functions
+
+        .. autosummary::
+            :nosignatures:
+
+            messages.ReadHistory
+            messages.DeleteMessages
+            messages.ReadMessageContents
+            channels.DeleteMessages
+    """
+
+    QUALNAME = "kymang.raw.base.messages.AffectedMessages"
+
+    def __init__(self):
+        raise TypeError("Base types can only be used for type checking purposes: "
+                        "you tried to use a base type instance as argument, "
+                        "but you need to instantiate one of its constructors instead. "
+                        "More info: https://docs.kymang.org/telegram/base/affected-messages")
