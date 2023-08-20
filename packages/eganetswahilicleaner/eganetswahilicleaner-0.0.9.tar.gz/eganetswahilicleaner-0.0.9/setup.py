f859e481f30373bf84e8a4d972bea721a3c3dc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:09:33 +0000

    Add package eganetswahilicleaner/0.0.9/eganetswahilicleaner-0.0.9.tar.gz

---
+++ packages/eganetswahilicleaner/eganetswahilicleaner-0.0.9.tar.gz/eganetswahilicleaner-0.0.9/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Operating System :: Unix',
+    'Operating System :: iOS',
+]
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='eganetswahilicleaner',
+    version='0.0.9',
+    description='SWAHILI TEXT CLEANING LIBRARY FOR NATURAL LANGUAGE PROCESSING',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    author='Shadrack Kajigili',
+    author_email='shadrackkajigili4@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='swahili_text_cleaner',
+    packages=find_packages(),
+    install_requires=['nltk','emoji']
+)
