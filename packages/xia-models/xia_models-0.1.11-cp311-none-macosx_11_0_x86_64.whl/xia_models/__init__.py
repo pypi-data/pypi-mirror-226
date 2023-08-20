1e505da514b0ac13901eb5666f90317a5e9946cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:46 +0000

    Add package xia-models/0.1.11/xia_models-0.1.11-cp311-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-models/xia_models-0.1.11-cp311-none-macosx_11_0_x86_64.whl/xia_models/__init__.py
@@ -0,0 +1,9 @@
+from xia_models.model import DataModel, DataField
+from xia_models.address import DataAddress, TableAddress, TableCatalog
+
+__all__ = [
+    "DataModel", "DataField",
+    "DataAddress", "TableAddress", "TableCatalog"
+]
+
+__version__ = "0.1.11"
