080011668886e828c2d9cc4c61473bbd9da9e763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:05 +0000

    Add package django-mirrors/1.0.1/django_mirrors-1.0.1.tar.gz

---
+++ packages/django-mirrors/django_mirrors-1.0.1.tar.gz/django_mirrors-1.0.1/mirrors/views.py
@@ -0,0 +1,38 @@
+import os
+import pathlib
+import shutil
+
+from django.conf import settings
+from django.http import FileResponse
+from django.shortcuts import render
+
+
+def mirrors(request, path):
+    if path == '': path = '/'
+    cur = pathlib.Path(settings.MIRRORS_DIR.as_posix() + path)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            if name is None:
+                for file in request.FILES.getlist('files'):
+                    file_path = cur.joinpath(file.name)
+                    if not file_path.parent.exists(): os.makedirs(file_path.parent)
+                    with open(file_path, 'wb+') as f:
+                        for chunk in file.chunks(): f.write(chunk)
+            else:
+                folder = cur.joinpath(name)
+                if not folder.exists(): os.makedirs(folder)
+        elif request.method == 'DELETE':
+            if cur.exists():
+                if cur.is_dir(): shutil.rmtree(cur)
+                else: os.remove(cur)
+    if cur.is_file(): return FileResponse(open(cur, 'rb'))
+    return render(request, 'mirrors.html', {
+        "path": pathlib.Path('/').joinpath(path).as_posix(),
+        "full_path": request.get_full_path(),
+        "parent": '/'.join(request.get_full_path().split('/')[:-1]),
+        "files": [{
+            "name": file.name,
+            "url": pathlib.Path(request.get_full_path()).joinpath(file.name).as_posix(),
+        } for file in cur.iterdir()] if cur.exists() else []
+    })
