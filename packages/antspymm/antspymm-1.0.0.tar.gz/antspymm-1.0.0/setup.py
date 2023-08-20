c0ab5c5a1c1f5acbc243b6ef0eeb47c0aa2e5419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-20 03:08:54 +0000

    Add package antspymm/1.0.0/antspymm-1.0.0.tar.gz

---
+++ packages/antspymm/antspymm-1.0.0.tar.gz/antspymm-1.0.0/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+
+with open("./requirements.txt") as f:
+      requirements = f.read().splitlines()
+
+setup(name='antspymm',
+      version='1.0.0',
+      description='multi-channel/time-series medical image processing with antspyx',
+      long_description=long_description,
+      long_description_content_type="text/markdown; charset=UTF-8; variant=GFM",
+      url='https://github.com/stnava/ANTsPyMM',
+      author='Avants, Gosselin, Tustison, Reardon',
+      author_email='stnava@gmail.com',
+      license='Apache 2.0',
+      install_requires=requirements,
+      packages=['antspymm'],
+      zip_safe=False)
