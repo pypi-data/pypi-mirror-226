e7312f4de1291aa73f58a070f2a6627690f64599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 04:10:46 +0000

    Add package datastructspawner/0.0.1/datastructspawner-0.0.1.tar.gz

---
+++ packages/datastructspawner/datastructspawner-0.0.1.tar.gz/datastructspawner-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# with open('README.md', 'r') as arq:
+#     readme = arq.read()
+
+setup(name='datastructspawner',
+    version='0.0.1',
+    license='MIT License',
+    author='Joao Neto e Lindaiely Rodrigues',
+    author_email='joaonetoprivado2001@ufpi.edu.br',
+    keywords='estrutura de dados',
+    description=u'Gerador de estruturas de dados',
+    packages=['datastructspawner'],)
