695b3b7af649853d125fdb0a972d86b0d66e77f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:56 +0000

    Add package windmill-api/1.152.0/windmill_api-1.152.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.152.0.tar.gz/windmill_api-1.152.0/windmill_api/models/new_schedule.py
@@ -0,0 +1,112 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+import attr
+
+from ..models.new_schedule_args import NewScheduleArgs
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="NewSchedule")
+
+
+@attr.s(auto_attribs=True)
+class NewSchedule:
+    """
+    Attributes:
+        path (str):
+        schedule (str):
+        timezone (str):
+        script_path (str):
+        is_flow (bool):
+        args (NewScheduleArgs):
+        enabled (Union[Unset, bool]):
+        on_failure (Union[Unset, str]):
+    """
+
+    path: str
+    schedule: str
+    timezone: str
+    script_path: str
+    is_flow: bool
+    args: NewScheduleArgs
+    enabled: Union[Unset, bool] = UNSET
+    on_failure: Union[Unset, str] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        path = self.path
+        schedule = self.schedule
+        timezone = self.timezone
+        script_path = self.script_path
+        is_flow = self.is_flow
+        args = self.args.to_dict()
+
+        enabled = self.enabled
+        on_failure = self.on_failure
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "path": path,
+                "schedule": schedule,
+                "timezone": timezone,
+                "script_path": script_path,
+                "is_flow": is_flow,
+                "args": args,
+            }
+        )
+        if enabled is not UNSET:
+            field_dict["enabled"] = enabled
+        if on_failure is not UNSET:
+            field_dict["on_failure"] = on_failure
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        path = d.pop("path")
+
+        schedule = d.pop("schedule")
+
+        timezone = d.pop("timezone")
+
+        script_path = d.pop("script_path")
+
+        is_flow = d.pop("is_flow")
+
+        args = NewScheduleArgs.from_dict(d.pop("args"))
+
+        enabled = d.pop("enabled", UNSET)
+
+        on_failure = d.pop("on_failure", UNSET)
+
+        new_schedule = cls(
+            path=path,
+            schedule=schedule,
+            timezone=timezone,
+            script_path=script_path,
+            is_flow=is_flow,
+            args=args,
+            enabled=enabled,
+            on_failure=on_failure,
+        )
+
+        new_schedule.additional_properties = d
+        return new_schedule
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
