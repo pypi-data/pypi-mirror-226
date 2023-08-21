4ed69a5265d8cea70a47c53a28f04c6b1cbf44c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:30 +0000

    Add package pascal-voc/2.0.0/pascal_voc-2.0.0.tar.gz

---
+++ packages/pascal-voc/pascal_voc-2.0.0.tar.gz/pascal_voc-2.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pascal_voc",
+    version="2.0.0",
+    author="Alexander Barmin",
+    author_email="barmin1@mail.ru",
+    description="Tool to work with annotation formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=["lxml>=4.6.2", "Pillow>=8.1.0", "xmltodict"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
