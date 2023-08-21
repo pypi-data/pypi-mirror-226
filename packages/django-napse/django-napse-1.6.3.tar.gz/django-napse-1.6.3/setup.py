7484b4b55a1a62bb18d43b4218fddb00b710fd80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:13 +0000

    Add package django-napse/1.6.3/django-napse-1.6.3.tar.gz

---
+++ packages/django-napse/django-napse-1.6.3.tar.gz/django-napse-1.6.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+print(find_packages(exclude=["test", "test.*"]))
+setup(
+    name="django-napse",
+    packages=find_packages(exclude=["test", "test.*"]),
+    version="v1.6.3",
+    license="MIT",
+    description="The heart and brains of the Napse Invest platform.",
+    long_description="The heart and brains of the Napse Invest platform.",
+    author="Napse Invest",
+    author_email="napse.invest@gmail.com",
+    url="https://github.com/napse-invest/django-napse",
+    download_url="https://github.com/napse-invest/django-napse/archive/refs/tags/vv1.6.3.tar.gz",
+    keywords=["Investing", "Django", "Trading"],
+    requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
