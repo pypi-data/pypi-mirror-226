6eb07b14f4e50704c33c26b080c21d251dda7b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:17 +0000

    Add package pyrockoeost/2023.6/pyrockoeost-2023.6-cp310-cp310-win_amd64.whl

---
+++ packages/pyrockoeost/pyrockoeost-2023.6-cp310-cp310-win_amd64.whl/pyrockoeost/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = None
+local_modifications = None
+version = '2023.006'
+long_version = 'pyrockoeost-2023.006-2023-08-20_17:33:36'  # noqa
+installed_date = '2023-08-20_17:33:36'
+src_path = 'C:\\Users\\kylia\\AppData\\Local\\Temp\\build-via-sdist-764xhjlj\\pyrockoeost-2023.6'
