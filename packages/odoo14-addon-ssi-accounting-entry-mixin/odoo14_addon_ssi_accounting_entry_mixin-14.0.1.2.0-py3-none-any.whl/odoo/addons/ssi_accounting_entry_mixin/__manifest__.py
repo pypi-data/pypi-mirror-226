163ed2df0948dfd2037534dabdec488943a7bab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:21 +0000

    Add package odoo14-addon-ssi-accounting-entry-mixin/14.0.1.2.0/odoo14_addon_ssi_accounting_entry_mixin-14.0.1.2.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-accounting-entry-mixin/odoo14_addon_ssi_accounting_entry_mixin-14.0.1.2.0-py3-none-any.whl/odoo/addons/ssi_accounting_entry_mixin/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Accounting Entry Mixin",
+    "version": "14.0.1.2.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [],
+}
