991c57e210bc2d045df0ccd72ba3d3556c26f29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:18 +0000

    Add package kymang/2.0.3/kymang-2.0.3.tar.gz

---
+++ packages/kymang/kymang-2.0.3.tar.gz/kymang-2.0.3/kymang/types/user_and_chats/chat_admin_with_invite_links.py
@@ -0,0 +1,63 @@
+#  kymang - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of kymang.
+#
+#  kymang is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  kymang is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with kymang.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Dict
+
+import kymang
+from kymang import raw
+from kymang import types
+from ..object import Object
+
+
+class ChatAdminWithInviteLinks(Object):
+    """Represents a chat administrator that has created invite links in a chat.
+
+    Parameters:
+        admin (:obj:`~kymang.types.User`):
+            The administrator.
+
+        chat_invite_links_count (``int``):
+            The number of valid chat invite links created by this administrator.
+
+        revoked_chat_invite_links_count (``int``):
+            The number of revoked chat invite links created by this administrator.
+    """
+
+    def __init__(
+        self, *,
+        admin: "types.User",
+        chat_invite_links_count: int,
+        revoked_chat_invite_links_count: int = None
+    ):
+        super().__init__()
+
+        self.admin = admin
+        self.chat_invite_links_count = chat_invite_links_count
+        self.revoked_chat_invite_links_count = revoked_chat_invite_links_count
+
+    @staticmethod
+    def _parse(
+        client: "kymang.Client",
+        admin: "raw.types.ChatAdminWithInvites",
+        users: Dict[int, "raw.types.User"] = None
+    ) -> "ChatAdminWithInviteLinks":
+        return ChatAdminWithInviteLinks(
+            admin=types.User._parse(client, users[admin.admin_id]),
+            chat_invite_links_count=admin.invites_count,
+            revoked_chat_invite_links_count=admin.revoked_invites_count
+        )
