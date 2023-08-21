f11de14f7d340a9b01b31a8adc8fd5b9172d2b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 04:10:38 +0000

    Add package acdh-transkribus-utils/2.10/acdh-transkribus-utils-2.10.tar.gz

---
+++ packages/acdh-transkribus-utils/acdh-transkribus-utils-2.10.tar.gz/acdh-transkribus-utils-2.10/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name="acdh-transkribus-utils",
+    version="v2.10",
+    description="""some utility function to interact with the Transkribus-API""",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Peter Andorfer, Matthias Schlögl, Carl Friedrich Haak",
+    author_email="peter.andorfer@oeaw.ac.at",
+    url="https://github.com/acdh-oeaw/acdh-transkribus-utils",
+    packages=[
+        "transkribus_utils",
+    ],
+    entry_points={
+        "console_scripts": [
+            "import-goobi-mets-to-transkribus=transkribus_utils.cli:import_goobi_mets_to_transkribus",
+        ]
+    },
+    include_package_data=True,
+    install_requires=["acdh-xml-pyutils", "click"],
+    license="MIT",
+    zip_safe=False,
+    keywords="acdh-transkribus-utils",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django :: 2.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
