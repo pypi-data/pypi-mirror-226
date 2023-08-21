822e222d4bec46ab2cee1c666cc975069719255a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:53 +0000

    Add package salinic/0.3.3/salinic-0.3.3-py3-none-any.whl

---
+++ packages/salinic/salinic-0.3.3-py3-none-any.whl/salinic/__init__.py
@@ -0,0 +1,18 @@
+from .engine import Engine, create_engine
+from .field import IdField, KeywordField, TextField, UUIDField
+from .index import IndexRO, IndexRW
+from .schema import Schema
+from .search import Search
+
+__all__ = [
+    'create_engine',
+    'Engine',
+    'IndexRW',
+    'IndexRO',
+    'Search',
+    'Schema',
+    'IdField',
+    'UUIDField',
+    'KeywordField',
+    'TextField'
+]
