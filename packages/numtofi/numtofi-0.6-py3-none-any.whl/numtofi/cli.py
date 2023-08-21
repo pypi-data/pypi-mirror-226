0c09c5a7f0f1f8c5256965d51a1c061ba803ea67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:25 +0000

    Add package numtofi/0.6/numtofi-0.6-py3-none-any.whl

---
+++ packages/numtofi/numtofi-0.6-py3-none-any.whl/numtofi/cli.py
@@ -0,0 +1,19 @@
+from .core import number_to_text
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Convert a number to its Finnish textual representation.")
+    parser.add_argument('number', type=int, help='The number to convert. Must be a positive integer.')
+    parser.add_argument('--nospaces', action='store_true', help='If set, removes spaces between words.')
+
+    args = parser.parse_args()
+
+    try:
+        result = number_to_text(args.number, not args.nospaces)
+        print(result)
+    except ValueError as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
