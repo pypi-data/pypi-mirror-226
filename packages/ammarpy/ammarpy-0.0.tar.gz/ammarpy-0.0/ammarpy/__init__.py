fac1fe56d04d9fee51a530d7715e41cae7a85f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:03 +0000

    Add package ammarpy/0.0/ammarpy-0.0.tar.gz

---
+++ packages/ammarpy/ammarpy-0.0.tar.gz/ammarpy-0.0/ammarpy/__init__.py
@@ -0,0 +1,6 @@
+def welcome():
+    print("____________Welcome!___________\n\
+Thank you for dowmloading ammarpy package\n\
+This package from AMMAR ALHASHEMI\n\
+IT student Level-3\n\
+Alsaeeda University")
