94a0947cbfe75eac9088238299376a9d2839b583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:08:58 +0000

    Add package auto-canoe-by-Fw/0.1/auto_canoe_by_Fw-0.1.tar.gz

---
+++ packages/auto-canoe-by-Fw/auto_canoe_by_Fw-0.1.tar.gz/auto_canoe_by_Fw-0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+
+
+
+
+classifiers = """\
+Development Status :: 0.1 - Beta
+Environment :: Console
+License :: OSI Approved :: MIT License
+Topic :: Scientific/Engineering
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+"""
+
+
+
+setup(
+    name='auto_canoe_by_Fw',
+    version='0.1',
+    maintainer = "wen.feng",
+    maintainer_email = "1805464096@qq.com",
+    packages=["auto_canoe_by_Fw"],
+    license = "MIT",
+    install_requires=[
+    ],
+)
