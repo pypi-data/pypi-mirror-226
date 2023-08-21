ebc562e8f8f06f49f9ea7bbf9a7ca87483992e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:35 +0000

    Add package ignition-api/8.1.31/ignition_api-8.1.31-py2-none-any.whl

---
+++ packages/ignition-api/ignition_api-8.1.31-py2-none-any.whl/java/math/__init__.py
@@ -0,0 +1,21 @@
+__all__ = ["RoundingMode"]
+
+from typing import List
+
+from java.lang import Enum
+
+
+class RoundingMode(Enum):
+    CEILING = None  # type: RoundingMode
+    DOWN = None  # type: RoundingMode
+    FLOOR = None  # type: RoundingMode
+    HALF_DOWN = None  # type: RoundingMode
+    HALF_EVEN = None  # type: RoundingMode
+    HALF_UP = None  # type: RoundingMode
+    UNNECESSARY = None  # type: RoundingMode
+    UP = None  # type: RoundingMode
+
+    @staticmethod
+    def values():
+        # type: () -> List[RoundingMode]
+        pass
