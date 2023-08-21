10850940b99715c9783a2b9f5661cd951b9313bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 15:08:44 +0000

    Add package fake-bpy-module-latest/20230821/fake-bpy-module-latest-20230821.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20230821.tar.gz/fake-bpy-module-latest-20230821/bl_operators/__init__.py
@@ -0,0 +1,50 @@
+import sys
+import typing
+from . import clip
+from . import console
+from . import freestyle
+from . import image
+from . import view3d
+from . import object_align
+from . import uvcalc_transform
+from . import object_randomize_transform
+from . import wm
+from . import sequencer
+from . import spreadsheet
+from . import uvcalc_follow_active
+from . import mesh
+from . import anim
+from . import node
+from . import rigidbody
+from . import vertexpaint_dirt
+from . import presets
+from . import uvcalc_lightmap
+from . import object_quick_effects
+from . import add_mesh_torus
+from . import geometry_nodes
+from . import file
+from . import assets
+from . import object
+from . import constraint
+from . import bmesh
+from . import screen_play_rendered_anim
+from . import userpref
+from . import text
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20230821.tar.gz/fake-bpy-module-latest-20230821/gpu/__init__.py
@@ -0,0 +1,12 @@
+import sys
+import typing
+from . import shader
+from . import select
+from . import state
+from . import types
+from . import matrix
+from . import texture
+from . import capabilities
+from . import platform
+
+GenericType = typing.TypeVar("GenericType")
