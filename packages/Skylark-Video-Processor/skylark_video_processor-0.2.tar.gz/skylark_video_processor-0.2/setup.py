0aedec484108fd5b1509a715f7cb2e7464a15d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 15:08:41 +0000

    Add package Skylark-Video-Processor/0.2/skylark_video_processor-0.2.tar.gz

---
+++ packages/Skylark-Video-Processor/skylark_video_processor-0.2.tar.gz/skylark_video_processor-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skylark_video_processor',
+    version='0.2',
+    description='A video processing packageg fop YOLOV8',
+    author='Prem Varma',
+    author_email='prem.varma@skylarklabs.ai',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'ultralytics',
+    ],
+)
