e464d189301a6c9cc4136c39169555ebe58d07fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 15:08:53 +0000

    Add package swit-core/0.0.16/swit_core-0.0.16.tar.gz

---
+++ packages/swit-core/swit_core-0.0.16.tar.gz/swit_core-0.0.16/switcore/ui/header.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+from switcore.ui.button import Button
+
+
+class ContextMenuItem(BaseModel):
+    label: str
+    action_id: str
+
+
+class Header(BaseModel):
+    title: str
+    subtitle: str | None
+    context_menu: list[ContextMenuItem] | None
+    buttons: list[Button] | None
