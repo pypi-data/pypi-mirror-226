c10c5235472668f0fada064bfe16cf474ef4f7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 15:08:58 +0000

    Add package calflops/0.0.3/calflops-0.0.3-py3-none-any.whl

---
+++ packages/calflops/calflops-0.0.3-py3-none-any.whl/calflops/__init__.py
@@ -0,0 +1,15 @@
+# !usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+ Description  : 
+ Version      : 1.0
+ Author       : MrYXJ
+ Mail         : code_job@163.com
+ Github       : https://github.com/MrYxJ
+ Date         : 2023-08-19 10:27:55
+ LastEditTime : 2023-08-21 14:25:38
+ Copyright (C) 2023 mryxj. All rights reserved.
+'''
+
+from .flops_counter import calculate_flops
