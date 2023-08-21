176a86d88d784db6b35aef4614482d2d5fe07e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:23 +0000

    Add package Nevzat-Probability/0.1/Nevzat_Probability-0.1.tar.gz

---
+++ packages/Nevzat-Probability/Nevzat_Probability-0.1.tar.gz/Nevzat_Probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Nevzat_Probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
