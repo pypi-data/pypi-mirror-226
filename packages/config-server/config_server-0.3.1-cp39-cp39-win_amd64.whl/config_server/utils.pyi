7194928cd602dcb4b1cb5b9c523c041d3661829e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:06:50 +0000

    Add package config-server/0.3.1/config_server-0.3.1-cp39-cp39-win_amd64.whl

---
+++ packages/config-server/config_server-0.3.1-cp39-cp39-win_amd64.whl/config_server/utils.pyi
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from pydantic.json_schema import JsonSchemaValue as JsonSchemaValue
+from typing import Any
+
+class ValidationResult(BaseModel):
+    result: bool
+    detail: list
+    @classmethod
+    def __get_pydantic_json_schema__(cls, *args, **kwargs) -> JsonSchemaValue: ...
+
+def remove_intent_of_text(text: str) -> str: ...
+def get_blank_repr_for_field(field_value: Any): ...
