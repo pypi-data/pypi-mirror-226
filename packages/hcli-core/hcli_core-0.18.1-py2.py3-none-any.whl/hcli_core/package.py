669217cde0002150c9f7e79e43438a63534750b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:33 +0000

    Add package hcli-core/0.18.1/hcli_core-0.18.1-py2.py3-none-any.whl

---
+++ packages/hcli-core/hcli_core-0.18.1-py2.py3-none-any.whl/hcli_core/package.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = "0.18.1"
+dependencies = ["falcon==3.1.1",
+                "openai==0.27.2",
+                "tiktoken==0.3.2",
+                "apscheduler==3.10.1",
+                "pyserial==3.5"]
