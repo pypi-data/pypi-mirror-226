f0d064d0817fa888be4d2bf797876db58c0d65e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:06:32 +0000

    Add package quantscape/0.1/quantscape-0.1.tar.gz

---
+++ packages/quantscape/quantscape-0.1.tar.gz/quantscape-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quantscape",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here (if any)
+    ],
+)
+
