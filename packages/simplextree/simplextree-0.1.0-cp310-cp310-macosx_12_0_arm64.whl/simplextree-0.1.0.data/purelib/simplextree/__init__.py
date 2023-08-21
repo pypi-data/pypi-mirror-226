e362e875f92cda68b9b8c03ac14674f6b52dfd60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:18 +0000

    Add package simplextree/0.1.0/simplextree-0.1.0-cp310-cp310-macosx_12_0_arm64.whl

---
+++ packages/simplextree/simplextree-0.1.0-cp310-cp310-macosx_12_0_arm64.whl/simplextree-0.1.0.data/purelib/simplextree/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# import os
+# import sys
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+__version__ = '0.1.0'
+
+from .SimplexTree import SimplexTree
+from .UnionFind import UnionFind
