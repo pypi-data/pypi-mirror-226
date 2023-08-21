8a950806990c8acc352e2c9da1342c0506972937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:16 +0000

    Add package numtofi/0.5/numtofi-0.5.tar.gz

---
+++ packages/numtofi/numtofi-0.5.tar.gz/numtofi-0.5/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='numtofi',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'numtofi=numtofi.cli:main',
+        ],
+    },
+    author='Marko T. Manninen',
+    author_email='elonmedia@gmail.com',
+    description='A package to convert numbers to Finnish textual representation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/markomanninen/numtofi',
+)
