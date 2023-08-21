0529e9cdf12967038a0c468388f7c28a990c9b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:08 +0000

    Add package anonpi/0.2.1/anonpi-0.2.1.tar.gz

---
+++ packages/anonpi/anonpi-0.2.1.tar.gz/anonpi-0.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='anonpi',
+    version='0.2.1',
+    author='Adistar',
+    author_email='adityasamalllll6@gmail.com',
+    description='The "anonpi" module is a powerful Python package that provides a convenient interface for interacting with calling systems. It simplifies the development of applications that require functionalities such as machine detection, IVR (Interactive Voice Response), DTMF (Dual-Tone Multi-Frequency) handling, recording, playback, and more',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['requests','flask','colorama'],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='MIT'
+)
