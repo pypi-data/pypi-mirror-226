a4374dfaaa5fa0eae87e2f7bb371a22f20f933b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:55 +0000

    Add package psusannx-pipeline/0.1.7/psusannx_pipeline-0.1.7.tar.gz

---
+++ packages/psusannx-pipeline/psusannx_pipeline-0.1.7.tar.gz/psusannx_pipeline-0.1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Get the current directory of the setup.py file (as this is where the README.md will be too)
+current_dir = Path(__file__).parent
+long_description = (current_dir / "README.md").read_text()
+
+# Set up the package metadata
+setup(
+    name="psusannx_pipeline",
+    author="Jamie O'Brien",
+    description="A package containing classes to be used in the preprocessing data pipeline for the PSUSANNX project.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.7",
+    packages=find_packages(include=["psusannx_pipeline", "psusannx_pipeline.*"]),
+    install_requires=[
+        "numpy", 
+        "pandas", 
+        "scikit-learn",
+        "category-encoders>=2.2.2"
+    ],
+    python_requires="==3.*"
+)
