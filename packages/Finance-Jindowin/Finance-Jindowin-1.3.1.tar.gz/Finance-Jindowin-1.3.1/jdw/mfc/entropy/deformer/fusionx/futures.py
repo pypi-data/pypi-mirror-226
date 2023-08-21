1a1cb388f20884fe1cef6ab9f3c6dfeef9e03902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:31 +0000

    Add package Finance-Jindowin/1.3.1/Finance-Jindowin-1.3.1.tar.gz

---
+++ packages/Finance-Jindowin/Finance-Jindowin-1.3.1.tar.gz/Finance-Jindowin-1.3.1/jdw/mfc/entropy/deformer/fusionx/futures.py
@@ -0,0 +1,11 @@
+from jdw.mfc.entropy.deformer.fusionx.base import Base
+
+
+class Futures(Base):
+
+    def __init__(self, batch, freq, id=None, directory=None, is_full=False):
+        super(Futures, self).__init__(batch=batch,
+                                      freq=freq,
+                                      id=id,
+                                      directory=directory,
+                                      is_full=is_full)
