22295442209f33e7019a94ca5f9b7a83abec2a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 15:09:25 +0000

    Add package pykoi/0.0.6/pykoi-0.0.6.tar.gz

---
+++ packages/pykoi/pykoi-0.0.6.tar.gz/pykoi-0.0.6/pykoi/component/chatbot_database_factory.py
@@ -0,0 +1,34 @@
+"""Chatbot Database Factory class."""
+from typing import Union
+
+from pykoi.component.constants import FeedbackType
+from pykoi.chat.db.qa_database import QuestionAnswerDatabase
+from pykoi.chat.db.ranking_database import RankingDatabase
+
+
+class ChatbotDatabaseFactory:
+    """
+    A factory class for creating chatbot databases.
+    """
+
+    @staticmethod
+    def create(feedback: Union[str, FeedbackType]):
+        """
+        Create a chatbot database.
+
+        Args:
+            feedback (Union[str, FeedbackType]): The type of the chatbot.
+
+        Returns:
+            Union[QuestionAnswerDatabase, RankingDatabase]: The created database.
+        """
+        feedback = FeedbackType(feedback)
+        if feedback == FeedbackType.VOTE:
+            return QuestionAnswerDatabase()
+        elif feedback == FeedbackType.RANK:
+            return RankingDatabase()
+        else:
+            raise ValueError(
+                f"Invalid feedback name: {feedback}. "
+                "Valid values are: 'question_answer', 'ranking'."
+            )
---
+++ packages/pykoi/pykoi-0.0.6.tar.gz/pykoi-0.0.6/pykoi/frontend/src/lib/Chatbots/Chat.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Chatbot from "./Chatbot.svelte";
+  import RankedChatbot from "./RankedChatbot.svelte";
+
+  export let feedback;
+  export let is_retrieval;
+</script>
+
+{#if !feedback}
+  <Chatbot {is_retrieval} />
+{/if}
+{#if feedback === "vote"}
+  <Chatbot feedback={true} {is_retrieval} />
+{/if}
+{#if feedback === "rank"}
+  <!-- TODO: Refactor to support ranking with retrieval-->
+  {#if is_retrieval}
+    <Chatbot feedback={true} {is_retrieval} />
+  {:else}
+    <RankedChatbot />
+  {/if}
+{/if}
