7dd66597d4c1f36b30bbb97552e6ad97acfffecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:02 +0000

    Add package tp-timesheet/2.1.1/tp_timesheet-2.1.1-py3-none-any.whl

---
+++ packages/tp-timesheet/tp_timesheet-2.1.1-py3-none-any.whl/tp_timesheet/config.py
@@ -0,0 +1,158 @@
+"""Module to store, create or read from a configuration file and parse the values and variables to
+globals for other modules to access.
+"""
+import logging
+import logging.handlers
+import os
+import re
+import configparser
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+class Config:
+    """Config class, manages the initialization of all the necessary globals."""
+
+    # pylint:disable = anomalous-backslash-in-string
+    ROOT_LOGGER = None
+    CONFIG_DIR = Path.home().joinpath(".config", "tp-timesheet")
+    CONFIG_DIR.mkdir(parents=True, exist_ok=True)
+    LOG_DIR = Path.joinpath(CONFIG_DIR, "logs")
+    LOG_DIR.mkdir(exist_ok=True)
+    LOG_PATH = LOG_DIR.joinpath("tp.logs")
+
+    # config parameters (need to be accessible to tests without invoking __init__)
+    sanity_check_bool_dict = {"sanity_check_start_date": "True"}
+    sanity_check_range_dict = {"sanity_check_range": "7"}
+    clockify_api_key = {
+        "clockify_api_key": "AbCD1234AbCD1234AbCD1234AbCD1234AbCD1234AbCD1234"
+    }
+    locale_list = ["en_AU", "en_SG", "ko_KR", "ms_MY", "th_TH"]
+    locale_tag = {"locale_tag": "xx_XX"}
+    DEFAULT_CONF = {
+        **sanity_check_bool_dict,
+        **sanity_check_range_dict,
+        **clockify_api_key,
+        **locale_tag,
+    }
+
+    @classmethod
+    def __init__(cls, verbose=False, config_filename="tp.conf"):
+        """This is the entry point for the class and running this will setup the tp-timesheet
+        config and make all the necessary globals available
+        """
+        cls.VERBOSE = verbose
+        cls.CONFIG_DIR = Config.CONFIG_DIR
+        cls.CONFIG_PATH = cls.CONFIG_DIR.joinpath(config_filename)
+
+        # Initialize root logger
+        cls.init_logger()
+
+        # Read from config file
+        config = cls._read_write_config()
+
+        # Load global configurations
+        cls.LOCALE = config.get("configuration", "locale_tag")
+        cls.SANITY_CHECK_START_DATE = config.get(
+            "configuration", next(iter(cls.sanity_check_bool_dict))
+        )
+        cls.SANITY_CHECK_RANGE = config.get(
+            "configuration", next(iter(cls.sanity_check_range_dict))
+        )
+        cls.CLOCKIFY_API_KEY = config.get(
+            "configuration", next(iter(cls.clockify_api_key))
+        )
+
+    @classmethod
+    def init_logger(cls):
+        """Initialze root logger."""
+
+        # create root logger
+        cls.ROOT_LOGGER.setLevel(logging.DEBUG)
+
+        # create formatter
+        log_format = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        # log to stdout
+        level = logging.DEBUG if cls.VERBOSE else logging.INFO
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(level)
+        stream_handler.setFormatter(log_format)
+        cls.ROOT_LOGGER.addHandler(stream_handler)
+
+        # log to file, rotate every 4 weeks, save up to 8 weeks
+        file_handler = logging.handlers.TimedRotatingFileHandler(
+            Config.LOG_PATH, when="W6", interval=4, backupCount=8
+        )
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(log_format)
+        cls.ROOT_LOGGER.addHandler(file_handler)
+
+    @staticmethod
+    def is_valid_key(api_key):
+        """Check api key is valid"""
+        regex_api = bool(re.match("^[A-Za-z0-9]*$", api_key))
+        len_api = len(api_key) > 40
+        return regex_api & len_api
+
+    @classmethod
+    def is_valid_locale(cls, locale):
+        """Check locale is valid"""
+        valid = locale in cls.locale_list
+        return valid
+
+    @classmethod
+    def _read_write_config(cls):
+        """Function to read the config file or create one if it doesn't exist"""
+        config = configparser.ConfigParser(allow_no_value=True)
+        # Write config template to file if it doesn't already exist
+        if not os.path.exists(cls.CONFIG_PATH):
+            logger.info(
+                "No config file was found, creating one at: %s", cls.CONFIG_PATH
+            )
+            # Set to default config values
+            config["configuration"] = cls.DEFAULT_CONF
+            with open(cls.CONFIG_PATH, "w", encoding="utf8") as config_file:
+                config.write(config_file)
+
+        # Read the config file
+        if cls.VERBOSE:
+            logger.debug("Reading config file at: %s", cls.CONFIG_PATH)
+        input_config = configparser.ConfigParser()
+        input_config.read(cls.CONFIG_PATH)
+
+        # Version compatibility (#20)
+        # Creates all config keys that don't exists yet and sets to default values
+        for config_key, config_value in cls.DEFAULT_CONF.items():
+            if not input_config.has_option("configuration", config_key):
+                input_config.set("configuration", config_key, config_value)
+
+        # Version compatability (#65)
+        # Will check to ensure API key has been changed from the default value
+        if (
+            input_config.get("configuration", next(iter(cls.clockify_api_key)))
+            == cls.clockify_api_key[next(iter(cls.clockify_api_key))]
+        ):
+            clockify_api = input("Enter clockify API key:")
+            while not cls.is_valid_key(clockify_api):
+                clockify_api = input("Invalid api key, please try again:")
+            input_config.set(
+                "configuration", next(iter(cls.clockify_api_key)), clockify_api
+            )
+        if (
+            input_config.get("configuration", next(iter(cls.locale_tag)))
+            == cls.locale_tag[next(iter(cls.locale_tag))]
+        ):
+            poss_locales = cls.locale_list
+            locale_tag = input(f"Enter locale tag ({poss_locales}):")
+            while not cls.is_valid_locale(locale_tag):
+                locale_tag = input(f"Please choose from {poss_locales}, try again:")
+            input_config.set("configuration", next(iter(cls.locale_tag)), locale_tag)
+
+        with open(cls.CONFIG_PATH, "w", encoding="utf8") as config_file:
+            input_config.write(config_file)
+
+        return input_config
---
+++ packages/tp-timesheet/tp_timesheet-2.1.1-py3-none-any.whl/tp_timesheet/date_utils.py
@@ -0,0 +1,86 @@
+""" Module containing methods to process `date` related """
+import re
+from typing import List, Tuple
+import datetime
+from datetime import datetime, timedelta
+import dateutil.parser
+from tp_timesheet.config import Config
+
+
+def get_working_dates(
+    start: datetime.date,
+    count: int,
+    cal,
+) -> List[Tuple[datetime.date, int]]:
+    """get workdays from `start` date to `start+count` date"""
+    working_dates = []
+    holidays = []
+    for i in range(count):
+        current_date = start + timedelta(days=i)
+        year = current_date.year
+        holidates = [date for (date, _) in cal.holidays(year)]
+        if current_date.isoweekday() < 6:
+            if current_date not in holidates:
+                working_dates.append(current_date)
+            else:
+                holidays.append(current_date)
+    return working_dates, holidays
+
+
+def get_start_date(start_date_arg: str) -> datetime.date:
+    """parse user's `start` argument"""
+    if start_date_arg.lower() == "today":
+        start_date = datetime.today().date()
+    elif start_date_arg.lower() == "yesterday":
+        start_date = datetime.today().date() - timedelta(days=1)
+    else:
+        # PR#22 Parsing Dates with dateutil
+        numbers = re.split("[-/ ]", start_date_arg)
+        # 4 Digit Year
+        if max(map(len, numbers)) == 4:
+            yearfirst = len(numbers[0]) == 4
+            dayfirst = not yearfirst
+            start_date = dateutil.parser.parse(
+                start_date_arg, yearfirst=yearfirst, dayfirst=dayfirst
+            ).date()
+        # 2 Digit Year
+        else:
+            cand1 = dateutil.parser.parse(start_date_arg, dayfirst=True).date()  # DMY
+            cand2 = dateutil.parser.parse(start_date_arg, yearfirst=True).date()  # YMD
+            today = datetime.today().date()
+            diff1 = abs((cand1 - today).total_seconds())
+            diff2 = abs((cand2 - today).total_seconds())
+            # Select Nearest
+            if diff1 < diff2:
+                start_date = cand1
+            else:
+                start_date = cand2
+    return start_date
+
+
+def assert_start_date(start_date: datetime.date) -> bool:
+    """
+    check the start date submitting is bounded within `n` days.
+    parse `n` from config file. default : 7
+
+    Returns:
+        True: when date is ok or verified by user as ok
+        False: when date is invalid and confirmed by user as invalid
+    """
+    start_date_str = start_date.strftime("%d/%m/%Y")
+    today = datetime.today().date()
+    if (
+        Config.SANITY_CHECK_START_DATE
+        and int(Config.SANITY_CHECK_RANGE) < abs(today - start_date).days
+    ):
+        user_confirm = input(
+            f"The entered date '{start_date_str}' "
+            f"is beyond the maximum '{Config.SANITY_CHECK_RANGE}' day window from today.\n"
+            f"You can modify the threshold and/or disable this confirmation option from '{Config.CONFIG_PATH}'.\n"
+            f" - To modify threshold, change value of '{next(iter(Config.sanity_check_range_dict))}'.\n"
+            f" - To disable, pass 'False' to '{next(iter(Config.sanity_check_bool_dict))}'.\n"
+            f"Do you want to submit the timesheet anyway? [y/N]: "
+        )
+        if user_confirm.lower() != "y":
+            return False
+    return True
