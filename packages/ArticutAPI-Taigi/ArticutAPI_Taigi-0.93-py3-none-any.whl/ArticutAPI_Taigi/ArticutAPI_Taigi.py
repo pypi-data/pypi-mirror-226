71fa2d9ccef531a50454092fdea70b4db69a0be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:06:53 +0000

    Add package ArticutAPI-Taigi/0.93/ArticutAPI_Taigi-0.93-py3-none-any.whl

---
+++ packages/ArticutAPI-Taigi/ArticutAPI_Taigi-0.93-py3-none-any.whl/ArticutAPI_Taigi/ArticutAPI_Taigi.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import json
+import os
+import platform
+import re
+import tempfile
+import unicodedata
+from pprint import pprint
+
+from ArticutAPI import Articut
+try:
+    from .defaultDict import Taigi_Lexicon
+    from .posShift import shiftRule
+except:
+    from defaultDict import Taigi_Lexicon
+    from posShift import shiftRule
+
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+
+class ArticutTG:
+    def __init__(self, username="", apikey="", usernameENG="", apikeyENG=""):
+        self.articut = Articut(username=username, apikey=apikey)
+        self.articutENG = Articut(username=usernameENG, apikey=apikeyENG, url="https://nlu.droidtown.co")
+        self.posPat = re.compile("<[^<]*>([^<]*)</([^<]*)>")
+        self.TLPat = re.compile("\s?[\-a-zA-Záíúéóàìùèòâîûêôǎǐǔěǒāīūēō̋̍]+(-+[a-zA-Záíúéóàìùèòâîûêôǎǐǔěǒāīūēō̋̍]+)*\s?")
+        self.userDefinedDICT = {}
+        self.cjkPAT = re.compile('[\u4e00-\u9fff]')
+        self.moeCSV = [[t.replace("\n", "") for t in l.split(",")] for l  in open("{}/defaultDict/moe_dict/詞目總檔.csv".format(BASEPATH), "r", encoding="utf-8").readlines()]
+        self.DT_TL = Taigi_Lexicon.DT_TL
+        self.purgePat = re.compile("</\w+(_\w+)?><\w+(_\w+)?>|</?\w+(_\w+)?>")
+        self.shiftRule = shiftRule
+        self.defaultDICT = Taigi_Lexicon.dictCombiner()
+        self.legacyLIST = ["", "𪜶𪹚𫝏𫝘𫝙𫝛𫝞𫝺𫝻𫝾𫞭𫞻𫞼𫟂𫟊𫟧𫠛𫣆"]
+    def _pos2Obj(self, posLIST):
+        resultLIST = []
+        for pos in posLIST:
+            if '</' in pos:
+                textPosLIST = [[p.group(1), p.group(2)] for p in self.posPat.finditer(pos)]
+                # group(0) <ACTION_verb>結帳</ACTION_verb>
+                # group(1) 結帳
+                # group(2) ACTION_verb
+                objLIST = []
+                for txt, p in textPosLIST:
+                    objLIST.append({
+                        "text": txt,
+                        "pos": p,
+                    })
+                if objLIST:
+                    resultLIST.append(objLIST)
+                else:
+                    resultLIST.append([{
+                        "text": pos,
+                        "pos": 'PUNCTUATION',
+                    }])
+            else:
+                resultLIST.append([{
+                    "text": pos,
+                    "pos": 'PUNCTUATION',
+                }])
+        return resultLIST
+
+    def _2TL(self, posLIST):
+        '''
+        把 posLIST 裡的每一個詞，一一轉為 TL (台羅拼音)
+        '''
+        resultLIST = []
+        for p in posLIST: #p 是一句
+            for word in p:
+                if "LATIN" in unicodedata.name(word["text"][0]):
+                    resultLIST.append(word["text"])
+                elif word["text"] in self.DT_TL:
+                    resultLIST.append(self.DT_TL[word["text"]])
+                elif word["text"] in [token[2] for token in self.moeCSV]:
+                    tokenLIST = []
+                    for token in self.moeCSV[1:]:
+                        if token[2] == word["text"]:
+                            tokenLIST.append(token[3])
+
+                    if len(tokenLIST) > 1:
+                        resultLIST.append("({})".format("/".join(tokenLIST)))
+                    elif len(tokenLIST) == 1:
+                        if "/" in tokenLIST[0]:
+                            resultLIST.append("({})".format(tokenLIST[0]))
+                        else:
+                            resultLIST.append("{}".format(tokenLIST[0]))
+                    else:
+                        pass
+                    tokenLIST = []
+                else:
+                    wordLIST = []
+                    tokenLIST = []
+                    for t in word["text"]:
+                        for token in self.moeCSV:
+                            if len(token[2]) == 1 and t == token[2]:
+                                tokenLIST.append("{}".format(token[3]))
+
+                        if len(tokenLIST) > 1:
+                            wordLIST.append("({})".format("/".join(tokenLIST)))
+                        elif len(tokenLIST) == 1:
+                            wordLIST.append("{}".format(tokenLIST[0]))
+                        tokenLIST = []
+                    resultLIST.append("-".join(wordLIST))
+        return resultLIST
+
+#<ToDo>
+    #def _2POJ(self, postLIST):
+        #'''
+        #把 posLIST 裡的每一個詞，一一轉為 POJ (白話字拼音)
+        #'''
+        #resultLIST = []
+        #return resultLIST
+
+    #def _2Word(self, posLIST):
+        #'''
+        #每次從 posLIST 中取一個詞彙，反查這個詞彙是 TL 或 POS 的拼音，依該拼音把同音詞彙找出來，並把這個詞彙同音的台語漢字加入回傳結果中。
+        #'''
+        #resultLIST = []
+        #return resultLIST
+
+    #def _TL2Word(self, TLLIST):
+        #'''
+        #把 TLLIST (台羅拼音)裡的每一個詞，一一轉為台語漢字
+        #'''
+        #resultLIST = []
+        #return resultLIST
+
+    #def _POJ2Word(self, POJLIST):
+        #'''
+        #把 POJLIST (白話字拼音)裡的每一個詞，一一轉為台語漢字
+        #'''
+        #resultLIST = []
+        #return resultLIST
+#</ToDo>
+    def _mixedInputDetector(self, inputSTR):
+        TLLIST = [t.group() for t in self.TLPat.finditer(inputSTR)]
+        with open(self.TaigiDictFILE.name) as f:
+            userDefinedDICT = json.load(f)
+
+        #<特殊區塊：如果使用者有購買英文版 Articut 的使用額度，將調用英文人名偵測。>
+        #<否則本功能會在每小時 2000 字免費額度用盡後失效，待下一個小時的免費額度啟用時才恢復>
+        knownLIST = []
+        for i in TLLIST:
+            resultDICT = self.articutENG.parse(i, level="lv1")
+            if resultDICT["status"] == True and resultDICT["msg"] == "Success!":
+                if "<ENTITY_person>{}</ENTITY_person>".format(i) in "".join(resultDICT["result_pos"]):
+                    knownLIST.append(i)
+                    userDefinedDICT["ENTITY_person"].append(i)
+                    self.userDefinedDICT["ENTITY_person"].append(i)
+            else:
+                pass
+        TLLIST = list(set(TLLIST)-set(knownLIST))
+        #</特殊區塊：如果使用者有購買英文版 Articut 的使用額度，將調用英文人名偵測>
+        #</否則本功能會在每小時 2000 字免費額度用盡後失效，待下一個小時的免費額度啟用時才恢復>
+
+        if TLLIST == []:
+            pass
+        else:
+            #with open(self.userDefinedDictFILE.name) as f:
+                #userDefinedDICT = json.load(f)
+            if "_ArticutTaigiUserDefined" in userDefinedDICT.keys():
+                userDefinedDICT["_ArticutTaigiUserDefined"].extend(TLLIST)
+            else:
+                userDefinedDICT["_ArticutTaigiUserDefined"] = TLLIST
+            if platform.system() == "Windows":
+                self.userDefinedDictFILE = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+            else:
+                self.userDefinedDictFILE = tempfile.NamedTemporaryFile(mode="w+")
+            json.dump(userDefinedDICT, self.userDefinedDictFILE)
+            self.userDefinedDictFILE.flush()
+        return None
+
+    def _spaceWalker(self, inputDICT):
+        '''
+        移除非 CJK 字元前後的空格
+        '''
+        fposPat = re.compile("<UserDefined>\s")
+        pposPat = re.compile("\s</UserDefined>")
+        posPat = re.compile("</?\w_?[^>]+?>")
+        for i in range(0, len(inputDICT["result_pos"])):
+            inputDICT["result_pos"][i] = re.sub(fposPat, " <UserDefined>", inputDICT["result_pos"][i])
+            inputDICT["result_pos"][i] = re.sub(pposPat, "</UserDefined> ", inputDICT["result_pos"][i])
+            inputDICT["result_pos"][i] = inputDICT["result_pos"][i].strip()
+        try:
+            inputDICT["result_pos"].remove("")
+        except:
+            pass
+
+        tmpSTR = ""
+        for i in range(0, len(inputDICT["result_pos"])):
+            tmpSTR = tmpSTR + re.sub(posPat, "<DROIDTOWN_TKBD>", inputDICT["result_pos"][i])
+            tmpSTR = tmpSTR.replace("<DROIDTOWN_TKBD><DROIDTOWN_TKBD>", "/").replace("<DROIDTOWN_TKBD> <DROIDTOWN_TKBD>", "/ /").replace("<DROIDTOWN_TKBD>", "/")
+        inputDICT["result_segmentation"] = tmpSTR
+        return inputDICT
+
+    def _posShift(self, posLIST):
+        for i in range (0, len(posLIST)):
+            if len(posLIST[i]) == 1:
+                pass
+            else:
+                for pat in self.shiftRule:
+                    shiftLIST = [(g.start(), g.end(), g.group(0)) for g in reversed(list(pat[0].finditer(posLIST[i])))]
+                    for s in shiftLIST:
+                        adjustedSTR = s[2]
+                        for adjust_s in pat[1]:
+                            adjustedSTR = adjustedSTR.replace(adjust_s, pat[2][pat[1].index(adjust_s)])
+                        posLIST[i] = "{}{}{}".format(posLIST[i][:s[0]], adjustedSTR, posLIST[i][s[1]:])
+        return posLIST
+
+    def _pos2Seg(self, posLIST):
+        resultSTR = ""
+        for s in posLIST:
+            resultSTR = resultSTR + re.sub(self.purgePat, "╱", s)
+        return resultSTR
+
+    def parse(self, inputSTR, level="lv2", userDefinedDictFILE=None, convert=None):
+        for i in self.legacyLIST[0]:
+            inputSTR = inputSTR.replace(i, self.legacyLIST[1][self.legacyLIST[0].index(i)])
+
+        if level=="lv3":
+            tgLV = "lv3"
+            level = "lv2"
+            if convert == None:
+                convert = "TL"
+            else:
+                if convert.upper() in ("TL", "POJ", "WORD"):
+                    convert = convert.upper()
+                else:
+                    raise
+        else:
+            tgLV = level
+
+        if platform.system() == "Windows":
+            self.TaigiDictFILE = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+        else:
+            self.TaigiDictFILE = tempfile.NamedTemporaryFile(mode="w+")
+
+        self.userDefinedDICT = self.defaultDICT
+        if userDefinedDictFILE == None:
+            pass
+        else:
+            with open(userDefinedDictFILE, encoding="utf-8") as f:
+                userDefinedDICT = json.load(f)
+            for k in userDefinedDICT.keys():
+                try:
+                    tmpLIST = userDefinedDICT[k]
+                    for POS in self.defaultDICT.keys():
+                        self.defaultDICT[POS] = list(set(self.defaultDICT[POS])-set(tmpLIST))
+                    if k in self.userDefinedDICT.keys():
+                        tmpLIST.extend(self.userDefinedDICT[k])
+                    tmpLIST = list(set(tmpLIST))
+                    self.userDefinedDICT[k].extend(tmpLIST)
+                except KeyError:
+                    self.userDefinedDICT[k] = tmpLIST
+
+        json.dump( self.userDefinedDICT, self.TaigiDictFILE)
+        self.TaigiDictFILE.flush()
+
+
+        self._mixedInputDetector(inputSTR)
+        articutResultDICT = self.articut.parse(inputSTR, level=level, userDefinedDictFILE=self.TaigiDictFILE.name)
+        articutResultDICT = self._spaceWalker(articutResultDICT)
+        POScandidateLIST = []
+        for tkn in articutResultDICT["result_segmentation"].split("/"):
+            for k in self.userDefinedDICT.keys():
+                if tkn in self.userDefinedDICT[k]:
+                    POScandidateLIST.append(("<UserDefined>{}</UserDefined>".format(tkn), "<{0}>{1}</{0}>".format(k, tkn)))
+
+        for i, s in enumerate(articutResultDICT["result_pos"]):
+            for p in POScandidateLIST:
+                articutResultDICT["result_pos"][i] = articutResultDICT["result_pos"][i].replace(p[0],p[1])
+
+        articutResultDICT["result_pos"] = self._posShift(articutResultDICT["result_pos"])
+        articutResultDICT["result_obj"] = self._pos2Obj(articutResultDICT["result_pos"])
+        articutResultDICT["result_segmentation"] = self._pos2Seg(articutResultDICT["result_pos"])
+
+        if tgLV in ("lv1", "lv2"):
+            return articutResultDICT
+        elif tgLV == "lv3":
+            resultDICT = {"person": self.articut.getPersonLIST(articutResultDICT),
+                          #"event": articutResultDICT["event"],
+                          "time": self.articut.getTimeLIST(articutResultDICT),
+                          "site": self.articut.getLocationStemLIST(articutResultDICT),
+                          "entity":  self.articut.getNounStemLIST(articutResultDICT),
+                          #"number": articutResultDICT["number"],
+                          #"user_defined": articutResultDICT["user_defined"],
+                          "utterance": [],
+                          #"input": articutResultDICT["input"],
+                          #"unit": articutResultDICT["unit"],
+                          "exec_time": articutResultDICT["exec_time"],
+                          "level": "lv3",
+                          "version": articutResultDICT["version"],
+                          "status": articutResultDICT["status"],
+                          "msg": articutResultDICT["msg"],
+                          "word_count_balance": articutResultDICT["word_count_balance"],
+                          }
+            # 呼叫一個 converter，把 resultDICT["result_obj"] 裡的詞彙一個一個轉成拼音。
+            if convert == "TL":
+                resultLIST = self._2TL(articutResultDICT["result_obj"])
+            #elif convert == "POJ":
+                #resultLIST = self._2POJ(articutResultDICT["result_obj"])
+            #else: #convert == "WORD":
+                #resultLIST = self._2Word(articutResultDICT["result_obj"])
+            resultDICT["utterance"] = "╱".join(resultLIST)
+            return resultDICT
+
+
+
+if __name__ == "__main__":
+    accountDICT = {"username":"peter.w@droidtown.co", "apikey":"Fs=YW-3@&3HO=hCXPPb2+Zw+ffjBzS4"}
+    #台語漢字 CWS/POS TEST
+    inputSTR = "你ē-sái請ta̍k-ke提供字句hō͘你做這個試驗。"
+    inputSTR = "跋倒, 佮意"
+    inputSTR = "安全帽".replace("。", "")
+    articutTaigi = ArticutTG(username=accountDICT["username"], apikey=accountDICT["apikey"])
+    resultDICT = articutTaigi.parse(inputSTR, level="lv2")
+    pprint(resultDICT)
+    #<ENTITY_pronoun>[^<]+</ENTITY_pronoun>(<FUNC_inner>[^<]+</FUNC_inner>)?<ENTITY_noun>[^<]+</ENTITY_noun><DegreeP>[^<]+</DegreeP>
