bf2a6e52b2ec595e9e2a9ccfe5f5d31378d2122e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 04:10:44 +0000

    Add package forty2/0.0.0/forty2-0.0.0.tar.gz

---
+++ packages/forty2/forty2-0.0.0.tar.gz/forty2-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='forty2',
+      version='0.0.0',
+      description='placeholder',
+      url='http://github.com/SK/forty2',
+      author='stephen king',
+      author_email='sking@gmail.com',
+      license='MIT',
+      packages=['forty2'],
+      zip_safe=False)
