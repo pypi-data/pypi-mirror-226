6c2a16f97998970f6d60aa5b352a82311b848fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:24 +0000

    Add package scrape-up/1.1.0/scrape_up-1.1.0-py3-none-any.whl

---
+++ packages/scrape-up/scrape_up-1.1.0-py3-none-any.whl/scrape_up/kindle_bookstore/kindle.py
@@ -0,0 +1,152 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class AmazonKindle:
+    """
+    Create an instance of `Book` class.
+    ```python
+    books = AmazonKindle()
+    ```
+    | Methods          | Details                                                |
+    | ---------------- | ------------------------------------------------------ |
+    | `.bestsellers()` | Returns the list of best-selling books on AmazonKindle |
+    | `.topbooks()`    | Returns the list of top books on AmazonKindle          |
+    """
+    def __init__(self):
+        pass
+
+    def bestsellers(self):
+        """
+        Class - `Book`\n
+        Example -\n
+        ```python
+        books = AmazonKindle()
+        book.bestsellers()
+        ```
+        Return\n
+        ```js
+        [
+            {
+                "Book_Title":"101 Brain Booster Activity Book: Fun Activity Book For Children",
+                "Author_Name":"Wonder House Books",
+                "Stars":"N/A",
+                "Price":"₹149.00",
+                "URL":"https://www.amazon.in/101-Brain-Booster-ActivityBook/dp/9388369793/ref=zg_bs_g_books_sccl_20/000-0000000-0000000?psc=1"
+            }
+        ]
+        ```
+        """
+        try:
+            url = "https://www.amazon.in/gp/bestsellers/books/"
+            response = requests.get(url)
+            page_contents = response.text
+            doc = BeautifulSoup(page_contents, "html.parser")
+
+            book_title_tags = doc.find_all("div", {"class": "zg-grid-general-faceout"})
+            author_name_tags = doc.find_all("div", {"class": "zg-grid-general-faceout"})
+            rating = "a-icon a-icon-star-small a-star-small-4-5 aok-align-top"
+            rating_tags = doc.find_all("i", {"class": rating})
+            book_price_tags = doc.find_all("div", {"class": "zg-grid-general-faceout"})
+            book_url_tag = doc.find_all("div", {"class": "zg-grid-general-faceout"})
+
+            all_books = []
+
+            for i in range(len(book_title_tags)):
+                book_title = book_title_tags[i].find("span").text
+                author_name = (
+                    author_name_tags[i]
+                    .find("div", {"class": "a-row a-size-small"})
+                    .text
+                )
+
+                # Check if the index is within the range of rating_tags
+                if i < len(rating_tags):
+                    star_rating = rating_tags[i].find("span").text
+                else:
+                    star_rating = (
+                        "N/A"  # Assign a default value if rating is not available
+                    )
+
+                book_price = (
+                    book_price_tags[i].find("span", {"class": "p13n-sc-price"}).text
+                )
+                book_url = (
+                    "https://www.amazon.in"
+                    + book_url_tag[i].find("a", {"class": "a-link-normal"})["href"]
+                )
+
+                book_details = {
+                    "Book_Title": book_title,
+                    "Author_Name": author_name,
+                    "Stars": star_rating,
+                    "Price": book_price,
+                    "URL": book_url,
+                }
+
+                all_books.append(book_details)
+
+            return all_books
+
+        except Exception as e:
+            return None
+
+    def topbooks(self):
+        """
+        Class - `Book`\n
+        Example -\n
+        ```python
+        books = AmazonKindle()
+        book.topbooks()
+        ```
+        Return\n
+        ```js
+        [
+            {
+                "Title":"Master Your Emotions: A Practical Guide to Overcome Negativity and Better Manage Your Feelings (Mastery Series Book 1)",
+                "Rating(out of 5)":"4.5",
+                "Price(in rupees)":"₹199.00",
+                "Link":"https://www.amazon.in//Master-Your-Emotions-Practical-Negativity-ebook/dp/B07CX8H6YH/ref=zg_bs_g_digital-text_sccl_1/260-0078955-4830376?psc=1"
+            }
+            ...
+        ]
+        ```
+        """
+        try:
+            all_books = []
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36"
+            }
+            for page_link in [
+                "https://www.amazon.in/gp/bestsellers/digital-text/ref=zg_bs_nav_0",
+                "https://www.amazon.in/gp/bestsellers/digital-text/ref=zg_bs_pg_2_digital-text?ie=UTF8&pg=2",
+            ]:
+                webpage = requests.get(page_link, headers=headers).text
+                soup = BeautifulSoup(webpage, "lxml")
+                books_container = soup.find(
+                    "div", {"class": "p13n-gridRow _cDEzb_grid-row_3Cywl"}
+                )
+                for items in books_container.find_all("div", {"id": "gridItemRoot"}):
+                    title = items.find(
+                        "div", {"class": "_cDEzb_p13n-sc-css-line-clamp-1_1Fn1y"}
+                    ).text
+                    stars = items.find("span", {"class": "a-icon-alt"})
+                    if stars is None:
+                        stars = "Rating not available"
+
+                    else:
+                        stars = stars.text.split()[0]
+                    price = items.find("span", {"class": "a-size-base"}).text
+                    link = items.find("a", {"class": "a-link-normal"}, href=True)
+                    url = "https://www.amazon.in/" + link["href"]
+                    books = {
+                        "Title": title,
+                        "Rating(out of 5)": stars,
+                        "Price(in rupees)": price,
+                        "Link": url,
+                    }
+                    all_books.append(books)
+            return all_books
+
+        except Exception as e:
+            return None
---
+++ packages/scrape-up/scrape_up-1.1.0-py3-none-any.whl/scrape_up/youtube/channel.py
@@ -0,0 +1,289 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+class Channel:
+    """
+    Create an instance of `Channel` class.
+    ```python
+    channel_data = Channel(channel_username="BeABetterDev")
+    ```
+
+    | Methods       | Details                                                                |
+    | ------------- | ---------------------------------------------------------------------- |
+    | `.get_about()` | Returns the channel details mentioned in the about page of the channel |
+    | `.get_videos()`| Returns all the video details in the videos page of the channel |
+    | `.get_community()`| Returns all the post details in the community page of the channel |
+    """
+
+    def __init__(self, channel_username):
+        self.channel_username = channel_username
+
+    def get_about(self):
+        """
+        Class - `Channel`
+        ```py
+        channel_data = Channel(channel_username="BeABetterDev")
+        channel_data.get_about()
+        ```
+        Returns:
+        ```js
+        {
+            "name": Name of the channel
+            "description": Description of the channel
+            "channel_url": Link to the channel
+            "channel_avatar": Channel avatar
+            "channel_banner": Channel banner
+            "subscriber_count": No. of subscribers of the channel
+            "toal_videos": Total videos uploaded in the channel
+            "total_views": Total views till date of the channel
+            "join_date": Date the channel joined YouTube
+            "country": Country of origin of the channel
+            "links": Additional links provided from the channel
+        }
+        ```
+        """
+        url = f"https://www.youtube.com/@{self.channel_username}/about"
+        try:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "html.parser")
+            channel_data = {"channel_data": []}
+            link_data = {"link_data": []}
+            scripts = soup.find_all("script")
+            req_script = scripts[35].text.strip()
+            script = req_script[20:-1]
+            data = json.loads(script)
+
+            metadata = data["metadata"]["channelMetadataRenderer"]
+            title = metadata["title"]
+            desc = metadata["description"]
+            channel_url = metadata["vanityChannelUrl"]
+            channel_avatar = metadata["avatar"]["thumbnails"][0]["url"]
+            header = data["header"]["c4TabbedHeaderRenderer"]
+            channel_banner = header["banner"]["thumbnails"][5]["url"]
+            subs = header["subscriberCountText"]["simpleText"]
+            total_videos = header["videosCountText"]["runs"][0]["text"]
+
+            baser = data["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]
+            for b in baser:
+                try:
+                    base = b["tabRenderer"]["content"]["sectionListRenderer"][
+                        "contents"
+                    ][0]["itemSectionRenderer"]["contents"][0][
+                        "channelAboutFullMetadataRenderer"
+                    ]
+
+                    total_views = base["viewCountText"]["simpleText"]
+                    join_date = base["joinedDateText"]["runs"][1]["text"]
+                    country = base["country"]["simpleText"]
+
+                    links = base["primaryLinks"]
+                    for i in links:
+                        link_data["link_data"].append(
+                            {
+                                "link_url": i["navigationEndpoint"]["urlEndpoint"][
+                                    "url"
+                                ],
+                                "link_name": i["title"]["simpleText"],
+                                "link_icon": i["icon"]["thumbnails"][0]["url"],
+                            }
+                        )
+                except:
+                    pass
+
+            channel_data["channel_data"].append(
+                {
+                    "name": title,
+                    "description": desc,
+                    "channel_url": channel_url,
+                    "channel_avatar": channel_avatar,
+                    "channel_banner": channel_banner,
+                    "subscriber_count": subs,
+                    "toal_videos": total_videos,
+                    "total_views": total_views,
+                    "join_date": join_date,
+                    "country": country,
+                    "links": link_data,
+                }
+            )
+            return channel_data["channel_data"][0]
+        except:
+            return None
+
+    def get_videos(self):
+        """
+        Class - `Channel`
+        ```py
+        channel_data = Channel(channel_username="BeABetterDev")
+        channel_data.get_videos()
+        ```
+        Returns:
+        ```js
+        [
+            {
+                "title": Title of the video
+                "description": Description of the video
+                "thumbnail_url": Link of the thumbnail image
+                "views_count": Total views till date of the video
+                "publishedAt": Date the video was published
+                "video_length": Time length of the video
+                "link": Link to the video
+            }
+            ...
+        ]
+        ```
+        """
+        url = f"https://www.youtube.com/@{self.channel_username}/videos"
+        try:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "html.parser")
+            videos_data = {"videos": []}
+            scripts = soup.find_all("script")
+            req_script = scripts[35].text.strip()
+            script = req_script[20:-1]
+            data = json.loads(script)
+
+            vids = data["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1][
+                "tabRenderer"
+            ]["content"]["richGridRenderer"]["contents"]
+            for v in vids:
+                try:
+                    base = v["richItemRenderer"]["content"]["videoRenderer"]
+                    title = base["title"]["runs"][0]["text"]
+                    thumbnail_url = base["thumbnail"]["thumbnails"][-1]["url"]
+                    desc = base["descriptionSnippet"]["runs"][0]["text"]
+                    publishedAt = base["publishedTimeText"]["simpleText"]
+                    length = base["lengthText"]["accessibility"]["accessibilityData"][
+                        "label"
+                    ]
+                    views = base["viewCountText"]["simpleText"]
+                    link = (
+                        "https://www.youtube.com/"
+                        + base["navigationEndpoint"]["commandMetadata"][
+                            "webCommandMetadata"
+                        ]["url"]
+                    )
+                except:
+                    pass
+
+                videos_data["videos"].append(
+                    {
+                        "title": title,
+                        "description": desc,
+                        "thumbnail_url": thumbnail_url,
+                        "views_count": views,
+                        "publishedAt": publishedAt,
+                        "video_length": length,
+                        "link": link,
+                    }
+                )
+            return videos_data["videos"]
+        except:
+            return None
+
+    def get_community(self):
+        """
+        Class - `Channel`
+        ```py
+        channel_data = Channel(channel_username="BeABetterDev")
+        channel_data.get_community()
+        ```
+        Returns:
+        ```js
+        [
+            {
+                "title":"Over the past few weeks, this channel crossed 10,000,000 total views and 150,000 subscribers. It's incredible to think how far its come and how many people this channel has managed to help. Thank you for your support!",
+                "images":[
+                    
+                ],
+                "likes_count":"157",
+                "comment_count":"10",
+                "publishedAt":"4 months ago"
+            },
+            ...
+        ]
+        ```
+        """
+        url = f"https://www.youtube.com/@{self.channel_username}/community"
+        try:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "html.parser")
+            posts_data = {"posts": []}
+            scripts = soup.find_all("script")
+            req_script = scripts[35].text.strip()
+            script = req_script[20:-1]
+            data = json.loads(script)
+
+            try:
+                posts = data["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][6][
+                    "tabRenderer"
+                ]["content"]["sectionListRenderer"]["contents"][0][
+                    "itemSectionRenderer"
+                ][
+                    "contents"
+                ]
+            except:
+                posts = data["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][5][
+                    "tabRenderer"
+                ]["content"]["sectionListRenderer"]["contents"][0][
+                    "itemSectionRenderer"
+                ][
+                    "contents"
+                ]
+
+            for p in posts:
+                images = {"images": []}
+                try:
+                    base = p["backstagePostThreadRenderer"]["post"][
+                        "backstagePostRenderer"
+                    ]
+                    try:
+                        title = base["contentText"]["runs"][0]["text"]
+                    except:
+                        title = ""
+                    try:
+                        try:
+                            imagebloc = base["backstageAttachment"][
+                                "postMultiImageRenderer"
+                            ]["images"]
+                            for i in imagebloc:
+                                images["images"].append(
+                                    i["backstageImageRenderer"]["image"]["thumbnails"][
+                                        -1
+                                    ]["url"]
+                                )
+                        except:
+                            images["images"].append(
+                                base["backstageAttachment"]["backstageImageRenderer"][
+                                    "image"
+                                ]["thumbnails"][-1]["url"]
+                            )
+                    except:
+                        images["images"] = []
+                    date = base["publishedTimeText"]["runs"][0]["text"]
+                    try:
+                        likes = base["voteCount"]["simpleText"]
+                    except:
+                        likes = "0"
+                    try:
+                        comment_count = base["actionButtons"][
+                            "commentActionButtonsRenderer"
+                        ]["replyButton"]["buttonRenderer"]["text"]["simpleText"]
+                    except:
+                        comment_count = "0"
+                except:
+                    pass
+
+                posts_data["posts"].append(
+                    {
+                        "title": title,
+                        "images": images["images"],
+                        "likes_count": likes,
+                        "comment_count": comment_count,
+                        "publishedAt": date,
+                    }
+                )
+            return posts_data["posts"]
+        except:
+            return None
