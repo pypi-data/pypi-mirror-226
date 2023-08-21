29c84fb53262332cd4025f47a2fad4c2458587af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:42 +0000

    Add package vecta-testa/0.1.1/vecta-testa-0.1.1.tar.gz

---
+++ packages/vecta-testa/vecta-testa-0.1.1.tar.gz/vecta-testa-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vecta-testa",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add your package dependencies here
+    ],
+    author="Hint Services",
+    author_email="ben@hint.services",
+    description="A package for testing vector embeddings.",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hint-Services/vekta-testa",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
