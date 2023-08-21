1c317ac0526eef07c38249de9830261300b97731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:06 +0000

    Add package cryptoguardian/0.0.3/cryptoguardian-0.0.3.tar.gz

---
+++ packages/cryptoguardian/cryptoguardian-0.0.3.tar.gz/cryptoguardian-0.0.3/cryptoguardian/__init__.py
@@ -0,0 +1,3 @@
+from .cryptoguardian import CryptoGuardian
+
+__all__ = ["CryptoGuardian"]
---
+++ packages/cryptoguardian/cryptoguardian-0.0.3.tar.gz/cryptoguardian-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Uma ferramenta de criptografia e descriptografia de arquivos.'
+with open('README.md', 'r', encoding='utf-8') as long_description_file:
+    LONG_DESCRIPTION = long_description_file.read()
+
+setup(
+    name = 'cryptoguardian',
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    author = 'Melissa Alves e Davi Rodolfo',
+    author_email = 'msnmelissaoa15@hotmail.com',
+    packages = ['cryptoguardian'],
+    install_requires = [
+        'cryptography',
+        'PyQt5',
+    ],
+
+    classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent"
+    ],
+
+    entry_points = {
+        'console_scripts': [
+            'cryptoguardian = cryptoguardian.main:main',
+        ],
+    },
+)
