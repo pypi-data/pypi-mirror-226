b3ae78a3db4ed00bcde8b50d2d6dd60158cf062a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:05 +0000

    Add package pypeepa/1.6/pypeepa-1.6-py3-none-any.whl

---
+++ packages/pypeepa/pypeepa-1.6-py3-none-any.whl/pypeepa/__init__.py
@@ -0,0 +1,42 @@
+from .fileInteraction import (
+    getFilePath,
+    asyncListDir,
+    asyncReadJSON,
+    createDirectory,
+    readData,
+    writeFile,
+)
+from .userInteraction import (
+    askYNQuestion,
+    getInputFilePath,
+    printArray,
+    selectOptionQuestion,
+    signature,
+    progressBarIterator,
+)
+from .optimisations import processCSVInChunks, concurrentFutures, ProgressSaver
+from .debugging import initLogging
+from .utils import loggingHandler
+
+__author__ = "Ishfaq Ahmed"
+__email__ = "ishfaqahmed0837@gmail.com"
+__description__ = ("Custom built utilities for general use",)
+__all__ = (
+    ProgressSaver,
+    loggingHandler,
+    processCSVInChunks,
+    getFilePath,
+    askYNQuestion,
+    getInputFilePath,
+    printArray,
+    selectOptionQuestion,
+    signature,
+    asyncListDir,
+    asyncReadJSON,
+    createDirectory,
+    readData,
+    writeFile,
+    progressBarIterator,
+    concurrentFutures,
+    initLogging,
+)
---
+++ packages/pypeepa/pypeepa-1.6-py3-none-any.whl/pypeepa/optimisations/processCSVInChunks.py
@@ -0,0 +1,41 @@
+from alive_progress import alive_bar
+import pandas as pd
+from typing import Callable, Optional, TypeVar, Any, Dict
+
+
+ArgsType = TypeVar("ArgsType", bound=Dict)
+
+
+def processCSVInChunks(
+    csv_file: str,
+    process_function: Callable[[pd.DataFrame, Any], pd.DataFrame],
+    pf_args: ArgsType,
+    chunk_size: Optional[int] = 10000,
+):
+    """
+    Process any CSV file in chunks instead of whole file at once\n\n
+    `csv_file`: Path to the csv file.\n
+    `chunk_size`: Size of chunks to work with\n
+    :strong:`process_function`: The function containing the main processing you want to get done.\n
+    `pf_args`: Arguments for the function in a dict eg:-\n
+                def deleteRowsInCSV(dataframe,delete_rows):\n
+                    # ...delete rows from csv.\n
+                await processCSVInChunks(path_to_csv,deleteRowsInCSV,{delete_rows:20})\n
+
+    """
+    # Create a generator to read the CSV file in chunks
+    chunk_reader = pd.read_csv(csv_file, chunksize=chunk_size)
+
+    # Process each chunk and concatenate the results
+    processed_chunks = []
+    total_chunks = int(sum(1 for row in open(csv_file, "r")) / chunk_size)
+    with alive_bar(total_chunks, force_tty=True, bar="filling", spinner="waves") as bar:
+        bar.title = "Processing file -> " + csv_file
+        for chunk in chunk_reader:
+            processed_chunk = process_function(chunk, pf_args)
+            processed_chunks.append(processed_chunk)
+            bar()
+    # Concatenate the processed chunks into a single DataFrame
+    df = pd.concat(processed_chunks, ignore_index=True)
+
+    return df
