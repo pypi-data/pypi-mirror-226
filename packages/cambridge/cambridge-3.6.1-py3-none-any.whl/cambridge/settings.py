fc916749853907bf7d30249fe24c529c5d7fd1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 15:09:27 +0000

    Add package cambridge/3.6.1/cambridge-3.6.1-py3-none-any.whl

---
+++ packages/cambridge/cambridge-3.6.1-py3-none-any.whl/cambridge/settings.py
@@ -0,0 +1,16 @@
+"""Script to store static data."""
+
+OP = [
+    "FETCHING",
+    "PARSING",
+    "RETRY FETCHING",
+    "RETRY PARSING",
+    "PRINTING",
+    "FOUND",
+    "NOT FOUND",
+    "CACHED",
+    "CANCELLED",
+    "DELETED",
+]
+
+DICTS = ["CAMBRIDGE", "MERRIAM WEBSTER"]
