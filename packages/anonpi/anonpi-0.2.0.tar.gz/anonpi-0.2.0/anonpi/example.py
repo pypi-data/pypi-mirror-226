3192f6a909d86b1192c63955b205dfedc7eb3309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:06 +0000

    Add package anonpi/0.2.0/anonpi-0.2.0.tar.gz

---
+++ packages/anonpi/anonpi-0.2.0.tar.gz/anonpi-0.2.0/anonpi/example.py
@@ -0,0 +1,16 @@
+from call import AnonApi
+
+
+anoncall = AnonApi("adistar123api")
+
+call = anoncall.create_call(
+    to_number = "1234567890",
+    from_number = "1234567890",
+    callback_url = "https://example.com"
+)
+
+
+
+call = anoncall.get_call("ANON::fzZhfvz4L904asdasd5236d5puoBphhO5hg8Pde9Z")
+
+
