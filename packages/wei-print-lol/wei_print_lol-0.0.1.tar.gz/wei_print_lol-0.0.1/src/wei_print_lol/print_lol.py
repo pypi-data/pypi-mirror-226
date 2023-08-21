dfca8966a430b905326aa8d47917baba7ba1d93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:06:38 +0000

    Add package wei-print-lol/0.0.1/wei_print_lol-0.0.1.tar.gz

---
+++ packages/wei-print-lol/wei_print_lol-0.0.1.tar.gz/wei_print_lol-0.0.1/src/wei_print_lol/print_lol.py
@@ -0,0 +1,9 @@
+def print_lol(the_list):
+    """
+    打印列表的每一项，各项单独占一行，列表可以是嵌套列表
+    """
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
