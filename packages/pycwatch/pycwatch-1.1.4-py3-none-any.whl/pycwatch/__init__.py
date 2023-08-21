57ba1fc777e014f28c32260a14320609d1f62a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:13 +0000

    Add package pycwatch/1.1.4/pycwatch-1.1.4-py3-none-any.whl

---
+++ packages/pycwatch/pycwatch-1.1.4-py3-none-any.whl/pycwatch/__init__.py
@@ -0,0 +1,13 @@
+"""Main package."""
+import sys
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+
+from pycwatch.client import CryptoWatchClient
+
+__version__ = version("pycwatch")
+
+__all__ = ("CryptoWatchClient",)
