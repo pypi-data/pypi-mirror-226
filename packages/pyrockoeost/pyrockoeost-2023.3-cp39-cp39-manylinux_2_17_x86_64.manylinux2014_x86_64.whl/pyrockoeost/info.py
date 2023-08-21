bd755118dc36ada095e4dd0fda62ac3a8fc5f99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:42 +0000

    Add package pyrockoeost/2023.3/pyrockoeost-2023.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrockoeost/pyrockoeost-2023.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrockoeost/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = None
+local_modifications = None
+version = '2023.06.07'
+long_version = 'pyrockoeost-2023.06.07-2023-08-20_15:46:42'  # noqa
+installed_date = '2023-08-20_15:46:42'
+src_path = '/home/user/Music'
