fc9528b590009426b4d1a5256000e6a468512920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:03 +0000

    Add package muffin-admin/1.45.8/muffin_admin-1.45.8-py3-none-any.whl

---
+++ packages/muffin-admin/muffin_admin-1.45.8-py3-none-any.whl/muffin_admin/peewee/__init__.py
@@ -0,0 +1,102 @@
+"""Peewee ORM Support."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Tuple, Type, cast
+
+import peewee as pw
+from muffin_peewee import JSONLikeField
+from muffin_rest.peewee.filters import PWFilter
+from muffin_rest.peewee.handler import PWRESTBase
+from muffin_rest.peewee.options import PWRESTOptions
+
+from muffin_admin.handler import AdminHandler, AdminOptions
+
+if TYPE_CHECKING:
+    import marshmallow as ma
+
+    from muffin_admin.types import TRAInfo
+
+
+class PWAdminOptions(AdminOptions, PWRESTOptions):
+
+    """Keep PWAdmin options."""
+
+    def setup(self, cls):
+        """Auto insert filter by id."""
+        super(PWAdminOptions, self).setup(cls)
+
+        for flt in self.filters:
+            name = flt
+
+            if isinstance(flt, PWFilter):
+                name = flt.name
+
+            elif isinstance(flt, tuple):
+                name = flt[0]
+
+            if name == "id":
+                break
+
+        else:
+            self.filters = [PWFilter("id", field=self.model_pk), *self.filters]  # type: ignore[]
+
+
+class PWAdminHandler(AdminHandler, PWRESTBase):
+
+    """Work with Peewee Models."""
+
+    meta_class: Type[PWAdminOptions] = PWAdminOptions
+    meta: PWAdminOptions
+
+    @classmethod
+    def to_ra_field(cls, field: ma.fields.Field, source: str) -> TRAInfo:
+        """Setup RA fields."""
+        model_field = getattr(cls.meta.model, field.attribute or source, None)
+        if (
+            model_field
+            and isinstance(model_field, pw.Field)
+            and (isinstance(model_field, JSONLikeField) or model_field.field_type.lower() == "json")
+        ):
+            return "JsonField", {}
+
+        return super(PWAdminHandler, cls).to_ra_field(field, source)
+
+    @classmethod
+    def to_ra_input(cls, field: ma.fields.Field, source: str) -> TRAInfo:
+        """Setup RA inputs."""
+        model_field = getattr(cls.meta.model, field.attribute or source, None)
+        ra_type, props = super(PWAdminHandler, cls).to_ra_input(field, source)
+        if model_field:
+            if model_field.choices:
+                return "SelectInput", dict(
+                    props,
+                    choices=[{"id": c[0], "name": c[1]} for c in model_field.choices],
+                )
+
+            if isinstance(model_field, pw.TextField):
+                return "TextInput", dict(props, multiline=True)
+
+            if isinstance(model_field, JSONLikeField) or model_field.field_type.lower() == "json":
+                return "JsonInput", props
+
+            if isinstance(model_field, pw.ForeignKeyField) and source in cls.meta.references:
+                ref, _, ref_source = cls.meta.references[source].partition(".")
+                return "FKInput", dict(
+                    props,
+                    refProp=ref_source or ref,
+                    refSource=model_field.rel_field.name,
+                    reference=ref_source and ref or model_field.rel_model._meta.table_name,
+                )
+
+        return ra_type, props
+
+
+class PWSearchFilter(PWFilter):
+
+    """Search in query by value."""
+
+    def query(self, qs: pw.Query, column: pw.Field, *ops: Tuple, **_) -> pw.Query:
+        """Apply the filters to Peewee QuerySet.."""
+        _, value = ops[0]
+        return cast(pw.Query, qs.where(column.contains(value)))
---
+++ packages/muffin-admin/muffin_admin-1.45.8-py3-none-any.whl/muffin_admin/sqlalchemy/__init__.py
@@ -0,0 +1,94 @@
+"""SQLAlchemy core support."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Type
+
+from muffin_rest.filters import Filter
+from muffin_rest.sqlalchemy import SARESTHandler, SARESTOptions
+from muffin_rest.sqlalchemy.filters import SAFilter
+from sqlalchemy import JSON, Enum, Text
+
+from muffin_admin.handler import AdminHandler, AdminOptions
+
+if TYPE_CHECKING:
+    import marshmallow as ma
+
+    from muffin_admin.types import TRAInfo
+
+
+class SAAdminOptions(AdminOptions, SARESTOptions):
+
+    """Keep SAAdmin options."""
+
+    def setup(self, cls):
+        """Auto insert filter by id."""
+        super(SAAdminOptions, self).setup(cls)
+
+        for f in self.filters:
+            if isinstance(f, Filter):
+                f = f.name  # noqa:
+
+            if f == "id":
+                break
+
+        else:
+            self.filters = [SAFilter("id", field=self.table_pk), *self.filters]
+
+
+class SAAdminHandler(AdminHandler, SARESTHandler):
+
+    """Work with SQLAlchemy Core."""
+
+    meta_class: Type[SAAdminOptions] = SAAdminOptions
+    meta: SAAdminOptions
+
+    @classmethod
+    def to_ra_field(cls, field: ma.fields.Field, source: str) -> TRAInfo:
+        """Setup RA fields."""
+        column = getattr(cls.meta.table.c, field.attribute or source, None)
+        if column is not None:
+            if column.foreign_keys:
+                fk = list(column.foreign_keys)[0]
+                ref = fk.column.table.name
+                if ref in cls.meta.references:
+                    return "FKField", {
+                        "reference": ref,
+                        "source": source,
+                        "refSource": cls.meta.references[ref],
+                    }
+
+            if isinstance(column.type, JSON):
+                return "JsonField", {}
+
+        return super(SAAdminHandler, cls).to_ra_field(field, source)
+
+    @classmethod
+    def to_ra_input(cls, field: ma.fields.Field, source: str) -> TRAInfo:
+        """Setup RA inputs."""
+        column = getattr(cls.meta.table.c, field.attribute or source, None)
+        ra_type, props = super(SAAdminHandler, cls).to_ra_input(field, source)
+        if column is not None:
+            if column.foreign_keys and (source in cls.meta.references):
+                ref, _, ref_source = cls.meta.references[source].partition(".")
+                fk = list(column.foreign_keys)[0]
+                return "FKInput", dict(
+                    props,
+                    emptyValue=None if column.nullable else "",
+                    refSource=fk.column.name,
+                    refProp=ref_source or ref,
+                    reference=ref_source and ref or fk.column.table.name,
+                )
+
+            if isinstance(column.type, Enum):
+                return "SelectInput", dict(
+                    props,
+                    choices=[{"id": c.value, "name": c.name} for c in column.type.enum_class],
+                )
+
+            if isinstance(column.type, Text):
+                return "TextInput", dict(props, multiline=True)
+
+            if isinstance(column.type, JSON):
+                return "JsonInput", props
+
+        return ra_type, props
