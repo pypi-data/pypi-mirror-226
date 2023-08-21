34c7817ca6b690a57a3390218d787e18acf2b7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:07:18 +0000

    Add package d8analysis/0.2.10/d8analysis-0.2.10-py3-none-any.whl

---
+++ packages/d8analysis/d8analysis-0.2.10-py3-none-any.whl/d8analysis/visual/seaborn/distribution.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Exploratory Data Analysis Framework                                                 #
+# Version    : 0.1.19                                                                              #
+# Python     : 3.10.12                                                                             #
+# Filename   : /d8analysis/visual/seaborn/distribution.py                                          #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/d8analysis                                         #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Sunday August 13th 2023 11:30:40 pm                                                 #
+# Modified   : Saturday August 19th 2023 07:38:35 pm                                               #
+# ------------------------------------------------------------------------------------------------ #
+# License    : MIT License                                                                         #
+# Copyright  : (c) 2023 John James                                                                 #
+# ================================================================================================ #
+"""Plotizations of Distributions."""
+from typing import Union
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from dependency_injector.wiring import inject, Provide
+
+from d8analysis.container import D8AnalysisContainer
+from d8analysis.visual.seaborn.base import SeabornVisual
+from d8analysis.visual.seaborn.plot import SeabornVisualizer
+
+
+# ------------------------------------------------------------------------------------------------ #
+class Histogram(SeabornVisual):  # pragma: no cover
+    """Wrapper for the lineplot method in SeabornVisualizer."""
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, np.ndarray],
+        x: str = None,
+        y: str = None,
+        hue: str = None,
+        stat: str = "count",
+        element: str = "bars",
+        fill: bool = True,
+        title: str = None,
+        visualizer: SeabornVisualizer = Provide[D8AnalysisContainer.visualizer.seaborn],
+        *args,
+        **kwargs,
+    ) -> None:  # pragma: no cover
+        self._visualizer = visualizer
+        self._data = data
+        self._x = x
+        self._y = y
+        self._hue = hue
+        self._stat = stat
+        self._element = element
+        self._fill = fill
+        self._title = title
+
+        self._args = args
+        self._kwargs = kwargs
+
+    def plot(self, ax: plt.Axes = None) -> None:
+        """Renders the plot"""
+        self._visualizer.histogram(
+            data=self._data,
+            x=self._x,
+            y=self._y,
+            hue=self._hue,
+            stat=self._stat,
+            element=self._element,
+            fill=self._fill,
+            title=self._title,
+            ax=ax,
+            *self._args,
+            **self._kwargs,
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class BoxPlot(SeabornVisual):  # pragma: no cover
+    """Wrapper for the lineplot method in SeabornVisualizer."""
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, np.ndarray],
+        x: str = None,
+        y: str = None,
+        hue: str = None,
+        title: str = None,
+        visualizer: SeabornVisualizer = Provide[D8AnalysisContainer.visualizer.seaborn],
+        *args,
+        **kwargs,
+    ) -> None:  # pragma: no cover
+        self._visualizer = visualizer
+        self._data = data
+        self._x = x
+        self._y = y
+        self._hue = hue
+        self._title = title
+
+        self._args = args
+        self._kwargs = kwargs
+
+    def plot(self, ax: plt.Axes = None) -> None:
+        """Renders the plot"""
+        self._visualizer.boxplot(
+            data=self._data,
+            x=self._x,
+            y=self._y,
+            hue=self._hue,
+            title=self._title,
+            ax=ax,
+            *self._args,
+            **self._kwargs,
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class KDEPlot(SeabornVisual):  # pragma: no cover
+    """Wrapper for the lineplot method in SeabornVisualizer."""
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, np.ndarray],
+        x: str = None,
+        y: str = None,
+        hue: str = None,
+        title: str = None,
+        visualizer: SeabornVisualizer = Provide[D8AnalysisContainer.visualizer.seaborn],
+        *args,
+        **kwargs,
+    ) -> None:  # pragma: no cover
+        self._visualizer = visualizer
+        self._data = data
+        self._x = x
+        self._y = y
+        self._hue = hue
+        self._title = title
+
+        self._args = args
+        self._kwargs = kwargs
+
+    def plot(self, ax: plt.Axes = None) -> None:
+        """Renders the plot"""
+        self._visualizer.kdeplot(
+            data=self._data,
+            x=self._x,
+            y=self._y,
+            hue=self._hue,
+            title=self._title,
+            ax=ax,
+            *self._args,
+            **self._kwargs,
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class ECDFPlot(SeabornVisual):  # pragma: no cover
+    """Wrapper for the lineplot method in SeabornVisualizer."""
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, np.ndarray],
+        x: str = None,
+        y: str = None,
+        hue: str = None,
+        title: str = None,
+        visualizer: SeabornVisualizer = Provide[D8AnalysisContainer.visualizer.seaborn],
+        *args,
+        **kwargs,
+    ) -> None:  # pragma: no cover
+        self._visualizer = visualizer
+        self._data = data
+        self._x = x
+        self._y = y
+        self._hue = hue
+        self._title = title
+
+        self._args = args
+        self._kwargs = kwargs
+
+    def plot(self, ax: plt.Axes = None) -> None:
+        """Renders the plot"""
+        self._visualizer.ecdfplot(
+            data=self._data,
+            x=self._x,
+            y=self._y,
+            hue=self._hue,
+            title=self._title,
+            ax=ax,
+            *self._args,
+            **self._kwargs,
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class ViolinPlot(SeabornVisual):  # pragma: no cover
+    """Wrapper for the lineplot method in SeabornVisualizer."""
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, np.ndarray],
+        x: str = None,
+        y: str = None,
+        hue: str = None,
+        title: str = None,
+        visualizer: SeabornVisualizer = Provide[D8AnalysisContainer.visualizer.seaborn],
+        *args,
+        **kwargs,
+    ) -> None:  # pragma: no cover
+        self._visualizer = visualizer
+        self._data = data
+        self._x = x
+        self._y = y
+        self._hue = hue
+        self._title = title
+
+        self._args = args
+        self._kwargs = kwargs
+
+    def plot(self, ax: plt.Axes = None) -> None:
+        """Renders the plot"""
+        self._visualizer.violinplot(
+            data=self._data,
+            x=self._x,
+            y=self._y,
+            hue=self._hue,
+            title=self._title,
+            ax=ax,
+            *self._args,
+            **self._kwargs,
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class HistPDFPlot(SeabornVisual):  # pragma: no cover
+    """Wrapper for the lineplot method in SeabornVisualizer."""
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, np.ndarray],
+        x: str = None,
+        y: str = None,
+        title: str = None,
+        visualizer: SeabornVisualizer = Provide[D8AnalysisContainer.visualizer.seaborn],
+        *args,
+        **kwargs,
+    ) -> None:  # pragma: no cover
+        self._visualizer = visualizer
+        self._data = data
+        self._x = x
+        self._y = y
+        self._title = title
+
+        self._args = args
+        self._kwargs = kwargs
+
+    def plot(self, ax: plt.Axes = None) -> None:
+        """Renders the plot"""
+        self._visualizer.histpdfplot(
+            data=self._data,
+            x=self._x,
+            y=self._y,
+            title=self._title,
+            ax=ax,
+            *self._args,
+            **self._kwargs,
+        )
+
+
+# ------------------------------------------------------------------------------------------------ #
+class PdfCdfPlot(SeabornVisual):  # pragma: no cover
+    """Wrapper for the lineplot method in SeabornVisualizer."""
+
+    @inject
+    def __init__(
+        self,
+        data: Union[pd.DataFrame, np.ndarray],
+        x: str = None,
+        y: str = None,
+        title: str = None,
+        visualizer: SeabornVisualizer = Provide[D8AnalysisContainer.visualizer.seaborn],
+        *args,
+        **kwargs,
+    ) -> None:  # pragma: no cover
+        self._visualizer = visualizer
+        self._data = data
+        self._x = x
+        self._y = y
+        self._title = title
+
+        self._args = args
+        self._kwargs = kwargs
+
+    def plot(self, ax: plt.Axes = None) -> None:
+        """Renders the plot"""
+        self._visualizer.pdfcdfplot(
+            data=self._data,
+            x=self._x,
+            y=self._y,
+            title=self._title,
+            ax=ax,
+            *self._args,
+            **self._kwargs,
+        )
