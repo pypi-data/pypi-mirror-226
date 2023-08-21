3bbc65974076d7ffaa91c984413cc5134d285e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 15:08:55 +0000

    Add package CheeseAPI/0.0.18/CheeseAPI-0.0.18.tar.gz

---
+++ packages/CheeseAPI/CheeseAPI-0.0.18.tar.gz/CheeseAPI-0.0.18/CheeseAPI/server.py
@@ -0,0 +1,105 @@
+import os, datetime
+
+from CheeseType import IPv4, Port, NonNegativeInt, Bool
+from CheeseLog import logger
+
+class Server:
+    def __init__(self, app):
+        from CheeseAPI.app import App
+
+        self._app: App = app
+        self.HOST: IPv4 = '127.0.0.1'
+        self.PORT: Port = 5214
+        self._WORKERS: NonNegativeInt = 1
+        self.IS_RELOAD: bool = False
+
+        self._IS_DEBUG: bool = False
+        self._IS_REQUEST_LOGGED: bool = True
+
+        self._STATIC_PATH: bool | str = False
+        self._LOG_FILENAME: bool | str = False
+
+    @property
+    def WORKERS(self) -> NonNegativeInt:
+        return self._WORKERS
+
+    @WORKERS.setter
+    def WORKERS(self, value):
+        value = NonNegativeInt(value)
+        if value == 0:
+            self._WORKERS = os.cpu_count() * 2
+        else:
+            self._WORKERS = value
+
+    @property
+    def IS_DEBUG(self) -> bool:
+        return self._IS_DEBUG
+
+    @IS_DEBUG.setter
+    def IS_DEBUG(self, value):
+        self._IS_DEBUG = Bool(value)
+        if isinstance(logger.filter, set):
+            if not self._IS_DEBUG:
+                logger.filter.add('DEBUG')
+            elif self._IS_DEBUG:
+                if 'DEBUG' in logger.filter:
+                    logger.filter.remove('DEBUG')
+
+    @property
+    def IS_REQUEST_LOGGED(self) -> bool:
+        return self._IS_REQUEST_LOGGED
+
+    @IS_REQUEST_LOGGED.setter
+    def IS_REQUEST_LOGGED(self, value):
+        self._IS_REQUEST_LOGGED = Bool(value)
+        if isinstance(logger.filter, set):
+            if not self._IS_REQUEST_LOGGED:
+                logger.filter.add('HTTP')
+                logger.filter.add('WEBSOCKET')
+            elif self._IS_REQUEST_LOGGED:
+                if 'HTTP' in logger.filter:
+                    logger.filter.remove('HTTP')
+                if 'WEBSOCKET' in logger.filter:
+                    logger.filter.remove('WEBSOCKET')
+
+    @property
+    def LOG_FILENAME(self) -> str | bool:
+        return self._LOG_FILENAME
+
+    @LOG_FILENAME.setter
+    def LOG_FILENAME(self, value):
+        if value is not False:
+            if self._app.process.name == 'MainProcess':
+                if value is not False:
+                    if value is True:
+                        self._LOG_FILENAME = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S.log')
+                    else:
+                        self._LOG_FILENAME = datetime.datetime.now().strftime(str(value))
+                    logger.filePath = os.path.join(self._app.workspace.BASE_PATH + self._app.workspace.LOG_PATH + self.LOG_FILENAME)
+                elif value is False:
+                    self._LOG_FILENAME = False
+                    logger.filePath = None
+            else:
+                logPath = os.path.join(self._app.workspace.BASE_PATH + self._app.workspace.LOG_PATH)
+                logFiles = [ os.path.join(logPath, file) for file in os.listdir(logPath) if os.path.isfile(os.path.join(logPath, file)) ]
+                if not logFiles:
+                    self._LOG_FILENAME = False
+                    logger.filePath = None
+                else:
+                    logFile = max(logFiles, key = os.path.getmtime)
+                    self._LOG_FILENAME = logFile.split('/')[-1]
+                    logger.filePath = logFile
+
+    @property
+    def STATIC_PATH(self) -> str | bool:
+        return self._STATIC_PATH
+
+    @STATIC_PATH.setter
+    def STATIC_PATH(self, value):
+        if value is not False:
+            if value is True:
+                self._STATIC_PATH = '/'
+            else:
+                self._STATIC_PATH = str(value)
+        elif value is False:
+            self._STATIC_PATH = False
---
+++ packages/CheeseAPI/CheeseAPI-0.0.18.tar.gz/CheeseAPI-0.0.18/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open('./README.md', 'r', encoding = 'utf-8') as f:
+    longDescription = f.read()
+
+setuptools.setup(
+    name = 'CheeseAPI',
+    version = '0.0.18',
+    author = 'Cheese Unknown',
+    author_email = 'cheese@cheese.ren',
+    description = '一款基于uvicorn的web协程框架',
+    long_description = longDescription,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/CheeseUnknown/CheeseAPI',
+    license = 'MIT',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11'
+    ],
+    keywords = 'api framework backend asyncio',
+    python_requires = '>=3.11',
+    entry_points = {
+        'console_scripts': [
+            'CheeseAPI = CheeseAPI.command:command'
+        ]
+    },
+    install_requires = [
+        'uvicorn[standard]',
+        'CheeseType',
+        'CheeseLog',
+        'xmltodict',
+        'blinker'
+    ],
+    packages = setuptools.find_packages()
+)
