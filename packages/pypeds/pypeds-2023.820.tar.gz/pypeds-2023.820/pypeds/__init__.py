1fa60e099088c7cace51254ee1d80ffe8689aac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:06:24 +0000

    Add package pypeds/2023.820/pypeds-2023.820.tar.gz

---
+++ packages/pypeds/pypeds-2023.820.tar.gz/pypeds-2023.820/pypeds/__init__.py
@@ -0,0 +1,12 @@
+"""A python package to help facilitate the collection and analysis of education-related datasets. """
+
+# change to calendar versioning of sorts, last digit is a version within the mmddv
+__version__ = '2023.0820'
+
+
+from pypeds.ipeds import *
+from pypeds.datasets import *
+from pypeds.views import *
+
+
+
