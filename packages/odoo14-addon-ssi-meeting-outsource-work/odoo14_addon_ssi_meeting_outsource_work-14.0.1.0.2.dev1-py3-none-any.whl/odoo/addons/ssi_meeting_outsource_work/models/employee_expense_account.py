440a4c954f851fa19949a25abf88ae62f8bbfe61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 14:06:38 +0000

    Add package odoo14-addon-ssi-meeting-outsource-work/14.0.1.0.2.dev1/odoo14_addon_ssi_meeting_outsource_work-14.0.1.0.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-meeting-outsource-work/odoo14_addon_ssi_meeting_outsource_work-14.0.1.0.2.dev1-py3-none-any.whl/odoo/addons/ssi_meeting_outsource_work/models/employee_expense_account.py
@@ -0,0 +1,11 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class EmployeeExpenseAccount(models.Model):
+    _name = "calendar.event"
+    _inherit = ["calendar.event", "mixin.outsource_work_object"]
+    _outsource_work_create_page = True
