523b1fad3912b342dcc22e59388063577c0bb869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:09:59 +0000

    Add package paketamacokbosbirpaket/0.1.0/paketamacokbosbirpaket-0.1.0.tar.gz

---
+++ packages/paketamacokbosbirpaket/paketamacokbosbirpaket-0.1.0.tar.gz/paketamacokbosbirpaket-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paketamacokbosbirpaket",
+    version="0.1.0",
+    packages=find_packages(),
+    description="This is a test package.",
+    long_description="Bu bir test paketidir.",
+    long_description_content_type = "text/markdown",
+    license="MIT",
+    author="Bard",
+    upload_requires = ["pypi-token==3.0.0"],
+    author_email="bard@example.com",
+    url="https://github.com/bard/my_package",
+    keywords="python test",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
