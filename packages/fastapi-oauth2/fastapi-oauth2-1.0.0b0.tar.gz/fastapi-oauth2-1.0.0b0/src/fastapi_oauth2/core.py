dfd10ed148e63e3d04972b036742dedb0feac513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:31 +0000

    Add package fastapi-oauth2/1.0.0b0/fastapi-oauth2-1.0.0b0.tar.gz

---
+++ packages/fastapi-oauth2/fastapi-oauth2-1.0.0b0.tar.gz/fastapi-oauth2-1.0.0b0/src/fastapi_oauth2/core.py
@@ -0,0 +1,133 @@
+import json
+import random
+import re
+import string
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from urllib.parse import urljoin
+
+import httpx
+from oauthlib.oauth2 import WebApplicationClient
+from oauthlib.oauth2.rfc6749.errors import CustomOAuth2Error
+from social_core.backends.oauth import BaseOAuth2
+from social_core.strategy import BaseStrategy
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+
+from .claims import Claims
+from .client import OAuth2Client
+
+
+class OAuth2LoginError(HTTPException):
+    """Raised when any login-related error occurs."""
+
+
+class OAuth2Strategy(BaseStrategy):
+    """Dummy strategy for using the `BaseOAuth2.user_data` method."""
+
+    def request_data(self, merge=True) -> Dict[str, Any]:
+        return {}
+
+    def absolute_uri(self, path=None) -> str:
+        return path
+
+    def get_setting(self, name) -> Any:
+        """Mocked setting method."""
+
+    @staticmethod
+    def get_json(url, method='GET', *args, **kwargs) -> httpx.Response:
+        return httpx.request(method, url, *args, **kwargs)
+
+
+class OAuth2Core:
+    """OAuth2 flow handler of a certain provider."""
+
+    client_id: str = None
+    client_secret: str = None
+    scope: Optional[List[str]] = None
+    claims: Optional[Claims] = None
+    provider: str = None
+    redirect_uri: str = None
+    backend: BaseOAuth2 = None
+    _oauth_client: Optional[WebApplicationClient] = None
+
+    authorization_endpoint: str = None
+    token_endpoint: str = None
+
+    def __init__(self, client: OAuth2Client) -> None:
+        self.client_id = client.client_id
+        self.client_secret = client.client_secret
+        self.scope = client.scope
+        self.claims = client.claims
+        self.provider = client.backend.name
+        self.redirect_uri = client.redirect_uri
+        self.backend = client.backend(OAuth2Strategy())
+        self.authorization_endpoint = client.backend.AUTHORIZATION_URL
+        self.token_endpoint = client.backend.ACCESS_TOKEN_URL
+
+    @property
+    def oauth_client(self) -> WebApplicationClient:
+        if self._oauth_client is None:
+            self._oauth_client = WebApplicationClient(self.client_id)
+        return self._oauth_client
+
+    def get_redirect_uri(self, request: Request) -> str:
+        return urljoin(str(request.base_url), "/oauth2/%s/token" % self.provider)
+
+    async def login_redirect(self, request: Request) -> RedirectResponse:
+        redirect_uri = self.get_redirect_uri(request)
+        state = "".join([random.choice(string.ascii_letters) for _ in range(32)])
+        return RedirectResponse(str(self.oauth_client.prepare_request_uri(
+            self.authorization_endpoint, redirect_uri=redirect_uri, state=state, scope=self.scope
+        )), 303)
+
+    async def token_redirect(self, request: Request) -> RedirectResponse:
+        if not request.query_params.get("code"):
+            raise OAuth2LoginError(400, "'code' parameter was not found in callback request")
+        if not request.query_params.get("state"):
+            raise OAuth2LoginError(400, "'state' parameter was not found in callback request")
+
+        url = request.url
+        scheme = "http" if request.auth.http else "https"
+        current_url = re.sub(r"^https?", scheme, str(url))
+        redirect_uri = self.get_redirect_uri(request)
+
+        token_url, headers, content = self.oauth_client.prepare_token_request(
+            self.token_endpoint,
+            redirect_url=redirect_uri,
+            authorization_response=current_url,
+            code=request.query_params.get("code"),
+            state=request.query_params.get("state"),
+        )
+
+        headers.update({
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        })
+        auth = httpx.BasicAuth(self.client_id, self.client_secret)
+        async with httpx.AsyncClient() as session:
+            response = await session.post(token_url, headers=headers, content=content, auth=auth)
+            try:
+                token = self.oauth_client.parse_request_body_response(json.dumps(response.json()))
+                token_data = self.standardize(self.backend.user_data(token.get("access_token")))
+                access_token = request.auth.jwt_create(token_data)
+            except (CustomOAuth2Error, Exception) as e:
+                raise OAuth2LoginError(400, str(e))
+
+        response = RedirectResponse(self.redirect_uri or request.base_url)
+        response.set_cookie(
+            "Authorization",
+            value=f"Bearer {access_token}",
+            max_age=request.auth.expires,
+            expires=request.auth.expires,
+            httponly=request.auth.http,
+        )
+        return response
+
+    def standardize(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        data["provider"] = self.provider
+        data["scope"] = self.scope
+        return data
---
+++ packages/fastapi-oauth2/fastapi-oauth2-1.0.0b0.tar.gz/fastapi-oauth2-1.0.0b0/src/fastapi_oauth2/middleware.py
@@ -0,0 +1,185 @@
+from datetime import datetime
+from datetime import timedelta
+from typing import Any
+from typing import Awaitable
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Union
+
+from fastapi.security.utils import get_authorization_scheme_param
+from jose.jwt import decode as jwt_decode
+from jose.jwt import encode as jwt_encode
+from starlette.authentication import AuthCredentials
+from starlette.authentication import AuthenticationBackend
+from starlette.authentication import BaseUser
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.requests import Request
+from starlette.types import ASGIApp
+from starlette.types import Receive
+from starlette.types import Scope
+from starlette.types import Send
+
+from .claims import Claims
+from .client import OAuth2Client
+from .config import OAuth2Config
+from .core import OAuth2Core
+
+
+class Auth(AuthCredentials):
+    """Extended auth credentials schema based on Starlette AuthCredentials."""
+
+    http: bool
+    secret: str
+    expires: int
+    algorithm: str
+    scopes: List[str]
+    clients: Dict[str, OAuth2Core] = {}
+
+    _provider: OAuth2Core = None
+
+    @property
+    def provider(self) -> Union[OAuth2Core, None]:
+        return self._provider
+
+    @provider.setter
+    def provider(self, identifier) -> None:
+        self._provider = self.clients.get(identifier)
+
+    @classmethod
+    def set_http(cls, http: bool) -> None:
+        cls.http = http
+
+    @classmethod
+    def set_secret(cls, secret: str) -> None:
+        cls.secret = secret
+
+    @classmethod
+    def set_expires(cls, expires: int) -> None:
+        cls.expires = expires
+
+    @classmethod
+    def set_algorithm(cls, algorithm: str) -> None:
+        cls.algorithm = algorithm
+
+    @classmethod
+    def register_client(cls, client: OAuth2Client) -> None:
+        cls.clients[client.backend.name] = OAuth2Core(client)
+
+    @classmethod
+    def jwt_encode(cls, data: dict) -> str:
+        return jwt_encode(data, cls.secret, algorithm=cls.algorithm)
+
+    @classmethod
+    def jwt_decode(cls, token: str) -> dict:
+        return jwt_decode(token, cls.secret, algorithms=[cls.algorithm])
+
+    @classmethod
+    def jwt_create(cls, token_data: dict) -> str:
+        expire = datetime.utcnow() + timedelta(seconds=cls.expires)
+        return cls.jwt_encode({**token_data, "exp": expire})
+
+
+class User(BaseUser, dict):
+    """Extended user schema based on Starlette BaseUser."""
+
+    @property
+    def is_authenticated(self) -> bool:
+        return bool(self)
+
+    @property
+    def display_name(self) -> str:
+        return self.__getprop__("display_name")
+
+    @property
+    def identity(self) -> str:
+        return self.__getprop__("identity")
+
+    @property
+    def picture(self) -> str:
+        return self.__getprop__("picture")
+
+    @property
+    def email(self) -> str:
+        return self.__getprop__("email")
+
+    def use_claims(self, claims: Claims) -> "User":
+        for attr, item in claims.items():
+            self[attr] = self.__getprop__(item)
+        return self
+
+    def __getprop__(self, item, default="") -> Any:
+        if callable(item):
+            return item(self)
+        return self.get(item, default)
+
+    __getattr__ = __getprop__
+
+
+class OAuth2Backend(AuthenticationBackend):
+    """Authentication backend for AuthenticationMiddleware."""
+
+    def __init__(
+            self,
+            config: OAuth2Config,
+            callback: Callable[[Auth, User], Union[Awaitable[None], None]] = None,
+    ) -> None:
+        Auth.set_http(config.allow_http)
+        Auth.set_secret(config.jwt_secret)
+        Auth.set_expires(config.jwt_expires)
+        Auth.set_algorithm(config.jwt_algorithm)
+        for client in config.clients:
+            Auth.register_client(client)
+        self.callback = callback
+
+    async def authenticate(self, request: Request) -> Optional[Tuple[Auth, User]]:
+        authorization = request.headers.get(
+            "Authorization",
+            request.cookies.get("Authorization"),
+        )
+        scheme, param = get_authorization_scheme_param(authorization)
+
+        if not scheme or not param:
+            return Auth(), User()
+
+        user = User(Auth.jwt_decode(param))
+        auth = Auth(user.pop("scope", []))
+        auth.provider = user.get("provider")
+        claims = auth.provider.claims if auth.provider else {}
+
+        # Call the callback function on authentication
+        if callable(self.callback):
+            coroutine = self.callback(auth, user.use_claims(claims))
+            if issubclass(type(coroutine), Awaitable):
+                await coroutine
+        return auth, user.use_claims(claims)
+
+
+class OAuth2Middleware:
+    """Wrapper for the Starlette AuthenticationMiddleware."""
+
+    auth_middleware: AuthenticationMiddleware = None
+
+    def __init__(
+            self,
+            app: ASGIApp,
+            config: Union[OAuth2Config, dict],
+            callback: Callable[[Auth, User], Union[Awaitable[None], None]] = None,
+            **kwargs,  # AuthenticationMiddleware kwargs
+    ) -> None:
+        """Initiates the middleware with the given configuration.
+
+        :param app: FastAPI application instance
+        :param config: middleware configuration
+        :param callback: callback function to be called after authentication
+        """
+        if isinstance(config, dict):
+            config = OAuth2Config(**config)
+        elif not isinstance(config, OAuth2Config):
+            raise TypeError("config is not a valid type")
+        self.auth_middleware = AuthenticationMiddleware(app, backend=OAuth2Backend(config, callback), **kwargs)
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        await self.auth_middleware(scope, receive, send)
