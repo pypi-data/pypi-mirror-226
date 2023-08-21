f16caa4e64388e24ac19833a3d614faa4ec94b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-21 03:10:54 +0000

    Add package fluxwallet/0.2.2/fluxwallet-0.2.2-py3-none-any.whl

---
+++ packages/fluxwallet/fluxwallet-0.2.2-py3-none-any.whl/fluxwallet/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+#    fluxwallet - Python Cryptocurrency Library
+#    © 2018 - 2019 March - 1200 Web Development <http://1200wd.com/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# import fluxwallet.blocks
+import fluxwallet.encoding
+import fluxwallet.keys
+import fluxwallet.mnemonic
+
+# import fluxwallet.tools
+import fluxwallet.transactions
+import fluxwallet.values
+import fluxwallet.wallet
+
+__all__ = [
+    "keys",
+    "transactions",
+    "wallets",
+    "encoding",
+    "mnemonic",
+    # "tools",
+    "blocks",
+    "values",
+]
