3562326c14e4d22d7cc505fef43a3122ab1dc0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:05 +0000

    Add package oceanlab-api-client/0.0.6/oceanlab_api_client-0.0.6.tar.gz

---
+++ packages/oceanlab-api-client/oceanlab_api_client-0.0.6.tar.gz/oceanlab_api_client-0.0.6/oceanlab_api_client/models/raw_data_count_response_200_response.py
@@ -0,0 +1,82 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define, field
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.raw_data_count_response_200_response_result_item import RawDataCountResponse200ResponseResultItem
+
+
+T = TypeVar("T", bound="RawDataCountResponse200Response")
+
+
+@define
+class RawDataCountResponse200Response:
+    """
+    Attributes:
+        varchar (Union[Unset, str]):
+        result (Union[Unset, List['RawDataCountResponse200ResponseResultItem']]):
+    """
+
+    varchar: Union[Unset, str] = UNSET
+    result: Union[Unset, List["RawDataCountResponse200ResponseResultItem"]] = UNSET
+    additional_properties: Dict[str, Any] = field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        varchar = self.varchar
+        result: Union[Unset, List[Dict[str, Any]]] = UNSET
+        if not isinstance(self.result, Unset):
+            result = []
+            for result_item_data in self.result:
+                result_item = result_item_data.to_dict()
+
+                result.append(result_item)
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if varchar is not UNSET:
+            field_dict[".VARCHAR"] = varchar
+        if result is not UNSET:
+            field_dict[".result"] = result
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        from ..models.raw_data_count_response_200_response_result_item import RawDataCountResponse200ResponseResultItem
+
+        d = src_dict.copy()
+        varchar = d.pop(".VARCHAR", UNSET)
+
+        result = []
+        _result = d.pop(".result", UNSET)
+        for result_item_data in _result or []:
+            result_item = RawDataCountResponse200ResponseResultItem.from_dict(result_item_data)
+
+            result.append(result_item)
+
+        raw_data_count_response_200_response = cls(
+            varchar=varchar,
+            result=result,
+        )
+
+        raw_data_count_response_200_response.additional_properties = d
+        return raw_data_count_response_200_response
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
