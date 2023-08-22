bf8156ee5e8b19592b0160a7276e92b51955ac6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:12:51 +0000

    Add package codeblocks-gpt/0.1.1/codeblocks-gpt-0.1.1.tar.gz

---
+++ packages/codeblocks-gpt/codeblocks-gpt-0.1.1.tar.gz/codeblocks-gpt-0.1.1/codeblocks/parser/java.py
@@ -0,0 +1,106 @@
+from typing import Optional, List
+
+from tree_sitter import Node
+
+from codeblocks.codeblocks import CodeBlockType
+from codeblocks.parser.language import find_block_node
+from codeblocks.parser.parser import CodeParser, _find_type
+
+class_node_types = [
+    "annotation_type_declaration",
+    "class_declaration",
+    "enum_declaration",
+    "interface_declaration",
+    "record_declaration"
+]
+
+function_node_types = [
+    "method_declaration",
+    "constructor_declaration"
+]
+
+statement_node_types = [
+    "static_initializer",
+    "instance_initializer",
+    "if_statement",
+    "for_statement",
+    "enhanced_for_statement",
+    "while_statement",
+    "do_statement",
+    "synchronized_statement",
+    "try_statement"
+    "switch_expression"
+]
+
+block_delimiters = [
+    "{",
+    "}"
+]
+
+
+class JavaParser(CodeParser):
+
+    def __init__(self):
+        super().__init__("java")
+
+    def get_block_type(self, node: Node) -> Optional[CodeBlockType]:
+        if node.type == "program":
+            return CodeBlockType.MODULE
+        elif node.type in function_node_types:
+            return CodeBlockType.FUNCTION
+        elif node.type in class_node_types:
+            return CodeBlockType.CLASS
+        elif node.type in statement_node_types:
+            return CodeBlockType.STATEMENT
+        elif node.type in block_delimiters:
+            return CodeBlockType.BLOCK_DELIMITER
+        elif node.type == "import_declaration":
+            return CodeBlockType.IMPORT
+        elif "comment" in node.type:
+            if "..." in node.text.decode("utf8"):
+                return CodeBlockType.COMMENTED_OUT_CODE
+            else:
+                return CodeBlockType.COMMENT
+        else:
+            return CodeBlockType.CODE
+
+    def get_block_delimiter_types(self):
+        return block_delimiters
+
+    def get_child_nodes(self, node: Node) -> List[Node]:
+        if node.type == "program":
+            for i, child in enumerate(node.children):
+                if child.type == "package_declaration":
+                    if len(node.children) > i+1:
+                        return node.children[i+1:]
+            return node.children
+
+        nodes = []
+        if node.type in ["local_variable_declaration", "field_declaration", "constant_declaration"] and node.children \
+                and any(child.children for child in node.children):
+            i, variable_declarator = _find_type(node, "variable_declarator")
+            if variable_declarator and variable_declarator.children:
+                delimiter, _ = _find_type(variable_declarator, "=")
+                if delimiter:
+                    return variable_declarator.children[delimiter+1:] + node.children[i+1:]
+
+        if node.type == "variable_declarator":
+            delimiter, _ = _find_type(node, "=")
+            if delimiter:
+                return node.children[delimiter + 1:]
+
+        if node.type == "switch_rule":
+            delimiter, _ = _find_type(node, "->")
+            if delimiter:
+                return node.children[delimiter + 1:]
+
+        block_node = find_block_node(node)
+        if block_node:
+            nodes.extend(block_node.children)
+
+            next_sibling = block_node.next_sibling
+            while next_sibling:
+                nodes.append(next_sibling)
+                next_sibling = next_sibling.next_sibling
+
+        return nodes
---
+++ packages/codeblocks-gpt/codeblocks-gpt-0.1.1.tar.gz/codeblocks-gpt-0.1.1/codeblocks/parser/python.py
@@ -0,0 +1,86 @@
+from typing import Optional, List
+
+from tree_sitter import Node
+
+from codeblocks import CodeBlockType, CodeBlock
+from codeblocks.parser.parser import CodeParser, COMMENTED_OUT_CODE_KEYWORDS, _find_type
+
+compound_node_types = [
+    "function_definition", "class_definition", "if_statement",
+    "for_statement", "while_statement", "try_statement", "with_statement",
+    "expression_statement", "dictionary"
+]
+
+child_block_types = ["ERROR", "block"]
+
+block_delimiters = [
+    ":"
+]
+
+
+def _find_delimiter_index(node: Node):
+    for i, child in enumerate(node.children):
+        if child.type == ":":
+            return i
+    return -1
+
+
+class PythonParser(CodeParser):
+
+    def __init__(self):
+        super().__init__("python")
+
+    def get_child_node_block_types(self):
+        return child_block_types
+
+    def get_compound_node_types(self):
+        return compound_node_types
+
+    def get_block_delimiter_types(self):
+        return block_delimiters
+
+    def get_block_type(self, node: Node) -> Optional[CodeBlockType]:
+        if node.type == "decorated_definition" and len(node.children) > 1:
+            node = node.children[-1]
+        if node.type == "module":
+            return CodeBlockType.MODULE
+        elif node.type == "function_definition":
+            return CodeBlockType.FUNCTION
+        elif node.type == "class_definition":
+            return CodeBlockType.CLASS
+        elif node.type in ["import_statement", "import_from_statement", "future_import_statement"]:
+            return CodeBlockType.IMPORT
+        elif node.type in block_delimiters:
+            return CodeBlockType.BLOCK_DELIMITER
+        elif "comment" in node.type:
+            comment = node.text.decode("utf8").strip()
+            if comment.startswith("# ...") or any(keyword in comment.lower() for keyword in COMMENTED_OUT_CODE_KEYWORDS):
+                return CodeBlockType.COMMENTED_OUT_CODE
+            else:
+                return CodeBlockType.COMMENT
+        else:
+            return CodeBlockType.CODE
+
+    def get_child_nodes(self, node: Node) -> List[Node]:
+        if node.type == "module":
+            return node.children
+
+        if node.type in ["decorated_definition", "expression_statement"] and node.children \
+                and any(child.children for child in node.children):
+            node = node.children[-1]
+
+        if node.type == "assignment":
+            delimiter, _ = _find_type(node, "=")
+            if delimiter:
+                return node.children[delimiter + 1:]
+
+        if node.type == "dictionary":
+            delimiter, _ = _find_type(node, "{")
+            if delimiter is not None:
+                return node.children[delimiter:]
+
+        delimiter_index = _find_delimiter_index(node)
+        if delimiter_index != -1:
+            return node.children[delimiter_index:]
+        else:
+            return []
