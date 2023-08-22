d2e2f2cb5b3e25327fa24078feba6ef7867e84cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:15 +0000

    Add package multicloud-data-catalogue/0.1.0/multicloud_data_catalogue-0.1.0.tar.gz

---
+++ packages/multicloud-data-catalogue/multicloud_data_catalogue-0.1.0.tar.gz/multicloud_data_catalogue-0.1.0/multicloud_data_catalogue/providers/aws.py
@@ -0,0 +1,7 @@
+import typer
+
+from multicloud_data_catalogue.providers.aws_storage import dynamo, rds
+
+aws_app = typer.Typer()
+aws_app.add_typer(dynamo.app, name="dynamodb")
+aws_app.add_typer(rds.app, name="rds")
