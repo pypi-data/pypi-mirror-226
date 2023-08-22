867fb3895bd37d7f46a091771357f23a90f016dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:10:57 +0000

    Add package local-candles/0.2.1/local_candles-0.2.1-py3-none-any.whl

---
+++ packages/local-candles/local_candles-0.2.1-py3-none-any.whl/local_candles/__init__.py
@@ -0,0 +1,39 @@
+import datetime
+from typing import List, Optional, Union
+from pathlib import Path
+
+from .data import Data
+from .models import Time
+from .sources import Source, SOURCES
+
+
+__all__ = ("load_candles",)
+
+
+def load_candles(
+    source: Union[Source, str],
+    start_ts: Union[Time, str, datetime.datetime],
+    stop_ts: Union[Time, str, datetime.datetime],
+    columns: Optional[List[str]] = None,
+    cache_root: Optional[Union[Path, str]] = None,
+    **source_kwargs
+):
+    if not isinstance(start_ts, Time):
+        start_ts = Time.from_string(start_ts)
+    if not isinstance(stop_ts, Time):
+        stop_ts = Time.from_string(stop_ts)
+
+    if not isinstance(source, Source):
+        source = SOURCES[source](**source_kwargs)
+
+    if not cache_root:
+        cache_root = Path(".cache")
+    if not isinstance(cache_root, Path):
+        cache_root = Path(cache_root)
+
+    return Data(cache_root=cache_root).load_df(
+        source=source,
+        start_ts=start_ts,
+        stop_ts=stop_ts,
+        columns=columns or ["open", "high", "low", "close", "volume"],
+    )
