843a6b9dd1f9f0a72253f6c2eda36336bf1645aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:12:46 +0000

    Add package syndesi/0.1.4/syndesi-0.1.4.tar.gz

---
+++ packages/syndesi/syndesi-0.1.4.tar.gz/syndesi-0.1.4/syndesi/adapters/visa.py
@@ -0,0 +1,52 @@
+
+from pyvisa import ResourceManager
+
+from .iadapter import IAdapter
+
+class VISA(IAdapter):
+    def __init__(self, descriptor : str):
+        """
+        USB VISA stack adapter
+
+        Parameters
+        ----------
+        descriptor : str
+            IP description
+        """
+        self._rm = ResourceManager()
+        self._inst = self._rm.open_resource(descriptor)
+        self._inst.write_termination = ''
+        self._inst.read_termination = ''
+
+    def list_devices(self=None):
+        """
+        Returns a list of VISA devices
+        """
+        rm = ResourceManager()
+
+        return rm.list_resources()
+
+    def flushRead(self):
+        pass
+
+    def open(self):
+        self._inst.open()
+
+    def close(self):
+        self._inst.close()
+            
+    def write(self, data : bytearray):
+        self._inst.write(data.decode('ASCII'))
+    
+    def read(self) -> bytearray:
+        return self._inst.read_raw()
+
+    def query(self, data : bytearray, timeout=None, continuation_timeout=None) -> bytearray:
+        """
+        Shortcut function that combines
+        - flush_read
+        - write
+        - read
+        """
+        # TODO : implement timeouts
+        return self._inst.query(data.encode('ASCII'))
---
+++ packages/syndesi/syndesi-0.1.4.tar.gz/syndesi-0.1.4/syndesi/protocols/__init__.py
@@ -0,0 +1,5 @@
+from syndesi.protocols.iprotocol import IProtocol
+from .delimited import Commands
+from .raw import Raw, RawStream
+from .scpi import SCPI
+from .sdp import SDP
