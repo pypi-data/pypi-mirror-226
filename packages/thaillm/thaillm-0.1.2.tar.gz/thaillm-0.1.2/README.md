4e1e41c7eccff36c8abcf3eae0d5c25628da25e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:09:07 +0000

    Add package thaillm/0.1.2/thaillm-0.1.2.tar.gz

---
+++ packages/thaillm/thaillm-0.1.2.tar.gz/thaillm-0.1.2/README.md
@@ -0,0 +1,11 @@
+Thai large language model(LLM) สำหรับ Tranformer
+# include
+* Tokenization: ตัดคำภาษาไทย
+
+# Colab
+https://colab.research.google.com/drive/1ll5VU4P1E-08oudsJBzynjQY6nQpbXDz?usp=sharing
+================================================================================
+# Update
+## 0.1.1
+* เพิ่ม library Tokenization
+
---
+++ packages/thaillm/thaillm-0.1.2.tar.gz/thaillm-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read() 
+
+setup(name='thaillm',
+    version='0.1.2',
+    description='Thai large language model(LLM)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    author='bablueza',
+    author_email='bablueza@gmail.com',
+    license='MIT',
+    packages = find_packages(where="src"),
+    package_dir = {"": "src"},
+    include_package_data=True,
+    install_requires= ['transformers','sentencepiece'],
+    python_requires='>=3.8')
