8bcc988e9bc7a9c6cbbd17fd59c9ff459c96d74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:45 +0000

    Add package farfuntool/202308221345/farfuntool-202308221345.tar.gz

---
+++ packages/farfuntool/farfuntool-202308221345.tar.gz/farfuntool-202308221345/README.md
@@ -0,0 +1,14 @@
+# install
+
+```bash
+
+pip install farfuntool
+
+pip install git+https://github.com/notechats/funtool.git
+```
+
+# tools
+
+| tool                        | desc               |
+| --------------------------- | ------------------ |
+| [secret](./notetool/secret) | 账号密码的加密解密 |
---
+++ packages/farfuntool/farfuntool-202308221345.tar.gz/farfuntool-202308221345/setup.py
@@ -0,0 +1,16 @@
+import sys
+import time
+from setuptools import find_packages, setup
+
+install_requires = ["cryptography", "funbuild"]
+
+setup(
+    name="farfuntool",
+    version=time.strftime("%Y%m%d%H%M", time.localtime()),
+    description="funtool",
+    author="niuliangtao",
+    author_email="1007530194@qq.com",
+    url="https://github.com/1007530194",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
