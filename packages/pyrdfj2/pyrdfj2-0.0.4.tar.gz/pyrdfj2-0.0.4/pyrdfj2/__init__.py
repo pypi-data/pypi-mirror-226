b6f0d3d934c5122ebdb5c160c4ee27c1bd651d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:11:21 +0000

    Add package pyrdfj2/0.0.4/pyrdfj2-0.0.4.tar.gz

---
+++ packages/pyrdfj2/pyrdfj2-0.0.4.tar.gz/pyrdfj2-0.0.4/pyrdfj2/__init__.py
@@ -0,0 +1,5 @@
+from pyrdfj2.j2_functions import Filters, Functions
+from pyrdfj2.pyrdfj2 import RDFSyntaxBuilder
+from pyrdfj2.syntax_builder import J2RDFSyntaxBuilder
+
+__all__ = ["Filters", "Functions", "RDFSyntaxBuilder", "J2RDFSyntaxBuilder"]
