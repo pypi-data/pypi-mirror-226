a60dc764f5393f03befe541093197cfff30a5b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:09:16 +0000

    Add package aicodebot/0.22.2/aicodebot-0.22.2.tar.gz

---
+++ packages/aicodebot/aicodebot-0.22.2.tar.gz/aicodebot-0.22.2/aicodebot/commands/__init__.py
@@ -0,0 +1,9 @@
+from .alignment import alignment
+from .commit import commit
+from .configure import configure
+from .debug import debug
+from .learn import learn
+from .review import review
+from .sidekick import sidekick, sidekick_agent
+
+__all__ = ["alignment", "commit", "configure", "debug", "learn", "review", "sidekick", "sidekick_agent"]
