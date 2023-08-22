b8c2816dc5dda2b20535ea710136e223b883b7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:08:45 +0000

    Add package jra-tools/0.0.7/jra_tools-0.0.7-py3-none-any.whl

---
+++ packages/jra-tools/jra_tools-0.0.7-py3-none-any.whl/jra_tools/machine_learning/input_creator.py
@@ -0,0 +1,15 @@
+﻿"""推論に必要なデータセットを作成する"""
+import numpy as np
+from jra_tools import KaisaiData
+from . import training_tool
+from . import category_data
+
+
+class InputCreator:
+    """KaisaiDataを元にndarrayを作成する"""
+
+    def __init__(self, kaisais: list[KaisaiData]):
+        score_data = training_tool.createScoreDataMatrix(kaisais)
+        sds = training_tool.standardize(score_data)
+        cd = category_data.getCategoryData(kaisais)
+        self.x_data = np.concatenate((sds, cd), axis=2)
