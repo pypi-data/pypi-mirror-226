109efbf253878e5137ea258ee4ae7939f539e7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 05:08:48 +0000

    Add package rabbitmq-email-notifier/0.0.2/rabbitmq-email-notifier-0.0.2.tar.gz

---
+++ packages/rabbitmq-email-notifier/rabbitmq-email-notifier-0.0.2.tar.gz/rabbitmq-email-notifier-0.0.2/rabbitmqresources/worker.py
@@ -0,0 +1,50 @@
+import os
+import logging
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+        OTLPSpanExporter)
+from opentelemetry.sdk.resources import SERVICE_NAME
+
+# tracing setup
+JAEGER_NAME = os.getenv('JAEGER_NAME')
+JAEGER_SERVICE_NAME = os.getenv('JAEGER_SERVICE_NAME')
+
+resource = Resource(attributes={SERVICE_NAME: JAEGER_SERVICE_NAME})
+provider = TracerProvider(resource=resource)
+otlp_exporter = OTLPSpanExporter(endpoint=JAEGER_NAME, insecure=True)
+span_processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(span_processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+logger = logging.getLogger('rabbitmq-email-notifier')
+
+"""
+This is a template of a basic worker class for etds.
+
+Since: 2023-05-23
+Author: cgoines
+"""
+
+
+class Worker():
+    version = None
+
+    def __init__(self):
+        self.version = os.getenv("APP_VERSION", "0.0.1")
+
+    def get_version(self):
+        return self.version
+
+    # this is a stub call
+    def call_api(self):
+        with tracer.start_as_current_span("notifier_endpoint",
+                                          attributes={"endpoint":
+                                                      "/notifier"}):
+            span = trace.get_current_span()
+            span.add_event("log", {"notifier worker"})
+            logger.debug("notifier worker")
+            return True
