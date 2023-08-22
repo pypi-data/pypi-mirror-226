4cd4ab635285ced6b714aaefbc18cfd55eaaf125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 16:10:37 +0000

    Add package swmonkey/0.5/swmonkey-0.5.tar.gz

---
+++ packages/swmonkey/swmonkey-0.5.tar.gz/swmonkey-0.5/swmonkey/monitor.py
@@ -0,0 +1,33 @@
+import psutil
+from log import logger
+import time
+
+INTERVAL = 2
+
+
+def monitor_system():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    mem_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    net_info = psutil.net_io_counters()
+    logger.info(
+        f'CPU:  {cpu_usage}%; Memory: {mem_usage}%; Disk: {disk_usage}%; Net: {net_info.bytes_sent} bytes sent, {net_info.bytes_recv} bytes received')
+
+    # 阈值告警
+    if cpu_usage > 90:
+        logger.warning(f'CPU usage is over 90%')
+    if mem_usage > 90:
+        logger.warning(f'Memory usage is over 90%')
+    if disk_usage > 90:
+        logger.warning(f'Disk usage is over 90%')
+
+
+def monkey_monitor():
+    while True:
+        monitor_system()
+        time.sleep(INTERVAL)
+
+
+if __name__ == '__main__':
+    monkey_monitor()
+    # Output: 'CPU:  0.0%; Memory: 0.0%; Disk: 0.0%; Net: 0 bytes sent, 0 bytes received
