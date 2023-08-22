e731212011407769efb75509d06527c4cd93d172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:55 +0000

    Add package ert/6.0.0rc2/ert-6.0.0rc2-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-6.0.0rc2-cp310-cp310-macosx_10_15_x86_64.whl/ert/logging/__init__.py
@@ -0,0 +1,24 @@
+import os
+import pathlib
+from datetime import datetime
+from logging import FileHandler
+from typing import Any
+
+LOGGING_CONFIG = pathlib.Path(__file__).parent.resolve() / "logger.conf"
+STORAGE_LOG_CONFIG = pathlib.Path(__file__).parent.resolve() / "storage_log.conf"
+
+
+class TimestampedFileHandler(FileHandler):
+    def __init__(self, filename: str, *args: Any, **kwargs: Any) -> None:
+        filename, extension = os.path.splitext(filename)
+        filename = (
+            f"{filename}-"
+            f"{datetime.now().strftime(datetime.now().strftime('%Y-%m-%dT%H%M'))}"
+            f"{extension}"
+        )
+
+        if kwargs.pop("use_log_dir_from_env", False) and "ERT_LOG_DIR" in os.environ:
+            log_dir = os.environ["ERT_LOG_DIR"]
+            filename = log_dir + "/" + filename
+
+        super().__init__(filename, *args, **kwargs)
