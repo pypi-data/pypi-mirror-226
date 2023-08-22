6f38e8b08030090b83a00eb845e8f5938c7ec8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:35 +0000

    Add package dran/0.1.0.dev0/dran-0.1.0.dev0-py3-none-any.whl

---
+++ packages/dran/dran-0.1.0.dev0-py3-none-any.whl/inits.py
@@ -0,0 +1,25 @@
+logfile="logfile.txt"
+__app__name = "dran"
+# for linux machines
+__version__ = "0.1.0.dev0"
+
+
+# series of return and error codes and assign integer numbers to them using range()
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    DB_READ_ERROR,
+    DB_WRITE_ERROR,
+    JSON_ERROR,
+    ID_ERROR,
+) = range(7)
+
+# ERROR is a dictionary that maps error codes to human-readable error messages     
+ERRORS = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+    DB_READ_ERROR: "database read error",
+    DB_WRITE_ERROR: "database write error",
+    ID_ERROR: "to-do id error",
+}
