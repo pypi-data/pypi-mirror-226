bc38438cc865b777c24c692fdd9550fb3c4c7338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 05:08:18 +0000

    Add package vectorama/0.1.3/vectorama-0.1.3.tar.gz

---
+++ packages/vectorama/vectorama-0.1.3.tar.gz/vectorama-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vectorama', 'vectorama.api', 'vectorama.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=2.2.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'vectorama',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': '# vectorama-python\n\n## Tests\n\n**Tests will clear out the current database as part of the test flows. Make sure you are only running a test server locally and you are not storing real data.**\n\nTests are intended to be run with a live vectorama server running on `localhost:50051`. We provide a docker-compose file for this purpose. To run the tests, first start the server:\n\n```bash\ndocker-compose up\n```\n\nThen, in a separate terminal, run the tests:\n\n```bash\npoetry run pytest\n```\n',
+    'author': 'Pierce Freeman',
+    'author_email': 'pierce@freeman.vc',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
