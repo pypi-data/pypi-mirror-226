6e64922f34b1bfb178b00f6f2466e7631370a6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:08 +0000

    Add package lineup-optimizer/0.0.2/lineup_optimizer-0.0.2-py3-none-any.whl

---
+++ packages/lineup-optimizer/lineup_optimizer-0.0.2-py3-none-any.whl/lineup_optimizer/__init__.py
@@ -0,0 +1,11 @@
+from .site_configs import SITES
+from .parsers import parse_csv
+from .lineup_builder import LineupBuilder
+
+
+def optimize_lineup(site, filename):
+    parser = parse_csv(filename)
+    lineup_builder = LineupBuilder(SITES[site].LINEUP_SLOTS,
+                                   SITES[site].NAME,
+                                   draftables=parser.players)
+    return lineup_builder.build()
