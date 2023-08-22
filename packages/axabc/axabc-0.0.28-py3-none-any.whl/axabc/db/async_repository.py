90cea57bf64fa6172f26327d9d8d8f063544f92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:17 +0000

    Add package axabc/0.0.28/axabc-0.0.28-py3-none-any.whl

---
+++ packages/axabc/axabc-0.0.28-py3-none-any.whl/axabc/db/async_repository.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from typing import Any, List, TypeVar, Union
+
+
+TModel = TypeVar('TModel')
+
+
+class AbstractAsyncRepository(ABC):
+    Model: Any
+    Schema: Any
+    OSchema: Any
+
+    def __init__(self, session: Any) -> None:
+        self.session = session
+
+    @abstractmethod
+    async def add(self, obj: Model) -> Union[Model, None]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get(self, *identifiers: List[Any]) -> Union[Model, None]:
+        raise NotImplementedError
+
+    async def update(self, obj: Model) -> Union[Model, None]:
+        raise NotImplementedError
+
+    async def delete(self, obj: Model) -> None:
+        raise NotImplementedError
+
+    async def all(self, *identifier: List[Any]) -> Union[List[OModel], None]:
+        raise NotImplementedError
