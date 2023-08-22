00d61403833e6d791a3f581e7ad1fe56446f1048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:06:57 +0000

    Add package openssm-dev/0.1.6.17/openssm_dev-0.1.6.17.tar.gz

---
+++ packages/openssm-dev/openssm_dev-0.1.6.17.tar.gz/openssm_dev-0.1.6.17/openssm/integrations/huggingface/ssm.py
@@ -0,0 +1,16 @@
+"""Falcon SSMs."""
+
+
+from openssm.core.ssm.base_ssm import BaseSSM
+from openssm.core.adapter.abstract_adapter import AbstractAdapter
+from openssm.core.backend.abstract_backend import AbstractBackend
+from openssm.integrations.huggingface.slm import Falcon7bSLM
+
+
+class Falcon7bSSM(BaseSSM):
+    """Falcon 7-billion-parameter SSM."""
+
+    def __init__(self,
+                 adapter: AbstractAdapter = None,
+                 backends: list[AbstractBackend] = None):
+        super().__init__(Falcon7bSLM(), adapter, backends)
