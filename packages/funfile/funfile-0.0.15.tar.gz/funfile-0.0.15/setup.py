6977c3abcb1a13af6ae868923e1a6bef329db592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:10:10 +0000

    Add package funfile/0.0.15/funfile-0.0.15.tar.gz

---
+++ packages/funfile/funfile-0.0.15.tar.gz/funfile-0.0.15/setup.py
@@ -0,0 +1,19 @@
+from os import path
+
+from notebuild.tool import read_version
+from setuptools import find_packages, setup
+
+
+install_requires = ["tqdm"]
+
+
+setup(
+    name="funfile",
+    version=read_version(path.join(path.abspath(path.dirname(__file__)), "script/__version__.md")),
+    description="funfile",
+    author="niuliangtao",
+    author_email="1007530194@qq.com",
+    url="https://github.com/1007530194",
+    packages=find_packages(),
+    install_requires=install_requires,
+)
