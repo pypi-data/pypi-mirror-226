12211194a7959126c09422e12ba9710aa0d3ddf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:06:57 +0000

    Add package huma-utils/0.8.3/huma_utils-0.8.3-py3-none-any.whl

---
+++ packages/huma-utils/huma_utils-0.8.3-py3-none-any.whl/huma_utils/constants.py
@@ -0,0 +1,3 @@
+HUMA_FINANCE_DOMAIN_NAME = "huma.finance"
+SERVER_ALLOW_ORIGIN_REGEX = r"^https://(deploy-preview-\d+--dev-app-huma\.netlify\.app"
+r"|dev\.app\.huma\.finance|app\.huma\.finance)$|^http://localhost:\d+$"
