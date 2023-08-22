1620add02c954dad04c0b8e6f9a7f45b7f82aedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:08:14 +0000

    Add package tidy3d/2.4.0rc2/tidy3d-2.4.0rc2.tar.gz

---
+++ packages/tidy3d/tidy3d-2.4.0rc2.tar.gz/tidy3d-2.4.0rc2/tests/test_package/test_log.py
@@ -0,0 +1,150 @@
+"""Test the logging."""
+
+import pytest
+
+import pydantic.v1 as pd
+import numpy as np
+import tidy3d as td
+from tidy3d.exceptions import Tidy3dError
+from tidy3d.log import DEFAULT_LEVEL, _get_level_int, set_logging_level
+
+
+def test_log():
+    td.log.debug("debug test")
+    td.log.info("info test")
+    td.log.warning("warning test")
+    td.log.error("error test")
+    td.log.critical("critical test")
+    td.log.log(0, "zero test")
+
+
+def test_log_config(tmp_path):
+    td.config.logging_level = "DEBUG"
+    td.set_logging_file(str(tmp_path / "test.log"))
+    assert len(td.log.handlers) == 2
+    assert td.log.handlers["console"].level == _get_level_int("DEBUG")
+    assert td.log.handlers["file"].level == _get_level_int(DEFAULT_LEVEL)
+    del td.log.handlers["file"]
+
+
+def test_log_level_not_found():
+    with pytest.raises(ValueError):
+        set_logging_level("NOT_A_LEVEL")
+
+
+def test_set_logging_level_deprecated():
+    with pytest.raises(DeprecationWarning):
+        td.set_logging_level("WARNING")
+
+
+def test_exception_message():
+    MESSAGE = "message"
+    e = Tidy3dError(MESSAGE)
+    assert str(e) == MESSAGE
+
+
+def test_logging_upper():
+    """Make sure we get an error if lowercase."""
+    td.config.logging_level = "WARNING"
+    with pytest.raises(ValueError):
+        td.config.logging_level = "warning"
+
+
+def test_logging_unrecognized():
+    """If unrecognized option, raise validation errorr."""
+    with pytest.raises(pd.ValidationError):
+        td.config.logging_level = "blah"
+
+
+def test_logging_warning_capture():
+    # create sim with warnings
+    domain_size = 12
+
+    wavelength = 1
+    f0 = td.C_0 / wavelength
+    fwidth = f0 / 10.0
+    source_time = td.GaussianPulse(freq0=f0, fwidth=fwidth)
+    run_time = 10 / fwidth
+    freqs = np.linspace(f0 - fwidth, f0 + fwidth, 11)
+
+    mode_mnt = td.ModeMonitor(
+        center=(0, 0, 0),
+        size=(domain_size, 0, domain_size),
+        freqs=list(freqs),
+        mode_spec=td.ModeSpec(num_modes=3),
+        name="mode",
+    )
+
+    # 1 warning: too high num_freqs
+    mode_source = td.ModeSource(
+        size=(domain_size, 0, domain_size),
+        source_time=source_time,
+        mode_spec=td.ModeSpec(num_modes=2, precision="single"),
+        mode_index=1,
+        num_freqs=50,
+        direction="-",
+    )
+
+    # 1 warning: ignoring "normal_dir"
+    monitor_flux = td.FluxMonitor(
+        center=(0, 0, 0),
+        size=(8, 8, 8),
+        freqs=freqs,
+        name="flux",
+        normal_dir="+",
+    )
+
+    # 6 warnings * 2 sources = 12 total: too close to each PML
+    box = td.Structure(
+        geometry=td.Box(center=(0, 0, 0), size=(11.5, 11.5, 11.5)),
+        medium=td.Medium(permittivity=4),
+    )
+
+    # 1 warning: too high "num_freqs"
+    gaussian_beam = td.GaussianBeam(
+        center=(4, 0, 0),
+        size=(0, 8, 9),
+        waist_radius=2.0,
+        waist_distance=1,
+        source_time=source_time,
+        direction="+",
+        num_freqs=30,
+    )
+
+    bspec_pml = td.BoundarySpec.all_sides(boundary=td.PML())
+
+    sim = td.Simulation(
+        size=[domain_size] * 3,
+        sources=[gaussian_beam, mode_source],
+        structures=[box],
+        monitors=[monitor_flux, mode_mnt],
+        run_time=run_time,
+        boundary_spec=bspec_pml,
+        grid_spec=td.GridSpec.uniform(dl=0.04),
+    )
+
+    # parse the entire simulation at once to capture warnings hierarchically
+    sim_json = sim.json()
+
+    td.log.set_capture(True)
+    sim = td.Simulation.parse_raw(sim_json)
+    warning_list = td.log.captured_warnings()
+    assert len(warning_list) == 15
+    td.log.set_capture(False)
+
+
+def test_log_suppression():
+    with td.log as suppressed_log:
+        assert td.log._counts is not None
+        for i in range(4):
+            suppressed_log.warning("Warning message")
+        assert td.log._counts[30] == 3
+
+    td.config.log_suppression = False
+    with td.log as suppressed_log:
+        assert td.log._counts is None
+        for i in range(4):
+            suppressed_log.warning("Warning message")
+        assert td.log._counts is None
+
+    td.config.log_suppression = True
---
+++ packages/tidy3d/tidy3d-2.4.0rc2.tar.gz/tidy3d-2.4.0rc2/tests/test_plugins/test_dispersion_fitter.py
@@ -0,0 +1,165 @@
+import numpy as np
+import pytest
+import responses
+
+import matplotlib.pyplot as plt
+
+import tidy3d as td
+from tidy3d.plugins.dispersion import DispersionFitter, FastDispersionFitter
+from tidy3d.plugins.dispersion import AdvancedFastFitterParam
+from tidy3d.plugins.dispersion.web import run as run_fitter
+
+advanced_param = AdvancedFastFitterParam(num_iters=1, passivity_num_iters=1)
+
+
+_, AX = plt.subplots()
+
+
+@pytest.fixture
+def random_data():
+    data_points = 11
+    wvl_um = np.linspace(1, 2, data_points)
+    n_data = np.random.random(data_points)
+    k_data = np.random.random(data_points)
+    return wvl_um, n_data, k_data
+
+
+@pytest.fixture
+def mock_remote_api(monkeypatch):
+    def mock_url(*args, **kwargs):
+        return "http://monkeypatched.com"
+
+    monkeypatch.setattr("tidy3d.plugins.dispersion.web.FitterData._set_url", mock_url)
+    responses.add(responses.GET, f"{mock_url()}/health", status=200)
+    responses.add(
+        responses.POST,
+        f"{mock_url()}/dispersion/fit",
+        json={"message": td.PoleResidue().json(), "rms": 1e-16},
+        status=200,
+    )
+
+
+def test_coeffs():
+    """make sure pack_coeffs and unpack_coeffs are reciprocal"""
+    num_poles = 10
+    coeffs = np.random.random(4 * num_poles)
+    a, c = DispersionFitter._unpack_coeffs(coeffs)
+    coeffs_ = DispersionFitter._pack_coeffs(a, c)
+    a_, c_ = DispersionFitter._unpack_coeffs(coeffs_)
+    assert np.allclose(coeffs, coeffs_)
+    assert np.allclose(a, a_)
+    assert np.allclose(c, c_)
+
+
+def test_pole_coeffs():
+    """make sure coeffs_to_poles and poles_to_coeffs are reciprocal"""
+    num_poles = 10
+    coeffs = np.random.random(4 * num_poles)
+    poles = DispersionFitter._coeffs_to_poles(coeffs)
+    coeffs_ = DispersionFitter._poles_to_coeffs(poles)
+    poles_ = DispersionFitter._coeffs_to_poles(coeffs_)
+    assert np.allclose(coeffs, coeffs_)
+    assert np.allclose(poles, poles_)
+
+
+@responses.activate
+def test_lossless_dispersion(random_data, mock_remote_api):
+    """perform fitting on random data"""
+    wvl_um, n_data, _ = random_data
+    fitter = DispersionFitter(wvl_um=wvl_um.tolist(), n_data=tuple(n_data))
+    medium, rms = fitter._fit_single()
+    medium, rms = fitter.fit(num_tries=2)
+    medium, rms = run_fitter(fitter)
+
+    fitter = FastDispersionFitter(wvl_um=wvl_um.tolist(), n_data=tuple(n_data))
+    medium, rms = fitter.fit(advanced_param=advanced_param)
+
+
+@responses.activate
+def test_lossy_dispersion(random_data, mock_remote_api):
+    """perform fitting on random lossy data"""
+    wvl_um, n_data, k_data = random_data
+    fitter = DispersionFitter(wvl_um=wvl_um, n_data=n_data, k_data=k_data)
+    medium, rms = fitter._fit_single()
+    medium, rms = fitter.fit(num_tries=2)
+    medium, rms = run_fitter(fitter)
+
+    fitter = FastDispersionFitter(wvl_um=wvl_um.tolist(), n_data=n_data, k_data=k_data)
+    medium, rms = fitter.fit(advanced_param=advanced_param)
+
+
+def test_dispersion_load():
+    """loads dispersion model from nk data file"""
+    fitter = DispersionFitter.from_file("tests/data/nk_data.csv", skiprows=1, delimiter=",")
+    medium, rms = fitter.fit(num_tries=20)
+
+    fitter = FastDispersionFitter.from_file("tests/data/nk_data.csv", skiprows=1, delimiter=",")
+    medium, rms = fitter.fit(advanced_param=advanced_param)
+
+
+def test_dispersion_plot(random_data):
+    """plots a medium fit from file"""
+    wvl_um, n_data, k_data = random_data
+
+    fitter = DispersionFitter(wvl_um=wvl_um, n_data=n_data)
+    fitter.plot(ax=AX)
+    plt.close()
+    medium, rms = fitter.fit(num_tries=2)
+    fitter.plot(medium, ax=AX)
+    plt.close()
+
+    fitter = DispersionFitter(wvl_um=wvl_um, n_data=n_data, k_data=k_data)
+    fitter.plot()
+    plt.close()
+    medium, rms = fitter.fit(num_tries=2)
+    fitter.plot(medium, ax=AX)
+    plt.close()
+
+
+def test_dispersion_set_wvg_range(random_data):
+    """set wavelength range function"""
+    wvl_um, n_data, k_data = random_data
+    fitter = DispersionFitter(wvl_um=wvl_um, n_data=n_data)
+    fastfitter = FastDispersionFitter(wvl_um=wvl_um, n_data=n_data)
+
+    wvl_range = [1.2, 1.8]
+    fitter = fitter.copy(update={"wvl_range": wvl_range})
+    assert len(fitter.freqs) == 7
+    medium, rms = fitter.fit(num_tries=2)
+    fastfitter = fastfitter.copy(update={"wvl_range": wvl_range})
+    assert len(fastfitter.freqs) == 7
+    medium, rms = fastfitter.fit(advanced_param=advanced_param)
+
+    wvl_range = [1.2, 2.8]
+    fitter = fitter.copy(update={"wvl_range": wvl_range, "k_data": k_data})
+    assert len(fitter.freqs) == 9
+    medium, rms = fitter.fit(num_tries=2)
+    fastfitter = fastfitter.copy(update={"wvl_range": wvl_range})
+    assert len(fastfitter.freqs) == 9
+    medium, rms = fastfitter.fit(advanced_param=advanced_param)
+
+    wvl_range = [0.2, 1.8]
+    fitter = fitter.copy(update={"wvl_range": wvl_range})
+    assert len(fitter.freqs) == 9
+    medium, rms = fitter.fit(num_tries=2)
+    fastfitter = fastfitter.copy(update={"wvl_range": wvl_range})
+    assert len(fastfitter.freqs) == 9
+    medium, rms = fastfitter.fit(advanced_param=advanced_param)
+
+    wvl_range = [0.2, 2.8]
+    fitter = fitter.copy(update={"wvl_range": wvl_range, "k_data": k_data})
+    assert len(fitter.freqs) == 11
+    medium, rms = fitter.fit(num_tries=2)
+    fastfitter = fastfitter.copy(update={"wvl_range": wvl_range})
+    assert len(fastfitter.freqs) == 11
+    medium, rms = fastfitter.fit(advanced_param=advanced_param)
+
+
+def test_dispersion_guess(random_data):
+    """plots a medium fit from file"""
+    wvl_um, n_data, k_data = random_data
+
+    fitter = DispersionFitter(wvl_um=wvl_um, n_data=n_data)
+    medium, rms = fitter.fit(num_tries=2)
+
+    medium_new, rms_new = fitter.fit(num_tries=1, guess=medium)
