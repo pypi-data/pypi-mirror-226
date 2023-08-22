6c5bcd4d19cac4524905e5b497f7832b2597f372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:13 +0000

    Add package fib142857/1.0/fib142857-1.0.tar.gz

---
+++ packages/fib142857/fib142857-1.0.tar.gz/fib142857-1.0/my_packages/__init__.py
@@ -0,0 +1,10 @@
+import re
+import urllib
+import sys
+import os
+
+import package
+
+print(package.os, package.urllib, package.sys, package.os)
+
+__all__ = ["fib"]
---
+++ packages/fib142857/fib142857-1.0.tar.gz/fib142857-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "fib142857",
+    version = "1.0",
+    description = "To solve fib by recursion",
+    author = "Linus Trovalds",
+    py_modules = ["my_packages.fib"]
+)
