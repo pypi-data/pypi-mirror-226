7d715c2e39ff618e57adc7458ee7df48a86797c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:07:02 +0000

    Add package agixt/1.3.119/agixt-1.3.119.tar.gz

---
+++ packages/agixt/agixt-1.3.119.tar.gz/agixt-1.3.119/agixt/providers/petal.py
@@ -0,0 +1,67 @@
+from providers.pipeline import PipelineProvider
+from transformers import AutoTokenizer
+
+try:
+    from petals import AutoDistributedModelForCausalLM
+except ImportError:
+    import subprocess, sys
+
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "petals"])
+    from petals import AutoDistributedModelForCausalLM
+
+
+class PetalsPipeline:
+    def __init__(self, model, tokenizer):
+        self.tokenizer = tokenizer
+        self.model = model
+
+    @classmethod
+    def from_pretrained(cls, model_name_or_path, **kwargs):
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        model = AutoDistributedModelForCausalLM.from_pretrained(
+            model_name_or_path, **kwargs
+        )
+        return cls(model, tokenizer)
+
+    def __call__(self, prompt: str, **kwargs) -> str:
+        input_ids = self.tokenizer(prompt, return_tensors="pt")["input_ids"]
+        outputs = self.model.generate(input_ids, **kwargs)[0]
+        return self.tokenizer.decode(
+            outputs[len(input_ids[0]) :], skip_special_tokens=True
+        )
+
+
+class PetalProvider(PipelineProvider):
+    def __init__(
+        self,
+        MODEL_PATH: str = "stabilityai/StableBeluga2",
+        AI_TEMPERATURE: float = 0.7,
+        MAX_TOKENS: int = 1024,
+        AI_MODEL: str = "",
+        HUGGINGFACE_API_KEY: str = None,
+        **kwargs,
+    ):
+        super().__init__(
+            MODEL_PATH,
+            AI_TEMPERATURE,
+            MAX_TOKENS,
+            AI_MODEL,
+            HUGGINGFACE_API_KEY,
+            **kwargs,
+        )
+        self.requirements = ["petals", "transformers[accelerate]", "torch"]
+
+    async def instruct(self, prompt, tokens: int = 0):
+        self.load_pipeline()
+        return self.pipeline(
+            prompt,
+            temperature=self.AI_TEMPERATURE,
+            max_new_tokens=self.get_max_new_tokens(tokens),
+        )
+
+    def load_pipeline(self):
+        if not self.pipeline:
+            self.load_args()
+            self.pipeline = PetalsPipeline.from_pretrained(
+                self.MODEL_PATH, **self.pipeline_kwargs
+            )
