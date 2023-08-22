922ba347571e58614f20152ec3d9ba1c88d0963f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:13:14 +0000

    Add package redguy-api/1.0.0/redguy-api-1.0.0.tar.gz

---
+++ packages/redguy-api/redguy-api-1.0.0.tar.gz/redguy-api-1.0.0/openapi_client/models/__init__.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from openapi_client.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from openapi_client.model.inline_object import InlineObject
+from openapi_client.model.inline_object1 import InlineObject1
+from openapi_client.model.inline_object2 import InlineObject2
+from openapi_client.model.inline_response200 import InlineResponse200
+from openapi_client.model.inline_response2001 import InlineResponse2001
+from openapi_client.model.inline_response2001_response import InlineResponse2001Response
+from openapi_client.model.inline_response2002 import InlineResponse2002
+from openapi_client.model.inline_response2002_response import InlineResponse2002Response
+from openapi_client.model.inline_response2003 import InlineResponse2003
+from openapi_client.model.inline_response200_response import InlineResponse200Response
+from openapi_client.model.inline_response200_response_value import InlineResponse200ResponseValue
+from openapi_client.model.inline_response200_response_value_values import InlineResponse200ResponseValueValues
+from openapi_client.model.v1_audience_user_response import V1AudienceUserResponse
