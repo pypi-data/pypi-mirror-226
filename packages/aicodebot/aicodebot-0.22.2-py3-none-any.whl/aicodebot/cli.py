459496e937ffb7542534efd844e998a75eea8db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:09:16 +0000

    Add package aicodebot/0.22.2/aicodebot-0.22.2-py3-none-any.whl

---
+++ packages/aicodebot/aicodebot-0.22.2-py3-none-any.whl/aicodebot/cli.py
@@ -0,0 +1,46 @@
+from aicodebot import AICODEBOT, version as aicodebot_version
+from aicodebot.commands import alignment, commit, configure, debug, learn, review, sidekick, sidekick_agent
+from aicodebot.config import read_config
+from aicodebot.output import get_console
+import click, langchain, os, sys
+
+# -------------------------- Top level command group ------------------------- #
+
+console = get_console()
+
+
+@click.group()
+@click.version_option(aicodebot_version, "--version", "-V")
+@click.help_option("--help", "-h")
+@click.option("-d", "--debug-output", is_flag=True, help="Enable debug output")
+@click.pass_context
+def cli(ctx, debug_output):
+    ctx.ensure_object(dict)
+    ctx.obj["config"] = existing_config = read_config()
+    if not existing_config:
+        if ctx.invoked_subcommand != "configure":
+            console.print(f"Welcome to {AICODEBOT}. Let's set up your config file.\n", style=console.bot_style)
+            configure.callback(openai_api_key=os.getenv("OPENAI_API_KEY"), verbose=0)
+            sys.exit(0)
+    else:
+        os.environ["OPENAI_API_KEY"] = existing_config["openai_api_key"]
+
+    # Turn on langchain debug output if requested
+    langchain.debug = debug_output
+
+
+# -------------------------- Subcommands ------------------------- #
+
+cli.add_command(alignment)
+cli.add_command(configure)
+cli.add_command(commit)
+cli.add_command(debug)
+cli.add_command(review)
+cli.add_command(sidekick)
+if os.getenv("AICODEBOT_ENABLE_EXPERIMENTAL_FEATURES"):
+    cli.add_command(learn)
+    cli.add_command(sidekick_agent)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    cli()
---
+++ packages/aicodebot/aicodebot-0.22.2-py3-none-any.whl/aicodebot/commands/alignment.py
@@ -0,0 +1,30 @@
+from aicodebot.helpers import logger
+from aicodebot.lm import CREATIVE_TEMPERATURE, LanguageModelManager
+from aicodebot.output import OurMarkdown, RichLiveCallbackHandler, get_console
+from aicodebot.prompts import get_prompt
+from rich.live import Live
+import click
+
+
+@click.command()
+@click.option("-t", "--response-token-size", type=int, default=350)
+def alignment(response_token_size):
+    """A message about AI Alignment 🤖 + ❤"""
+    console = get_console()
+
+    # Load the prompt
+    prompt = get_prompt("alignment")
+    logger.trace(f"Prompt: {prompt}")
+    lmm = LanguageModelManager()
+
+    with Live(OurMarkdown(f"Talking to {lmm.model_name} via {lmm.provider}"), auto_refresh=True) as live:
+        chain = lmm.chain_factory(
+            prompt=prompt,
+            response_token_size=response_token_size,
+            temperature=CREATIVE_TEMPERATURE,
+            streaming=True,
+            callbacks=[RichLiveCallbackHandler(live, console.bot_style)],
+        )
+
+        response = chain.run({})
+        live.update(OurMarkdown(response))
