97bb11b4fea6344444524e0075d4fbb9fa224e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 05:08:24 +0000

    Add package bcdata/0.8.1.post1/bcdata-0.8.1.post1.tar.gz

---
+++ packages/bcdata/bcdata-0.8.1.post1.tar.gz/bcdata-0.8.1.post1/bcdata/bcdc.py
@@ -0,0 +1,121 @@
+import json
+import logging
+from urllib.parse import urlparse
+
+from tenacity import retry
+from tenacity.stop import stop_after_delay
+from tenacity.wait import wait_random_exponential
+import requests
+
+import bcdata
+
+
+log = logging.getLogger(__name__)
+
+BCDC_API_URL = "https://catalogue.data.gov.bc.ca/api/3/action/"
+
+
+@retry(stop=stop_after_delay(120), wait=wait_random_exponential(multiplier=1, max=60))
+def _package_show(package):
+    try:
+        r = requests.get(BCDC_API_URL + "package_show", params={"id": package})
+    except Exception:
+        log.error("BCDC API Error")
+    return r
+
+
+@retry(stop=stop_after_delay(120), wait=wait_random_exponential(multiplier=1, max=60))
+def _table_definition(table_name):
+    try:
+        r = requests.get(BCDC_API_URL + "package_search", params={"q": table_name})
+        status_code = r.status_code
+        if status_code != 200:
+            raise ValueError(f"Error searching BC Data Catalogue API: {status_code}")
+    except Exception:
+        log.error("BCDC API Error")
+    return r
+
+
+def get_table_name(package):
+    """Query DataBC API to find WFS table/layer name for given package"""
+    package = package.lower()  # package names are lowercase
+    r = _package_show(package)
+    if r.status_code != 200:
+        raise ValueError("{d} is not present in DataBC API list".format(d=package))
+    result = r.json()["result"]
+    # Because the object_name in the result json is not a 100% reliable key
+    # for WFS requests, parse URL in WMS resource(s).
+    # Also, some packages may have >1 WFS layer - if this is the case, bail
+    # and provide user with a list of layers
+    layer_urls = [r["url"] for r in result["resources"] if r["format"] == "wms"]
+    layer_names = [urlparse(url).path.split("/")[3] for url in layer_urls]
+    if len(layer_names) > 1:
+        raise ValueError(
+            "Package {} includes more than one WFS resource, specify one of the following: \n{}".format(
+                package, "\n".join(layer_names)
+            )
+        )
+    return layer_names[0]
+
+
+def get_table_definition(table_name):
+    """
+    Given a table/object name, search BCDC for the first package/resource with a
+    matching "object_name", returns tuple (table comments, table schema)
+    """
+    # only allow searching for tables present in WFS list
+    table_name = table_name.upper()
+    if table_name not in bcdata.list_tables():
+        raise ValueError(
+            f"Only tables available via WFS are supported, {table_name} not found"
+        )
+    # search the api for the provided table
+    r = _table_definition(table_name)
+    # if there are no matching results, let the user know
+    if r.json()["result"]["count"] == 0:
+        log.warning(
+            f"BC Data Catalouge API search provides no results for: {table_name}"
+        )
+        return []
+    else:
+        matches = []
+        # iterate through results of search (packages)
+        for result in r.json()["result"]["results"]:
+            # iterate through resources associated with each package
+            for resource in result["resources"]:
+                # wms format resource
+                if resource["format"] == "wms":
+                    # if wms, check for table name match in this location
+                    if urlparse(resource["url"]).path.split("/")[3] == table_name:
+                        if "object_table_comments" in resource.keys():
+                            table_comments = resource["object_table_comments"]
+                        else:
+                            table_comments = None
+                        # only add to matches if schema details found
+                        if "details" in resource.keys() and resource["details"] != "":
+                            table_details = resource["details"]
+                            matches.append((table_comments, table_details))
+                            log.debug(resource)
+
+                # multiple format resource
+                elif resource["format"] == "multiple":
+                    # if multiple format, check for table name match in this location
+                    if json.loads(resource["preview_info"])["layer_name"] == table_name:
+                        if "object_table_comments" in resource.keys():
+                            table_comments = resource["object_table_comments"]
+                        else:
+                            table_comments = None
+                        # only add to matches if schema details found
+                        if "details" in resource.keys() and resource["details"] != "":
+                            table_details = resource["details"]
+                            matches.append((table_comments, table_details))
+                            log.debug(resource)
+
+        # uniquify the result
+        if len(matches) > 0:
+            matched = list(set(matches))[0]
+            return (matched[0], json.loads(matched[1]))
+        else:
+            raise ValueError(
+                f"BCDC search for {table_name} does not return a table schema"
+            )
---
+++ packages/bcdata/bcdata-0.8.1.post1.tar.gz/bcdata-0.8.1.post1/tests/test_wfs.py
@@ -0,0 +1,127 @@
+import os
+
+import bcdata
+
+from geopandas.geodataframe import GeoDataFrame
+
+AIRPORTS_PACKAGE = "bc-airports"
+AIRPORTS_TABLE = "WHSE_IMAGERY_AND_BASE_MAPS.GSR_AIRPORTS_SVW"
+UTMZONES_KEY = "utm-zones-of-british-columbia"
+BEC_KEY = "biogeoclimatic-ecosystem-classification-bec-map"
+ASSESSMENTS_TABLE = "whse_fish.pscis_assessment_svw"
+GLACIERS_TABLE = "whse_basemapping.fwa_glaciers_poly"
+STREAMS_TABLE = "whse_basemapping.fwa_stream_networks_sp"
+
+
+def test_validate_table_lowercase():
+    table = bcdata.validate_name(AIRPORTS_TABLE.lower())
+    assert table == AIRPORTS_TABLE
+
+
+# def test_cache_file(tmp_path):
+#    BCWFS(cache_path=tmp_path / ".bcdata")
+#    assert os.path.exists(tmp_path / ".bcdata" / "capabilities.xml")
+
+
+def test_get_table_name_urlparse():
+    table = bcdata.get_table_name("natural-resource-nr-district")
+    assert table == "WHSE_ADMIN_BOUNDARIES.ADM_NR_DISTRICTS_SPG"
+
+
+def test_get_count():
+    table = bcdata.get_table_name(UTMZONES_KEY)
+    assert bcdata.get_count(table) == 6
+
+
+def test_get_count_filtered():
+    assert bcdata.get_count(UTMZONES_KEY, query="UTM_ZONE=10") == 1
+
+
+def test_get_count_bounds():
+    assert (
+        bcdata.get_count(AIRPORTS_TABLE, bounds=[1188000, 377051, 1207437, 390361]) == 8
+    )
+
+
+def test_get_data_asgdf():
+    gdf = bcdata.get_data(UTMZONES_KEY, query="UTM_ZONE=10", as_gdf=True)
+    assert type(gdf) == GeoDataFrame
+
+
+def test_get_null_gdf():
+    gdf = bcdata.get_data(UTMZONES_KEY, query="UTM_ZONE=9999", as_gdf=True)
+    assert type(gdf) == GeoDataFrame
+
+
+def test_get_data_small():
+    data = bcdata.get_data(AIRPORTS_TABLE)
+    assert data["type"] == "FeatureCollection"
+
+
+def test_get_data_lowercase():
+    data = bcdata.get_data(AIRPORTS_TABLE, lowercase=True)
+    assert "airport_name" in data["features"][0]["properties"].keys()
+
+
+def test_get_data_crs():
+    data = bcdata.get_data(AIRPORTS_TABLE, crs="EPSG:3005")
+    assert (
+        data["crs"]
+        == """{"type":"name","properties":{"name":"urn:ogc:def:crs:EPSG::3005"}}"""
+    )
+
+
+def test_get_features():
+    data = [f for f in bcdata.get_features(AIRPORTS_TABLE)]
+    assert len(data) == 455
+
+
+def test_get_data_paged():
+    data = bcdata.get_data(AIRPORTS_TABLE, pagesize=250)
+    assert len(data["features"]) == 455
+
+
+def test_get_data_count():
+    data = bcdata.get_data(AIRPORTS_TABLE, count=100)
+    assert len(data["features"]) == 100
+
+
+def test_get_data_paged_count():
+    data = bcdata.get_data(AIRPORTS_TABLE, pagesize=250, count=300)
+    assert len(data["features"]) == 300
+
+
+def test_get_data_sortby():
+    data = bcdata.get_data(AIRPORTS_TABLE, count=1, sortby="AIRPORT_NAME")
+    assert data["features"][0]["properties"]["AIRPORT_NAME"] == "100 Mile House Airport"
+
+
+def test_cql_filter():
+    data = bcdata.get_data(
+        AIRPORTS_TABLE,
+        query="AIRPORT_NAME='Terrace (Northwest Regional) Airport'",
+    )
+    assert len(data["features"]) == 1
+    assert (
+        data["features"][0]["properties"]["AIRPORT_NAME"]
+        == "Terrace (Northwest Regional) Airport"
+    )
+
+
+def test_bounds_filter():
+    data = bcdata.get_data(AIRPORTS_TABLE, bounds=[1188000, 377051, 1207437, 390361])
+    assert len(data["features"]) == 8
+
+
+def test_cql_bounds_filter():
+    data = bcdata.get_data(
+        AIRPORTS_TABLE,
+        query="AIRPORT_NAME='Victoria International Airport'",
+        bounds=[1167680.0, 367958.0, 1205720.0, 432374.0],
+        bounds_crs="EPSG:3005",
+    )
+    assert len(data["features"]) == 1
+    assert (
+        data["features"][0]["properties"]["AIRPORT_NAME"]
+        == "Victoria International Airport"
+    )
