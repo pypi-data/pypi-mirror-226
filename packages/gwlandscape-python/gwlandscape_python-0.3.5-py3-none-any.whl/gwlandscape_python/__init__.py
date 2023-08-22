e98155e09505b38f40ccd04f40597d101ef47952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:08:46 +0000

    Add package gwlandscape-python/0.3.5/gwlandscape_python-0.3.5-py3-none-any.whl

---
+++ packages/gwlandscape-python/gwlandscape_python-0.3.5-py3-none-any.whl/gwlandscape_python/__init__.py
@@ -0,0 +1,12 @@
+from .gwlandscape import GWLandscape
+from .keyword_type import Keyword
+from .publication_type import Publication
+from .model_type import Model
+from .dataset_type import Dataset
+
+
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version
+__version__ = version('gwlandscape_python')
