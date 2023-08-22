40bcdf58d154978f235244b1cf956db0d0d7bc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:26 +0000

    Add package radio81/1.4.2/radio81-1.4.2-py3-none-any.whl

---
+++ packages/radio81/radio81-1.4.2-py3-none-any.whl/radio81/__main__.py
@@ -0,0 +1,8 @@
+
+from radio81.console import start
+
+# Press the green button in the gutter to run the script.
+
+
+if __name__ == '__main__':
+    start()
---
+++ packages/radio81/radio81-1.4.2-py3-none-any.whl/radio81/skeleton.py
@@ -0,0 +1,6 @@
+
+
+__author__ = "Jacopo Corzani"
+__copyright__ = "Jacopo Corzani"
+__license__ = "MIT"
+
