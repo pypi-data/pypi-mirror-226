cdda2afb7be15026717fea7d8534d0ce744f2ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 05:08:24 +0000

    Add package ursactl/0.8.3/ursactl-0.8.3-py3-none-any.whl

---
+++ packages/ursactl/ursactl-0.8.3-py3-none-any.whl/ursactl/core/version.py
@@ -0,0 +1,9 @@
+
+from cement.utils.version import get_version as cement_get_version
+
+VERSION = (0, 8, 3)
+PRERELEASE = ('final', 0)
+
+
+def get_version(version=VERSION):
+    return cement_get_version(version + PRERELEASE)
