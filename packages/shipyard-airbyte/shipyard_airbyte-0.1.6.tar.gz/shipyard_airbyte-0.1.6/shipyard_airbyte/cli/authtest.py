be8264ab9a8f730f7a7f5304fe506f56c7194377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:08:43 +0000

    Add package shipyard-airbyte/0.1.6/shipyard_airbyte-0.1.6.tar.gz

---
+++ packages/shipyard-airbyte/shipyard_airbyte-0.1.6.tar.gz/shipyard_airbyte-0.1.6/shipyard_airbyte/cli/authtest.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from shipyard_blueprints import AirbyteClient
+
+
+def main():
+    token = os.getenv("AIRBYTE_API_TOKEN")
+    client = AirbyteClient(token)
+    response = client.connect()
+    if response == 200:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
