4e88f7c3b4cb4d7609379acb6fe1bc55856de663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 16:10:44 +0000

    Add package nexi/1.0.7/nexi-1.0.7-py3-none-any.whl

---
+++ packages/nexi/nexi-1.0.7-py3-none-any.whl/nexi/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present Quentin Uhl <quentin.uhl@gmail.com>
+#
+# SPDX-License-Identifier: Apache 2.0
+
+from .estimate_nexi_noiseless import estimate_nexi_noiseless
+from .estimate_nexi import estimate_nexi
+from .powderaverage.powderaverage import powder_average, save_data_as_npz, normalize_sigma
