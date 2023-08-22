b8e1b62f6ea568c1ceb1241647971a339207a321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:21 +0000

    Add package fate-flow/2.0.0a0/fate_flow-2.0.0a0.tar.gz

---
+++ packages/fate-flow/fate_flow-2.0.0a0.tar.gz/fate_flow-2.0.0a0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fate_flow",
+    version="2.0.0.a0",
+    description="FATE Flow",
+    author="tonly",
+    author_email="1642899961@qq.com",
+    url="https://github.com/tonly/fate_flow",
+    packages=[],
+)
