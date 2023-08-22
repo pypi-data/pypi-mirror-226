833d454ebea9ca37640c170084a2a52c1883e3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:07:58 +0000

    Add package funsecret/202308221117/funsecret-202308221117.tar.gz

---
+++ packages/funsecret/funsecret-202308221117.tar.gz/funsecret-202308221117/funsecret/__init__.py
@@ -0,0 +1,3 @@
+from .fernet import decrypt, encrypt, generate_key
+from .secret import (SecretManage, get_md5_str, load_secret_str, read_secret,
+                     save_secret_str, write_secret)
