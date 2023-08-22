4ff71d0e5aaf0e5ac3783777d8058941e30624e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 05:08:32 +0000

    Add package roboto-sdk/0.1.20/roboto_sdk-0.1.20.tar.gz

---
+++ packages/roboto-sdk/roboto_sdk-0.1.20.tar.gz/roboto_sdk-0.1.20/src/roboto_sdk/domain/users/__init__.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2023 Roboto Technologies, Inc.
+
+from .http_delegate import UserHttpDelegate
+from .user import User
+from .user_delegate import UserDelegate
+from .user_record import UserRecord
+
+__all__ = ["User", "UserDelegate", "UserRecord", "UserHttpDelegate"]
