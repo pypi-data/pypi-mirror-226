0244855b23b0ff5aa3874629f3e20f792dead8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:10:25 +0000

    Add package abqpy/2018.5.6/abqpy-2018.5.6-py3-none-any.whl

---
+++ packages/abqpy/abqpy-2018.5.6-py3-none-any.whl/abaqus/__init__.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import sys
+from math import *
+
+from abqpy import run
+
+from .builtin import *
+from .Canvas.Highlight import *
+from .Mdb.Mdb import Mdb
+from .Mdb.MdbCommands import *
+from .Odb.Odb import Odb
+from .Session.Session import Session
+from .UtilityAndView import abaqusConstants
+from .UtilityAndView.abaqusConstants import OFF, Boolean
+from .UtilityAndView.BackwardCompatibility import BackwardCompatibility
+from .UtilityAndView.SymbolicConstant import SymbolicConstant
+from .UtilityAndView.User import *
+
+session = Session()
+mdb = Mdb()
+
+backwardCompatibility = BackwardCompatibility()
+
+YES = abaqusConstants.YES
+NO = abaqusConstants.NO
+
+run(cae=True)
+
+__main__ = sys.modules["__main__"]
+__all__ = [
+    "backwardCompatibility",
+    "BackwardCompatibility",
+    "Boolean",
+    "Mdb",
+    "mdb",
+    "Odb",
+    "session",
+    "Session",
+    "SymbolicConstant",
+    "YES",
+    "NO",
+    "OFF",
+]
