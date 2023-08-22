a620a5f8f87ea3a51d2635d9ec03c6ca5ae167e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:11:47 +0000

    Add package speedruncompy/0.0.4/speedruncompy-0.0.4-py3-none-any.whl

---
+++ packages/speedruncompy/speedruncompy-0.0.4-py3-none-any.whl/speedruncompy/endpoints.py
@@ -0,0 +1,171 @@
+from .api import BasePaginatedRequest, GetRequest, PostRequest
+from .exceptions import SrcpyException
+from .enums import *
+
+"""
+GET requests are all unauthed & do not require PHPSESSID.
+"""
+
+class GetGameLeaderboard2(GetRequest, BasePaginatedRequest):
+    def __init__(self, gameId: str, categoryId: str, **params) -> None:
+        page = params.pop("page", None)
+        param_construct = {"params": {"gameId": gameId, "categoryId": categoryId}}
+        param_construct["params"].update(params)
+        if page is not None: 
+            param_construct["page"] = page
+        super().__init__("GetGameLeaderboard2", **param_construct)
+
+class GetGameData(GetRequest):
+    def __init__(self, gameId: str, **params) -> None:
+        super().__init__("GetGameData", gameId=gameId, **params)
+
+class GetGameRecordHistory(GetRequest):
+    """This takes a page field, but does not return a pagination object, and does not appear to be paginated."""
+    def __init__(self, gameId: str, categoryId: str, **params) -> None:
+        page = params.pop("page", None)
+        param_construct = {"params": {"gameId": gameId, "categoryId": categoryId}}
+        param_construct["params"].update(params)
+        if page is not None: 
+            param_construct["page"] = page
+        super().__init__("GetGameRecordHistory", **param_construct)
+
+class GetSearch(GetRequest):
+    def __init__(self, query: str, **params) -> None:
+        super().__init__("GetSearch", query=query, **params)
+
+class GetLatestLeaderboard(GetRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetLatestLeaderboard", **params)
+
+"""
+POST requests may require auth
+"""
+
+# Session
+class PutAuthLogin(PostRequest):
+    def __init__(self, name: str, password: str, token: str = None, **params) -> None:
+        super().__init__("PutAuthLogin", name=name, password=password, token=token, **params)
+
+class PutAuthLogout(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("PutAuthLogout", **params)
+
+class GetSession(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetSession", **params)
+
+class PutSessionPing(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("PutSessionPing", **params)
+
+# Supermod actions
+class GetAuditLogList(PostRequest):
+    def __init__(self, gameId: str = None, seriesId: str = None, eventType: eventType = eventType.NONE, page: int = 1, **params) -> None:
+        if gameId is None and seriesId is None: raise SrcpyException("GetAuditLogList requires gameId or seriesId")
+        super().__init__("GetAuditLogList", gameId=gameId, eventType=eventType, page=page, **params)
+
+# Mod actions
+class GetGameSettings(PostRequest):
+    def __init__(self, gameId: str, **params) -> None:
+        super().__init__("GetGameSettings", gameId=gameId, **params)
+
+class PutGameSettings(PostRequest):
+    def __init__(self, gameId: str, settings: dict, **params) -> None:
+        super().__init__("PutGameSettings", gameId=gameId, settings=settings, **params)
+
+# Run verification
+class GetModerationGames(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetModerationGames", **params)
+
+class GetModerationRuns(PostRequest, BasePaginatedRequest):
+    def __init__(self, gameId: str, limit: int = 100, page: int = 1, **params) -> None:
+        super().__init__("GetModerationRuns", gameId=gameId, limit=limit, page=page, **params)
+
+class PutRunAssignee(PostRequest):
+    def __init__(self, assigneeId: str, runId: str, **params) -> None:
+        super().__init__("PutRunAssignee", assigneeId=assigneeId, runId=runId, **params)
+
+class PutRunVerification(PostRequest):
+    def __init__(self, runId: str, verified: int, **params) -> None:
+        super().__init__("PutRunVerification", runId=runId, verified=verified, **params)
+
+# Run management
+class GetRunSettings(PostRequest):
+    def __init__(self, runId: str, **params) -> None:
+        super().__init__("GetRunSettings", runId=runId, **params)
+
+class PutRunSettings(PostRequest):
+    def __init__(self, csrfToken: str, settings: dict, **params) -> None:
+        """Sets a run's settings. Note that the runId is contained in `settings`."""
+        super().__init__("PutRunSettings", csrfToken=csrfToken, settings=settings, **params)
+
+# User inbox actions
+class GetConversations(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetConversations", **params)
+
+class GetConversationMessages(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetConversationMessages", **params)
+
+# User notifications
+class GetNotifications(PostRequest, BasePaginatedRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetNotifications", **params)
+
+# User settings
+class GetUserSettings(PostRequest):
+    """Gets a user's settings. Note that unless you are a site mod, you can only get your own settings."""
+    def __init__(self, userUrl: str, **params) -> None:
+        super().__init__("GetUserSettings", userUrl=userUrl, **params)
+
+class PutUserSettings(PostRequest):
+    def __init__(self, userUrl: str, settings: dict, **params) -> None:
+        super().__init__("PutUserSettings", userUrl=userUrl, settings=settings, **params)
+
+# Comment Actions
+class GetCommentList(PostRequest):
+    def __init__(self, itemId: str, itemType: int, **params) -> None:
+        super().__init__("GetCommentList", itemId=itemId, itemType=itemType, **params)
+
+class GetCommentable(PostRequest):
+    def __init__(self, itemId: str, itemType: int, **params) -> None:
+        super().__init__("GetCommentable", itemId=itemId, itemType=itemType, **params)
+
+class PutComment(PostRequest):
+    def __init__(self, itemId: str, itemType: int, text: str, **params) -> None:
+        super().__init__("PutComment", itemId=itemId, itemType=itemType, text=text, **params)
+
+#TODO: test params
+class PutCommentableSettings(PostRequest):
+    def __init__(self, itemId: str, itemType: int, **params) -> None:
+        super().__init__("PutCommentableSettings", itemId=itemId, itemType=itemType, **params)
+
+# Thread Actions
+class GetThread(PostRequest, BasePaginatedRequest):
+    def __init__(self, id: str, **params) -> None:
+        super().__init__("GetThread", id=id, **params)
+
+class GetThreadReadStatus(PostRequest):
+    def __init__(self, threadIds: list[str], **params) -> None:
+        super().__init__("GetThreadReadStatus", threadIds=threadIds, **params)
+
+class PutThreadRead(PostRequest):
+    def __init__(self, threadId: str, **params) -> None:
+        super().__init__("PutThreadRead", threadId=threadId, **params)
+
+# Forum actions
+class GetForumList(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetForumList", **params)
+
+class GetForumReadStatus(PostRequest):
+    def __init__(self, forumIds: list[str], **params) -> None:
+        super().__init__("GetForumReadStatus", forumIds=forumIds, **params)
+
+# Theme actions
+class GetThemeSettings(PostRequest):
+    def __init__(self, **params) -> None:
+        """Provide either userId or gameId"""
+        super().__init__("GetThemeSettings", **params)
---
+++ packages/speedruncompy/speedruncompy-0.0.4-py3-none-any.whl/speedruncompy/enums.py
@@ -0,0 +1,93 @@
+from enum import IntEnum, StrEnum
+
+class itemType(IntEnum):
+    LIKE = 1 # Unsure if "LIKE" is the correct term here.
+    RUN = 2
+    THREAD = 7  # Thread (comment?)
+    GAME_NEWS = 27
+    SITE_NEWS = 30
+
+class verified(IntEnum):
+    PENDING = 0
+    VERIFIED = 1
+    REJECTED = 2
+
+class forumType(IntEnum):
+    FRONT_PAGE = 0
+    SUPPORTER = 1
+    GAME = 2
+
+class timerType(IntEnum):
+    """DO NOT USE!
+    
+    Hollow Knight has default 0, valid [0,1], which suggests 0 = LRT and 1 = RTA,
+       but Celeste has default 2 and allowed [0,2], which suggests 2 = IGT and 0 = RTA? but this is listed as just Time"""
+    RTA = 0
+    LRT = 1
+    IGT = 2
+
+class modLevel(IntEnum):
+    VERIFIER = -1
+    MODERATOR = 0
+    SUPERMOD = 1
+
+class gameType(IntEnum):
+    ROM_HACK = 1
+    MODIFICATION = 2
+    FAN_GAME = 3
+    WEB_GAME = 4
+    PRERELEASE_GAME = 5
+    MOBILE_GAME = 6
+    EXPANSION = 7
+    CATEGORY_EXTENSIONS = 8
+    MULTIPLE_GAMES = 9
+    MINI_GAME = 10
+    SERVER_MAP = 11
+    HOMEBREW_GAME = 12
+
+class permissionType(IntEnum):
+    ALL = 0
+    DISABLED = 1
+    VERIFIED_HERE = 2
+    VERIFIED_ANY = 3
+    MODS_ONLY = 4
+
+class resourceType(IntEnum):
+    TOOL = 1
+    SAVE = 2
+    SPLITS = 3
+    PATCH = 4
+
+class GamePowerLevel(IntEnum):
+    VERIFIER = -1
+    MOD = 0
+    SUPERMOD = 1
+
+class PowerLevel(IntEnum):
+    BANNED = 0
+    USER = 1
+    CONTENT_MOD = 2
+    SITE_MOD = 3
+    SITE_ADMIN = 4
+
+class eventType(StrEnum):
+    NONE = ""
+    CATEGORY_CREATED = "category-created"
+    CATEGORY_REMOVED = "category-removed"
+    CATEGORY_RESTORED = "category-restored"
+    CATEGORY_UPDATED = "category-updated"
+    COMMENT_CREATED = "comment-created"
+    COMMENT_DELETED = "comment-deleted"
+    COMMENT_UPDATED = "comment-updated"
+    GAME_COVERS_UPDATED = "game-covers-updated"
+    GAME_CREATED = "game-created"
+    GAME_MODERATOR_CREATED = "game-moderator-created"
+    GAME_MODERATOR_REMOVED = "game-moderator-removed"
+    GAME_MODERATOR_UPDATED = "game-moderator-updated"
+    GAME_NEWS_POST_CREATED = "game-news-post-created"
+    GAME_NEWS_POST_EDITED = "game-news-post-edited"
+    GAME_NEWS_POST_REMOVED = "game-news-post-removed"
+    GAME_RESTORED = "game-restored"
+    GAME_UPDATED = "game-updated"
+    GAMEREQUEST_REVIEWED = "gamerequest-reviewed"
+    LEVEL_CREATED = "level-created"
