b0e4d572a034e2f6046446c5794722237401fe8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:05 +0000

    Add package hdr-local-processing/0.1.4/hdr-local-processing-0.1.4.tar.gz

---
+++ packages/hdr-local-processing/hdr-local-processing-0.1.4.tar.gz/hdr-local-processing-0.1.4/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hdr-local-processing',
+    version='0.1.4',
+    url='https://github.com/Elessar11777',
+    author='Elessar11777',
+    author_email='Elessar11777@gmail.com',
+    description='Petri Local Tonemapping Package',
+    long_description="None",
+    long_description_content_type='text/markdown',
+    packages=find_packages(include=['hdrlocal']),
+    license="GPL",
+    install_requires=[
+        "numpy",
+        "opencv-python"
+    ],
+)
