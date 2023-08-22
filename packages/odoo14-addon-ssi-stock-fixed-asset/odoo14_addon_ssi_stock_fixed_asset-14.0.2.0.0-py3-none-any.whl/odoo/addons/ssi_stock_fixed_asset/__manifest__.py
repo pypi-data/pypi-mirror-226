ed8be8ba35766c70b236387160261bb4ce8be580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:10:10 +0000

    Add package odoo14-addon-ssi-stock-fixed-asset/14.0.2.0.0/odoo14_addon_ssi_stock_fixed_asset-14.0.2.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-stock-fixed-asset/odoo14_addon_ssi_stock_fixed_asset-14.0.2.0.0-py3-none-any.whl/odoo/addons/ssi_stock_fixed_asset/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Inventory + Fixed Asset Integration",
+    "version": "14.0.2.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "ssi_stock_account",
+        "ssi_fixed_asset",
+    ],
+    "data": ["views/product_product_views.xml"],
+    "demo": [],
+}
