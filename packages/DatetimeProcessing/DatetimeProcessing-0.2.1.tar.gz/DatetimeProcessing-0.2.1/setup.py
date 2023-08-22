0b1b0dcba0716a8ad985d17334210fb8358c56fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:09:26 +0000

    Add package DatetimeProcessing/0.2.1/DatetimeProcessing-0.2.1.tar.gz

---
+++ packages/DatetimeProcessing/DatetimeProcessing-0.2.1.tar.gz/DatetimeProcessing-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='DatetimeProcessing',
+    version='0.2.1',  # Phiên bản của gói
+    description='Rules date_time',
+    author='ntt1210',
+    author_email='ndtan.fpt@gmail.com',
+    packages=['DatetimeProcessing'],  # Danh sách các thư mục chứa mã của bạn
+)
