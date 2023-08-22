44a3b9ed14b88a0e6cfbb13f3d20db49e56c73db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:12:44 +0000

    Add package surge-api/1.1.4/surge_api-1.1.4-py3-none-any.whl

---
+++ packages/surge-api/surge_api-1.1.4-py3-none-any.whl/surge/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+from surge.projects import Project
+from surge.tasks import Task
+from surge.teams import Team
+from surge.reports import Report
+
+api_key = os.environ.get("SURGE_API_KEY", None)
