9c35b609dc2058bf2ddb8019d49999780403c3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 05:08:30 +0000

    Add package snickerdoodle-demo/0.1.0/snickerdoodle_demo-0.1.0-py3-none-any.whl

---
+++ packages/snickerdoodle-demo/snickerdoodle_demo-0.1.0-py3-none-any.whl/snickerdoodle_demo/__init__.py
@@ -0,0 +1,10 @@
+"""Demonstration repository of the snickerdoodle template"""
+
+from __future__ import annotations
+
+
+__version__ = '0.1.0'
+
+__author__: str = 'Corey Rayburn Yung'
+
+__all__: list[str] = []
