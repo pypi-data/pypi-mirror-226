aad639b50d85c22b81b139620ac6618f0735b3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:15 +0000

    Add package tomoscan/2.0.0a0/tomoscan-2.0.0a0-py3-none-any.whl

---
+++ packages/tomoscan/tomoscan-2.0.0a0-py3-none-any.whl/tomoscan/esrf/hdf5scan.py
@@ -0,0 +1,11 @@
+from silx.utils.deprecation import deprecated_warning
+
+deprecated_warning(
+    "Module",
+    name="tomoscan.esrf.hdf5scan",
+    reason="Have been moved",
+    replacement="tomoscan.esrf.scan.nxtomoscan",
+    only_once=True,
+)
+
+from .scan.nxtomoscan import *  # noqa F401
