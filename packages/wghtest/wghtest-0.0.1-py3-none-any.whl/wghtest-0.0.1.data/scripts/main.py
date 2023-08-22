fe9db0eae63a24e7eec73833b61bed8731871c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:07:53 +0000

    Add package wghtest/0.0.1/wghtest-0.0.1-py3-none-any.whl

---
+++ packages/wghtest/wghtest-0.0.1-py3-none-any.whl/wghtest-0.0.1.data/scripts/main.py
@@ -0,0 +1,40 @@
+#!python
+# -*- coding: utf-8 -*-
+# @Time     : 
+# @Author   : wgh
+
+
+"""
+文件说明：
+"""
+
+class YTObject:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def set_username(self, username):
+        self.username = username
+
+    def set_password(self, password):
+        self.password = password
+
+
+    def upload_doc(self, doc):
+        """
+            文档上传
+        """
+        url = ""
+        pass
+
+    def qa(self, question):
+        """
+            文档问答
+        """
+        url = ""
+
+        pass
+
+
+if __name__ == '__main__':
+    exit(0)
