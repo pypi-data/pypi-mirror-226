691f3af631a4a19d99b4f53a8e969dcac9adc8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:56 +0000

    Add package video-images-creator/0.1.1/video_images_creator-0.1.1.tar.gz

---
+++ packages/video-images-creator/video_images_creator-0.1.1.tar.gz/video_images_creator-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='video_images_creator',
+    version='0.1.1',
+    description='A example Python package',
+    url='',
+    author='Nikhil Sharma',
+    author_email='nikhilsharma972@gmail.com',
+    license='BSD 2-clause',
+    packages=['video_images_creator'],
+    install_requires=['opencv-python', 'numpy', 'pillow'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+)
