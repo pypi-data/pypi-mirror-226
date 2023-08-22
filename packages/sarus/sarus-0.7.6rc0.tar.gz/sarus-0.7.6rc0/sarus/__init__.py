3abe7bc45e85602879cd894558f39004148e02b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:10:39 +0000

    Add package sarus/0.7.6rc0/sarus-0.7.6rc0.tar.gz

---
+++ packages/sarus/sarus-0.7.6rc0.tar.gz/sarus-0.7.6rc0/sarus/__init__.py
@@ -0,0 +1,31 @@
+"""SDK classes and functions."""
+import warnings
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from sarus import (
+        numpy,
+        pandas,
+        pandas_profiling,
+        plotly,
+        sklearn,
+        skopt,
+        std,
+        xgboost,
+    )
+
+    from .sarus import Client, Dataset
+    from .utils import eval, eval_policy, floating, integer, length
+
+VERSION = "0.7.5.rc0"
+
+__all__ = [
+    "Dataset",
+    "Client",
+    "length",
+    "eval",
+    "eval_policy",
+    "config",
+    "floating",
+    "integer",
+]
