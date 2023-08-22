9d370a5702018a7b9b64e25601d7f96404474637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 16:10:09 +0000

    Add package summers/0.2.2/summers-0.2.2-py3-none-any.whl

---
+++ packages/summers/summers-0.2.2-py3-none-any.whl/summers/cc.py
@@ -0,0 +1,21 @@
+from summers import CleanArch
+
+
+class CleanArchImpl(CleanArch):
+    """
+    洁架构校验器(实现类)
+    """
+
+    @staticmethod
+    def dependencies(*args, **kwargs):
+        """
+        获取依赖关系信息, JSON 格式 TODO
+        """
+        pass
+
+    @staticmethod
+    def validate(*args, **kwargs):
+        """
+        整洁架构框风格依赖关系校验 TODO
+        """
+        pass
