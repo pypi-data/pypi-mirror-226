68f99757704d7362a88a1c87997928f5a3f1ba42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:07:47 +0000

    Add package stumpy/1.12.0/stumpy-1.12.0.tar.gz

---
+++ packages/stumpy/stumpy-1.12.0.tar.gz/stumpy-1.12.0/tests/test_gpu_aamp_ostinato.py
@@ -0,0 +1,55 @@
+from unittest.mock import patch
+
+import numpy as np
+import numpy.testing as npt
+from numba import cuda
+
+try:
+    from numba.errors import NumbaPerformanceWarning
+except ModuleNotFoundError:
+    from numba.core.errors import NumbaPerformanceWarning
+
+import naive
+import pytest
+
+from stumpy import gpu_aamp_ostinato
+
+TEST_THREADS_PER_BLOCK = 10
+
+if not cuda.is_available():  # pragma: no cover
+    pytest.skip("Skipping Tests No GPUs Available", allow_module_level=True)
+
+
+@pytest.mark.filterwarnings("ignore", category=NumbaPerformanceWarning)
+@pytest.mark.parametrize(
+    "seed", np.random.choice(np.arange(10000), size=2, replace=False)
+)
+@patch("stumpy.config.STUMPY_THREADS_PER_BLOCK", TEST_THREADS_PER_BLOCK)
+def test_random_gpu_aamp_ostinato(seed):
+    m = 50
+    np.random.seed(seed)
+    Ts = [np.random.rand(n) for n in [64, 128, 256]]
+
+    ref_radius, ref_Ts_idx, ref_subseq_idx = naive.aamp_ostinato(Ts, m)
+    comp_radius, comp_Ts_idx, comp_subseq_idx = gpu_aamp_ostinato(Ts, m)
+
+    npt.assert_almost_equal(ref_radius, comp_radius)
+    npt.assert_almost_equal(ref_Ts_idx, comp_Ts_idx)
+    npt.assert_almost_equal(ref_subseq_idx, comp_subseq_idx)
+
+
+@pytest.mark.filterwarnings("ignore", category=NumbaPerformanceWarning)
+@pytest.mark.parametrize("seed", [41, 88])
+@patch("stumpy.config.STUMPY_THREADS_PER_BLOCK", TEST_THREADS_PER_BLOCK)
+def test_deterministic_gpu_aamp_ostinato(seed):
+    m = 50
+    np.random.seed(seed)
+    Ts = [np.random.rand(n) for n in [64, 128, 256]]
+
+    for p in [1.0, 2.0, 3.0]:
+        ref_radius, ref_Ts_idx, ref_subseq_idx = naive.aamp_ostinato(Ts, m, p=p)
+        comp_radius, comp_Ts_idx, comp_subseq_idx = gpu_aamp_ostinato(Ts, m, p=p)
+
+        npt.assert_almost_equal(ref_radius, comp_radius)
+        npt.assert_almost_equal(ref_Ts_idx, comp_Ts_idx)
+        npt.assert_almost_equal(ref_subseq_idx, comp_subseq_idx)
---
+++ packages/stumpy/stumpy-1.12.0.tar.gz/stumpy-1.12.0/tests/test_gpu_ostinato.py
@@ -0,0 +1,116 @@
+import functools
+
+import numpy as np
+import numpy.testing as npt
+from numba import cuda
+
+try:
+    from numba.errors import NumbaPerformanceWarning
+except ModuleNotFoundError:
+    from numba.core.errors import NumbaPerformanceWarning
+
+from unittest.mock import patch
+
+import naive
+import pytest
+
+from stumpy import gpu_ostinato
+
+TEST_THREADS_PER_BLOCK = 10
+
+if not cuda.is_available():  # pragma: no cover
+    pytest.skip("Skipping Tests No GPUs Available", allow_module_level=True)
+
+
+@pytest.mark.filterwarnings("ignore", category=NumbaPerformanceWarning)
+@pytest.mark.parametrize(
+    "seed", np.random.choice(np.arange(10000), size=2, replace=False)
+)
+@patch("stumpy.config.STUMPY_THREADS_PER_BLOCK", TEST_THREADS_PER_BLOCK)
+def test_random_gpu_ostinato(seed):
+    m = 50
+    np.random.seed(seed)
+    Ts = [np.random.rand(n) for n in [64, 128, 256]]
+
+    ref_radius, ref_Ts_idx, ref_subseq_idx = naive.ostinato(Ts, m)
+    comp_radius, comp_Ts_idx, comp_subseq_idx = gpu_ostinato(Ts, m)
+
+    npt.assert_almost_equal(ref_radius, comp_radius)
+    npt.assert_almost_equal(ref_Ts_idx, comp_Ts_idx)
+    npt.assert_almost_equal(ref_subseq_idx, comp_subseq_idx)
+
+
+@pytest.mark.filterwarnings("ignore", category=NumbaPerformanceWarning)
+@pytest.mark.parametrize("seed", [79, 109])
+@patch("stumpy.config.STUMPY_THREADS_PER_BLOCK", TEST_THREADS_PER_BLOCK)
+def test_deterministic_gpu_ostinato(seed):
+    m = 50
+    np.random.seed(seed)
+    Ts = [np.random.rand(n) for n in [64, 128, 256]]
+
+    ref_radius, ref_Ts_idx, ref_subseq_idx = naive.ostinato(Ts, m)
+    comp_radius, comp_Ts_idx, comp_subseq_idx = gpu_ostinato(Ts, m)
+
+    npt.assert_almost_equal(ref_radius, comp_radius)
+    npt.assert_almost_equal(ref_Ts_idx, comp_Ts_idx)
+    npt.assert_almost_equal(ref_subseq_idx, comp_subseq_idx)
+
+
+@pytest.mark.filterwarnings("ignore", category=NumbaPerformanceWarning)
+@pytest.mark.parametrize(
+    "seed", np.random.choice(np.arange(10000), size=25, replace=False)
+)
+@patch("stumpy.config.STUMPY_THREADS_PER_BLOCK", TEST_THREADS_PER_BLOCK)
+def test_random_gpu_ostinato_with_isconstant(seed):
+    isconstant_custom_func = functools.partial(
+        naive.isconstant_func_stddev_threshold, quantile_threshold=0.05
+    )
+
+    m = 50
+    np.random.seed(seed)
+    Ts = [np.random.rand(n) for n in [64, 128, 256]]
+    Ts_subseq_isconstant = [isconstant_custom_func for _ in range(len(Ts))]
+
+    ref_radius, ref_Ts_idx, ref_subseq_idx = naive.ostinato(
+        Ts, m, Ts_subseq_isconstant=Ts_subseq_isconstant
+    )
+    comp_radius, comp_Ts_idx, comp_subseq_idx = gpu_ostinato(
+        Ts, m, Ts_subseq_isconstant=Ts_subseq_isconstant
+    )
+
+    npt.assert_almost_equal(ref_radius, comp_radius)
+    npt.assert_almost_equal(ref_Ts_idx, comp_Ts_idx)
+    npt.assert_almost_equal(ref_subseq_idx, comp_subseq_idx)
+
+
+@pytest.mark.filterwarnings("ignore", category=NumbaPerformanceWarning)
+@pytest.mark.parametrize("seed", [79, 109, 112, 133, 151, 161, 251, 275, 309, 355])
+@patch("stumpy.config.STUMPY_THREADS_PER_BLOCK", TEST_THREADS_PER_BLOCK)
+def test_deterministic_gpu_ostinato_with_isconstant(seed):
+    isconstant_custom_func = functools.partial(
+        naive.isconstant_func_stddev_threshold, quantile_threshold=0.05
+    )
+
+    m = 50
+    np.random.seed(seed)
+    Ts = [np.random.rand(n) for n in [64, 128, 256]]
+
+    l = 64 - m + 1
+    subseq_isconsant = np.full(l, 0, dtype=bool)
+    subseq_isconsant[np.random.randint(0, l)] = True
+    Ts_subseq_isconstant = [
+        subseq_isconsant,
+        None,
+        isconstant_custom_func,
+    ]
+
+    ref_radius, ref_Ts_idx, ref_subseq_idx = naive.ostinato(
+        Ts, m, Ts_subseq_isconstant=Ts_subseq_isconstant
+    )
+    comp_radius, comp_Ts_idx, comp_subseq_idx = gpu_ostinato(
+        Ts, m, Ts_subseq_isconstant=Ts_subseq_isconstant
+    )
+
+    npt.assert_almost_equal(ref_radius, comp_radius)
+    npt.assert_almost_equal(ref_Ts_idx, comp_Ts_idx)
+    npt.assert_almost_equal(ref_subseq_idx, comp_subseq_idx)
