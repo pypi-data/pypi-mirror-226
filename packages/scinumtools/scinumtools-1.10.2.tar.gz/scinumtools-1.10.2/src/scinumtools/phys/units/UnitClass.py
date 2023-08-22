2fe93d6b7f48358845bf08183c35521db616f265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:07:56 +0000

    Add package scinumtools/1.10.2/scinumtools-1.10.2.tar.gz

---
+++ packages/scinumtools/scinumtools-1.10.2.tar.gz/scinumtools-1.10.2/src/scinumtools/phys/units/UnitClass.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from .UnitList import UnitStandard
+from .QuantityClass import Quantity
+
+class Unit:
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+        
+    def __new__(cls, unit=None):
+        if unit:
+            return Quantity(1,str(unit))
+        else:
+            return object.__new__(cls)
+    
+    def __getattr__(self, unit):
+        return Quantity(1,str(unit))
+
+class Constant:
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+    
+    def __new__(cls, unit=None):
+        if unit:
+            return Quantity(1,f"[{str(unit)}]")
+        else:
+            return object.__new__(cls)
+    
+    def __getattr__(self, unit):
+        return Quantity(1,f"[{str(unit)}]")
+
+class NaN:
+
+    def __new__(cls, unit=None):
+        if unit:
+            return Quantity(np.nan,str(unit))
+        else:
+            return Quantity(np.nan)
+        
