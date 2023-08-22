151d149b4ed56c7c5ffb743a887b2cbeb369d96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:08:11 +0000

    Add package sveetch-python-sample/0.4.0/sveetch-python-sample-0.4.0.tar.gz

---
+++ packages/sveetch-python-sample/sveetch-python-sample-0.4.0.tar.gz/sveetch-python-sample-0.4.0/sample/cli/version.py
@@ -0,0 +1,12 @@
+import click
+
+from sample import __version__
+
+
+@click.command()
+@click.pass_context
+def version_command(context):
+    """
+    Print out version information.
+    """
+    click.echo("sveetch-python-sample {}".format(__version__))
