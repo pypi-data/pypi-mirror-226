8479878b98d7693fa943c2af72846298478b0274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:00 +0000

    Add package autoblocksai/0.0.5/autoblocksai-0.0.5-py3-none-any.whl

---
+++ packages/autoblocksai/autoblocksai-0.0.5-py3-none-any.whl/autoblocks/_impl/api/client.py
@@ -0,0 +1,56 @@
+import logging
+from typing import List
+from typing import Optional
+
+import httpx
+
+from autoblocks._impl.api.models import Event
+from autoblocks._impl.api.models import GetTracesFromViewResponse
+from autoblocks._impl.api.models import Trace
+from autoblocks._impl.api.models import View
+from autoblocks._impl.config.constants import API_ENDPOINT
+
+log = logging.getLogger(__name__)
+
+
+class AutoblocksAPIClient:
+    def __init__(self, api_key: str) -> None:
+        self._client = httpx.Client(
+            base_url=API_ENDPOINT,
+            headers={"Authorization": f"Bearer {api_key}"},
+        )
+
+    def get_views(self) -> List[View]:
+        req = self._client.get("/views")
+        req.raise_for_status()
+        resp = req.json()
+        return [View(id=view["id"], name=view["name"]) for view in resp]
+
+    def get_traces_from_view(
+        self, view_id: str, *, page_size: int, cursor: Optional[str] = None
+    ) -> GetTracesFromViewResponse:
+        req = self._client.get(
+            f"/views/{view_id}/traces",
+            params={"pageSize": page_size, "cursor": cursor or ""},
+        )
+        req.raise_for_status()
+        resp = req.json()
+        return GetTracesFromViewResponse(
+            next_cursor=resp.get("nextCursor"),
+            traces=[
+                Trace(
+                    id=trace["id"],
+                    events=[
+                        Event(
+                            id=event["id"],
+                            trace_id=trace["id"],
+                            message=event["message"],
+                            timestamp=event["timestamp"],
+                            properties=event.get("properties") or {},
+                        )
+                        for event in trace["events"]
+                    ],
+                )
+                for trace in resp["traces"]
+            ],
+        )
---
+++ packages/autoblocksai/autoblocksai-0.0.5-py3-none-any.whl/autoblocks/_impl/api/models.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Dict
+from typing import List
+from typing import Optional
+
+
+@dataclass
+class Event:
+    id: str
+    trace_id: str
+    message: str
+    timestamp: str
+    properties: Dict
+
+
+@dataclass
+class Trace:
+    id: str
+    events: List[Event]
+
+
+@dataclass
+class View:
+    id: str
+    name: str
+
+
+@dataclass
+class GetTracesFromViewResponse:
+    next_cursor: Optional[str]
+    traces: List[Trace]
