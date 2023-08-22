e4a60261e13494920438c221f0fc372e91edcf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:07:56 +0000

    Add package nexi/1.0.3/nexi-1.0.3-py3-none-any.whl

---
+++ packages/nexi/nexi-1.0.3-py3-none-any.whl/nexi/__main__.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    from src.nexi.estimate_nexi_noiseless import estimate_nexi_noiseless
+    from src.nexi.estimate_nexi import estimate_nexi
+    from src.nexi.powderaverage.powderaverage import powder_average, save_data_as_npz, normalize_sigma
