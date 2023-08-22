7ee87464f64565d9efa5584fb78697c4923dc0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:08:51 +0000

    Add package stablefused/0.1.7/stablefused-0.1.7.tar.gz

---
+++ packages/stablefused/stablefused-0.1.7.tar.gz/stablefused-0.1.7/stablefused/diffusion/__init__.py
@@ -0,0 +1,5 @@
+from .base_diffusion import BaseDiffusion
+from .image_to_image_diffusion import ImageToImageDiffusion
+from .latent_walk_diffusion import LatentWalkDiffusion
+from .text_to_image_diffusion import TextToImageDiffusion
+from .text_to_video_diffusion import TextToVideoDiffusion
