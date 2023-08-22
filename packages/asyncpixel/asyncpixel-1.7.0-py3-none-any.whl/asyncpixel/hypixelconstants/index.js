f54583c800b21add33f4670494cfa68808a07732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:06:58 +0000

    Add package asyncpixel/1.7.0/asyncpixel-1.7.0-py3-none-any.whl

---
+++ packages/asyncpixel/asyncpixel-1.7.0-py3-none-any.whl/asyncpixel/hypixelconstants/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+achievements: require(__dirname + '/build/achievements.json'),
+achievements_extended: require(__dirname + '/build/achievements_extended.json'),
+challenges: require(__dirname + '/build/challenges.json'),
+game_types: require(__dirname + '/build/game_types.json'),
+guild_achievements: require(__dirname + '/build/guild_achievements.json'),
+languages: require(__dirname + '/build/languages.json'),
+maps: require(__dirname + '/build/maps.json'),
+modes: require(__dirname + '/build/modes.json'),
+pet_xp: require(__dirname + '/build/pet_xp.json'),
+quests: require(__dirname + '/build/quests.json'),
+skyblock_bazaar: require(__dirname + '/build/skyblock_bazaar.json'),
+skyblock_collections: require(__dirname + '/build/skyblock_collections.json'),
+skyblock_items: require(__dirname + '/build/skyblock_items.json'),
+skyblock_skills: require(__dirname + '/build/skyblock_skills.json')
+};
---
+++ packages/asyncpixel/asyncpixel-1.7.0-py3-none-any.whl/asyncpixel/models/utils.py
@@ -0,0 +1,44 @@
+"""Utils for pydantic models."""
+from typing import Union
+
+
+def safe_divide(dividend: Union[int, float], divisor: Union[int, float]) -> float:
+    """Return dividend / divisor without raising ZeroDivisionError.
+
+    Args:
+        dividend (int, float): the dividend (a in a/b)
+        divisor (int, float): the divisor (b in a/b)
+
+    Returns:
+        float: dividend / divisor, or floating point infinity if divisor == 0
+    """
+    if divisor == 0:
+        return float("inf") * (1 if dividend >= 0 else -1)
+    return dividend / divisor
+
+
+def to_camel(string: str) -> str:
+    """Convert str to camel case.
+
+    Args:
+        string (str): input.
+
+    Returns:
+        str: camelCase version of str.
+    """
+    out = string.split("_")[0]
+    for word in string.split("_")[1:]:
+        out += word.capitalize()
+    return out
+
+
+def to_upper(string: str) -> str:
+    """Convert str to upper case.
+
+    Args:
+        string (str): input.
+
+    Returns:
+        str: upperCase version of str.
+    """
+    return string.upper()
