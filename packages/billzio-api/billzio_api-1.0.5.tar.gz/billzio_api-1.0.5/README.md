725a42db1f2eaf5c8995f560ce48a8d62b5d88ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:22 +0000

    Add package billzio-api/1.0.5/billzio_api-1.0.5.tar.gz

---
+++ packages/billzio-api/billzio_api-1.0.5.tar.gz/billzio_api-1.0.5/README.md
@@ -0,0 +1,52 @@
+# billzio-api
+Asynchronous Python wrapper for [Billz.io](https://billz.io) Public API (v2)
+
+## Installation
+`pip install billzio-api`
+
+## Supported Billz API methods
+- [x] Auth login
+- [x] Get Products list
+- [x] Get Categories list
+- [x] Get Shops list
+- [x] Get Currencies list
+- [x] Get Payment types list
+- [x] Get Brands list
+- [x] Get all Clients
+- [ ] Create a new Client
+- [ ] Update a Client
+- [ ] Create a draft Order
+- [ ] Add an Item (product) to the draft Order
+- [ ] Add a Consumer to the draft Order
+- [ ] Create an Order from the draft Order (make payment)
+
+## Usage:
+```python
+from billzio_api import BillzHandler, ShopsListFilters
+
+handler = BillzHandler("<secret_key>")
+filters = ShopsListFilters(limit=1)
+shops = handler.get_shops(filters)
+print(shops.count)
+print(shops.shops)
+```
+
+### Asynchronous
+```python 
+import asyncio  # for running synchronously
+
+from billzio_api import AsyncBillzHandler, ShopsListFilters
+
+...
+handler = AsyncBillzHandler("<secret_key>")
+filters = ShopsListFilters(limit=1)
+shops = asyncio.run(handler.get_shops(filters))
+print(shops.count)
+print(shops.shops)
+```
+
+## TODO
+- [x] Upload to PyPi as a python package
+- [x] Synchronous handler
+- [ ] Write unit tests
+- [ ] Caching auth data with its expiration
---
+++ packages/billzio-api/billzio_api-1.0.5.tar.gz/billzio_api-1.0.5/billzio_api/handlers/handler.py
@@ -0,0 +1,80 @@
+from typing import Optional
+
+from httpx import AsyncClient, Client
+from .base import BaseBillzHandler
+from ..exceptions import *
+from ..models.brands import BrandsListData, BrandsListFilters
+from ..models.categories import CategoriesListData, CategoriesListFilters
+from ..models.clients import ClientsListFilters, ClientsListData
+from ..models.currencies import CurrenciesListData
+from ..models.payment_types import PaymentTypesListData
+from ..models.products import ProductsListFilters, ProductListData
+from ..models.shops import ShopsListFilters, ShopsListData
+
+
+class BillzHandler(BaseBillzHandler):
+    def __init__(self, secret_token: str):
+        self.http_client = Client()
+        super().__init__(secret_token)
+
+    def _auth(self):
+        if self._auth_data is None:
+            resp = self.http_client.post(self._auth_route(),
+                                         json={"secret_token": self._secret_token})
+            json_resp: dict = resp.json()
+            if resp.status_code == 200 and json_resp.get("code") == 200 and json_resp.get("data"):
+                self._set_auth_data(resp.json()["data"])
+            else:
+                raise AuthLoginError
+
+    def get_products(self, filters: Optional[ProductsListFilters]) -> ProductListData:
+        self._auth()
+        request_params = filters.model_dump(exclude_unset=True, exclude_none=True) if filters is not None else None
+        resp = self.http_client.get(self._products_route(),
+                                    params=request_params,
+                                    headers=self._request_auth_headers())
+        return self._resp_to_model(resp, ProductListData)
+
+    def get_categories(self, filters: Optional[CategoriesListFilters]) -> CategoriesListData:
+        self._auth()
+        request_params = filters.model_dump(exclude_unset=True, exclude_none=True) if filters is not None else None
+        resp = self.http_client.get(self._categories_route(),
+                                    params=request_params,
+                                    headers=self._request_auth_headers())
+        return self._resp_to_model(resp, CategoriesListData)
+
+    def get_shops(self, filters: Optional[ShopsListFilters]) -> ShopsListData:
+        self._auth()
+        request_params = filters.model_dump(exclude_unset=True, exclude_none=True) if filters is not None else None
+        resp = self.http_client.get(self._shops_route(),
+                                    params=request_params,
+                                    headers=self._request_auth_headers())
+        return self._resp_to_model(resp, ShopsListData)
+
+    def get_currencies(self) -> CurrenciesListData:
+        self._auth()
+        resp = self.http_client.get(self._currencies_route(),
+                                    headers=self._request_auth_headers())
+        return self._resp_to_model(resp, CurrenciesListData)
+
+    def get_payment_types(self) -> PaymentTypesListData:
+        self._auth()
+        resp = self.http_client.get(self._payment_types_route(),
+                                    headers=self._request_auth_headers())
+        return self._resp_to_model(resp, PaymentTypesListData)
+
+    def get_brands(self, filters: Optional[BrandsListFilters]) -> BrandsListData:
+        self._auth()
+        request_params = filters.model_dump(exclude_unset=True, exclude_none=True) if filters is not None else None
+        resp = self.http_client.get(self._brands_list_route(),
+                                    params=request_params,
+                                    headers=self._request_auth_headers())
+        return self._resp_to_model(resp, BrandsListData)
+
+    def get_clients(self, filters: Optional[ClientsListFilters]) -> ClientsListData:
+        self._auth()
+        request_params = filters.model_dump(exclude_unset=True, exclude_none=True) if filters is not None else None
+        resp = self.http_client.get(self._clients_list_create_route(),
+                                    params=request_params,
+                                    headers=self._request_auth_headers())
+        return self._resp_to_model(resp, ClientsListData)
