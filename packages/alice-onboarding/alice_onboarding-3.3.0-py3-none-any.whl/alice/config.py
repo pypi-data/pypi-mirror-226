31c13d6a2180b0f03040bbbee7aadef024a939c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:10:39 +0000

    Add package alice-onboarding/3.3.0/alice_onboarding-3.3.0-py3-none-any.whl

---
+++ packages/alice-onboarding/alice_onboarding-3.3.0-py3-none-any.whl/alice/config.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from pydantic import BaseModel, Field
+from requests import Session
+
+
+class Config(BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+
+    onboarding_url: str = Field(default="https://apis.alicebiometrics.com/onboarding")
+    sandbox_url: str = Field(
+        default="https://apis.alicebiometrics.com/onboarding/sandbox"
+    )
+    api_key: Union[str, None] = Field(default=None)
+    sandbox_token: Union[str, None] = Field(default=None)
+    timeout: Union[float, None] = Field(
+        default=None, description="Timeout for every request in seconds", ge=0, le=100
+    )
+    send_agent: bool = Field(default=True)
+    verbose: bool = Field(default=False)
+    session: Union[Session, None] = Field(default=None)
