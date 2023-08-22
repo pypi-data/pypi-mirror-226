610fac1f330bb5aeb8f772f1f56a456b1c61e83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:55 +0000

    Add package ert/6.0.0rc2/ert-6.0.0rc2-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-6.0.0rc2-cp38-cp38-macosx_10_15_x86_64.whl/ert/config/parsing/config_errors.py
@@ -0,0 +1,78 @@
+from typing import Callable, List, Optional, Sequence, Union
+
+from .error_info import ErrorInfo, WarningInfo
+
+
+class ConfigWarning(UserWarning):
+    info: WarningInfo
+
+    def __init__(self, info: Union[str, WarningInfo]):
+        if isinstance(info, str):
+            super().__init__(info)
+            self.info = WarningInfo(message=info)
+        else:
+            super().__init__(info.message)
+            self.info = info
+
+    def __str__(self) -> str:
+        return str(self.info)
+
+
+class ConfigValidationError(ValueError):
+    def __init__(
+        self,
+        errors: Union[str, List[ErrorInfo]],
+        config_file: Optional[str] = None,
+    ) -> None:
+        self.errors: List[ErrorInfo] = []
+        if isinstance(errors, list):
+            for err in errors:
+                if isinstance(err, ErrorInfo):
+                    self.errors.append(err)
+        else:
+            self.errors.append(ErrorInfo(message=errors, filename=config_file))
+        super().__init__(
+            ";".join([self.get_value_error_message(error) for error in self.errors])
+        )
+
+    @classmethod
+    def from_info(cls, info: ErrorInfo) -> "ConfigValidationError":
+        return cls([info])
+
+    @classmethod
+    def get_value_error_message(cls, info: ErrorInfo) -> str:
+        """
+        :returns: The error message as used by cls as a ValueError.
+        Can be overridden.
+        """
+        return (
+            (
+                f"Parsing config file `{info.filename}` "
+                f"resulted in the following errors: {info.message}"
+            )
+            if info.filename is not None
+            else info.message
+        )
+
+    def get_cli_message(self) -> str:
+        return "\n".join(self.get_error_messages())
+
+    def get_error_messages(
+        self, formatter: Callable[[ErrorInfo], str] = str
+    ) -> List[str]:
+        return [formatter(info) for info in sorted(self.errors)]
+
+    @classmethod
+    def from_collected(
+        cls, errors: Sequence[Union[ErrorInfo, "ConfigValidationError"]]
+    ) -> "ConfigValidationError":
+        # Turn into list of only ConfigValidationErrors
+        all_error_infos = []
+
+        for e in errors:
+            if isinstance(e, ConfigValidationError):
+                all_error_infos += e.errors
+            else:
+                all_error_infos.append(e)
+
+        return cls(all_error_infos)
