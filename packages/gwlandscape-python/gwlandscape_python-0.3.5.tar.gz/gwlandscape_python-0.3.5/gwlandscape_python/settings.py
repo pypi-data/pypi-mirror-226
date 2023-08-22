78e3bf30fcb347de0ca2c54a6619e16ccae46df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:08:46 +0000

    Add package gwlandscape-python/0.3.5/gwlandscape_python-0.3.5.tar.gz

---
+++ packages/gwlandscape-python/gwlandscape_python-0.3.5.tar.gz/gwlandscape_python-0.3.5/gwlandscape_python/settings.py
@@ -0,0 +1,5 @@
+from gwdc_python.settings import GWDC_FILE_DOWNLOAD_ENDPOINT
+
+GWLANDSCAPE_ENDPOINT = 'https://gwlandscape.org.au/compas/graphql'
+GWLANDSCAPE_AUTH_ENDPOINT = 'https://gwlandscape.org.au/auth/graphql'
+GWLANDSCAPE_FILE_DOWNLOAD_ENDPOINT = GWDC_FILE_DOWNLOAD_ENDPOINT
