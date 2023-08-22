073f5aea6456db1f9a4d7a54c3100a5501ddc1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:33 +0000

    Add package bewe/0.1.1b0/bewe-0.1.1b0.tar.gz

---
+++ packages/bewe/bewe-0.1.1b0.tar.gz/bewe-0.1.1b0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bewe',
+    version='0.1.1.b',
+    description='The package implements some investment strategies.',
+    long_description='Bewe.',
+    author='Berlin Hsin',
+    author_email='berlin.hsin@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
