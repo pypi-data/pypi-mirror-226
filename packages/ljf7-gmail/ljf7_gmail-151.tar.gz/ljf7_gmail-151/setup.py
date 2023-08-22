fa8765db8253021858b9e247b8c4133ebac796e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:08:45 +0000

    Add package ljf7-gmail/151/ljf7_gmail-151.tar.gz

---
+++ packages/ljf7-gmail/ljf7_gmail-151.tar.gz/ljf7_gmail-151/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "ljf7_gmail",
+    version = "151",
+    description = "To solve fib by recursion",
+    author = "Linus Trovalds",
+    py_modules = ["gmail.fib", "gmail.email"],
+    long_description = "I am Andy Woz, it is nice to meet pypi.org. I can write scripts for myself and other XD"
+)
