2fd847fa0dac19ea519a10040b11cb965eabc70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 16:10:37 +0000

    Add package solacalc/0.0.0/solacalc-0.0.0.tar.gz

---
+++ packages/solacalc/solacalc-0.0.0.tar.gz/solacalc-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name= 'solacalc' ,
+    author= 'Asala Hussien' ,
+    description= 'simple calc library' ,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
