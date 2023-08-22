b9941ea1719a9f16cbbe967e053343bee82d60a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 16:10:40 +0000

    Add package bowtie-json-schema/2023.8.11/bowtie_json_schema-2023.8.11.tar.gz

---
+++ packages/bowtie-json-schema/bowtie_json_schema-2023.8.11.tar.gz/bowtie_json_schema-2023.8.11/frontend/src/components/CopyToClipboard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Check, Clipboard } from "react-bootstrap-icons";
+
+function CopyToClipboard({ textToCopy }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(textToCopy);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  return (
+    <button
+      type="button"
+      className="btn mt-0 me-0"
+      onClick={handleClick}
+      aria-label="Copy to clipboard"
+      data-bs-toggle="tooltip"
+      data-bs-placement="top"
+      data-bs-custom-class="custom-tooltip"
+      data-bs-title="Copy to clipboard"
+    >
+      {copied ? <Check /> : <Clipboard />}
+    </button>
+  );
+}
+
+export default CopyToClipboard;
