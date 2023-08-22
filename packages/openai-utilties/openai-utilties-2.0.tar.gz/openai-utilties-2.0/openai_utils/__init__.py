6162b0b03f191b51fdd19f87ae64d05b107afd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 15:09:57 +0000

    Add package openai-utilties/2.0/openai-utilties-2.0.tar.gz

---
+++ packages/openai-utilties/openai-utilties-2.0.tar.gz/openai-utilties-2.0/openai_utils/__init__.py
@@ -0,0 +1,34 @@
+import openai
+import numpy as np
+from openai.embeddings_utils import cosine_similarity
+
+def AskGPT(model: str, Context, temperature: float = 1) -> str:
+    if type(Context) == list:
+
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=Context,
+            temperature=temperature
+        )
+        return response["choices"][0]["message"]["content"]
+
+    elif type(Context) == str:
+
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages={"role": "user", "content": Context},
+            temperature=temperature
+        )
+        return response["choices"][0]["message"]["content"]
+
+
+def CreateEmbedding(String: str) -> list:
+    data = openai.Embedding.create(
+        model="text-embedding-ada-002",
+        input=String
+    )
+    return data["data"][0]["embedding"]
+
+
+def GetEmbeddingDistance(Embedding1: list, Embedding2: list) -> float:
+    cosine_similarity(Embedding1, Embedding2)
