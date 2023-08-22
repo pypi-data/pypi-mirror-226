bb6925aa0c5efd909a4d834a466ad56cd286b09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 16:10:04 +0000

    Add package SMAdiffz/1.0/SMAdiffz-1.0.tar.gz

---
+++ packages/SMAdiffz/SMAdiffz-1.0.tar.gz/SMAdiffz-1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '1.0'
+DESCRIPTION = 'Diffusivity analytics'
+LONG_DESCRIPTION = 'A package that allows to work with information diffusion'
+
+# Setting up
+setup(
+    name="SMAdiffz",
+    version=VERSION,
+    author="H.M.M.Caldera",
+    author_email="<maneeshac2020@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description='This package is for social media informaiton diffusion analysis',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy'],
+    keywords=['python', 'social media'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
