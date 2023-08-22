bb62d8dcac35c53d8911e8b149de40be45b5ac86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 14:07:13 +0000

    Add package simplecalculatorpython/0.0.1/simplecalculatorpython-0.0.1.tar.gz

---
+++ packages/simplecalculatorpython/simplecalculatorpython-0.0.1.tar.gz/simplecalculatorpython-0.0.1/README.txt
@@ -0,0 +1,1 @@
+A very basic calculator that can perform addition, subtraction, multiplication, and division operations between two numbers.
---
+++ packages/simplecalculatorpython/simplecalculatorpython-0.0.1.tar.gz/simplecalculatorpython-0.0.1/simplecalculatorpython/__init__.py
@@ -0,0 +1,13 @@
+def add(number1, number2):
+    return number1 + number2
+
+def subtract(number1, number2):
+    return number1 - number2
+
+def multiply(number1, number2):
+    return number1 * number2
+
+def divide(number1, number2):
+    return number1 / number2
+
+print(add(10,9))
