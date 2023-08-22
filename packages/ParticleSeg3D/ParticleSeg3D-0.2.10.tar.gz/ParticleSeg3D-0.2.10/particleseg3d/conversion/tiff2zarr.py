62c2cbc22cd262af39f2afd34c196b336949b6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:02 +0000

    Add package ParticleSeg3D/0.2.10/ParticleSeg3D-0.2.10.tar.gz

---
+++ packages/ParticleSeg3D/ParticleSeg3D-0.2.10.tar.gz/ParticleSeg3D-0.2.10/particleseg3d/conversion/tiff2zarr.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+from particleseg3d.utils import utils
+import tifffile
+from tqdm import tqdm
+from os.path import join
+import zarr
+
+
+def tiff2zarr(load_dir: str, save_dir: str) -> None:
+    """
+    Converts a set of TIFF image slices to a Zarr image.
+
+    Args:
+        load_dir (str): Path to the TIFF files.
+        save_dir (str): Path to where the Zarr image should be saved.
+    """
+    name = os.path.basename(os.path.normpath(load_dir))
+    filepaths = utils.load_filepaths(load_dir, extension=["tif", "tiff", "TIF", "TIFF"])
+    image_shape = tifffile.imread(filepaths[0]).shape
+    image_shape = (len(filepaths), *image_shape)
+    image_zarr = zarr.open(join(save_dir, name + ".zarr"), shape=image_shape, mode='w')
+    for i, filepath in enumerate(tqdm(filepaths)):
+        image_slice = tifffile.imread(filepath)    
+        image_zarr[i] = image_slice
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', "--input", required=True,
+                        help="Absolute input path to the folder that contains the TIFF image slices that should be converted to a Zarr image.")
+    parser.add_argument('-o', "--output", required=True, help="Absolute output path to the folder that should be used to save the Zarr image.")
+    args = parser.parse_args()
+
+    tiff2zarr(args.input, args.output)
+
+
+if __name__ == '__main__':
+    main()
