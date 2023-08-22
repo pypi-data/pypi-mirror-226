1774d68fd8c05031293c4b6ec3a05e8439f6383a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:33 +0000

    Add package egenix-telegram-antispam-bot/0.5.0/egenix_telegram_antispam_bot-0.5.0-py3-none-any.whl

---
+++ packages/egenix-telegram-antispam-bot/egenix_telegram_antispam_bot-0.5.0-py3-none-any.whl/telegram_antispam_bot/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" eGenix Antispam Bot for Telegram Package
+
+    Written by Marc-Andre Lemburg in 2022.
+    Copyright (c) 2022, eGenix.com Software GmbH; mailto:info@egenix.com
+    License: MIT
+"""
+### Globals
+
+# Package version
+#__version__ = '0.6.0.dev1'
+__version__ = '0.5.0'
