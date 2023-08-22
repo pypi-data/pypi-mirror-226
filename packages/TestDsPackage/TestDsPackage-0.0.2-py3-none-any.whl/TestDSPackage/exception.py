907756ec219918663450077c0082a249323ef66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:32 +0000

    Add package TestDsPackage/0.0.2/TestDsPackage-0.0.2-py3-none-any.whl

---
+++ packages/TestDsPackage/TestDsPackage-0.0.2-py3-none-any.whl/TestDSPackage/exception.py
@@ -0,0 +1,17 @@
+import sys
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    lineno = exc_tb.tb_lineno
+    error_message = f"Error occurred in python script\nname: {filename}\nLine number: {lineno}\nError message: {error}"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self) -> str:
+        return self.error_message
