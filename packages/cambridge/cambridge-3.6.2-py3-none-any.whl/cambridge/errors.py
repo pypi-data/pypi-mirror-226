03b365de3c05dabea77d74384aa4c3eea701e1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:50 +0000

    Add package cambridge/3.6.2/cambridge-3.6.2-py3-none-any.whl

---
+++ packages/cambridge/cambridge-3.6.2-py3-none-any.whl/cambridge/errors.py
@@ -0,0 +1,36 @@
+"""
+This script sets up self-defined errors.
+"""
+
+import sys
+
+from .log import logger
+
+
+class ParsedNoneError(Exception):
+    """Used when bs4 parsing returned None, when there's document fetched, which may not be the right document for the word."""
+
+    def __init__(self, response_url):
+        self.message = "The word isn't in the Cambridge Dictionary yet; OR the fetched html is not complete because of your temporary slow network. Try again. \nOr see " + response_url 
+
+    def __str__(self):
+        return self.message
+
+
+class NoResultError(Exception):
+    """Used when bs4 returned None because Cambridge dict has no result"""
+
+    def __init__(self):
+        self.message = "No result found in Cambridge Dictionary"
+
+    def __str__(self):
+        return self.message
+
+
+def call_on_error(error, url, attempt, op):
+    attempt += 1
+    logger.debug(f"{op} {url} {attempt} times")
+    if attempt == 3:
+        print(f"Maximum {op} reached: {error}")
+        sys.exit()
+    return attempt
---
+++ packages/cambridge/cambridge-3.6.2-py3-none-any.whl/cambridge/settings.py
@@ -0,0 +1,19 @@
+"""Script to store static data."""
+
+VERSION = "3.6.2"
+
+OP = [
+    "FETCHING",
+    "PARSING",
+    "RETRY FETCHING",
+    "RETRY PARSING",
+    "PRINTING",
+    "FOUND",
+    "NOT FOUND",
+    "CACHED",
+    "CANCELLED",
+    "DELETED",
+]
+
+DICTS = ["CAMBRIDGE", "MERRIAM WEBSTER"]
+
