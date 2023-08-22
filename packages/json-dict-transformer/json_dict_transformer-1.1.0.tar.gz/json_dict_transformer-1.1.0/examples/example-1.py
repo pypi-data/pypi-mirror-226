acb37c3074c331bb81ac39fb4d049fab57e159c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:10:50 +0000

    Add package json-dict-transformer/1.1.0/json_dict_transformer-1.1.0.tar.gz

---
+++ packages/json-dict-transformer/json_dict_transformer-1.1.0.tar.gz/json_dict_transformer-1.1.0/examples/example-1.py
@@ -0,0 +1,50 @@
+# Add parent directory to path to be able to import json_transformer
+import sys
+sys.path.append('../')
+# The above part can be omitted if json_transformer is installed as a package
+
+
+from json_dict_transformer import translateDictToDict
+
+
+def upperLetters(input: str) -> str:
+    return input.upper()
+
+input_dict = {
+    "name": "John",
+    "surname": "Doe",
+    "age": 30,
+    "address": {
+        "street": "Main Street",
+        "number": 123,
+        "city": "New York",
+        "country": "USA"
+    },
+    "hobbies": [
+        "football",
+        "basketball",
+        "tennis"
+    ]
+}
+
+schema_dict = {
+    "full name": ["json::name", "txt:: ", "json::surname"],
+    "age": ["json::age"],
+    "address": [
+        "json::address.street",
+        "txt::,",
+        "json::address.number",
+        "txt::,",
+        "json::address.city",
+        "txt::,",
+        "json::address.country"
+    ],
+    "hobbies": [
+        "txt::My hobbies are: ",
+        upperLetters, # this is function 
+        "json::hobbies",
+    ]
+}
+
+output_dict = translateDictToDict(input_dict, schema_dict)
+print(output_dict)
