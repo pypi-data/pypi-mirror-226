14ace8c31dacf17a5ef57998f1a27a65eaacddc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:09:39 +0000

    Add package mediatech-cag-library/0.0.1/mediatech-cag-library-0.0.1.tar.gz

---
+++ packages/mediatech-cag-library/mediatech-cag-library-0.0.1.tar.gz/mediatech-cag-library-0.0.1/README.md
@@ -0,0 +1,1 @@
+# cag_library
---
+++ packages/mediatech-cag-library/mediatech-cag-library-0.0.1.tar.gz/mediatech-cag-library-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mediatech-cag-library',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Зависимости вашей библиотеки
+    ],
+    author='Semyon Shilovskiy',
+    description='A library for working with REST API of CAG',
+    url='https://github.com/Dragon4231/cag_library',
+)
