f85905add2062893ae4d328008c82245048005e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 05:08:16 +0000

    Add package pycrossva/0.95/pycrossva-0.95.tar.gz

---
+++ packages/pycrossva/pycrossva-0.95.tar.gz/pycrossva-0.95/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(name="pycrossva",
+      version="0.95",
+      description="prepare data from WHO and PHRMC instruments for verbal autopsy algorithms",
+      url="https://github.com/verbal-autopsy-software/pyCrossVA",
+      license="GNU General Public License v3.0",
+      packages=[
+          "pycrossva",
+          "pycrossva.resources.mapping_configuration_files",
+          "pycrossva.resources.sample_data",
+          "pycrossva.scripts",
+      ],
+      entry_points='''
+                    [console_scripts]
+                    pycrossva-transform=pycrossva.scripts.pycrossva_transform:main
+                    ''',
+      # include_package_data=True,
+      # package_data={
+      #     "pycrossva": ["resources/mapping/*.csv",
+      #                   "resources/sample_data/*.csv"],
+      # },
+      keywords="verbal autopsy data preparation",
+      install_requires=["numpy", "pandas", "Click"],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3.11",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: POSIX :: Linux",
+      ],
+      zip_safe=False,
+)
