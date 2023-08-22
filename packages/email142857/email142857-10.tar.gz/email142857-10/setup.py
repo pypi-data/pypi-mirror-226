60ac0c1a7e2b248b0e6b52e1de4b36c050af5087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 03:08:58 +0000

    Add package email142857/10/email142857-10.tar.gz

---
+++ packages/email142857/email142857-10.tar.gz/email142857-10/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "email142857",
+    version = "10",
+    description = "To solve fib by recursion",
+    author = "Linus Trovalds",
+    py_modules = ["ljf7.fib", "ljf7.email"]
+)
