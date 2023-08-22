cb66ac260254bb5ab391fe6ebcbdba3e5574e515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-22 04:10:17 +0000

    Add package solrstice/0.3.0/solrstice-0.3.0.tar.gz

---
+++ packages/solrstice/solrstice-0.3.0.tar.gz/solrstice-0.3.0/local_dependencies/solrstice/src/queries/mod.rs
@@ -0,0 +1,18 @@
+//! The different query types this library supports.
+
+/// Alias API
+pub mod alias;
+/// Collection API
+pub mod collection;
+/// Components for select queries
+pub mod components;
+/// Config API
+pub mod config;
+/// Def types for select queries. Eg: `luscene`, `edismax`
+pub mod def_type;
+/// Helper functions
+pub mod helpers;
+/// Index and Delete API
+pub mod index;
+/// Select query API
+pub mod select;
