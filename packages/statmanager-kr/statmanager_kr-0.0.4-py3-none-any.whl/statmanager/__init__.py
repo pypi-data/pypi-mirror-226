be7d27637bd79c0e18486ffad72c68797b082d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:21 +0000

    Add package statmanager-kr/0.0.4/statmanager_kr-0.0.4-py3-none-any.whl

---
+++ packages/statmanager-kr/statmanager_kr-0.0.4-py3-none-any.whl/statmanager/__init__.py
@@ -0,0 +1,4 @@
+from manager import Stat_Manager
+
+__version__ = '0.0.4'
+__all__ = ['statmanager']
---
+++ packages/statmanager-kr/statmanager_kr-0.0.4-py3-none-any.whl/statmanager/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='statmanager-kr',
+    version='0.0.4',
+    description='사회과학의 맥락에서 통계를 손쉽게 사용할 수 있는 패키지 by Changseok Lee',
+    author='ckdckd145',
+    author_email='ckdckd145@gmail.com',
+    url='',
+    install_requires=['pandas', 'scipy', 'statsmodels'],
+    packages=find_packages(),
+    py_modules=['statmanager'] ,
+    keywords=['statistic', 'socialscience', 'stats',],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
