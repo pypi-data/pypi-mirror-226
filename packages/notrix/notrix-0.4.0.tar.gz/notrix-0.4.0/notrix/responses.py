479c26a5b184fc5fa7d0f0fe58d538ec5646df7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:06:36 +0000

    Add package notrix/0.4.0/notrix-0.4.0.tar.gz

---
+++ packages/notrix/notrix-0.4.0.tar.gz/notrix-0.4.0/notrix/responses.py
@@ -0,0 +1,9 @@
+class PaymentPageResponse:
+    def __init__(self, uuid: str, price: float, webhook_url: str, link: str):
+        self.uuid = uuid
+        self.price = price
+        self.webhook_url = webhook_url
+        self._link = link
+
+    def link(self, user_id: str) -> str:
+        return f"{self._link}?user_id={user_id}"
