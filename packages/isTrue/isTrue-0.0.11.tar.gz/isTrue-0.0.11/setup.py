1f81d6739162ece7183dcd47291fa7f59511cc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:10:23 +0000

    Add package isTrue/0.0.11/isTrue-0.0.11.tar.gz

---
+++ packages/isTrue/isTrue-0.0.11.tar.gz/isTrue-0.0.11/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import pkgutil
+import sys
+
+install_requires = []
+
+if sys.version_info < (3, 7):
+    install_requires.append('pyside2')
+else:
+    if not pkgutil.find_loader('pyside2'):
+        install_requires.append('pyside6')
+
+setuptools.setup(
+    name="isTrue",
+    version="0.0.11",
+    description="IDK???",
+    keywords="IDK???",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.4',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "License :: Free To Use But Restricted",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+
+    ],
+    install_requires=install_requires
+)
