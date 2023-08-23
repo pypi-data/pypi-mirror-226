66e7176c1229e472a8b4e1bb0e5b3292081b2c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:59 +0000

    Add package xclass-sdk/1.7.4/xclass_sdk-1.7.4.tar.gz

---
+++ packages/xclass-sdk/xclass_sdk-1.7.4.tar.gz/xclass_sdk-1.7.4/src/xclass_sdk/xclass_helpers.py
@@ -0,0 +1,21 @@
+def multipartify(data, parent_key=None, formatter: callable = None) -> dict:
+    if formatter is None:
+        formatter = lambda v: (None, v)  # Multipart representation of value
+
+    if type(data) is not dict:
+        return {parent_key: formatter(data)}
+
+    converted = []
+
+    for key, value in data.items():
+        current_key = key if parent_key is None else f"{parent_key}[{key}]"
+        if type(value) is dict:
+            converted.extend(multipartify(value, current_key, formatter).items())
+        elif type(value) is list:
+            for ind, list_value in enumerate(value):
+                iter_key = f"{current_key}[{ind}]"
+                converted.extend(multipartify(list_value, iter_key, formatter).items())
+        else:
+            converted.append((current_key, formatter(value)))
+
+    return dict(converted)
