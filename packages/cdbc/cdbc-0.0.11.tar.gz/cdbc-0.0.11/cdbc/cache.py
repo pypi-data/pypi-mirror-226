fdbe5807f4ae355f445bbbcde12fa709e539533d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:22 +0000

    Add package cdbc/0.0.11/cdbc-0.0.11.tar.gz

---
+++ packages/cdbc/cdbc-0.0.11.tar.gz/cdbc-0.0.11/cdbc/cache.py
@@ -0,0 +1,17 @@
+def redis_connection_pool(redis_host: str, redis_port: int, redis_db: int):
+    _pool = redis.ConnectionPool(
+        host = redis_host,
+        port = redis_port,
+        db = redis_db,
+        decode_responses = True
+    )
+    return redis.Redis(connection_pool=_pool)
+
+
+def redis_get_kvs(pool):
+    _kvs = dict()
+    _pool = pool
+    _keys = _pool.keys()
+    for _k in _keys:
+        _kvs[_k] = json.loads(_pool.get(_k))
+    return _kvs
