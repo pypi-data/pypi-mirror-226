a91ad9ccf1686c4c041bc9a52535b0649e78bea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:35 +0000

    Add package edc-visit-tracking/0.3.59/edc-visit-tracking-0.3.59.tar.gz

---
+++ packages/edc-visit-tracking/edc-visit-tracking-0.3.59.tar.gz/edc-visit-tracking-0.3.59/edc_visit_tracking/models/__init__.py
@@ -0,0 +1,5 @@
+from .signals import visit_tracking_check_in_progress_on_post_save
+from .subject_visit import SubjectVisit
+from .subject_visit_missed import SubjectVisitMissed
+from .subject_visit_missed_reasons import SubjectVisitMissedReasons
+from .visit_reasons import VisitReasons
---
+++ packages/edc-visit-tracking/edc-visit-tracking-0.3.59.tar.gz/edc-visit-tracking-0.3.59/edc_visit_tracking/models/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, weak=False, dispatch_uid="visit_tracking_check_in_progress_on_post_save")
+def visit_tracking_check_in_progress_on_post_save(
+    sender, instance, raw, created, using, update_fields, **kwargs  # noqa
+):
+    """Calls method on the visit tracking instance"""
+    if not raw and not update_fields:
+        try:
+            instance.appointment
+        except AttributeError:
+            pass
+        else:
+            try:
+                instance.check_appointment_in_progress()
+            except AttributeError as e:
+                if "check_appointment_in_progress" not in str(e):
+                    raise
