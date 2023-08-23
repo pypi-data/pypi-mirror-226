2f25396c9e16f5dd42c4f59611ee5c1cbe0dd10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:33 +0000

    Add package rhkpy/1.0.2/rhkpy-1.0.2.tar.gz

---
+++ packages/rhkpy/rhkpy-1.0.2.tar.gz/rhkpy-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "rhkpy",
+    version = "1.0.2",
+    packages = find_packages(),
+    # python_requires = '>=3.10',
+    install_requires = [
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "xarray>=2023.7.0",
+        "hvplot>=0.8.4",
+        "panel>=1.2.1"
+    ],
+    description = "A python package for processing Scanning Tunneling Microscopy (STM) data from RHK, based on the spym project.",
+    long_description = open('README.md', 'r').read(),
+    long_description_content_type = "text/markdown",
+    author = "Peter Nemes-Incze",
+    url = "https://github.com/zrbyte/rhkpy",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+)
