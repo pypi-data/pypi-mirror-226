eff0430ad02f02b1975bb94c627f2f0c797b116b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:53 +0000

    Add package lenio-ai-prompt-engineer/1.4.8/lenio-ai-prompt-engineer-1.4.8.tar.gz

---
+++ packages/lenio-ai-prompt-engineer/lenio-ai-prompt-engineer-1.4.8.tar.gz/lenio-ai-prompt-engineer-1.4.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='lenio-ai-prompt-engineer',
+    version='1.4.8',
+    packages=['cli', 'cli.evals', 'cli.cost', 'cli.approximate_cost', 'cli.validation_yaml'],
+    python_requires='>=3.7',
+    install_requires=[
+        'openai>=0.27.0',
+        'PyYAML>=5.4',
+        'matplotlib',
+        'numpy',
+        'marshmallow',
+        'tiktoken>=0.4.0',
+        'tenacity'
+    ],
+    entry_points={
+        'console_scripts': [
+            'lenio-ai-prompt-engineer=cli.main:main',
+        ],
+    },
+)
