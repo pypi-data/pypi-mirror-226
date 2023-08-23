f97a5c6e6095160e2f19076e47250ea5b0dc4e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:23 +0000

    Add package ephios/0.15.10/ephios-0.15.10.tar.gz

---
+++ packages/ephios/ephios-0.15.10.tar.gz/ephios-0.15.10/ephios/extra/permissions.py
@@ -0,0 +1,102 @@
+from functools import wraps
+
+from django.contrib.auth.models import Group, Permission
+from django.core.exceptions import PermissionDenied
+from django.forms import BooleanField
+from guardian.ctypes import get_content_type
+from guardian.shortcuts import assign_perm, remove_perm
+from guardian.utils import get_group_obj_perms_model
+
+
+def staff_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_staff:
+            return view_func(request, *args, **kwargs)
+        if not request.user.is_authenticated:
+            from django.contrib.auth.views import redirect_to_login
+
+            return redirect_to_login(request.get_full_path())
+        raise PermissionDenied
+
+    return _wrapped_view
+
+
+def get_groups_with_perms(obj, only_with_perms_in):
+    ctype = get_content_type(obj)
+    group_model = get_group_obj_perms_model(obj)
+
+    group_rel_name = group_model.group.field.related_query_name()
+
+    if group_model.objects.is_generic():
+        group_filters = {
+            f"{group_rel_name}__content_type": ctype,
+            f"{group_rel_name}__object_pk": obj.pk,
+        }
+    else:
+        group_filters = {f"{group_rel_name}__content_object": obj}
+
+    permission_ids = Permission.objects.filter(
+        content_type=ctype, codename__in=only_with_perms_in
+    ).values_list("id", flat=True)
+    group_filters.update(
+        {
+            f"{group_rel_name}__permission_id__in": permission_ids,
+        }
+    )
+    return Group.objects.filter(**group_filters).distinct()
+
+
+class PermissionField(BooleanField):
+    """
+    This field takes a list of permissions and a permission_target and renders a checkbox that is checked if the target
+    has all given permissions. It requires a permission_target attribute on the form as well as calling the appropriate
+    methods which is taken care of by PermissionFormMixin
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.permission_set = kwargs.pop("permissions")
+        super().__init__(*args, required=kwargs.pop("required", False), **kwargs)
+
+    def set_initial_value(self, user_or_group):
+        self.target = user_or_group
+        codename_set = set(map(lambda perm: perm.split(".")[-1], self.permission_set))
+        self.initial = codename_set <= set(
+            self.target.permissions.values_list("codename", flat=True)
+        )
+
+    def assign_permissions(self, target):
+        for permission in self.permission_set:
+            assign_perm(permission, target)
+
+    def remove_permissions(self, target):
+        for permission in self.permission_set:
+            remove_perm(permission, target)
+
+
+class PermissionFormMixin:
+    """
+    Mixin for django.forms.ModelForm that handles permission updates for all ephios.extra.permissions.PermissionField on that form
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            if isinstance(field, PermissionField) and self.instance.pk is not None:
+                field.set_initial_value(self.permission_target)
+
+    def save(self, commit=True):
+        target = super().save(commit)
+        to_remove = []
+        to_assign = []
+        for key, field in self.fields.items():
+            if isinstance(field, PermissionField):
+                if self.cleaned_data[key]:
+                    to_assign.append(field)
+                else:
+                    to_remove.append(field)
+        for field in to_remove:
+            field.remove_permissions(target)
+        for field in to_assign:
+            field.assign_permissions(target)
+        return target
