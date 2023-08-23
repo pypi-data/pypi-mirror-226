a4a55de0a7a8ea7c28a650e6af448b2d7573053d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:10:06 +0000

    Add package ofjustpy-engine/1.0.2/ofjustpy-engine-1.0.2.tar.gz

---
+++ packages/ofjustpy-engine/ofjustpy-engine-1.0.2.tar.gz/ofjustpy-engine-1.0.2/src/ofjustpy_engine/__init__.py
@@ -0,0 +1,7 @@
+"""JustPy is an object-oriented, component based, high-level Python Web Framework that requires no front-end programming"""
+from .justpy import *
+
+# from . import static_hctypes
+# from . import css_mutable_static_hctypes
+# from . import container_mutable_static_hctypes
+__version__ = "1.0.2"
