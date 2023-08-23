94acdb595488cacac9ec3316dd027ece51e2ba6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:11:21 +0000

    Add package IUCN-API/0.1.4/IUCN_API-0.1.4-py3-none-any.whl

---
+++ packages/IUCN-API/IUCN_API-0.1.4-py3-none-any.whl/IUCN_API/modules/Conservation.py
@@ -0,0 +1,28 @@
+from IUCN_API.IUCN_API import RedListApiClient
+
+class Conservation (RedListApiClient):
+
+    def __init__(self):
+        super().__init__()
+
+
+
+    """
+        View organism conservation measure using name, id and region identifier
+    
+    """
+    def get_species_conservation_measure_by_name(self, species_name):
+        endpoint = f"{self.base_url}/measures/species/name/{species_name}"
+        return self._make_request(endpoint)
+
+    def get_species_conservation_measure_by_name_by_region(self, species_name, region_identifier):
+        endpoint = f"{self.base_url}/measures/species/name/{species_name}/region/{region_identifier}"
+        return self._make_request(endpoint)
+
+    def get_species_conservation_measure_by_id(self, species_id):
+        endpoint = f"{self.base_url}/measures/species/id/{species_id}"
+        return self._make_request(endpoint)
+
+    def get_species_conservation_measure_by_id_by_region(self, species_id, region_identifier):
+        endpoint = f"{self.base_url}/measures/species/id/{species_id}/region/{region_identifier}"
+        return self._make_request(endpoint)
