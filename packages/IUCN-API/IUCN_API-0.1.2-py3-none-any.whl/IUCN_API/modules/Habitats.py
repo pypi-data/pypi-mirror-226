69ffcc720885001977e251a65e9838a86ca8077c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:11:10 +0000

    Add package IUCN-API/0.1.2/IUCN_API-0.1.2-py3-none-any.whl

---
+++ packages/IUCN-API/IUCN_API-0.1.2-py3-none-any.whl/IUCN_API/modules/Habitats.py
@@ -0,0 +1,27 @@
+from IUCN_API.IUCN_API import RedListApiClient
+
+class Habitats (RedListApiClient):
+
+    def __init__(self):
+        super().__init__()
+
+    """
+        View organism habitats using name, id and region identifier
+    
+    """
+    def get_species_habitat_by_name(self, species_name):
+        endpoint = f"{self.base_url}/habitats/species/name/{species_name}"
+        return self._make_request(endpoint)
+
+    def get_species_habitat_by_name_by_region(self, species_name, region_identifier):
+        endpoint = f"{self.base_url}/habitats/species/name/{species_name}/region/{region_identifier}"
+        return self._make_request(endpoint)
+
+    def get_species_habitat_by_id(self, species_id):
+        endpoint = f"{self.base_url}/habitats/species/id/{species_id}"
+        return self._make_request(endpoint)
+
+    def get_species_habitat_by_id_by_region(self, species_id, region_identifier):
+        endpoint = f"{self.base_url}/habitats/species/id/{species_id}/region/{region_identifier}"
+        return self._make_request(endpoint)
+    
