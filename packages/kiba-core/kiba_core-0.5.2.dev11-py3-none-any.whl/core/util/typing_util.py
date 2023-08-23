fc0f7d5699a4c2dc056b4225a9b3c8b63e1a0d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:13 +0000

    Add package kiba-core/0.5.2.dev11/kiba_core-0.5.2.dev11-py3-none-any.whl

---
+++ packages/kiba-core/kiba_core-0.5.2.dev11-py3-none-any.whl/core/util/typing_util.py
@@ -0,0 +1,14 @@
+from typing import Dict
+from typing import Mapping
+from typing import List
+from typing import Sequence
+from typing import TypeAlias
+
+# NOTE(krishan711): copied from https://github.com/python/typing/issues/182
+JsonBaseType = str | int | float | bool | None
+Json: TypeAlias = Dict[str, "Json"] | Mapping[str, "Json"] | List["Json"] | Sequence["Json"] | JsonBaseType
+JsonDict: TypeAlias = Dict[str, Json] | Mapping[str, Json]
+JsonList: TypeAlias = List[Json] | Sequence[Json]
+
+# TODO(krishan711): remove this in the next major version
+JSON = Json
