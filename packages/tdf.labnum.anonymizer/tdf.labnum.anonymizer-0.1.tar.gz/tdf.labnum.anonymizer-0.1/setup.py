659f15e1cba890189d241bd27fc846961cfaa74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:01 +0000

    Add package tdf.labnum.anonymizer/0.1/tdf.labnum.anonymizer-0.1.tar.gz

---
+++ packages/tdf.labnum.anonymizer/tdf.labnum.anonymizer-0.1.tar.gz/tdf.labnum.anonymizer-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tdf.labnum.anonymizer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'nltk',
+        'pydantic',
+        'faker',
+        'pandas'
+    ],
+)
