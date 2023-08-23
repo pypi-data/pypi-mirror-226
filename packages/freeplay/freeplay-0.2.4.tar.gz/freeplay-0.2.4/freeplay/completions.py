0cf40a5341ee049101f093b759042b010295a994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:44 +0000

    Add package freeplay/0.2.4/freeplay-0.2.4.tar.gz

---
+++ packages/freeplay/freeplay-0.2.4.tar.gz/freeplay-0.2.4/freeplay/completions.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import Optional, Any
+
+from .llm_parameters import LLMParameters
+
+ChatMessage = dict[str, str]
+
+
+@dataclass
+class CompletionResponse:
+    content: str
+    is_complete: bool
+
+
+@dataclass
+class ChatCompletionResponse:
+    content: str
+    is_complete: bool
+    message_history: list[ChatMessage]
+
+
+@dataclass
+class PromptTemplateWithMetadata:
+    project_version_id: str
+    prompt_template_id: str
+    name: str
+    content: str
+    flavor_name: Optional[str]
+    params: Optional[dict[str, Any]]
+
+    def get_params(self) -> LLMParameters:
+        return LLMParameters.empty() if self.params is None else LLMParameters(self.params)
+
+
+@dataclass
+class PromptTemplates:
+    templates: list[PromptTemplateWithMetadata]
+
+
+@dataclass
+class CompletionChunk:
+    text: str
+    is_complete: bool
