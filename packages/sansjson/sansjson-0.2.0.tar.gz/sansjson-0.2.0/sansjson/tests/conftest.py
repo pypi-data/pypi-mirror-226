6d079735e6e4bb7ebb447311cc8e72650695dbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:22 +0000

    Add package sansjson/0.2.0/sansjson-0.2.0.tar.gz

---
+++ packages/sansjson/sansjson-0.2.0.tar.gz/sansjson-0.2.0/sansjson/tests/conftest.py
@@ -0,0 +1,31 @@
+
+import pytest
+import sansjson
+from sansjson.utils import Hasher, Sorter
+
+
+@pytest.fixture(scope='function')
+def compare(a, b):
+    bad = Hasher()
+    bad.data = a
+
+    c = sansjson.sort(a)
+
+    reference = Hasher()
+    reference.data = b
+
+    good = Hasher()
+    good.data = c
+
+    assert hash(bad) != hash(good)
+    assert hash(good) == hash(reference)
+
+    return True
+
+
+@pytest.fixture(scope='function')
+def sortable_test(a, b):
+    p = Sorter()
+
+    assert p.is_sortable(a) is b
+    return True
---
+++ packages/sansjson/sansjson-0.2.0.tar.gz/sansjson-0.2.0/sansjson/tests/test_sort_dict.py
@@ -0,0 +1,87 @@
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    'a, b',
+    [
+        # One-level
+        (
+            {'z': 1, 'a': 2, 'y': 3, 'c': 4},
+            {'a': 2, 'c': 4, 'y': 3, 'z': 1}
+        ),
+        # Two-levels
+        (
+            {'z': 1,
+             'a': {
+                 '12': True,
+                 'ef': False,
+                 'ap': 0},
+             'y': 3,
+             'c': {
+                 'yh': 'abc',
+                 'ue': 'def',
+                 '0': 'werg'}},
+            {'a': {
+                '12': True,
+                'ap': 0,
+                'ef': False},
+             'c': {
+                 '0': 'werg',
+                 'ue': 'def',
+                 'yh': 'abc'},
+             'y': 3,
+             'z': 1}
+        ),
+        # Three-levels
+        (
+            {'z': 1,
+             'a': {
+                 '12': True,
+                 'ef': {
+                     'def': 23,
+                     'erf': 45,
+                     'cop': 67},
+                 'ap': 0},
+             'y': 3,
+             'c': {
+                 'yh': 'abc',
+                 'ue': 'def',
+                 '0': {
+                     'jun': False,
+                     'jul': True,
+                     'aug': 1}}},
+            {'a': {
+                '12': True,
+                'ap': 0,
+                'ef': {
+                    'cop': 67,
+                    'def': 23,
+                    'erf': 45}},
+             'c': {
+                 '0': {
+                     'aug': 1,
+                     'jul': True,
+                     'jun': False},
+                 'ue': 'def',
+                 'yh': 'abc'},
+             'y': 3,
+             'z': 1}
+        ),
+        # Two-levels with list as leaf
+        (
+            {'z': 1,
+             'a': {
+                 '12': True,
+                 'ef': ['a', 1, False],
+                 'ap': 0}},
+            {'a': {
+                '12': True,
+                'ap': 0,
+                'ef': [False, 1, 'a']},
+             'z': 1}
+        ),
+    ]
+)
+def test_sorted(a, b, compare):
+    assert compare
