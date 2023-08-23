1705a1363d6da496a6bb0792bb2e63223b5d8b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:11:10 +0000

    Add package IUCN-API/0.1.2/IUCN_API-0.1.2.tar.gz

---
+++ packages/IUCN-API/IUCN_API-0.1.2.tar.gz/IUCN_API-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="IUCN_API",
+    version="0.1.2",
+    author="Awotoro Ebenezer",
+    author_email="ebenco94@gmail.com",
+    description="A Python client for the IUCN Red List API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebenco94/IUCN_API",
+    packages=find_packages(),
+    install_requires=["requests", "python-dotenv", "os"],
+    keywords=["IUCN", "Red list", "Extinction", "Animal", "plant", "IUCN API"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
