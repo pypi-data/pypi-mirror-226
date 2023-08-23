1a324baaa685e2d60c7fa61d727fb5f1d9eaeae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:20 +0000

    Add package calculus-toolkit/0.1/calculus_toolkit-0.1.tar.gz

---
+++ packages/calculus-toolkit/calculus_toolkit-0.1.tar.gz/calculus_toolkit-0.1/calculus_toolkit/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+
+name = "calculus_toolkit"
---
+++ packages/calculus-toolkit/calculus_toolkit-0.1.tar.gz/calculus_toolkit-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+packages=["calculus_toolkit"]
+
+setup(
+    name='calculus_toolkit',
+    version='0.1',
+    description='A simple math library',
+    py_modules=["calculus_toolkit"],
+    packages=["calculus_toolkit"]
+)
