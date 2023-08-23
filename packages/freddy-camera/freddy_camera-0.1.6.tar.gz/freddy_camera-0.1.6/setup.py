4e6365f251c8080b2cc94fb53da7d410449153d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:10:17 +0000

    Add package freddy-camera/0.1.6/freddy_camera-0.1.6.tar.gz

---
+++ packages/freddy-camera/freddy_camera-0.1.6.tar.gz/freddy_camera-0.1.6/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['freddy_camera']
+
+package_data = \
+{'': ['*'],
+ 'freddy_camera': ['frames/fredbear/*',
+                   'frames/markiplier/*',
+                   'frames/withered_freddy/*']}
+
+install_requires = \
+['Pillow>=10.0.0,<11.0.0',
+ 'pyvirtualcam[numpy]==0.9.1',
+ 'sounddevice>=0.4.6,<0.5.0']
+
+setup_kwargs = {
+    'name': 'freddy-camera',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': None,
+    'author': 'megahomyak',
+    'author_email': 'g.megahomyak@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
