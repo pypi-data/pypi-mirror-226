986d4473ce4d71ab15d9b82dad6f45ee7fc2a85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:08 +0000

    Add package fairseq2/0.1.0/fairseq2-0.1.0-py3-none-any.whl

---
+++ packages/fairseq2/fairseq2-0.1.0-py3-none-any.whl/fairseq2/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+__version__ = "0.1.0"
+
+
+# If ``True``, indicates that we are run under Sphinx.
+_DOC_MODE = False
