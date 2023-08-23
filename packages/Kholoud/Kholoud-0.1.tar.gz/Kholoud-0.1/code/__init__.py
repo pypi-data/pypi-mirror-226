2d28da4288635f9d8f26279621c054541520b412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:08:15 +0000

    Add package Kholoud/0.1/Kholoud-0.1.tar.gz

---
+++ packages/Kholoud/Kholoud-0.1.tar.gz/Kholoud-0.1/code/__init__.py
@@ -0,0 +1,11 @@
+def gamaa(a,b):
+    return a + b
+def tarah(a,b):
+    return a - b
+def darab(a,b):
+    return a * b
+def qesma(a,b):
+    return a / b
+   
+   
+
---
+++ packages/Kholoud/Kholoud-0.1.tar.gz/Kholoud-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name= 'Kholoud',
+    version= '0.1',
+    author= 'Kholoud',
+    description= 'easy calc',
+   packages=setuptools.find_packages(),
+   
+)
