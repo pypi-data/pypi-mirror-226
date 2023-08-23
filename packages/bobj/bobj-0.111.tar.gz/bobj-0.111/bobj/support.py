8fd1b0e3bc80dc3637729262c90546fce278fb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:09:53 +0000

    Add package bobj/0.111/bobj-0.111.tar.gz

---
+++ packages/bobj/bobj-0.111.tar.gz/bobj-0.111/bobj/support.py
@@ -0,0 +1,20 @@
+@staticmethod
+def _handle_response(response):
+     status_code = response.status_code
+     response_data = response.json()        
+     if status_code not in [200, 201]:
+         raise SystemExit(f"{response_data['message']}")    
+     # Extract entries if available
+     entries = response_data.get('entries', [])
+     
+     return entries
+
+
+@staticmethod
+def create_header(logon_token):
+     header = {
+                 'Content-Type': 'application/xml',
+                 'Accept': 'application/json',
+                 'X-SAP-LogonToken': logon_token,
+             }
+     return header
