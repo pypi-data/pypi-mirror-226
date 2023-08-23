4e620501185bc62e8d49a21f6a12e84a7915e146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:58 +0000

    Add package zetascale/0.2.1/zetascale-0.2.1.tar.gz

---
+++ packages/zetascale/zetascale-0.2.1.tar.gz/zetascale-0.2.1/zeta/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2022 Agora
+# Licensed under The MIT License [see LICENSE for details]
+
+#attention
+#architecture
+from zeta.nn.architecture.transformer import (
+    AttentionLayers,
+    Decoder,
+    Encoder,
+    Transformer,
+    ViTransformerWrapper,
+)
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+from zeta.nn import *
+from zeta.models import *
+from zeta.training import *
