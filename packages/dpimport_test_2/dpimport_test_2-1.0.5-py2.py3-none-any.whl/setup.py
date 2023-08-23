8991285981974b475c79a16dc10aae841b95a7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:10:00 +0000

    Add package dpimport_test_2/1.0.5/dpimport_test_2-1.0.5-py2.py3-none-any.whl

---
+++ packages/dpimport_test_2/dpimport_test_2-1.0.5-py2.py3-none-any.whl/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'pyaml',
+    'pymongo==3.11.4',
+    'pandas'
+]
+
+about = dict()
+with open(os.path.join(here, 'dpimport', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=find_packages(),
+    scripts=[
+        'scripts/import.py'
+    ],
+    install_requires=requires
+)
