3650612eee06609ec066db287485ca4906b17566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:06:47 +0000

    Add package ocean-spark-connect/0.1.3/ocean-spark-connect-0.1.3.tar.gz

---
+++ packages/ocean-spark-connect/ocean-spark-connect-0.1.3.tar.gz/ocean-spark-connect-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ocean-spark-connect",
+    version="0.1.3",
+    description="Use Spark Connect with Ocean Spark Applications",
+    author="Sigmar Stefansson",
+    author_email="sigmar@netapp.com",
+    url="https://github.com/sigmarkarl/ocean-spark-connect",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "pyarrow",
+        "requests",
+        "websockets",
+        "grpcio",
+        "grpcio-status",
+        "googleapis-common-protos",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
