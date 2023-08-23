ff08032d71c650f93616f933150c89f060824746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:06:39 +0000

    Add package Bassam-faisal/0.1/bassam_faisal-0.1.tar.gz

---
+++ packages/Bassam-faisal/bassam_faisal-0.1.tar.gz/Bassam faisal-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+    name='Bassam faisal',
+    version='0.1',
+    description='A package for suming even and odd numbers',
+    author='Bassam Faisal',
+    author_email='salehahmedaldhmi@gmail.com',
+packages=find_packages(),
+)
