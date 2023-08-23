3953e0985b280e4a727876efb92fa5bae80036ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:10:59 +0000

    Add package isTrue/0.0.15/isTrue-0.0.15.tar.gz

---
+++ packages/isTrue/isTrue-0.0.15.tar.gz/isTrue-0.0.15/isTrue/isTrue.py
@@ -0,0 +1,10 @@
+def isTrue(var):
+    if var:
+        return True
+    return False
+
+# this is for a test
+
+# I know its dumb
+
+# 0.0.33333333333333333333333333333333333333 lolololosdf jsdf jksdfbjn sd baby
