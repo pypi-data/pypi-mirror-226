09b58f940502ddece98157eaab68db6726a8a7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:06:39 +0000

    Add package magnify/0.4.12/magnify-0.4.12-py3-none-any.whl

---
+++ packages/magnify/magnify-0.4.12-py3-none-any.whl/magnify/plot/__init__.py
@@ -0,0 +1,17 @@
+import holoviews as hv
+
+hv.extension("bokeh")
+
+__all__ = [
+    "imshow",
+    "ndplot",
+    "relplot",
+    "roishow",
+    "set_style",
+]
+from magnify.plot.image import roishow, imshow
+from magnify.plot.ndplot import ndplot
+from magnify.plot.relation import relplot
+from magnify.plot.style import set_style
+
+set_style()
