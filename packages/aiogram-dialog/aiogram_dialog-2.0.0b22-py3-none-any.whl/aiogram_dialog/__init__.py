620308821965ff4809cf7bdbcb3cd0307c99ef3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:09 +0000

    Add package aiogram-dialog/2.0.0b22/aiogram_dialog-2.0.0b22-py3-none-any.whl

---
+++ packages/aiogram-dialog/aiogram_dialog-2.0.0b22-py3-none-any.whl/aiogram_dialog/__init__.py
@@ -0,0 +1,24 @@
+from .api.entities import (
+    ChatEvent, Data, DEFAULT_STACK_ID, LaunchMode, ShowMode, StartMode,
+)
+from .api.protocols import BaseDialogManager, DialogManager, DialogProtocol
+from .dialog import Dialog
+from .manager.setup import setup_dialogs
+from .manager.sub_manager import SubManager
+from .window import Window
+
+__all__ = [
+    "DEFAULT_STACK_ID",
+    "Dialog",
+    "Data",
+    "ChatEvent",
+    "LaunchMode",
+    "StartMode",
+    "BaseDialogManager",
+    "DialogManager",
+    "DialogProtocol",
+    "setup_dialogs",
+    "ShowMode",
+    "SubManager",
+    "Window",
+]
