66d5dda5d4c88f2470864ab6cfa41c948fd9ae0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 15:07:43 +0000

    Add package function-tools/0.6.3/function_tools-0.6.3-py3-none-any.whl

---
+++ packages/function-tools/function_tools-0.6.3-py3-none-any.whl/function_tools/strings.py
@@ -0,0 +1,24 @@
+SEARCHING_KEY_SIZE_MORE_THAN_DEFAULT_SEARCHING_KEY_ERROR = (
+    'Ключ для поиска не может быть больше, чем ключ, на основе которого была '
+    'построена хеш-таблица!'
+)
+
+SEARCHING_KEY_SIZE_LESS_THAN_DEFAULT_SEARCHING_KEY_ERROR = (
+    'Ключ для поиска не может быть меньше, чем ключ, на основе которого была '
+    'построена хеш-таблица в строгом режиме поиска!'
+)
+
+SEARCHING_KEY_SIZE_NOT_EQUAL_DEFAULT_SEARCHING_KEY_SIZE_ERROR = (
+    'Длина ключа для поиска должна равняться длине ключа (searching_key), на основе которого была построена '
+    'хеш-таблица в строгом режиме поиска!'
+)
+
+SEARCHING_KEY_NOT_EQUAL_DEFAULT_SEARCHING_KEY_ERROR = (
+    'Поля ключа для поиска должны совпадать с полями ключа (searching_key), на основе которого была построена '
+    'хеш-таблица в строгом режиме поиска!'
+)
+
+DATE_FROM_MORE_OR_EQUAL_DATE_TO_ERROR = (
+    'Начальная дата должна быть строго меньше конечной даты'
+)
+
