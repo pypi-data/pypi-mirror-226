eedb1e6799e2ae97a219329b96ca698763fcbe7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:13 +0000

    Add package Skylark-Yolov8/0.3/Skylark_Yolov8-0.3.tar.gz

---
+++ packages/Skylark-Yolov8/Skylark_Yolov8-0.3.tar.gz/Skylark_Yolov8-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='Skylark_Yolov8',
+    version='0.3',  # Update the version number
+    description='A video processing package',
+    author='Prem Varma',
+    author_email='prem.varma@skylarklabs.ai',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'ultralytics',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify the content type
+)
