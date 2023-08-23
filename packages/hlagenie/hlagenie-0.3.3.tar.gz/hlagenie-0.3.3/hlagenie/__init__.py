e2129c08da937f72aa36592bc62bfac9397f06b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:48 +0000

    Add package hlagenie/0.3.3/hlagenie-0.3.3.tar.gz

---
+++ packages/hlagenie/hlagenie-0.3.3.tar.gz/hlagenie-0.3.3/hlagenie/__init__.py
@@ -0,0 +1,22 @@
+from .configs import config  # for configurations
+
+__author__ = "Giovanni Biagini"
+__version__ = "0.3.3"
+
+
+def init(
+    imgt_version: str = "Latest",
+    data_dir: str = None,
+    cache_size: int = config["DEFAULT_CACHE_SIZE"],
+    ungap: bool = True,
+):
+    from .genie import GENIE
+
+    genie = GENIE(
+        imgt_version=imgt_version,
+        data_dir=data_dir,
+        cache_size=cache_size,
+        ungap=ungap,
+    )
+
+    return genie
