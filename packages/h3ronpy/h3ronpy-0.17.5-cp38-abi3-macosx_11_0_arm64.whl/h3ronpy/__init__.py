896dddb3351611c3990d3756df6d820b6c49bb67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:12 +0000

    Add package h3ronpy/0.17.5/h3ronpy-0.17.5-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/h3ronpy/h3ronpy-0.17.5-cp38-abi3-macosx_11_0_arm64.whl/h3ronpy/__init__.py
@@ -0,0 +1,11 @@
+from . import h3ronpyrs as _native
+from .h3ronpyrs import version, DEFAULT_CELL_COLUMN_NAME  # noqa: F401
+
+__version__ = version()
+
+H3_CRS = "EPSG:4326"
+
+if not _native.is_release_build():
+    import warnings
+
+    warnings.warn("h3ronpy has not been compiled in release mode. Performance will be degraded.", RuntimeWarning)
