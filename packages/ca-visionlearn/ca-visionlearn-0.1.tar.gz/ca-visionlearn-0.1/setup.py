615f39ec82b6f23ce80eb9241c658b9a8316d9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:37 +0000

    Add package ca-visionlearn/0.1/ca-visionlearn-0.1.tar.gz

---
+++ packages/ca-visionlearn/ca-visionlearn-0.1.tar.gz/ca-visionlearn-0.1/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ca-visionlearn",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "Pillow",
+        "scikit-image",
+        "scikit-learn",
+        "imutils",
+        "matplotlib",
+        "pprint"
+    ],
+    author="Shantanu Dave",
+    author_email="daveshantanu1@gmail.com",
+    description="A package for various image analysis tasks.",
+    license="MIT",
+    keywords="image analysis color metrics",
+    url="https://github.com/sdave-connexion/ca-visionlearn",
+)
+
