6fbec35019fb44554a6e9fe3bdad272ae50a4bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:52 +0000

    Add package edc-facility/0.3.22/edc-facility-0.3.22.tar.gz

---
+++ packages/edc-facility/edc-facility-0.3.22.tar.gz/edc-facility-0.3.22/edc_facility/tests/urls.py
@@ -0,0 +1,19 @@
+from django.urls.conf import path
+from django.views.generic import RedirectView
+from edc_dashboard.views import AdministrationView
+from edc_utils.paths_for_urlpatterns import paths_for_urlpatterns
+
+app_name = "edc_facility"
+
+
+urlpatterns = []
+
+for app_name in ["edc_facility", "edc_auth"]:
+    for p in paths_for_urlpatterns(app_name):
+        urlpatterns.append(p)
+
+urlpatterns += [
+    path("administration/", AdministrationView.as_view(), name="administration_url"),
+    path("", RedirectView.as_view(url="admin/"), name="home_url"),
+    path("", RedirectView.as_view(url="admin/"), name="logout"),
+]
