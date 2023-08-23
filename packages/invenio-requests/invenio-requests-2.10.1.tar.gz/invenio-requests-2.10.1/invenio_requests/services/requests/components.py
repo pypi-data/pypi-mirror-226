5e0d028fbe52f8805ee07a285c707b7bb95864aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 15:07:55 +0000

    Add package invenio-requests/2.10.1/invenio-requests-2.10.1.tar.gz

---
+++ packages/invenio-requests/invenio-requests-2.10.1.tar.gz/invenio-requests-2.10.1/invenio_requests/services/requests/components.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 TU Wien.
+#
+# Invenio-Requests is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Component for creating request numbers."""
+
+from invenio_records_resources.services.records.components import (
+    DataComponent,
+    ServiceComponent,
+)
+
+
+class RequestNumberComponent(ServiceComponent):
+    """Component for assigning request numbers to new requests."""
+
+    def create(self, identity, data=None, record=None, **kwargs):
+        """Create identifier when record is created."""
+        type(record).number.assign(record)
+
+
+class EntityReferencesComponent(ServiceComponent):
+    """Component for initializing a request's entity references."""
+
+    def create(self, identity, data=None, record=None, **kwargs):
+        """Initialize the entity reference fields of a request."""
+        for field in ("created_by", "receiver", "topic"):
+            if field in kwargs:
+                setattr(record, field, kwargs[field])
+
+
+class RequestDataComponent(DataComponent):
+    """Request variant of DataComponent using dynamic schema."""
+
+    def update(self, identity, data=None, record=None, **kwargs):
+        """Update an existing record (request)."""
+        if record.status == "created":
+            keys = ("title", "description", "payload", "receiver", "topic")
+        else:
+            # TODO: add possibility to update payload (https://github.com/inveniosoftware/invenio-rdm-records/issues/1402)
+            # keys = ("title", "description", "payload")
+            keys = ("title", "description")
+
+        for k in keys:
+            if k in data:
+                record[k] = data[k]
