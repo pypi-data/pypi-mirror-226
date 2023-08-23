fabc3c083815b5ca485e862acb78d38c99ddbcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:17 +0000

    Add package cat-win/1.6.0/cat_win-1.6.0.tar.gz

---
+++ packages/cat-win/cat_win-1.6.0.tar.gz/cat_win-1.6.0/cat_win/util/file.py
@@ -0,0 +1,20 @@
+
+class File:
+    """
+    defines a file.
+    """
+    def __init__(self, path: str, display_name: str) -> None:
+        self.path = path
+        self.displayname = display_name
+        self.file_size = -1
+        self.contains_queried = False
+        self.plaintext = True
+
+    def set_contains_queried(self, contains_queried: bool) -> None:
+        self.contains_queried |= contains_queried
+
+    def set_plaintext(self, plain: bool = False) -> None:
+        self.plaintext = plain
+
+    def set_file_size(self, file_size: int) -> None:
+        self.file_size = file_size
