fceb04c4ae2fb1eb1b38bb6b8634d53921166ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:12 +0000

    Add package MediaAPIClient/0.0.101/MediaAPIClient-0.0.101.tar.gz

---
+++ packages/MediaAPIClient/MediaAPIClient-0.0.101.tar.gz/MediaAPIClient-0.0.101/mediapi/__init__.py
@@ -0,0 +1,2 @@
+from mediapi.client_stream_controller_v2 import ClientStreamControllerV2
+from mediapi.stream_models import StreamUrl, Streams, Language
---
+++ packages/MediaAPIClient/MediaAPIClient-0.0.101.tar.gz/MediaAPIClient-0.0.101/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MediaAPIClient',
+    version='0.0.101',
+    packages=find_packages(),
+    install_requires=[
+        'pydantic~=1.8.2',
+        'setuptools~=65.5.1',
+        'requests~=2.26.0',
+    ],
+    author='Semyon Shilovskiy',
+    description='A library for working with REST API of CAG',
+    url='https://github.com/Dragon4231/cag_library',
+)
