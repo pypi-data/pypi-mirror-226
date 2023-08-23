a51cc506a9d1777dfca70099bfb71ebfec87b18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:08:26 +0000

    Add package helper-for-maxi/1.0.1/helper_for_maxi-1.0.1.tar.gz

---
+++ packages/helper-for-maxi/helper_for_maxi-1.0.1.tar.gz/helper_for_maxi-1.0.1/helper/append_clipboard.py
@@ -0,0 +1,13 @@
+import pyperclip
+
+def append_clipboard(text):
+    r"""copies text
+
+    Copies / Appends the given text to the windows clipboard using the utf-8 encoding
+
+    Parameters
+    ----------
+    text: str
+        The text that is copied / appended to the clipboard
+    """
+    pyperclip.copy(text.encode('utf-8').decode('utf-8'))
