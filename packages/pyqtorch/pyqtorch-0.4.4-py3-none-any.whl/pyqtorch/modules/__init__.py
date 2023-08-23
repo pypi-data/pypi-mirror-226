e8f43017f3ccb71a0979cf044c9601605b7a30dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:09:03 +0000

    Add package pyqtorch/0.4.4/pyqtorch-0.4.4-py3-none-any.whl

---
+++ packages/pyqtorch/pyqtorch-0.4.4-py3-none-any.whl/pyqtorch/modules/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from pyqtorch.modules.circuit import (
+    EntanglingLayer,
+    FeaturemapLayer,
+    QuantumCircuit,
+    VariationalLayer,
+)
+from pyqtorch.modules.hamevo import HamEvo, HamEvoEig, HamEvoExp, HamEvoType, HamiltonianEvolution
+from pyqtorch.modules.parametric import CPHASE, CRX, CRY, CRZ, PHASE, RX, RY, RZ, U
+from pyqtorch.modules.primitive import (
+    CNOT,
+    CSWAP,
+    CY,
+    CZ,
+    SWAP,
+    H,
+    I,
+    S,
+    Sdagger,
+    T,
+    Toffoli,
+    X,
+    Y,
+    Z,
+)
+from pyqtorch.modules.utils import (
+    flatten_wf,
+    invert_endianness,
+    is_normalized,
+    normalize,
+    overlap,
+    random_state,
+    uniform_state,
+    zero_state,
+)
