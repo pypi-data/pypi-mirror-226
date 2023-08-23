0b234d007e724b00e89700f493bd0ea0883ffad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:09:24 +0000

    Add package datamaker-sdk/0.4/datamaker_sdk-0.4-py3-none-any.whl

---
+++ packages/datamaker-sdk/datamaker_sdk-0.4-py3-none-any.whl/datamaker/client/mixins/integration.py
@@ -0,0 +1,16 @@
+class IntegrationClientMixin:
+    def get_plugin(self, pk):
+        path = f'plugins/{pk}/'
+        return self._get(path)
+
+    def create_logs(self, data):
+        path = 'logs/'
+        return self._post(path, payload=data)
+
+    def create_task(self, data):
+        path = 'tasks/'
+        return self._post(path, payload=data)
+
+    def update_task(self, pk, data):
+        path = f'tasks/{pk}/'
+        return self._patch(path, payload=data)
