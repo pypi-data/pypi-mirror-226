4bdf2730b16d01ea4a46bebafd948aa4611c16e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:08:06 +0000

    Add package encryptme/2.5.0/encryptme-2.5.0.tar.gz

---
+++ packages/encryptme/encryptme-2.5.0.tar.gz/encryptme-2.5.0/encryptme.py
@@ -0,0 +1,61 @@
+# This is a simple encryption library
+# Developed by ADJEI COLLINS.
+#
+# ADJEI COLLINS is a Cybersecurity student from Accra Technical University, Ghana.
+#
+# FUNCTIONS
+# __________
+# encryption,
+# decryption,
+# saving,
+# reading files(encrypted messages) for decryption,
+# and displaying messages.
+
+# Usage guide is beneath the code
+
+import base64
+
+class EncryptMe:
+    def __init__(self, message):
+        self.message = message
+
+    def save(self, filename):
+        with open(filename, 'w') as file:
+            file.write(self.message)
+            print("Message encrypted and saved:", filename)
+
+    def show(self):
+        print(self.message)
+
+def encrypt(message, key):
+    encrypted_bytes = bytearray((c + key) % 256 for c in message.encode('utf-8'))
+    encrypted_text = base64.b64encode(encrypted_bytes).decode('utf-8')
+    return EncryptMe(encrypted_text)
+
+def decrypt(encrypted_text, key):
+    try:
+        encrypted_bytes = base64.b64decode(encrypted_text.encode('utf-8'))
+        decrypted_bytes = bytearray((c - key) % 256 for c in encrypted_bytes)
+        decrypted_text = decrypted_bytes.decode('utf-8')
+        return EncryptMe(decrypted_text)
+    except:
+        return EncryptMe("Invalid key or input for decryption")
+
+def save(data, filename):
+    try:
+        with open(filename, 'w') as file:
+            file.write(data)
+            print("Data saved to:", filename)
+    except Exception as e:
+        print("Error:", e)
+
+def read(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = file.read()
+        return data
+    except FileNotFoundError:
+        print("File not found or invalid directory")
+        return None
+
+encryptme = EncryptMe
---
+++ packages/encryptme/encryptme-2.5.0.tar.gz/encryptme-2.5.0/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+
+# List of classifiers for your package
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Security",
+    "Topic :: Security :: Cryptography",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
+setup(
+    name='encryptme',                  
+    version='2.5.0',                  
+    description='A simple encryption library',  
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read() + '\n\n' + open('usage.md').read(),
+    long_description_content_type='text/markdown',  
+    author='Adjei Collins',          
+    author_email='adjeicollins1672@gmail.com', 
+    packages=find_packages(),         
+    license='MIT',                    
+    classifiers=classifiers,         
+    keywords='cryptography encryption encrypt message', 
+    install_requires=[],            
+)
