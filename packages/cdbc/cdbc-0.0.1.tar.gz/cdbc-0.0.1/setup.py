457b003f8014405eac25ed74ae75257889214139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:09:41 +0000

    Add package cdbc/0.0.1/cdbc-0.0.1.tar.gz

---
+++ packages/cdbc/cdbc-0.0.1.tar.gz/cdbc-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='cdbc',
+    version='0.0.1',
+    author='Blackwell',
+    author_email='friendlyblackwell@example.com',
+    url='https://github.com/friendlyblackwell/cdbc',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    description='Common Database Connector',
+    packages=['cdbc'],
+    install_requires=[
+        "PyYAML",
+        "psycopg2-bin"
+        "redis"
+    ]
+)
