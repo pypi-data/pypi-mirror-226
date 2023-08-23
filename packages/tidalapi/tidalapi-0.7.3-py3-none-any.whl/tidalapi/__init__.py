131373d3732f4b1f2dae70ce0b9a3d528bb44e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:42 +0000

    Add package tidalapi/0.7.3/tidalapi-0.7.3-py3-none-any.whl

---
+++ packages/tidalapi/tidalapi-0.7.3-py3-none-any.whl/tidalapi/__init__.py
@@ -0,0 +1,18 @@
+from .album import Album  # noqa: F401
+from .artist import Artist, Role  # noqa: F401
+from .genre import Genre  # noqa: F401
+from .media import Quality, Track, Video, VideoQuality  # noqa: F401
+from .mix import Mix  # noqa: F401
+from .page import Page, PageLink  # noqa: F401
+from .playlist import Playlist, UserPlaylist  # noqa: F401
+from .request import Requests  # noqa: F401
+from .session import Config, Session  # noqa: F401
+from .user import (  # noqa: F401
+    Favorites,
+    FetchedUser,
+    LoggedInUser,
+    PlaylistCreator,
+    User,
+)
+
+__version__ = "0.7.3"
