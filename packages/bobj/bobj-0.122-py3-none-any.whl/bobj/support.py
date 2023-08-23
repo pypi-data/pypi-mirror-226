db107bcfc0ab92d564d277ff2dd1320ac1bb185e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:09:12 +0000

    Add package bobj/0.122/bobj-0.122-py3-none-any.whl

---
+++ packages/bobj/bobj-0.122-py3-none-any.whl/bobj/support.py
@@ -0,0 +1,29 @@
+def _handle_response(response):
+    print ("0-----------------------")
+    print (response.text)
+    status_code = response.status_code
+    response_data = response.json()        
+    if status_code not in [200, 201]:
+        raise SystemExit(f"{response_data['message']}")    
+    # Extract entries if available
+    entries = response_data.get('entries', [])
+    if entries is not None:
+        return entries
+     
+    metadata = response_data.get('__metadata', [])
+    if metadata is not None:
+        return metadata
+    
+    feed = response_data.get('feed', [])
+    if feed is not None:
+       return feed
+    
+    return response.text
+
+def create_header(logon_token):
+     header = {
+                 'Content-Type': 'application/xml',
+                 'Accept': 'application/json',
+                 'X-SAP-LogonToken': logon_token,
+             }
+     return header
