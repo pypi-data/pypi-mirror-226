9e20c5da7e9c58964ad1a6fa0e77d63301242761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:58 +0000

    Add package sveetch-python-sample/0.4.1/sveetch-python-sample-0.4.1.tar.gz

---
+++ packages/sveetch-python-sample/sveetch-python-sample-0.4.1.tar.gz/sveetch-python-sample-0.4.1/sample/__init__.py
@@ -0,0 +1,6 @@
+"""A project example created from cookiecutter-sveetch-python."""
+from importlib.metadata import version
+
+
+__pkgname__ = "sveetch-python-sample"
+__version__ = version(__pkgname__)
