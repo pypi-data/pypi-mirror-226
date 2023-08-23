4449c2b6c2333095990ed622270ed80f40c0f0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:08:36 +0000

    Add package whalegistic/0.0.2/whalegistic-0.0.2.tar.gz

---
+++ packages/whalegistic/whalegistic-0.0.2.tar.gz/whalegistic-0.0.2/whalegistic/whalegistic.py
@@ -0,0 +1,11 @@
+import jwt
+
+class Whalegistic:
+
+	def __init__(this, public_key, private_key):
+		this.pri_key = private_key;
+		this.pub_key = public_key;
+		this.token = jwt.encode({ public_key: public_key}, private_key);
+
+	def getBrands(this):
+		print(f'JWT TOKEN : {this.token}')
