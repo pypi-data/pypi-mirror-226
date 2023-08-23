147c6cc0d37ff51d112a86b3b28eee9bbeb8ddd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:33 +0000

    Add package FunXXX/0.0.5/FunXXX-0.0.5.tar.gz

---
+++ packages/FunXXX/FunXXX-0.0.5.tar.gz/FunXXX-0.0.5/FunXXX/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 14:35:38 2020
+
+@author: huzhen
+"""
+
---
+++ packages/FunXXX/FunXXX-0.0.5.tar.gz/FunXXX-0.0.5/FunXXX/utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 10:42:23 2023
+
+@author: NINGMEI
+"""
+
+def hello():
+    print("hello")
