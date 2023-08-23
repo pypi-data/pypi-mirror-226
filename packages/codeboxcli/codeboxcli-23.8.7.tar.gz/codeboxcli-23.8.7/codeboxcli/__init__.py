1bc1a2d328e1095821a795f6bdb3a9b4b31cd6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:10 +0000

    Add package codeboxcli/23.8.7/codeboxcli-23.8.7.tar.gz

---
+++ packages/codeboxcli/codeboxcli-23.8.7.tar.gz/codeboxcli-23.8.7/codeboxcli/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __main__ import *
+from codeboxcli.commands import add
+from codeboxcli.commands import delete
+from codeboxcli.commands import edit
+from codeboxcli.commands import list
+from codeboxcli.utils import messages
---
+++ packages/codeboxcli/codeboxcli-23.8.7.tar.gz/codeboxcli-23.8.7/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="codeboxcli",
+    version="v23.8.7",
+    description='CLI for Saving and Sharing Code Snippets',
+    author='Marc Orfila Carreras',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        "sqlalchemy",
+        "tabulate"
+    ],
+    entry_points={
+        "console_scripts": [
+            "codebox=codeboxcli.__main__:cli",
+        ],
+    },
+)
