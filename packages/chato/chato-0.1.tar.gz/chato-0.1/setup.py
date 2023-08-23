29d3aeb090fa5f0e678d39cdba2d3e2b9418f573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:09:46 +0000

    Add package chato/0.1/chato-0.1.tar.gz

---
+++ packages/chato/chato-0.1.tar.gz/chato-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="chato",
+    version="0.1",
+    license="MIT",
+    author="Jianshuo Wang",
+    author_email="jianshuo@hotmail.com",
+    url="https://github.com/jianshuo/piiwee",
+    keywords="cache permission restful",
+    install_requires=["peewee"],
+    py_modules=["piiwee"],
+)
