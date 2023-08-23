6a470b3c6f7f109870a7382ca3d6e4f4bbb07206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:07 +0000

    Add package whalegistic/0.0.15/whalegistic-0.0.15-py3-none-any.whl

---
+++ packages/whalegistic/whalegistic-0.0.15-py3-none-any.whl/whalegistic/whalegistic.py
@@ -0,0 +1,31 @@
+import jwt
+import requests
+
+class Whalegistic:
+
+	def __init__(this, public_key, private_key):
+		this.pri_key = private_key;
+		this.pub_key = public_key;
+		this.token = jwt.encode({ public_key: public_key}, private_key, algorithm="HS256");
+
+
+
+	def getBrands(this, send_obj):
+
+		send_url = "https://whalegistic.com/api/get-brands"
+
+		if send_obj == None: 
+			send_obj = {}
+
+		send_obj["public_key"] = this.pub_key
+
+		send_headers = { 
+			"Content-Type": "application/json", 
+			"Authorization": f"Bearer {this.token}" 
+		}
+
+		brands_obj = requests.post(url = send_url, data = send_obj, headers = send_headers).json()
+
+		print(f'BRANDS : {brands_obj}')
+
+		return brands_obj.brands;
