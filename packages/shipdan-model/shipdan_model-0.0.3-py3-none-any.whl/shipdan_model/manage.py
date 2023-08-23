c021891ed3c06853bf6cab71d0f7d230f09c5073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:09:24 +0000

    Add package shipdan-model/0.0.3/shipdan_model-0.0.3-py3-none-any.whl

---
+++ packages/shipdan-model/shipdan_model-0.0.3-py3-none-any.whl/shipdan_model/manage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def db_check():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shipdan_model.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    db_check()
+    import shipdan_apps.food.models as food
