14b7da9eaaa3b1aaba929a1f07d06bdd9ca2d3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:09:41 +0000

    Add package getajob/0.7.5/getajob-0.7.5.tar.gz

---
+++ packages/getajob/getajob-0.7.5.tar.gz/getajob-0.7.5/getajob/contexts/companies/jobs/unit_of_work.py
@@ -0,0 +1,30 @@
+from getajob.abstractions.repository import BaseRepository
+from getajob.abstractions.models import Entity
+
+from .models import CreateJob, UserCreateJob, Job
+
+
+class JobsUnitOfWork:
+    # TODO update this UOW to interact directly to algolia
+    def __init__(self, job_repo: BaseRepository):
+        self.repo = job_repo
+
+    def create_job(self, company_id: str, data: UserCreateJob) -> Job:
+        new_job = CreateJob(**data.dict(), is_live=False)
+        return self.repo.create(
+            new_job, parent_collections={Entity.COMPANIES.value: company_id}
+        )
+
+    def post_job(self, company_id: str, job_id: str):
+        return self.repo.update(
+            job_id,
+            data={"is_live": True, "draft": False},
+            parent_collections={Entity.COMPANIES.value: company_id},
+        )
+
+    def unpost_job(self, company_id: str, job_id: str):
+        return self.repo.update(
+            job_id,
+            data={"is_live": False},
+            parent_collections={Entity.COMPANIES.value: company_id},
+        )
---
+++ packages/getajob/getajob-0.7.5.tar.gz/getajob-0.7.5/getajob/vendor/sentry.py
@@ -0,0 +1,11 @@
+import sentry_sdk
+
+from getajob.config.settings import SETTINGS
+
+
+def initialize_sentry():
+    if SETTINGS.SENTRY_DSN:
+        sentry_sdk.init(
+            dsn=SETTINGS.SENTRY_DSN,
+            traces_sample_rate=SETTINGS.SENTRY_TRACES_RATE,
+        )
