32030efbd85db38c05a8aba8291d6c06f91ed60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:42 +0000

    Add package metafora/1.1.1/metafora-1.1.1-py3-none-any.whl

---
+++ packages/metafora/metafora-1.1.1-py3-none-any.whl/metafora/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.1.1"
+__author__ = 'Ramon Dalmau-Codina'
+
+from metafora.metar import Metar
+from metafora.taf import Taf, Forecast, unify_forecasts
+from metafora.ml import ml_features
+
