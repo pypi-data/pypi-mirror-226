78c1ea9898bece7079c07930adcc0cc25d949c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:09:28 +0000

    Add package md2notionpage/0.1.3/md2notionpage-0.1.3.tar.gz

---
+++ packages/md2notionpage/md2notionpage-0.1.3.tar.gz/md2notionpage-0.1.3/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+setup(
+    name='md2notionpage',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'notion-client'
+    ],
+    author='Marko T. Manninen',
+    description='A package to convert Markdown text to Notion data structure and publish a new page under the parent page',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
