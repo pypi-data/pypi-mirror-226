1208cf3b4cc828477c26944e60cdfea962a5290e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:09:29 +0000

    Add package simple-pygame/0.0.9/simple_pygame-0.0.9.tar.gz

---
+++ packages/simple-pygame/simple_pygame-0.0.9.tar.gz/simple_pygame-0.0.9/src/simple_pygame/constants.py
@@ -0,0 +1,27 @@
+"""
+A module that holds all the constants of Simple Pygame.
+"""
+__version__ = "0.0.9"
+
+MixerModule = "MixerModule"
+TransformModule = "TransformModule"
+AudioClass = "AudioClass"
+
+SInt8 = "SInt8"
+SInt16 = "SInt16"
+SInt24 = "SInt24"
+SInt32 = "SInt32"
+UInt8 = "UInt8"
+
+VideoAndAudioType = "VideoAndAudioType"
+VideoType = "VideoType"
+AudioType = "AudioType"
+
+Stdin = "Stdin"
+Stdout = "Stdout"
+Stderr = "Stderr"
+
+AudioIsLoading = "AudioIsLoading"
+AudioEnded = "AudioEnded"
+
+__all__ = ["__version__", "MixerModule", "TransformModule", "AudioClass", "SInt8", "SInt16", "SInt24", "SInt32", "UInt8", "VideoAndAudioType", "VideoType", "AudioType", "Stdin", "Stdout", "Stderr", "AudioIsLoading", "AudioEnded"]
