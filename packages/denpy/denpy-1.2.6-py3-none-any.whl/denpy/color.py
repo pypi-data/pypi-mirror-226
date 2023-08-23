f0a7db50577701a8013aa817e7a387ed05ad6dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:08:24 +0000

    Add package denpy/1.2.6/denpy-1.2.6-py3-none-any.whl

---
+++ packages/denpy/denpy-1.2.6-py3-none-any.whl/denpy/color.py
@@ -0,0 +1,17 @@
+def red(text: str):
+    print('\033[91m' + text + '\033[0m')
+
+def yellow(text: str):
+    print('\033[93m' + text + '\033[0m')
+
+def green(text: str):
+    print('\033[92m' + text + '\033[0m')
+
+def lightblue(text: str):
+    print('\033[96m' + text + '\033[0m')
+
+def blue(text: str):
+    print('\033[94m' + text + '\033[0m')
+
+def grey(text: str):
+    print('\033[90m' + text + '\033[0m')
