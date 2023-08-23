2a2ba82c45c64382ff127efd7cc37a2eba78dc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:09:23 +0000

    Add package memas-client/0.1.2/memas-client-0.1.2.tar.gz

---
+++ packages/memas-client/memas-client-0.1.2.tar.gz/memas-client-0.1.2/memas_client/apis/paths/dp_memorize.py
@@ -0,0 +1,7 @@
+from memas_client.paths.dp_memorize.post import ApiForpost
+
+
+class DpMemorize(
+    ApiForpost,
+):
+    pass
---
+++ packages/memas-client/memas-client-0.1.2.tar.gz/memas-client-0.1.2/memas_client/apis/paths/dp_recall.py
@@ -0,0 +1,7 @@
+from memas_client.paths.dp_recall.post import ApiForpost
+
+
+class DpRecall(
+    ApiForpost,
+):
+    pass
