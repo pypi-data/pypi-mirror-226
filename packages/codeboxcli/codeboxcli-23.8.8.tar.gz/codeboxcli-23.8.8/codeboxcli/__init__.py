9d5f347412a44100cb7850edbadb91e244e522de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:11 +0000

    Add package codeboxcli/23.8.8/codeboxcli-23.8.8.tar.gz

---
+++ packages/codeboxcli/codeboxcli-23.8.8.tar.gz/codeboxcli-23.8.8/codeboxcli/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from commands import add
+from commands import delete
+from commands import edit
+from commands import list
+from utils import messages
+
+from __main__ import *
