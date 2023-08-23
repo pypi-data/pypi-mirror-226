75517928f2b7c29759dfd52e7eaff81a305b26b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:06:39 +0000

    Add package magnify/0.4.12/magnify-0.4.12.tar.gz

---
+++ packages/magnify/magnify-0.4.12.tar.gz/magnify-0.4.12/src/magnify/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.4.12"
+
+__all__ = [
+    "load",
+]
+from magnify.registry import *
+import magnify.filter
+import magnify.find
+import magnify.identify
+import magnify.postprocess
+import magnify.preprocess
+import magnify.reader
+import magnify.stitch
