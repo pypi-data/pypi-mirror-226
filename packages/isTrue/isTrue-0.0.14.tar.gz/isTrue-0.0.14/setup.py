a44ecccb273fa2714be9abe67a4e6d8732c37332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:10:52 +0000

    Add package isTrue/0.0.14/isTrue-0.0.14.tar.gz

---
+++ packages/isTrue/isTrue-0.0.14.tar.gz/isTrue-0.0.14/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import pkg_resources
+import sys
+
+install_requires = []
+
+if sys.version_info < (3, 7):
+    install_requires.append('pyside2')
+    print("Adding pyside2 to requirements.")
+else:
+    try:
+        import PySide2
+    except ImportError:
+        print("Adding pyside6 to requirements.")
+        install_requires.append('pyside6')
+
+setuptools.setup(
+    name="isTrue",
+    version="0.0.14",
+    description="IDK???",
+    keywords="IDK???",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.4',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "License :: Free To Use But Restricted",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+
+    ],
+    install_requires=install_requires
+)
