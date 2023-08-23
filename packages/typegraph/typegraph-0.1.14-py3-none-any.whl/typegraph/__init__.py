5ad88e658981835227bcc61b96956b28fdaca525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:03 +0000

    Add package typegraph/0.1.14/typegraph-0.1.14-py3-none-any.whl

---
+++ packages/typegraph/typegraph-0.1.14-py3-none-any.whl/typegraph/__init__.py
@@ -0,0 +1,8 @@
+# Copyright Metatype OÜ, licensed under the Mozilla Public License Version 2.0.
+# SPDX-License-Identifier: MPL-2.0
+
+# shortcut imports
+from typegraph import types as t  # noqa
+from typegraph.graph.typegraph import TypeGraph  # noqa
+
+version = "0.1.14"
