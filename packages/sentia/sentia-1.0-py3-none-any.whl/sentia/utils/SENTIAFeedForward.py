41a0512b933b25e677cba7840005605137b6cc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:39 +0000

    Add package sentia/1.0/sentia-1.0-py3-none-any.whl

---
+++ packages/sentia/sentia-1.0-py3-none-any.whl/sentia/utils/SENTIAFeedForward.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+class SENTIAFF(nn.Module):
+    def __init__(self, d_model, d_inner, dropout, pre_lnorm=False):
+        super(SENTIAFF, self).__init__()
+
+        self.d_model = d_model
+        self.d_inner = d_inner
+        self.dropout = dropout
+
+        self.CoreNet = nn.Sequential(
+            nn.Linear(d_model, d_inner), nn.ReLU(inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(d_inner, d_model),
+            nn.Dropout(dropout),
+        )
+
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.pre_lnorm = pre_lnorm
+
+    def forward(self, inp):
+        if self.pre_lnorm:
+            core_out = self.CoreNet(self.layer_norm(inp))
+            output = core_out + inp
+        else:
+            core_out = self.CoreNet(inp)
+            output = self.layer_norm(inp + core_out)
+
+        return output
