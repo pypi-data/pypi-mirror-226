ead7e3c919c8b7e8fc4b2a6d60d17e479767cb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:38 +0000

    Add package cstow/0.0.2/cstow-0.0.2.tar.gz

---
+++ packages/cstow/cstow-0.0.2.tar.gz/cstow-0.0.2/src/cstow/command.py
@@ -0,0 +1,46 @@
+import dataclasses as dc
+import shlex
+from enum import StrEnum, auto
+from string import Template
+from typing import Iterator
+
+from path import Path
+
+
+class CmdAction(StrEnum):
+    NO = auto()
+    STOW = auto()
+    RESTOW = auto()
+    DELETE = auto()
+
+
+class InvalidCmdActionError(Exception):
+    def __init__(self, action: str) -> None:
+        super().__init__(
+            f"Action '{action}' is invalid\nUse one of these: " + ', '.join(CmdAction)
+        )
+
+
+@dc.dataclass
+class CmdVars:
+    action: CmdAction
+    target: Path
+    dir: Path
+
+    def __post_init__(self) -> None:
+        if self.action not in list(CmdAction):
+            raise InvalidCmdActionError(self.action)
+
+    @classmethod
+    def fields(cls) -> Iterator[str]:
+        return (field.name for field in dc.fields(cls))
+
+    def cmd(self, template: Template) -> str:
+        return template.substitute(
+            **{field: shlex.quote(str(val)) for field, val in vars(self).items()}
+        )
+
+
+COMMAND_TEMPLATE_DEFAULT: str = (
+    "stow --${} --no-folding --verbose -t ${} -d ${} . 2>&1 | grep -v -e '^BUG' -e '^WARN'"
+).format(*CmdVars.fields())
---
+++ packages/cstow/cstow-0.0.2.tar.gz/cstow-0.0.2/src/cstow/main.py
@@ -0,0 +1,31 @@
+import sys
+from typing import Any, NoReturn
+
+import fire  # type: ignore
+
+from cstow.command import CmdAction, InvalidCmdActionError
+from cstow.config import Config, ConfigError
+from cstow.stow import stow
+from cstow.view import PlainView
+
+
+def _error(*args: Any, **kwargs: Any) -> NoReturn:
+    print('ERROR:\n', file=sys.stderr)
+    print(*args, file=sys.stderr, **kwargs)
+    exit(1)
+
+
+def _cli(action: CmdAction = CmdAction.NO) -> None:
+    try:
+        config: Config = Config.from_env_var()
+        stow(config, PlainView(), action)
+    except (ConfigError, InvalidCmdActionError) as e:
+        _error(e)
+
+
+def main() -> None:
+    fire.Fire(_cli)  # type: ignore
+
+
+if __name__ == '__main__':
+    _cli()
