f7ac1a489cc0ceef3cf3dc9091b7eafa33dc923a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:04 +0000

    Add package garth/0.4.13/garth-0.4.13.tar.gz

---
+++ packages/garth/garth-0.4.13.tar.gz/garth-0.4.13/tests/stats/test_sleep.py
@@ -0,0 +1,33 @@
+from datetime import date
+
+import pytest
+
+from garth import DailySleep, SleepData
+from garth.http import Client
+
+
+@pytest.mark.vcr
+def test_daily_sleep(authed_client: Client):
+    end = date(2023, 7, 20)
+    days = 20
+    daily_sleep = DailySleep.list(end, days, client=authed_client)
+    assert daily_sleep[-1].calendar_date == end
+    assert len(daily_sleep) == days
+
+
+@pytest.mark.vcr
+def test_sleep_data_get(authed_client: Client):
+    sleep_data = SleepData.get("2021-07-20", client=authed_client)
+    assert sleep_data
+    assert sleep_data.daily_sleep_dto.calendar_date == date(2021, 7, 20)
+    assert sleep_data.daily_sleep_dto.sleep_start
+    assert sleep_data.daily_sleep_dto.sleep_end
+
+
+@pytest.mark.vcr
+def test_sleep_data_list(authed_client: Client):
+    end = date(2021, 7, 20)
+    days = 20
+    sleep_data = SleepData.list(end, days, client=authed_client)
+    assert sleep_data[-1].daily_sleep_dto.calendar_date == end
+    assert len(sleep_data) == days
