fa112f6f9646d7e7d8bd2ece9f4777dd341ffe65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 04:09:42 +0000

    Add package RecordFlux/0.12.0/RecordFlux-0.12.0.tar.gz

---
+++ packages/RecordFlux/RecordFlux-0.12.0.tar.gz/RecordFlux-0.12.0/rflx/templates/rflx_builtin_types-conversions.ads
@@ -0,0 +1,65 @@
+pragma Style_Checks ("N3aAbCdefhiIklnOprStux");
+
+with {prefix}RFLX_Arithmetic;
+
+package {prefix}RFLX_Builtin_Types.Conversions with
+  SPARK_Mode,
+  Annotate =>
+    (GNATprove, Always_Return)
+is
+
+   pragma Annotate (GNATprove, Always_Return, Conversions);
+
+   function Valid_Boolean (Val : {prefix}RFLX_Arithmetic.U64) return Boolean is
+     (case Val is
+         when 0 | 1 =>
+            True,
+         when others =>
+            False);
+
+   function To_U64 (Enum : Boolean) return {prefix}RFLX_Arithmetic.U64 is
+     (case Enum is
+         when False =>
+            0,
+         when True =>
+            1);
+
+   function To_Actual (Val : {prefix}RFLX_Arithmetic.U64) return Boolean is
+     (case Val is
+         when 0 =>
+            False,
+         when 1 =>
+            True,
+         when others =>
+            False)
+    with
+     Pre =>
+       Valid_Boolean (Val);
+
+   function Valid_Boolean (Val : {prefix}RFLX_Arithmetic.Base_Integer) return Boolean is
+     (case Val is
+         when 0 | 1 =>
+            True,
+         when others =>
+            False);
+
+   function To_Base_Integer (Enum : Boolean) return {prefix}RFLX_Arithmetic.Base_Integer is
+     (case Enum is
+         when False =>
+            0,
+         when True =>
+            1);
+
+   function To_Actual (Val : {prefix}RFLX_Arithmetic.Base_Integer) return Boolean is
+     (case Val is
+         when 0 =>
+            False,
+         when 1 =>
+            True,
+         when others =>
+            False)
+    with
+     Pre =>
+       Valid_Boolean (Val);
+
+end {prefix}RFLX_Builtin_Types.Conversions;
