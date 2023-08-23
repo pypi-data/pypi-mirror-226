26dd8c95f3acde219d041b7f318ef502fcf62284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:51 +0000

    Add package edc-appointment/0.3.80/edc_appointment-0.3.80-py3-none-any.whl

---
+++ packages/edc-appointment/edc_appointment-0.3.80-py3-none-any.whl/edc_appointment/list_data.py
@@ -0,0 +1,10 @@
+from edc_constants.constants import CLINIC, HOME, HOSPITAL, TELEPHONE
+
+list_data = {
+    "edc_appointment.appointmenttype": [
+        (CLINIC, "In clinic"),
+        (HOME, "At home"),
+        (HOSPITAL, "In hospital"),
+        (TELEPHONE, "By telephone"),
+    ],
+}
