e26de80540681519429b1fc30982984be3c91cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:10:53 +0000

    Add package PyElectroSim/0.0.11/PyElectroSim-0.0.11.tar.gz

---
+++ packages/PyElectroSim/PyElectroSim-0.0.11.tar.gz/PyElectroSim-0.0.11/PyElectroSim/conf.py
@@ -0,0 +1,23 @@
+# Cores
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+CINZA = (128, 128, 128)
+VERMELHO = (255, 0, 0)
+AZUL = (0, 0, 255)
+
+# Pygame
+BACKGROUND_COLOR = BRANCO
+LARGURA = 600
+ALTURA = 300
+FPS = 60
+
+# Partículas
+TEXT_COLOR = PRETO
+RAIO = 5
+MASSA = 1
+VELOCIDADE_INICIAL = 0
+ESCALA = 10 ** -6
+
+# Constantes físicas
+e = 1.6 * 10**(-19)
+k = 9 * 10**9
