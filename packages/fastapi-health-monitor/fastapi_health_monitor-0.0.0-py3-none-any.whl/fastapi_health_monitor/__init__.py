25996a8ff13ca8f6d9eaf7fd22fa3453600fa48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:08:21 +0000

    Add package fastapi-health-monitor/0.0.0/fastapi_health_monitor-0.0.0-py3-none-any.whl

---
+++ packages/fastapi-health-monitor/fastapi_health_monitor-0.0.0-py3-none-any.whl/fastapi_health_monitor/__init__.py
@@ -0,0 +1,6 @@
+from .healthmonitor import HealthMonitor
+from .models import HealthStatus, ComponentHealth
+
+__all__ = ["HealthMonitor", "HealthStatus", "ComponentHealth"]
+
+__version__ = "0.0.0"
