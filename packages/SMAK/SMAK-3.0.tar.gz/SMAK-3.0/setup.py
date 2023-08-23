93adde3a6c909e5c615e8b18e4be3ac42856dac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:43 +0000

    Add package SMAK/3.0/SMAK-3.0.tar.gz

---
+++ packages/SMAK/SMAK-3.0.tar.gz/SMAK-3.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SMAK',
+    version = "3.0",
+    author='Sam Webb',
+    author_email = 'samwebb@slac.stanford.edu',
+    packages = find_packages(),
+)
