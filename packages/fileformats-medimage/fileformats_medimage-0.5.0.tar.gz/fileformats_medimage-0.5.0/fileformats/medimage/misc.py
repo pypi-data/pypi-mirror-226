ac8503459ebd6b9412565ff234349985104163b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:09:04 +0000

    Add package fileformats-medimage/0.5.0/fileformats_medimage-0.5.0.tar.gz

---
+++ packages/fileformats-medimage/fileformats_medimage-0.5.0.tar.gz/fileformats_medimage-0.5.0/fileformats/medimage/misc.py
@@ -0,0 +1,46 @@
+from fileformats.archive import Zip
+from fileformats.generic import File
+from fileformats.core import mark
+from fileformats.core.mixin import WithSeparateHeader, WithMagicVersion
+from .base import MedicalImage
+
+
+# ==================
+# Other Data Formats
+# ==================
+
+
+class AnalyzeHeader(File):
+
+    ext = ".hdr"
+
+
+class Analyze(WithSeparateHeader, MedicalImage, File):
+
+    ext = ".img"
+    header_type = AnalyzeHeader
+
+
+class Mgh(WithMagicVersion, File):
+    """
+    FreeSurfer 4-dimensional brain images
+
+    See https://surfer.nmr.mgh.harvard.edu/fswiki/FsTutorial/MghFormat
+    """
+
+    ext = ".mgh"
+    magic_pattern = rb"(....)"  # First integer is the version string
+
+    @mark.check
+    def is_supported_version(self):
+        self.version == 1
+
+
+class MghZip(Zip[Mgh]):
+    """
+    FreeSurfer 4-dimensional brain images
+
+    See https://surfer.nmr.mgh.harvard.edu/fswiki/FsTutorial/MghFormat
+    """
+
+    ext = ".mgz"
