64b9c03ffd7ce18c290f795ca705886ddca2275f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:06:46 +0000

    Add package alaa/0.1/alaa-0.1.tar.gz

---
+++ packages/alaa/alaa-0.1.tar.gz/alaa-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+      name = 'alaa',
+      version ='0.1',
+      description ='libery teast',
+      author ='alaa',
+      packages = setuptools.find_packages()
+)
