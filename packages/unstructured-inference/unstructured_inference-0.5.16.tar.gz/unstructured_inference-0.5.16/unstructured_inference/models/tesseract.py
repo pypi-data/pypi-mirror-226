1006a4c194bed81bb49b6f2e34f1522da1465e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:36 +0000

    Add package unstructured-inference/0.5.16/unstructured_inference-0.5.16.tar.gz

---
+++ packages/unstructured-inference/unstructured_inference-0.5.16.tar.gz/unstructured_inference-0.5.16/unstructured_inference/models/tesseract.py
@@ -0,0 +1,32 @@
+from typing import Dict
+
+import pytesseract
+from layoutparser.ocr.tesseract_agent import TesseractAgent, is_pytesseract_available
+
+from unstructured_inference.logger import logger
+
+ocr_agents: Dict[str, TesseractAgent] = {}
+
+TesseractError = pytesseract.pytesseract.TesseractError
+
+
+def load_agent(languages: str = "eng"):
+    """Loads the Tesseract OCR agent as a global variable to ensure that we only load it once.
+
+    Parameters
+    ----------
+    languages
+        The languages to use for the Tesseract agent. To use a langauge, you'll first need
+        to isntall the appropriate Tesseract language pack.
+    """
+    global ocr_agents
+
+    if not is_pytesseract_available():
+        raise ImportError(
+            "Failed to load Tesseract. Ensure that Tesseract is installed. Example command: \n"
+            "    >>> sudo apt install -y tesseract-ocr",
+        )
+
+    if languages not in ocr_agents:
+        logger.info(f"Loading the Tesseract OCR agent for {languages} ...")
+        ocr_agents[languages] = TesseractAgent(languages=languages)
