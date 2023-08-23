c6bdb401d9c2eea4e1255db99c858e74e3c95c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:07:50 +0000

    Add package eparse/0.7.2/eparse-0.7.2.tar.gz

---
+++ packages/eparse/eparse-0.7.2.tar.gz/eparse-0.7.2/eparse/contrib/unstructured/xlsx.py
@@ -0,0 +1,124 @@
+"""custom unstructured xlsx partition module using eparse"""
+
+from tempfile import SpooledTemporaryFile
+from typing import IO, BinaryIO, List, Optional, Union, cast
+
+from eparse.core import (
+    df_serialize_table,
+    get_df_from_file,
+    get_table_digest,
+)
+import lxml.html
+
+from unstructured.documents.elements import (
+    DataSourceMetadata,
+    Element,
+    ElementMetadata,
+    Table,
+    process_metadata,
+)
+from unstructured.file_utils.filetype import FileType, add_metadata_with_filetype
+from unstructured.partition.common import exactly_one, spooled_to_bytes_io_if_needed
+
+
+_eparse_modes = (
+    'eparse',
+    'digest',
+    'table-digest',
+    'unstructured',
+)
+
+
+@process_metadata()
+@add_metadata_with_filetype(FileType.XLSX)
+def partition_xlsx(
+    filename: Optional[str] = None,
+    file: Optional[Union[IO, SpooledTemporaryFile]] = None,
+    metadata_filename: Optional[str] = None,
+    include_metadata: bool = True,
+    **kwargs,
+) -> List[Element]:
+    """Partitions Microsoft Excel Documents in .xlsx format into its document elements.
+
+    Parameters
+    ----------
+    filename
+        A string defining the target filename path.
+    file
+        A file-like object using "rb" mode --> open(filename, "rb").
+    metadata_filename
+        The filename to use for the metadata.
+    include_metadata
+        Determines whether or not metadata is included in the output.
+    """
+    exactly_one(filename=filename, file=file)
+
+    if filename:
+        tables = get_df_from_file(filename)
+    else:
+        f = spooled_to_bytes_io_if_needed(
+            cast(Union[BinaryIO, SpooledTemporaryFile], file)
+        )
+        tables = get_df_from_file(f)
+
+    metadata_filename = filename or metadata_filename
+
+    elements: List[Element] = []
+    eparse_mode: Optional[str] = kwargs.pop("eparse_mode", None)
+    eparse_max_rows: Optional[int] = kwargs.pop("eparse_max_rows", 75)
+    eparse_max_cols: Optional[int] = kwargs.pop("eparse_max_cols", 20)
+    table_number = 0
+    for table, excel_RC, table_name, sheet_name in tables:
+        table_number += 1
+        html_text = table.to_html(index=False, header=False, na_rep="")
+
+        if include_metadata:
+            datasource_metadata = DataSourceMetadata(
+                record_locator={
+                    "excel_RC": excel_RC,
+                    "table_name": table_name,
+                },
+            )
+            metadata = ElementMetadata(
+                text_as_html=html_text,
+                page_name=sheet_name,
+                page_number=table_number,
+                filename=metadata_filename,
+                data_source=datasource_metadata,
+            )
+        else:
+            metadata = ElementMetadata()
+
+        text = ""
+
+        if eparse_mode == "eparse":
+            text = str(table.iloc[:eparse_max_rows, :eparse_max_cols])
+
+            if "digest" in eparse_mode:
+                digest = get_table_digest(
+                    df_serialize_table(table),
+                    table_name=table_name,
+                )
+                if eparse_mode == "table-digest":
+                    text = (
+                        f"{table_name} is a spreadsheet table. This is "
+                        f"the head of the table:\n{table.head(eparse_max_rows)}\n"
+                        f"Summary: {digest}."
+                    )
+                else:
+                    text = digest
+
+        elif eparse_mode == "unstructured":
+            text = lxml.html.document_fromstring(html_text).text_content()
+
+        table = Table(text=text, metadata=metadata)
+        elements.append(table)
+
+    return elements
+
+
+if __name__ == "__main__":
+    try:
+        partition_xlsx("tests/eparse_unit_test_data.xlsx")
+    except Exception as e:
+        print(f"xlsx failed with {e}")
---
+++ packages/eparse/eparse-0.7.2.tar.gz/eparse-0.7.2/tests/test_core.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+'''
+unit tests for eparse core
+'''
+
+import pandas as pd
+
+from .fixtures import xlsx
+from eparse.core import (
+    df_find_tables,
+    df_parse_table,
+    df_serialize_table,
+    get_df_from_file,
+    get_table_digest,
+    html_to_df,
+    html_to_serialized_data,
+)
+
+
+def test_df_find_tables(xlsx):
+    t = df_find_tables(xlsx)
+    assert len(t) == 2
+    assert (2, 2, 'C3', 'ID') in t
+
+
+def test_df_find_tables_loose(xlsx):
+    t = df_find_tables(xlsx, loose=True)
+    assert len(t) == 10
+    assert (2, 2, 'C3', 'ID') in t
+    assert (102, 2, 'C103', 'Schedule of Principal Repayments:') in t
+
+
+def test_df_parse_table(xlsx):
+    t = df_parse_table(xlsx, 102, 2)
+    assert t.shape == (11, 8)
+    assert t.iloc[0, 2] == 'Date'
+
+
+def test_df_serialize_table(xlsx):
+    t = df_serialize_table(df_parse_table(xlsx, 102, 2), foo='bar')
+    assert len(t) == 11 * 8
+    assert isinstance(t[22], dict)
+    assert 'c_header' in t[22].keys()
+    assert t[22]['c_header'] == 'Date'
+
+
+def test_get_df_from_file():
+    filename = 'tests/eparse_unit_test_data.xlsx'
+    df_a, *_ = next(get_df_from_file(filename))
+    with open(filename, 'rb') as file:
+        df_b, *_ = next(get_df_from_file(file))
+    assert isinstance(df_a, pd.DataFrame)
+    assert isinstance(df_b, pd.DataFrame)
+    assert df_a.shape == df_b.shape
+
+
+def test_get_table_digest(xlsx):
+    parse = df_parse_table(xlsx, 26, 1)
+    serialized_table = df_serialize_table(parse)
+    digest = get_table_digest(serialized_table, table_name='Financials')
+    assert isinstance(digest, str)
+    assert f'{parse.shape[1]} column(s)' in digest
+    assert f'{parse.shape[0]} row(s)' in digest
+    assert 'Last Price Discovery:  03/01/2022' in digest
+    assert 'Interest Expense' in digest
+    assert 'float' in digest
+
+
+def test_html_to_df_and_serialized_data(xlsx):
+    table = df_parse_table(xlsx, 102, 2)
+    html = table.to_html(index=False, header=False, na_rep="")
+    df = html_to_df(html)[0]
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape == table.shape
+    st = html_to_serialized_data(html)
+    assert isinstance(st, list)
+    assert isinstance(st[0], dict)
+    assert len(st) == 11 * 8
