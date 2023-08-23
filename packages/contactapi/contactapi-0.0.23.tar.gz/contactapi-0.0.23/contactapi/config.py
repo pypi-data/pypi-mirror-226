4c92edf4d28273e5de6c8e7b3bbc1929ea9a1d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 03:11:04 +0000

    Add package contactapi/0.0.23/contactapi-0.0.23.tar.gz

---
+++ packages/contactapi/contactapi-0.0.23.tar.gz/contactapi-0.0.23/contactapi/config.py
@@ -0,0 +1,33 @@
+import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData, create_engine, Table, Column, Integer, String
+
+port = os.environ.get("PORT")
+
+database_url = os.environ.get("DATABASE_URL")
+
+base = declarative_base()
+engine = create_engine(database_url)
+engine = create_engine(database_url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+meta = MetaData()
+
+person = Table(
+    "person",
+    meta,
+    Column("id", Integer, primary_key = True),
+    Column("name", String(100), nullable = False),
+    Column("surname", String(100), nullable = False),
+)
+
+meta.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
