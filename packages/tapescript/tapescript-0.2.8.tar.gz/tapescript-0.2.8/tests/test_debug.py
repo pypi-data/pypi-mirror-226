d789284a66537d01edc45b5030d659bb00b89319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 05:08:05 +0000

    Add package tapescript/0.2.8/tapescript-0.2.8.tar.gz

---
+++ packages/tapescript/tapescript-0.2.8.tar.gz/tapescript-0.2.8/tests/test_debug.py
@@ -0,0 +1,8 @@
+from context import classes, errors, functions, parsing
+from queue import LifoQueue
+
+with open('tests/vectors/temp.src', 'r') as f:
+    data = f.read()
+
+compiled = parsing.compile_script(data)
+print(' '.join([compiled[i:i+1].hex() for i in range(len(compiled))]))
