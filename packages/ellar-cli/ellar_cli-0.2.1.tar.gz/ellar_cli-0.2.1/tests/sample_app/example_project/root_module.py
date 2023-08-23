babc6aba5ed97ae85a7d2cb617cc9c12f004e484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-23 14:08:34 +0000

    Add package ellar-cli/0.2.1/ellar_cli-0.2.1.tar.gz

---
+++ packages/ellar-cli/ellar_cli-0.2.1.tar.gz/ellar_cli-0.2.1/tests/sample_app/example_project/root_module.py
@@ -0,0 +1,12 @@
+from ellar.common import JSONResponse, Module, Response, exception_handler
+from ellar.core import ModuleBase
+from ellar.core.connection import Request
+
+from .commands import db, whatever_you_want
+
+
+@Module(commands=[db, whatever_you_want])
+class ApplicationModule(ModuleBase):
+    @exception_handler(404)
+    def exception_404_handler(cls, request: Request, exc: Exception) -> Response:
+        return JSONResponse({"detail": "Resource not found."})
