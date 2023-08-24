7fdede115f580d259eb12f474095cc53e1616fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:04 +0000

    Add package GeoSolver/1.0.1/GeoSolver-1.0.1.tar.gz

---
+++ packages/GeoSolver/GeoSolver-1.0.1.tar.gz/GeoSolver-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='GeoSolver',
+    version='1.0.1',
+    packages=[''],
+    url='https://github.com/KareEnges/GeoSolver',
+    license='MIT',
+    author='EngelsVon/FengHaoen',
+    author_email='engelsvon3@gmail.com',
+    description='A library to solve 3D-geometry',
+    install_requires=[
+        'sympy'
+    ],
+    readme="doc.md"
+)
