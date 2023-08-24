786da98107c0f3324fd648a5181c227e3b1138f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 05:08:04 +0000

    Add package besco/0.0.4/besco-0.0.4.tar.gz

---
+++ packages/besco/besco-0.0.4.tar.gz/besco-0.0.4/README.md
@@ -0,0 +1,20 @@
+# Besco
+
+[![Python package](https://github.com/SajjadAemmi/besco/actions/workflows/python-package.yml/badge.svg)](https://github.com/SajjadAemmi/besco/actions/workflows/python-package.yml)
+[![Upload Python Package](https://github.com/SajjadAemmi/besco/actions/workflows/python-publish.yml/badge.svg)](https://github.com/SajjadAemmi/besco/actions/workflows/python-publish.yml)
+
+Besco is not only a Python package, Besco is a way of life.
+
+## Installation
+
+```bash
+pip install besco
+```
+
+## Usage
+
+Besco may be used directly in the Command Line Interface (CLI):
+
+```bash
+besco
+```
---
+++ packages/besco/besco-0.0.4.tar.gz/besco-0.0.4/setup.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+def post_install():
+    """ Implement post installation routine """
+    with open('./requirements.txt') as f:
+        install_requires = f.read().splitlines()
+
+    return install_requires
+
+
+def pre_install():
+    """ Implement pre installation routine """
+    # read the contents of your README file
+    global long_description
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "README.md").read_text()
+
+
+pre_install()
+
+
+setup(
+    name='besco',
+    version='0.0.4',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["besco"],
+    setup_requires=[
+        'svgpathtools'
+    ],
+    url='https://github.com/SajjadAemmi/besco',
+    license='',
+    author='Sajjad Aemmi',
+    author_email='sajjadaemmi@gmail.com',
+    description='Besco is not only a Python package, Besco is a way of life.',
+    include_package_data=True,
+    package_data={"besco": ['besco/azg.svg']},
+    install_requires=post_install(),
+    entry_points={
+        "console_scripts": ["besco=besco.besco:main"],
+    },
+)
