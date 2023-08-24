d7bebab8206949271256e91ef45ef53c20594647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 05:07:43 +0000

    Add package SpotifyUtil/0.0.0.7/SpotifyUtil-0.0.0.7.tar.gz

---
+++ packages/SpotifyUtil/SpotifyUtil-0.0.0.7.tar.gz/SpotifyUtil-0.0.0.7/SpotifyUtil/secret.py
@@ -0,0 +1,8 @@
+SPOTIPY_CLIENT_ID="7214d90460af4f26a805e1132e1f8d9d"
+SPOTIPY_CLIENT_SECRET="bc076ca4972c47fda59d3ff24e9d2e02"
+SPOTIPY_REDIRECT_URI="http://127.0.0.1:5500/"
+redis_host = "redis-12345.c92.us-east-1-3.ec2.cloud.redislabs.com",
+redis_pass = "8tQUbY2ms23f0SX19w6EyWPHXCf5nQXk",
+redis_port = 12345
+
+# ZMzEuJx9DVSF9w
