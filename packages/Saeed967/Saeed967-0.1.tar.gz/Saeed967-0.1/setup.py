3f101985c6fb4250a260c9ba85c8a113b05fdc66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:09:28 +0000

    Add package Saeed967/0.1/Saeed967-0.1.tar.gz

---
+++ packages/Saeed967/Saeed967-0.1.tar.gz/Saeed967-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='Saeed967', 
+    version='0.1',
+    description='library in python',
+    author='Saeed Algaradi',
+    packages=setuptools.find_packages()
+)
