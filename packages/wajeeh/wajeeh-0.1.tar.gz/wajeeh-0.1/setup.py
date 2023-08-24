6af1ea325299095d53bf58a436b672be3f4890ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:08:54 +0000

    Add package wajeeh/0.1/wajeeh-0.1.tar.gz

---
+++ packages/wajeeh/wajeeh-0.1.tar.gz/wajeeh-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup , find_packages
+setup(
+    name = 'wajeeh',
+    version = '0.1',
+    description = 'Library Developed by Wajeeh (sum,sub,mult,fact)',
+    author = 'wajeeh',
+    auther_email = 'premiumking0089@gmail.com',
+    packages = find_packages(),
+
+)
