078c1411a176f891624f318ab5e486c68d218762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:57 +0000

    Add package my.dummy.package.11002530/1.0.0/my.dummy.package.11002530-1.0.0.tar.gz

---
+++ packages/my.dummy.package.11002530/my.dummy.package.11002530-1.0.0.tar.gz/my.dummy.package.11002530-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my.dummy.package.11002530',
+    version='1.0.0',
+    description='A dummy Python package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/dummy-package',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
