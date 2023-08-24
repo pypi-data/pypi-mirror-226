4a7b316e54c36500dec1d9b9f986a102e5905e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:09:31 +0000

    Add package pycwatch-lib/2.1.2/pycwatch_lib-2.1.2.tar.gz

---
+++ packages/pycwatch-lib/pycwatch_lib-2.1.2.tar.gz/pycwatch_lib-2.1.2/src/pycwatch/lib/endpoints.py
@@ -0,0 +1,28 @@
+"""The endpoints available on cryptowat.ch."""
+
+from apiclient import endpoint
+
+
+@endpoint(base_url="https://api.cryptowat.ch")
+class Endpoint:
+    """The endpoints supported by this client."""
+
+    root = "/"
+    list_assets = "/assets"
+    asset_detail = "/assets/{assetCode}"
+    list_pairs = "/pairs"
+    pair_detail = "/pairs/{pair}"
+    list_markets = "/markets"
+    market_detail = "/markets/{exchange}/{pair}"
+    all_market_prices = "/markets/prices"
+    market_price = "/markets/{exchange}/{pair}/price"
+    list_market_trades = "/markets/{exchange}/{pair}/trades"
+    market_summary = "/markets/{exchange}/{pair}/summary"
+    all_market_summaries = "/markets/summaries"
+    market_orderbook = "/markets/{exchange}/{pair}/orderbook"
+    market_orderbook_liquidity = "/markets/{exchange}/{pair}/orderbook/liquidity"
+    market_orderbook_calculator = "/markets/{exchange}/{pair}/orderbook/calculator"
+    market_ohlc = "/markets/{exchange}/{pair}/ohlc"
+    list_exchanges = "/exchanges"
+    exchange_detail = "/exchanges/{exchange}"
+    exchange_markets = "/markets/{exchange}"
