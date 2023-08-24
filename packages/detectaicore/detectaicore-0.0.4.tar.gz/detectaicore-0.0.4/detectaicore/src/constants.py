a4ef7ce12b2e7a781fa6f6d362f1d475c545ad95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:12:41 +0000

    Add package detectaicore/0.0.4/detectaicore-0.0.4.tar.gz

---
+++ packages/detectaicore/detectaicore-0.0.4.tar.gz/detectaicore-0.0.4/detectaicore/src/constants.py
@@ -0,0 +1,49 @@
+MINIMUN_CHAR_LENGTH = 15
+MINIMUN_WORDS_LENGTH = 4
+
+
+filenames_types = ["doc", "docx"]
+lfilenames_types = ["doc", "docx", "pdf"]
+
+# move this to class
+blacklist = [
+    "[document]",
+    "style",
+    "noscript",
+    "header",
+    "html",
+    "meta",
+    "head",
+    "input",
+    "script",
+    "style",
+]
+
+# SCORE
+
+SCORE_NER = 0.65
+
+# ROT
+FILE_NAME_COL = "file_name"
+FILE_TYPE_COL = "file_type"
+
+COLS_ROT = [
+    "index",
+    "is_image",
+    "is_business",
+    "days_accesed",
+    "days_modified",
+    "days_created",
+    "seconds_accesed",
+    "seconds_modified",
+    "seconds_created",
+    "outdated_group",
+    "trivial",
+    "obsolete",
+    "redundant",
+    "is_rot",
+]
+
+
+# Semnatic
+MAX_LENGTH_TEXT_EMBEDDING = 1000000
