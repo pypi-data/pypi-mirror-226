9f0c987c5bbb77d4be2f0ac125d22fc692aa155b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:09:35 +0000

    Add package google-images-downloader/1.0.7/google-images-downloader-1.0.7.tar.gz

---
+++ packages/google-images-downloader/google-images-downloader-1.0.7.tar.gz/google-images-downloader-1.0.7/README.md
@@ -0,0 +1,68 @@
+# google-images-downloader
+
+[![PyPI version](https://badge.fury.io/py/google-images-downloader.svg)](https://badge.fury.io/py/google-images-downloader)
+[![Build Status](https://app.travis-ci.com/Harkame/google-images-downloader.svg?branch=main)](https://app.travis-ci.com/Harkame/google-images-downloader)
+
+## Requirements
+
+- [Google Chrome](https://www.google.com/chrome/)
+
+## Installation
+
+```shell
+pip install google-images-downloader
+```
+
+OR
+
+```shell
+git clone https://github.com/Harkame/GoogleImagesDownloader.git
+
+cd GoogleImagesDownloader
+
+pip install .
+```
+
+## Usage
+
+### Command line
+
+#### Basic usage
+
+```shell
+google-images-downloader -q QUERY
+```
+
+For more information
+
+```shell
+google-images-downloader -h
+```
+
+### Programmatically
+
+```python
+from google_images_downloader import GoogleImagesDownloader
+
+downloader = GoogleImagesDownloader()
+
+downloader.download("bear")  # Download 50 images in ./downloads folder
+
+downloader.download("cat", destination="C:\download\destination")  # Download at specified destination
+
+downloader.download("bird", limit=100)  # Download 100 images
+
+downloader.download("dog", resize=(180, 180))  # Download with images resizing
+
+downloader.quiet = True  # Disable status messages
+
+downloader.download("fish")  # Quiet download
+```
+
+## Tests
+
+```shell
+pip install tox
+
+tox
+```
---
+++ packages/google-images-downloader/google-images-downloader-1.0.7.tar.gz/google-images-downloader-1.0.7/google_images_downloader/__main__.py
@@ -0,0 +1,88 @@
+from google_images_downloader import GoogleImagesDownloader, DEFAULT_DESTINATION, DEFAULT_LIMIT, \
+    DEFAULT_RESIZE
+import sys
+import argparse
+import os
+import re
+
+
+def get_arguments():
+    argument_parser = argparse.ArgumentParser(
+        description="Script to download images from a \"Google Images\" query",
+        formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999),
+    )
+
+    argument_parser.add_argument(
+        "-q",
+        "--query",
+        help="Google Images query" + os.linesep +
+             "Example : google-images-downloader -q cat",
+        required=True
+    )
+
+    argument_parser.add_argument(
+        "-d",
+        "--destination",
+        help="Download destination" + os.linesep +
+             "Default : ./downloads\n" + os.linesep +
+             "Example : google-images-downloader -d C:\\my\\download\\destination",
+        default=DEFAULT_DESTINATION
+    )
+
+    argument_parser.add_argument(
+        "-l",
+        "--limit",
+        help="Downloads limit" + os.linesep +
+             "Default : 50\n" + os.linesep +
+             "Example : google-images-downloader -l 500",
+        default=DEFAULT_LIMIT,
+        type=int
+    )
+
+    argument_parser.add_argument(
+        "-r",
+        "--resize",
+        help="Resize images" + os.linesep +
+             "Default : No resizing\n" + os.linesep +
+             "Example : google-images-downloader -r 180x180",
+        default=DEFAULT_RESIZE,
+    )
+
+    argument_parser.add_argument(
+        "-Q",
+        "--quiet",
+        help="Disable program output" + os.linesep +
+             "Example : google-images-downloader -q",
+        action="count"
+    )
+
+    argument_parser.add_argument(
+        "-D",
+        "--debug",
+        help="Enable debug logs, disable progression bar and messages" + os.linesep +
+             "Example : google-images-downloader -D",
+        action="count"
+    )
+    return argument_parser.parse_args(sys.argv[1:])
+
+
+def main():
+    downloader = GoogleImagesDownloader()
+
+    arguments = get_arguments()
+    downloader.init_arguments(arguments)
+
+    resize = None
+
+    if arguments.resize is not None:
+        if re.match('^[0-9]+x[0-9]+$', arguments.resize) is None:
+            raise Exception(f"Invalid size format" + os.linesep +
+                            "Expected format example : 180x180")
+        else:
+            resize = [int(x) for x in arguments.resize.split("x")]
+
+    downloader.download(arguments.query, destination=arguments.destination, limit=arguments.limit, resize=resize)
+
+
+if __name__ == "__main__":
+    main()
