1247d95d6e4a18956237747ccdc263b47395033c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:08:31 +0000

    Add package mobi-synpop/1.0.0/mobi-synpop-1.0.0.tar.gz

---
+++ packages/mobi-synpop/mobi-synpop-1.0.0.tar.gz/mobi-synpop-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mobi-synpop',
+      version='1.0.0',
+      description='Python Package by Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False)
