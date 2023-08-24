62850247d36db0eea2c6044baaf903c16b5ee9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 05:07:27 +0000

    Add package besco/0.0.1/besco-0.0.1.tar.gz

---
+++ packages/besco/besco-0.0.1.tar.gz/besco-0.0.1/setup.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+def post_install():
+    """ Implement post installation routine """
+    with open('./requirements.txt') as f:
+        install_requires = f.read().splitlines()
+
+    return install_requires
+
+
+def pre_install():
+    """ Implement pre installation routine """
+    # read the contents of your README file
+    global long_description
+    this_directory = Path(__file__).parent
+    long_description = (this_directory / "README.md").read_text()
+
+
+pre_install()
+
+
+setup(
+    name='besco',
+    version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=["besco"],
+    setup_requires=[
+        'svgpathtools'
+    ],
+    url='https://github.com/SajjadAemmi/besco',
+    license='',
+    author='Sajjad Aemmi',
+    author_email='sajjadaemmi@gmail.com',
+    description='A powerful plant identifier to identify flowers, leaves, trees, and more in seconds! 🌱',
+    include_package_data=True,
+    install_requires=post_install(),
+    entry_points={
+        "console_scripts": ["besco=besco.besco:main"],
+    },
+)
