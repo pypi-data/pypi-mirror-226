631fd4650343ede52bd504a7e1db4482a2c39e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:08:54 +0000

    Add package whylogs/1.3.0.dev1/whylogs-1.3.0.dev1.tar.gz

---
+++ packages/whylogs/whylogs-1.3.0.dev1.tar.gz/whylogs-1.3.0.dev1/whylogs/api/pyspark/experimental/__init__.py
@@ -0,0 +1,17 @@
+from .profile_distributed_upload import profile_distributed_upload
+from .profiler import (
+    collect_column_profile_views,
+    collect_dataset_profile_view,
+    column_profile_bytes_aggregator,
+    whylogs_pandas_map_profiler,
+)
+from .segmented_profiler import collect_segmented_results
+
+__ALL__ = [
+    whylogs_pandas_map_profiler,
+    column_profile_bytes_aggregator,
+    collect_column_profile_views,
+    collect_dataset_profile_view,
+    collect_segmented_results,
+    profile_distributed_upload,
+]
