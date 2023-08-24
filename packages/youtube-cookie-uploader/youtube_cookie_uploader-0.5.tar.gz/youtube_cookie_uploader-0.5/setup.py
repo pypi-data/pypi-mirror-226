2340889f3a572076b1e92c8110893a870c3ffb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:14 +0000

    Add package youtube-cookie-uploader/0.5/youtube_cookie_uploader-0.5.tar.gz

---
+++ packages/youtube-cookie-uploader/youtube_cookie_uploader-0.5.tar.gz/youtube_cookie_uploader-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='youtube_cookie_uploader',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=["selenium", "webdriver_manager"],  # Add your dependencies here
+    author='Abdulazeez Olabode',
+    author_email="dataslid@gmail.com",
+    description='A Youtube 3rd Party Library that uses selenium and webdriver_manager',
+    url='https://github.com/dataslid/youtube-cookie-uploader',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
