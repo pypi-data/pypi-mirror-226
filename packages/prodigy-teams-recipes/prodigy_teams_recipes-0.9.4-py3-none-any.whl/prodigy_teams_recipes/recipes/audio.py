8c07a6b50930fe7bff3e966831408c8e5287e2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:11:23 +0000

    Add package prodigy-teams-recipes/0.9.4/prodigy_teams_recipes-0.9.4-py3-none-any.whl

---
+++ packages/prodigy-teams-recipes/prodigy_teams_recipes-0.9.4-py3-none-any.whl/prodigy_teams_recipes/recipes/audio.py
@@ -0,0 +1,157 @@
+from dataclasses import dataclass
+from typing import List, Literal, Union
+
+from prodigy.types import RecipeSettingsType, StreamType
+from prodigy_teams_recipes_sdk import (
+    ChoiceProps,
+    Dataset,
+    Goal,
+    Input,
+    IntProps,
+    props,
+    task_recipe,
+    teams_type,
+)
+
+av_mode = ChoiceProps(
+    title="Annotation Mode",
+    description="Annotate audio regions with labels, classificate media into categories, transcribe media or add notes",
+)
+
+
+@teams_type(
+    "transcribe",
+    title="Transcribe media or add notes",
+    description="Add a free-form text input field that can be used to transcribe the audio or add other notes about the contents of the media file.",
+    field_props={"rows": IntProps(title="Text field rows", min=1, max=10, step=1)},
+)
+@dataclass
+class AVTranscription:
+    rows: int = 3
+
+
+@teams_type(
+    "regions",
+    title="Annotate audio regions",
+    description="You'll be able to highlight overlapping regions in the audio waveform and assign labels to them",
+    field_props={"label": props.label},
+)
+@dataclass
+class AVRegions:
+    label: List[str]
+
+
+@teams_type(
+    "classify",
+    title="Annotate categories",
+    description="Select one or more category labels that apply to the media file",
+    field_props={"label": props.label, "labels_exclusive": props.labels_exclusive},
+)
+@dataclass
+class AVClassification:
+    label: List[str]
+    labels_exclusive: bool = False
+
+
+@task_recipe(
+    title="Annotate Audio",
+    description="Annotate regions, assign categories to audio content or transcribe audio files.",
+    field_props={
+        "dataset": props.dataset_choice,
+        "mode": av_mode,
+        "goal": props.goal,
+    },
+)
+def audio(
+    *,
+    dataset: Dataset[Literal["audio"]],
+    input: Input,
+    mode: Union[AVRegions, AVClassification, AVTranscription],
+    goal: Goal = "nooverlap",
+) -> RecipeSettingsType:
+    return audio_video(
+        dataset,
+        input,
+        mode,
+        goal,
+    )
+
+
+@task_recipe(
+    title="Annotate Video",
+    description="Annotate regions, assign categories to video content or transcribe video files.",
+    field_props={
+        "dataset": props.dataset_choice,
+        "mode": av_mode,
+        "goal": props.goal,
+    },
+)
+def video(
+    *,
+    dataset: Dataset[Literal["video"]],
+    input: Input,
+    mode: Union[AVRegions, AVClassification, AVTranscription],
+    goal: Goal = "nooverlap",
+) -> RecipeSettingsType:
+    return audio_video(
+        dataset,
+        input,
+        mode,
+        goal,
+    )
+
+
+def audio_video(
+    dataset: Union[Dataset[Literal["video"]], Dataset[Literal["audio"]]],
+    input: Input,
+    mode: Union[AVRegions, AVClassification, AVTranscription],
+    goal: Goal = "nooverlap",
+) -> RecipeSettingsType:
+    stream = input.load(rehash=True, dedup=False)
+    overlap = goal == "overlap"
+    if isinstance(mode, AVClassification):
+        labels = mode.label
+        labels_exclusive = mode.labels_exclusive
+        is_binary = len(labels) == 1
+        stream = make_av_classification_tasks(stream, labels, is_binary)
+        view_id = "classification" if is_binary else "choice"
+        config = {
+            "feed_overlap": overlap,
+            "labels": labels,
+            "choice_style": "single" if labels_exclusive else "multiple",
+            "choice_auto_accept": labels_exclusive,
+        }
+    elif isinstance(mode, AVTranscription):
+        rows = mode.rows or 3
+        view_id = "blocks"
+        blocks = [
+            {"view_id": "audio"},
+            {"view_id": "text_input", "field_rows": rows, "field_autofocus": True},
+        ]
+        config = {"feed_overlap": overlap, "blocks": blocks}
+    else:
+        view_id = "audio_manual"
+        config = {"feed_overlap": overlap, "labels": mode.label}
+
+    return {
+        "dataset": dataset.name,
+        "stream": stream,
+        "view_id": view_id,
+        "config": config,
+    }
+
+
+def make_av_classification_tasks(
+    stream: StreamType, labels: List[str], is_binary: bool = False
+) -> StreamType:
+    if is_binary:
+        for eg in stream:
+            eg["label"] = labels[0]
+            yield eg
+    else:
+        # Add "options" to each outgoing task
+        labels = labels or []
+        options = [{"id": label, "text": label} for label in labels]
+        for eg in stream:
+            eg["options"] = options
+            yield eg
