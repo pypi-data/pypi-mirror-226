95eb93a28a7f7de6cf1a9693ed2f24e35f749e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:09:05 +0000

    Add package zetascale/0.3.1/zetascale-0.3.1-py3-none-any.whl

---
+++ packages/zetascale/zetascale-0.3.1-py3-none-any.whl/zeta/nn/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022 Agora
+# Licensed under The MIT License [see LICENSE for details]
+
+######### Attention
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+from zeta.nn import *
