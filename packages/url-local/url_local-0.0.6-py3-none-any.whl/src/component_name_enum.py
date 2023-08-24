7ae0e1304c52294639f2d9063ff833361e9392c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:08:59 +0000

    Add package url-local/0.0.6/url_local-0.0.6-py3-none-any.whl

---
+++ packages/url-local/url_local-0.0.6-py3-none-any.whl/src/component_name_enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class ComponentName(Enum):
+    MARKETPLACE_GOODS = "marketplace-goods-microservice"
+    EVENT = "event"
+    GENDER_DETECTION = "gender-detection"
+    LOGGER = "logger"
+    GROUP = "group"
+    USER_REGISTRATION = "user-registration"
+    AUTH_LOGIN = "auth"
+    WS = "websocket"
+
