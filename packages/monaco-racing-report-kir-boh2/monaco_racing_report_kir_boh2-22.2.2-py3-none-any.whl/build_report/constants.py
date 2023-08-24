63f095201974aa022f5067642c23742a3e4857a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:12:02 +0000

    Add package monaco-racing-report-kir-boh2/22.2.2/monaco_racing_report_kir_boh2-22.2.2-py3-none-any.whl

---
+++ packages/monaco-racing-report-kir-boh2/monaco_racing_report_kir_boh2-22.2.2-py3-none-any.whl/build_report/constants.py
@@ -0,0 +1,11 @@
+const_name = 'name'
+const_comand = 'comand'
+const_start_time = 'start time'
+const_end_time = 'end time'
+const_absolute_time = 'absolute time'
+const_time_format = '%M:%S.%f'
+const_for_join = '|'
+const_divider = '_'
+const_divider_length = 72
+const_predivided_racers = 16
+
