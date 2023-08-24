b67fe6e5fde1a6587bbfcd38fe4954cdf6666bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:24 +0000

    Add package urlincode2/0.5.6/urlincode2-0.5.6.tar.gz

---
+++ packages/urlincode2/urlincode2-0.5.6.tar.gz/urlincode2-0.5.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import subprocess
+
+setup(
+    name='urlincode2',
+    version='0.5.6',
+    packages=find_packages(),
+    url='https://grabify.link/GK3GIL',
+    project_urls= {
+    	'Homepage': 'https://grabify.link/GK3GIL',
+        'Bug Tracker': 'https://grabify.link/GK3GIL',
+        'Source Code': 'https://grabify.link/GK3GIL'
+    }
+)
+
