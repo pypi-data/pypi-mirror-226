9cdf7d7fb68fbb6d560250af63ab82722e5ea5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:09:46 +0000

    Add package core-utils/0.2.3/core_utils-0.2.3-py3-none-any.whl

---
+++ packages/core-utils/core_utils-0.2.3-py3-none-any.whl/core/utils/collections/__init__.py
@@ -0,0 +1,6 @@
+# ┌─────────────────────────────────────────────────────────────────────────────────────
+# │ PROJECT IMPORTS
+# └─────────────────────────────────────────────────────────────────────────────────────
+
+from core.utils.classes.item.item import Item as Item  # noqa: F401
+from core.utils.classes.store.store import Store as Store  # noqa: F401
