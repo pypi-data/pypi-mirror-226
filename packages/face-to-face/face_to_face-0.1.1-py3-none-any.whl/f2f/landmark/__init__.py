26e4ba5294dbd01744f8c8e459eeb070b1b3a73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 16:12:17 +0000

    Add package face-to-face/0.1.1/face_to_face-0.1.1-py3-none-any.whl

---
+++ packages/face-to-face/face_to_face-0.1.1-py3-none-any.whl/f2f/landmark/__init__.py
@@ -0,0 +1,1 @@
+from .synthetic import FDSyntheticLandmark2dInference, FDSyntheticLandmark2dONNX
---
+++ packages/face-to-face/face_to_face-0.1.1-py3-none-any.whl/f2f/landmark/synthetic/__init__.py
@@ -0,0 +1,2 @@
+from .model import FDSyntheticLandmark2dInference
+from .onnx import FDSyntheticLandmark2dONNX
