8c57ef94b9bab0674a8d81e698abd80cf3171744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:42 +0000

    Add package ape-arbitrum/0.6.2/ape-arbitrum-0.6.2.tar.gz

---
+++ packages/ape-arbitrum/ape-arbitrum-0.6.2.tar.gz/ape-arbitrum-0.6.2/tests/conftest.py
@@ -0,0 +1,22 @@
+import ape
+import pytest
+
+
+@pytest.fixture
+def networks():
+    return ape.networks
+
+
+@pytest.fixture
+def accounts():
+    return ape.accounts
+
+
+@pytest.fixture
+def Contract():
+    return ape.Contract
+
+
+@pytest.fixture
+def arbitrum(networks):
+    return networks.arbitrum
