963433116ea47aa323daa4b4c9922a14529708fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:56 +0000

    Add package huma-utils/0.8.4/huma_utils-0.8.4.tar.gz

---
+++ packages/huma-utils/huma_utils-0.8.4.tar.gz/huma_utils-0.8.4/huma_utils/constants.py
@@ -0,0 +1,10 @@
+HUMA_FINANCE_DOMAIN_NAME = "huma.finance"
+SERVER_ALLOW_ORIGIN_REGEX = (
+    r"^https://deploy-preview-\d+--dev-app-huma\.netlify\.app"
+    r"|https://dev\.app\.huma\.finance"
+    r"|https://app\.huma\.finance"
+    r"|https://local\.bulla\.network:1234"
+    r"|https://dev\.bulla\.network"
+    r"|https://banker\.bulla\.network"
+    r"|http://localhost:\d+$"
+)
