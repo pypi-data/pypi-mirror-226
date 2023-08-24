f6ec5c2c010fc8822393b1f5be72ace7d3d6291b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:09:41 +0000

    Add package deepeval/0.9.7/deepeval-0.9.7.tar.gz

---
+++ packages/deepeval/deepeval-0.9.7.tar.gz/deepeval-0.9.7/deepeval/test_case.py
@@ -0,0 +1,67 @@
+import hashlib
+from typing import Any, List
+from collections import UserList
+from .metrics.metric import Metric
+from .metrics.randomscore import RandomMetric
+from .metrics.factual_consistency import FactualConsistencyMetric
+
+
+class TestCase:
+    def __init__(
+        self,
+        input: Any,
+        expected_output: Any,
+        metrics: List[Metric] = None,
+        id: str = None,
+    ):
+        if metrics is None:
+            fact_consistency_metric = FactualConsistencyMetric(minimum_score=0.3)
+            self.metrics = [fact_consistency_metric]
+        else:
+            self.metrics = metrics
+        self.input = input
+        self.expected_output = expected_output
+        if id is None:
+            id_string = str(self.input) + str(self.expected_output)
+            self.id = hashlib.md5(id_string.encode()).hexdigest()
+        else:
+            self.id = id
+
+    def dict(self):
+        return {
+            "input": self.input,
+            "expected_output": self.expected_output,
+            "metrics": self.metrics,
+            "id": self.id,
+        }
+
+
+class TestCases(UserList):
+    """A list of test cases for easy access"""
+
+    @classmethod
+    def from_csv(
+        self,
+        csv_filename: str,
+        input_column: str,
+        expected_output_column: str,
+        id_column: str = None,
+        metrics: List[Metric] = None,
+    ):
+        import pandas as pd
+
+        df = pd.read_csv(csv_filename)
+        inputs = df[input_column].values
+        expected_outputs = df[expected_output_column].values
+        if id_column is not None:
+            ids = df[id_column].values
+        for i, input in enumerate(inputs):
+            self.data.append(
+                TestCase(
+                    input=input,
+                    expected_output=expected_outputs[i],
+                    metrics=metrics,
+                    id=ids[i],
+                )
+            )
+        return self
