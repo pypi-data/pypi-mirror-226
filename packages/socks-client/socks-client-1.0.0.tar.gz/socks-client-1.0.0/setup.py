ccbc31179aea166cdfe491b787878fd17ce5d60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:11:23 +0000

    Add package socks-client/1.0.0/socks-client-1.0.0.tar.gz

---
+++ packages/socks-client/socks-client-1.0.0.tar.gz/socks-client-1.0.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+from setuptools import setup, find_packages
+
+base_path = os.path.dirname(__file__)
+
+
+with open(os.path.join(base_path, 'README.md')) as f:
+    long_description = f.read()
+
+with open(os.path.join(base_path, 'socks_client/__version__.py')) as f:
+    try:
+        VERSION = re.findall(r"^__version__ = '([^']+)'\r?$", f.read(), re.M)[0]
+    except:
+        VERSION = None
+
+
+setup(
+    name="socks-client",
+    version=VERSION,
+    description="Supports both TCP and UDP client with the implementation of SOCKS5 and SOCKS4 protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Plattanus",
+    author_email="plattanus@outlook.com",
+    url="https://github.com/plattanus/socks-client.git",
+    license="MIT License",
+    
+    keywords=['socks', 'socks5', 'socks4', 'proxy', 'asyncio'],
+    install_requires=[],
+    python_requires=">=3.7",
+    packages=[
+        'socks_client',
+        # 'socks_client.__init__.py',
+        # 'socks_client.__version__',
+        # 'socks_client._errors',
+        # 'socks_client._type',
+        # 'socks_client.tcp_async',
+        # 'socks_client.tcp_sync',
+        # 'socks_client.udp_async',
+        # 'socks_client.udp_sync',
+    ]
+)
+
