fd7fbacd85b62c5affae31c9f2c273971c1da052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:01 +0000

    Add package swagger-ui-py/23.8.23/swagger-ui-py-23.8.23.tar.gz

---
+++ packages/swagger-ui-py/swagger-ui-py-23.8.23.tar.gz/swagger-ui-py-23.8.23/setup.py
@@ -0,0 +1,32 @@
+import platform
+from distutils.version import StrictVersion
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+if StrictVersion(platform.python_version()) < StrictVersion('3.0.0'):
+    raise Exception("`swagger-ui-py` support python version >= 3.0.0 only.")
+
+setup(
+    name='swagger-ui-py',
+    version='23.08.23',
+    description=(
+        'Swagger UI for Python web framework, '
+        'such as Tornado, Flask, Quart, Sanic and Falcon.'
+    ),
+    long_description=Path(__file__).parent.joinpath('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    package_data={
+        'swagger_ui': ['static/*', 'templates/*'],
+    },
+    install_requires=[
+        "jinja2>=2.0",
+        "PyYaml>=5.0",
+    ],
+    url='https://github.com/PWZER/swagger-ui-py',
+    author='PWZER',
+    author_email='pwzergo@gmail.com',
+)
