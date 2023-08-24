fb37849841c202d2dd376db46bf210c171f77741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:06 +0000

    Add package bumplus/0.11.0/bumplus-0.11.0.tar.gz

---
+++ packages/bumplus/bumplus-0.11.0.tar.gz/bumplus-0.11.0/bumplus/__main__.py
@@ -0,0 +1,4 @@
+"""Entry for ``python -m bumplus ...``."""
+from __future__ import absolute_import, division, print_function
+
+__import__("bumplus").main()
---
+++ packages/bumplus/bumplus-0.11.0.tar.gz/bumplus-0.11.0/bumplus/constants.py
@@ -0,0 +1,4 @@
+"""Constants."""
+from __future__ import absolute_import, division, print_function
+
+CONF_FILE = ".bumplus.toml"
