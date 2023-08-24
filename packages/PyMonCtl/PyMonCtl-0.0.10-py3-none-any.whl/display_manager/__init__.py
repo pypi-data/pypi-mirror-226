769a334a858c58be3bf810ad3391f5437f14b91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:12:17 +0000

    Add package PyMonCtl/0.0.10/PyMonCtl-0.0.10-py3-none-any.whl

---
+++ packages/PyMonCtl/PyMonCtl-0.0.10-py3-none-any.whl/display_manager/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Incomplete type stubs for pyobjc
+# mypy: disable_error_code = no-any-return
+from __future__ import annotations
+
+import sys
+assert sys.platform == "darwin"
+
+from display_manager import *
+from display_manager.display_manager_lib import *
