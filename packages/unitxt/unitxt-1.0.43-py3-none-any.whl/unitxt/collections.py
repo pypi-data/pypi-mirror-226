4ea8bfc3512f999f56be92c4b6d26e8a20f4118d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:11:06 +0000

    Add package unitxt/1.0.43/unitxt-1.0.43-py3-none-any.whl

---
+++ packages/unitxt/unitxt-1.0.43-py3-none-any.whl/unitxt/collections.py
@@ -0,0 +1,55 @@
+from abc import abstractmethod
+from dataclasses import field
+
+from .artifact import Artifact
+from .random_utils import random
+
+
+class Collection(Artifact):
+    @abstractmethod
+    def __getitem__(self, key):
+        pass
+
+
+class ListCollection(Collection):
+    items: list = field(default_factory=list)
+
+    def __getitem__(self, index):
+        return self.items[index]
+
+    def __len__(self):
+        return len(self.items)
+
+    def append(self, item):
+        self.items.append(item)
+
+    def extend(self, other):
+        self.items.extend(other.items)
+
+    def __add__(self, other):
+        return ListCollection(self.items.__add__(other.items))
+
+
+class DictCollection(Collection):
+    items: dict = field(default_factory=dict)
+
+    def __getitem__(self, key):
+        return self.items[key]
+
+
+class ItemPicker(Artifact):
+    item: object = None
+
+    def __call__(self, collection: Collection):
+        try:
+            return collection[int(self.item)]
+        except (SyntaxError, KeyError, ValueError) as e:  # in case picking from a dictionary
+            return collection[self.item]
+
+
+class RandomPicker(Artifact):
+    def __call__(self, collection: Collection):
+        if isinstance(collection, ListCollection):
+            return random.choice(list(collection.items))
+        elif isinstance(collection, DictCollection):
+            return random.choice(list(collection.items.values()))
