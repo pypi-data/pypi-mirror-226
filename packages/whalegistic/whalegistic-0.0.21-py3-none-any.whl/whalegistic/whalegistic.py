28000418040c3ff895b586e6f79b133a67791720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:03 +0000

    Add package whalegistic/0.0.21/whalegistic-0.0.21-py3-none-any.whl

---
+++ packages/whalegistic/whalegistic-0.0.21-py3-none-any.whl/whalegistic/whalegistic.py
@@ -0,0 +1,56 @@
+import jwt
+import requests
+
+class Whalegistic:
+
+	def __init__(this, public_key, private_key):
+		this.pri_key = private_key;
+		this.pub_key = public_key;
+		this.token = jwt.encode({ "public_key": public_key }, private_key, algorithm="HS256");
+		print(f"TOKEN : {this.token}")
+
+
+
+	def getBrands(this, send_obj):
+
+		send_url = "https://whalegistic.com/api/get-brands"
+
+		if send_obj == None: 
+			send_obj = {}
+
+		send_obj["public_key"] = this.pub_key
+
+		send_headers = { 
+			"Content-Type": "application/json", 
+			"Authorization": f"Bearer {this.token}" 
+		}
+
+		response = requests.post(url = send_url, json = send_obj, headers = send_headers)
+
+		print(f"JSON Response : { response.json() }")
+		print(f'BRANDS : {response}')
+
+		return response;
+
+
+
+	def getProducts(this, send_obj):
+
+		send_url = "https://whalegistic.com/api/get-products"
+
+		if send_obj == None: 
+			send_obj = {}
+
+		send_obj["public_key"] = this.pub_key
+
+		send_headers = { 
+			"Content-Type": "application/json", 
+			"Authorization": f"Bearer {this.token}" 
+		}
+
+		response = requests.post(url = send_url, json = send_obj, headers = send_headers)
+
+		print(f"JSON Response : { response.json() }")
+		print(f'PRODUCTS : { response }')
+
+		return response;
