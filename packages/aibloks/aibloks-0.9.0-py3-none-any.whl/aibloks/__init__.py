188b3642901e8d0121ac6bde0b0643929be2dd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:10:03 +0000

    Add package aibloks/0.9.0/aibloks-0.9.0-py3-none-any.whl

---
+++ packages/aibloks/aibloks-0.9.0-py3-none-any.whl/aibloks/__init__.py
@@ -0,0 +1,14 @@
+# Ai Bloks Python SDK
+# Copyright 2023 Ai Bloks
+# See LICENSE for details.
+
+__version__ = '0.9.0'
+__author__ = 'Ai Bloks'
+__license__ = 'Apache License 2.0'
+
+from .client import AiBloksClient
+from .errors import AiBloksException
+from .libraries import Libraries
+from .library import Library
+from .requestor import Requestor
+from .instantai import InstantAi
