d5b714a8de68ccfda4a3d2cd26d0271700a449b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:14 +0000

    Add package yadm/2.0.9/yadm-2.0.9.tar.gz

---
+++ packages/yadm/yadm-2.0.9.tar.gz/yadm-2.0.9/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+# python setup.py sdist --formats=bztar
+
+version = '2.0.9'
+description = 'Yet Another Document Mapper (ODM) for MongoDB'
+long_description = open('README.rst', 'rb').read().decode('utf8')
+
+
+setup(
+    name='yadm',
+    version=version,
+    description=description,
+    long_description=long_description,
+    author='Zelenyak "ZZZ" Alexander',
+    author_email='zzz.sochi@gmail.com',
+    url='https://github.com/zzzsochi/yadm',
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'pymongo>=3.7.0',
+        'zope.dottedname',
+        'python-dateutil',
+        'pytz',
+        'Faker',
+    ],
+    extras_require={
+        'asyncio': [
+            'motor>=2.0.0',
+        ],
+        'test': [
+            'pytest',
+            'pytest-asyncio',
+            'pytest-cov',
+            'coveralls'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Database',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+
+    packages=['yadm', 'yadm.aio', 'yadm.fields', 'yadm.fields.money'],
+)
