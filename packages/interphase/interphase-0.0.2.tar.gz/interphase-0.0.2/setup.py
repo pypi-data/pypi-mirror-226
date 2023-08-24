41235a85dc65917d5ee450634eb1c3c186f908e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 05:07:50 +0000

    Add package interphase/0.0.2/interphase-0.0.2.tar.gz

---
+++ packages/interphase/interphase-0.0.2.tar.gz/interphase-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='interphase',
+    version='0.0.2',
+    # install_requires=[
+    #     'requests',
+    #     'importlib-metadata; python_version == "3.8"',
+    # ],
+    packages=find_packages(
+        where='interphase',
+        include=['*'],
+        exclude=[],
+    ),
+    # entry_points={
+    #     'console_scripts': [
+    #         'cli-name = mypkg.mymodule:some_func',
+    #     ]
+    # }
+)
