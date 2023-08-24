f1b44d7387af92b8001d1a148e72fc318d6730aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 05:08:15 +0000

    Add package cicdservice/0.1/cicdservice-0.1.tar.gz

---
+++ packages/cicdservice/cicdservice-0.1.tar.gz/cicdservice-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(name='cicdservice',
+version='0.1',
+description='Testing installation of Package',
+#url='#',
+author='auth',
+author_email='author@email.com',
+license='MIT',
+#packages=['servicecicd'],
+packages=find_packages(),
+#home_page='https://pypi.org/project/mujoco-dev/',
+#url='https://github.com/novateva/NewVersionPackage',
+zip_safe=False)
