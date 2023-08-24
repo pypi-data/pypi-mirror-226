9fe0bf3a0457d5a392976e09c6c5df2cfc99d0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:09:26 +0000

    Add package vcenter-bindings/0.1/vcenter-bindings-0.1.tar.gz

---
+++ packages/vcenter-bindings/vcenter-bindings-0.1.tar.gz/vcenter-bindings-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vcenter-bindings",
+    version="0.1",
+    author="Hiren Vikhram",
+    description="Python bindings for vCenter",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
