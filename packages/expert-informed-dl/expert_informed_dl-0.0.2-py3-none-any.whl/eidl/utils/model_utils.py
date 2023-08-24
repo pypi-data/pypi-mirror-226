5f46f4bc0d71336e5e5cf51cc3d2baa4e60cafad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:09:34 +0000

    Add package expert-informed-dl/0.0.2/expert_informed_dl-0.0.2-py3-none-any.whl

---
+++ packages/expert-informed-dl/expert_informed_dl-0.0.2-py3-none-any.whl/eidl/utils/model_utils.py
@@ -0,0 +1,93 @@
+import os
+import pickle
+import tempfile
+import urllib
+
+import timm
+import torch
+
+from eidl.Models.ExpertAttentionViT import ViT_LSTM
+from eidl.Models.ExtensionModels import ExpertTimmVisionTransformer
+from eidl.datasets.OCTDataset import load_oct_image
+
+
+def get_vit_model(model_name, image_size, depth, device):
+    if model_name == 'base':
+        model = ViT_LSTM(image_size=reverse_tuple(image_size), num_patches=32, num_classes=2, embed_dim=128, depth=depth, heads=1,
+                         mlp_dim=2048, weak_interaction=False).to(device)
+    else:  # assuming any other name is timm models
+        model = timm.create_model(model_name, img_size=reverse_tuple(image_size), pretrained=True, num_classes=2)  # weights from 'https://storage.googleapis.com/vit_models/augreg/L_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.1-sd_0.1.npz', official Google JAX implementation
+        model = ExpertTimmVisionTransformer(model).to(device)
+    return model, model.get_grid_size()
+
+def reverse_tuple(t):
+    if len(t) == 0:
+        return t
+    else:
+        return(t[-1],)+reverse_tuple(t[:-1])
+
+def parse_model_parameter(model_config_string: str, parameter_name: str):
+    assert parameter_name in model_config_string
+    parameter_string = [x for x in model_config_string.split('_') if parameter_name in x][0]
+    parameter_value = parameter_string.split('-')[1]
+    if parameter_name == 'dist':
+        return parameter_string.strip(f'{parameter_name}-')
+    elif parameter_name in ['alpha', 'dist', 'depth', 'lr']:
+        return float(parameter_string.strip(f'{parameter_name}-'))
+    elif parameter_name == 'model':
+        return model_config_string[:model_config_string.find('_alpha')].split('-')[1]
+    else:
+        return parameter_value
+
+
+def get_trained_model(device):
+    """
+    to use the model returned by this function, user should use model_utils.load_image and pass the returns (image_mean, image_std, image_size)
+    as arguments.
+    Parameters
+    ----------
+    device
+
+    Returns
+    a tuple of four items
+    model: the trained model
+    image_mean: means of the RGB channels of the data on which the model is trained
+    image_std: stds of the
+    image_size: the size of the image used by the model
+    -------
+
+    """
+    image_size = 1024, 512
+    model_name = 'base'
+    depth = 1
+
+    github_file_url = "https://raw.githubusercontent.com/ApocalyVec/ExpertInformedDL/master/trained_model/0.0.1"
+    model_url = f"{github_file_url}/best_model-base_alpha-0.01_dist-cross-entropy_depth-1_lr-0.0001_statedict.pt"
+    image_mstd_url = f"{github_file_url}/image_means_stds.p"
+    compound_label_encoder_url = f"{github_file_url}/compound_label_encoder.p"
+
+    temp_dir = tempfile.mkdtemp()
+    model_file_path = os.path.join(temp_dir, "model_weights.pt")
+    image_mstd_file_path = os.path.join(temp_dir, "image_means_stds.pt")
+    compound_label_encoder_file_path = os.path.join(temp_dir, "compound_label_encoder.p")
+
+    # Download the file using urlretrieve
+    urllib.request.urlretrieve(model_url, model_file_path)
+    urllib.request.urlretrieve(image_mstd_url, image_mstd_file_path)
+    urllib.request.urlretrieve(compound_label_encoder_url, compound_label_encoder_file_path)
+
+    print(f"File downloaded successfully and saved to {model_file_path}")
+    model, grid_size = get_vit_model(model_name, image_size=image_size, depth=depth, device=device)
+    model.load_state_dict(torch.load(model_file_path))
+
+    image_mean, image_std = pickle.load(open(image_mstd_file_path, 'rb'))
+
+    compound_label_encoder = pickle.load(open(compound_label_encoder_file_path, 'rb'))
+    return model, image_mean, image_std, image_size, compound_label_encoder
+
+def load_image(image_path, image_size, image_mean, image_std):
+    image = load_oct_image(image_path, image_size)
+    image_normalized = (image - image_mean) / image_std
+    # transpose to channel first
+    image_normalized = image_normalized.transpose((2, 0, 1))
+    return image_normalized, image
