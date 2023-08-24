711e9e00d18315f082c5ce89592d1d0d2f71b419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:09:12 +0000

    Add package new-pyproject/0.1.9/new_pyproject-0.1.9-py3-none-any.whl

---
+++ packages/new-pyproject/new_pyproject-0.1.9-py3-none-any.whl/new_pyproject/__init__.py
@@ -0,0 +1,16 @@
+
+"""
+new python project description
+"""
+
+# flit uses this as one source version #
+# note: pep whatever doesnt like > 'x.y.<number>c'
+# for example: 0.01.08d doesnt fly building from pyproject.toml
+
+__version__ = '0.01.09'
+
+# import acts to export 
+
+from .code import Project
+
+
