cbac41bc4de95ee6385b2dfc309086a2922f1cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:10:15 +0000

    Add package anonpi/0.2.12/anonpi-0.2.12.tar.gz

---
+++ packages/anonpi/anonpi-0.2.12.tar.gz/anonpi-0.2.12/anonpi/control/_api.py
@@ -0,0 +1,32 @@
+from typing import Literal
+from requests import request
+from json import dumps
+# from ...resources import ServerSideError
+
+class apibaserequest:
+    def __init__(self,menthod:Literal["get","post","delete","patch"],url:str,token:str,**data):
+        self._method = menthod
+        self._url = url
+        self._token = token
+        self._data = data
+
+    def __repr__(self):
+        _headers = {
+            "Authorization": self._token
+        }
+        res = request(
+            method=self._method,
+            url=self._url,
+            json= self._data,
+            headers=_headers
+        )
+        try:
+            return dumps(res.json())
+        except:
+            raise Exception("Server side error occured")
+
+
+
+
+
+    
