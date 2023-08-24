1b6117543610a3f628280a56d8c645bb735f95e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:01 +0000

    Add package redk-share-common/2.0.0/redk_share_common-2.0.0-py3-none-any.whl

---
+++ packages/redk-share-common/redk_share_common-2.0.0-py3-none-any.whl/share_common/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module Name: __init__
+Description: 关于多个微服务之间的共享配置
+Author: jason.jiang@cloudfall.cn
+Date: 2023-08-23
+Version: 2.0
+"""
+
+import logging
---
+++ packages/redk-share-common/redk_share_common-2.0.0-py3-none-any.whl/share_common/configurations/plugins.py
@@ -0,0 +1,6 @@
+# [public] 默认配置
+# CORSHEADERS 配置，解决跨域
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = "*"
+CSRF_TRUSTED_ORIGINS = ["https://ixcloud-test.cloudfall.cn:8002"]
