34142f06acbbba100d8490a4d33182e60751653d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:10:52 +0000

    Add package System-to-Matlab/0.0.4/System_to_Matlab-0.0.4-py3-none-any.whl

---
+++ packages/System-to-Matlab/System_to_Matlab-0.0.4-py3-none-any.whl/System_to_Matlab/Systems/System.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class System(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self._Equations: list = []
+        self._Parameters: list = []
+        self._Inputs: list = []
+        self._Outputs: list = []
+
+    
+        
+    @abstractmethod 
+    def addInput(self, input: Any, name:str) -> None:
+        pass
+        
+    @abstractmethod  
+    def addOutput(self, output: Any, name:str) -> None:
+        pass
+    
+    
