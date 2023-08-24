c0b3500c47429d6c7c9954a6d74876bbe1cefc08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:14 +0000

    Add package jlogr/0.1.212/jlogr-0.1.212.tar.gz

---
+++ packages/jlogr/jlogr-0.1.212.tar.gz/jlogr-0.1.212/src/structures/logger.rs
@@ -0,0 +1,118 @@
+use crate::structures::logging::Log;
+use pyo3::prelude::*;
+use std::{fs, io::Write};
+
+/// Handles reading and writing of logs to the file system.
+/// Keeps a buffer of logs in memory, but can dump to file.
+#[pyclass]
+pub struct Logger {
+    pub logs_path: String,
+    pub logs_buffer: Vec<Log>,
+}
+
+#[pymethods]
+impl Logger {
+    #[new]
+    #[pyo3(text_signature = "(logs_path)")]
+    #[pyo3(signature = (logs_path))]
+    pub fn new(logs_path: String) -> Logger {
+        Logger {
+            logs_path,
+            logs_buffer: Vec::new(),
+        }
+    }
+
+    #[pyo3(name = "push_log")]
+    #[pyo3(text_signature = "(message, level, module, function, class)")]
+    #[pyo3(signature = (
+        message,
+        level,
+        module,
+        function,
+        class))]
+    pub fn push_log(
+        &mut self,
+        message: &str,
+        level: &str,
+        module: Option<&str>,
+        function: Option<&str>,
+        class: Option<&str>,
+    ) {
+        let log = Log::new(message, level, module, function, class);
+        self.logs_buffer.push(log);
+    }
+
+    #[pyo3(name = "push_log_from_log")]
+    #[pyo3(text_signature = "(log)")]
+    #[pyo3(signature = (log))]
+    pub fn push_log_from_log(&mut self, log: Log) {
+        self.logs_buffer.push(log);
+    }
+
+    #[pyo3(name = "display_list_of_logs")]
+    #[pyo3(text_signature = "()")]
+    #[pyo3(signature = ())]
+    pub fn display_list_of_logs(&self) {
+        for log in &self.logs_buffer {
+            log.pretty_print()
+        }
+    }
+
+    #[pyo3(name = "pull_logs_from_file")]
+    #[pyo3(text_signature = "()")]
+    #[pyo3(signature = ())]
+    pub fn pull_logs_from_file(&mut self) {
+        fs::read_to_string(&self.logs_path)
+            .unwrap_or_else(|e| {
+                eprintln!("Failed to read file: {}", e.to_string());
+                return "".to_string();
+            })
+            .lines()
+            .for_each(|line| {
+                self.logs_buffer.push(Log::from_log_string(line));
+            });
+    }
+
+    #[pyo3(name = "sort_logs_by_ctime")]
+    #[pyo3(text_signature = "()")]
+    #[pyo3(signature = ())]
+    pub fn sort_logs_by_ctime(&mut self) {
+        self.logs_buffer
+            .sort_by(|a, b| a.created_at.cmp(&b.created_at));
+    }
+
+    #[pyo3(name = "dump_logs_to_file")]
+    #[pyo3(text_signature = "()")]
+    #[pyo3(signature = ())]
+    pub fn dump_logs_to_file(&self) {
+        let mut file = fs::OpenOptions::new()
+            .append(true)
+            .open(&self.logs_path)
+            .expect("Open to work properly");
+        for log in &self.logs_buffer {
+            file.write_all(format!("{}\n", log).as_bytes())
+                .unwrap_or_else(|e| {
+                    eprintln!("Failed to write to file: {}", e.to_string());
+                });
+        }
+        file.sync_all().unwrap_or_else(|e| {
+            eprintln!("Failed to sync file: {}", e.to_string());
+        });
+    }
+
+    #[pyo3(name = "clear_log_buffer")]
+    #[pyo3(text_signature = "()")]
+    #[pyo3(signature = ())]
+    pub fn clear_log_buffer(&mut self) {
+        self.logs_buffer.clear();
+    }
+
+    #[pyo3(name = "clear_log_file")]
+    #[pyo3(text_signature = "()")]
+    #[pyo3(signature = ())]
+    pub fn clear_log_file(&self) {
+        fs::write(&self.logs_path, "").unwrap_or_else(|e| {
+            eprintln!("Failed to clear file: {}", e.to_string());
+        });
+    }
+}
