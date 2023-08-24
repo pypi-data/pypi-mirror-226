23ab742e6d212dca38ea34fcd4e6e92b78742f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:10:13 +0000

    Add package wwwml/0.2.0/wwwml-0.2.0-py3-none-any.whl

---
+++ packages/wwwml/wwwml-0.2.0-py3-none-any.whl/wwwml/__main__.py
@@ -0,0 +1,26 @@
+from os import environ
+
+import openai
+from langchain.agents import AgentType, Tool, initialize_agent
+from langchain.chat_models import ChatOpenAI
+
+from wwwml.tools import load_tools
+
+INIT_PROMPT = "My Linux server is slow. Use providered function to check the server."
+
+
+def main():
+    if "WWWML_DEPLOYMENT" in environ:
+        llm = ChatOpenAI(temperature=0, deployment_id=environ["WWWML_DEPLOYMENT"])
+    else:
+        llm = ChatOpenAI(
+            temperature=0, model=environ.get("WWWML_MODEL", "gpt-3.5-turbo")
+        )
+    agent = initialize_agent(
+        load_tools(llm), llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True
+    )
+    agent.run(INIT_PROMPT)
+
+
+if __name__ == "__main__":
+    main()
