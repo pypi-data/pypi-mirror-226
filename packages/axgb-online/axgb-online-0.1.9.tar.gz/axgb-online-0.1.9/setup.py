480898ea9ad94a543e97f891ec1e84825c700073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:32 +0000

    Add package axgb-online/0.1.9/axgb-online-0.1.9.tar.gz

---
+++ packages/axgb-online/axgb-online-0.1.9.tar.gz/axgb-online-0.1.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="axgb-online",
+    packages=[
+        "adaxgboost",
+    ],
+    version="0.1.9",
+    description="ada-xgboost package",
+    install_requires=[
+        "numpy==1.23.4",
+        "pandas==1.4.0",
+        "scikit-learn>=1.0.2",
+        "tqdm==4.61.0",
+        "river==0.14.0",
+        "xgboost==1.6.2"
+    ]
+)
