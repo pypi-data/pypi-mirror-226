089735e44baf73b979d91ca92ae7c9f9329eb8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:44 +0000

    Add package ammarpy/0.1/ammarpy-0.1.tar.gz

---
+++ packages/ammarpy/ammarpy-0.1.tar.gz/ammarpy-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ammarpy',
+      version='0.1',
+      description='Just for fun, doesn\'t contains any useful function',
+      packages=['ammarpy'],
+      author_email='sjahnaviprasad@gmail.com',
+      zip_safe=False)
