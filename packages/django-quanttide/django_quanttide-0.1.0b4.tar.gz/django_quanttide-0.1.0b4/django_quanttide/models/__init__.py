b0f1c825c8abd0ba1b0c8c3545410d7cb1fae8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:47 +0000

    Add package django-quanttide/0.1.0b4/django_quanttide-0.1.0b4.tar.gz

---
+++ packages/django-quanttide/django_quanttide-0.1.0b4.tar.gz/django_quanttide-0.1.0b4/django_quanttide/models/__init__.py
@@ -0,0 +1,31 @@
+"""
+模型层
+"""
+
+from .choices import StageChoices
+from .fields import (
+    IDField, NumberField, NameField,
+    VerboseNameField, TitleField, DescriptionField,
+    TypeField, StatusField, StageField,
+    CreatedAtField, UpdatedAtField,
+    CreatedByField, UpdatedByField,
+)
+from .models import Model
+from .polymorphic import PolymorphicModel
+
+__all__ = [
+    'Model', 'IDField',
+    'NumberField',
+    'NameField',
+    'VerboseNameField',
+    'TitleField',
+    'DescriptionField',
+    'TypeField',
+    'StatusField',
+    'StageField',
+    'StageChoices',
+    'CreatedAtField',
+    'UpdatedAtField',
+    'CreatedByField',
+    'UpdatedByField',
+]
