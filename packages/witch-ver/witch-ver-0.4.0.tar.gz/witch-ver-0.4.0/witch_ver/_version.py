157f09b252b0658c54a943eee864f5df71c12cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 05:08:08 +0000

    Add package witch-ver/0.4.0/witch-ver-0.4.0.tar.gz

---
+++ packages/witch-ver/witch-ver-0.4.0.tar.gz/witch-ver-0.4.0/witch_ver/_version.py
@@ -0,0 +1,15 @@
+"""Static module version information
+"""
+
+version_dict = {
+    "tag": "v0.4.0",
+    "tag_prefix": "v",
+    "sha": "00a0b977414555d87206c833fe09380d50657160",
+    "sha_abbrev": "00a0b97",
+    "branch": None,
+    "date": "2023-08-23T13:02:50-07:00",
+    "dirty": False,
+    "distance": 0,
+    "pretty_str": "0.4.0",
+    "git_dir": None
+}
