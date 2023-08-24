e8dc8131d328f5ec2e2fda5f5981ed0f4a896686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:07:53 +0000

    Add package dynadojo/0.2.4/dynadojo-0.2.4.tar.gz

---
+++ packages/dynadojo/dynadojo-0.2.4.tar.gz/dynadojo-0.2.4/dynadojo/systems/__init__.py
@@ -0,0 +1,14 @@
+from . import opinion
+from . import lv
+from . import epidemic
+from .lds import LDSSystem
+from .ctln import CTLNSystem
+from .ca import CASystem
+from .snn import SNNSystem
+from .lorenz import LorenzSystem
+from .kuramoto import KuramotoSystem
+''' 
+must run first: pip install dynadojo[nbody] 
+NBody uses packages only supported on Linux/Mac OSX
+'''
+# from .santi import NBodySystem
---
+++ packages/dynadojo/dynadojo-0.2.4.tar.gz/dynadojo-0.2.4/dynadojo/utils/__init__.py
@@ -0,0 +1,5 @@
+from . import lds
+from . import ca
+from . import lv
+from . import opinion
+from . import kuramoto
