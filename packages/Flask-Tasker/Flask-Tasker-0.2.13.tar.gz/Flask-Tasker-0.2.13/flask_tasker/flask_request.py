3e2438592db00eef843edce3b11df5997270455f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:11:39 +0000

    Add package Flask-Tasker/0.2.13/Flask-Tasker-0.2.13.tar.gz

---
+++ packages/Flask-Tasker/Flask-Tasker-0.2.13.tar.gz/Flask-Tasker-0.2.13/flask_tasker/flask_request.py
@@ -0,0 +1,29 @@
+class FlaskRequest():
+
+    def __init__(self, request):
+        self.args = request.args
+        self.method = request.method
+        self.headers = request.headers
+        self.cookies = request.cookies
+        self.content_type = request.content_type
+        self.files = request.files
+        self.environ = request.environ
+        self.remote_addr = request.remote_addr
+        self.url = request.url
+        self.base_url = request.base_url
+        self.url_root = request.url_root
+        self.host_url = request.host_url
+        self.host = request.host
+        self.script_root = request.script_root
+        self.path = request.path
+        self.full_path = request.full_path
+
+        if self.content_type == 'application/json':
+            self.json = request.json
+        else:
+            self.json = {}
+
+        if self.content_type in ['application/x-www-form-urlencoded', 'multipart/form-data']:
+            self.form = request.form
+        else:
+            self.form = {}
