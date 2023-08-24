2494b70bebb46ec55c950744db6c5959e92fa518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:20 +0000

    Add package ouija/1.2.4/ouija-1.2.4-py3-none-any.whl

---
+++ packages/ouija/ouija-1.2.4-py3-none-any.whl/ouija/server.py
@@ -0,0 +1,86 @@
+import asyncio
+import logging
+import sys
+
+from .tuning import StreamTuning, DatagramTuning
+from .telemetry import StreamTelemetry, DatagramTelemetry
+from .relay import StreamRelay, DatagramRelay
+from .proxy import StreamProxy, DatagramProxy
+from .config import Config, Mode, Protocol
+
+
+async def main_async() -> None:
+    if len(sys.argv[1:]) != 1:
+        print('Usage: ouija <config.json>')
+        sys.exit(0)
+
+    config = Config(path=sys.argv[1])
+
+    logging.basicConfig(
+        format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+        datefmt='%Y-%m-%d:%H:%M:%S',
+        level=logging.DEBUG if config.debug else logging.ERROR,
+    )
+
+    match config.protocol:
+        case Protocol.TCP:
+            telemetry_class, relay_class, proxy_class = StreamTelemetry, StreamRelay, StreamProxy
+            tuning = StreamTuning(
+                fernet=config.fernet,
+                token=config.token,
+                serving_timeout=config.serving_timeout,
+                tcp_buffer=config.tcp_buffer,
+                tcp_timeout=config.tcp_timeout,
+                message_timeout=config.message_timeout,
+            )
+        case Protocol.UDP:
+            telemetry_class, relay_class, proxy_class = DatagramTelemetry, DatagramRelay, DatagramProxy
+            tuning = DatagramTuning(
+                fernet=config.fernet,
+                token=config.token,
+                serving_timeout=config.serving_timeout,
+                tcp_buffer=config.tcp_buffer,
+                tcp_timeout=config.tcp_timeout,
+                udp_payload=config.udp_payload,
+                udp_timeout=config.udp_timeout,
+                udp_retries=config.udp_retries,
+                udp_capacity=config.udp_capacity,
+                udp_resend_sleep=config.udp_resend_sleep,
+            )
+        case _:
+            raise NotImplementedError
+
+    match config.mode:
+        case Mode.RELAY:
+            server = relay_class(
+                telemetry=telemetry_class(),
+                tuning=tuning,
+                relay_host=config.relay_host,
+                relay_port=config.relay_port,
+                proxy_host=config.proxy_host,
+                proxy_port=config.proxy_port,
+            )
+        case Mode.PROXY:
+            server = proxy_class(
+                telemetry=telemetry_class(),
+                tuning=tuning,
+                proxy_host=config.proxy_host,
+                proxy_port=config.proxy_port,
+            )
+        case _:
+            raise NotImplementedError
+
+    if config.monitor:
+        asyncio.create_task(server.debug())
+
+    await server.serve()
+
+
+def main() -> None:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main_async())
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ouija/ouija-1.2.4-py3-none-any.whl/ouija/telemetry.py
@@ -0,0 +1,139 @@
+from dataclasses import dataclass
+import datetime
+
+
+@dataclass(kw_only=True)
+class StreamTelemetry:
+    active: int = 0
+    opened: int = 0
+    closed: int = 0
+    bytes_sent: int = 0
+    bytes_recv: int = 0
+    token_errors: int = 0
+    timeout_errors: int = 0
+    connection_errors: int = 0
+    serving_errors: int = 0
+
+    def __str__(self) -> str:
+        return \
+            f'{datetime.datetime.now()}\n' \
+            f'\tactive: {self.active:,}\n' \
+            f'\topened|closed: {self.opened:,}|{self.closed:,}\n' \
+            f'\tbytes sent|received: {self.bytes_sent:,}|{self.bytes_recv:,}\n' \
+            f'\ttoken|timeout|connection|serving errors: {self.token_errors:,}|{self.timeout_errors:,}' \
+            f'|{self.connection_errors:,}|{self.serving_errors:,}'
+
+    def collect(self, *, active: int) -> None:
+        self.active = active
+
+    def open(self) -> None:
+        self.opened += 1
+
+    def close(self) -> None:
+        self.closed += 1
+
+    def send(self, *, data: bytes) -> None:
+        self.bytes_sent += len(data)
+
+    def recv(self, *, data: bytes) -> None:
+        self.bytes_recv += len(data)
+
+    def token_error(self) -> None:
+        self.token_errors += 1
+
+    def timeout_error(self) -> None:
+        self.timeout_errors += 1
+
+    def connection_error(self) -> None:
+        self.connection_errors += 1
+
+    def serving_error(self) -> None:
+        self.serving_errors += 1
+
+
+@dataclass(kw_only=True)
+class DatagramTelemetry:
+    active: int = 0
+    opened: int = 0
+    closed: int = 0
+    packets_sent: int = 0
+    packets_recv: int = 0
+    bytes_sent: int = 0
+    bytes_recv: int = 0
+    min_packet_size: int = 0
+    max_packet_size: int = 0
+    processing_errors: int = 0
+    token_errors: int = 0
+    type_errors: int = 0
+    timeout_errors: int = 0
+    connection_errors: int = 0
+    serving_errors: int = 0
+    resending_errors: int = 0
+    send_buf_overloads: int = 0
+    recv_buf_overloads: int = 0
+
+    def __str__(self) -> str:
+        return \
+            f'{datetime.datetime.now()}\n' \
+            f'\tactive: {self.active:,}\n' \
+            f'\topened|closed: {self.opened:,}|{self.closed:,}\n' \
+            f'\tpackets sent|received: {self.packets_sent:,}|{self.packets_recv:,}\n' \
+            f'\tbytes sent|received: {self.bytes_sent:,}|{self.bytes_recv:,}\n' \
+            f'\tmin|max packet size: {self.min_packet_size}|{self.max_packet_size}\n' \
+            f'\tprocessing|token|type errors: {self.processing_errors:,}|{self.token_errors:,}' \
+            f'|{self.type_errors:,}\n' \
+            f'\ttimeout|connection|serving|resending errors: {self.timeout_errors:,}|{self.connection_errors:,}' \
+            f'|{self.serving_errors:,}|{self.resending_errors:,}\n' \
+            f'\tsend|recv buf overloads: {self.send_buf_overloads:,}|{self.recv_buf_overloads:,}'
+
+    def collect(self, *, active: int) -> None:
+        self.active = active
+
+    def open(self) -> None:
+        self.opened += 1
+
+    def close(self) -> None:
+        self.closed += 1
+
+    def send(self, *, data: bytes) -> None:
+        self.packets_sent += 1
+        self.bytes_sent += len(data)
+        if len(data) < self.min_packet_size or self.min_packet_size == 0:
+            self.min_packet_size = len(data)
+        if len(data) > self.max_packet_size:
+            self.max_packet_size = len(data)
+
+    def recv(self, *, data: bytes) -> None:
+        self.packets_recv += 1
+        self.bytes_recv += len(data)
+        if len(data) < self.min_packet_size or self.min_packet_size == 0:
+            self.min_packet_size = len(data)
+        if len(data) > self.max_packet_size:
+            self.max_packet_size = len(data)
+
+    def processing_error(self) -> None:
+        self.processing_errors += 1
+
+    def token_error(self) -> None:
+        self.token_errors += 1
+
+    def type_error(self) -> None:
+        self.type_errors += 1
+
+    def timeout_error(self) -> None:
+        self.timeout_errors += 1
+
+    def connection_error(self) -> None:
+        self.connection_errors += 1
+
+    def serving_error(self) -> None:
+        self.serving_errors += 1
+
+    def resending_error(self) -> None:
+        self.resending_errors += 1
+
+    def send_buf_overload(self) -> None:
+        self.send_buf_overloads += 1
+
+    def recv_buf_overload(self) -> None:
+        self.recv_buf_overloads += 1
