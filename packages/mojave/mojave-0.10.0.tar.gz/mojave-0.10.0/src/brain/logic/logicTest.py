7ad44fd5d6eba86dce2a816d3ab7ca00fb759824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:14 +0000

    Add package mojave/0.10.0/mojave-0.10.0.tar.gz

---
+++ packages/mojave/mojave-0.10.0.tar.gz/mojave-0.10.0/src/brain/logic/logicTest.py
@@ -0,0 +1,3 @@
+# There are a few things we need to do go really understand the logic of the system.
+# The human input is a goal. the human specifies what their learning goal is and how long they have to achieve it.
+# The system then uses the goal to determine a schedule of subjects to learn in order to achive that goal.
