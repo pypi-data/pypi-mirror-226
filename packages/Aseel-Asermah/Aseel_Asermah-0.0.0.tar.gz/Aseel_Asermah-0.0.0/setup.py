73640ed7dc0c714ae7a3ee912384ccf1cb22adcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 04:11:53 +0000

    Add package Aseel-Asermah/0.0.0/Aseel_Asermah-0.0.0.tar.gz

---
+++ packages/Aseel-Asermah/Aseel_Asermah-0.0.0.tar.gz/Aseel_Asermah-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools 
+setuptools.setup(
+name= 'Aseel_Asermah',
+versoin='0.1',
+uther=' Aseel Asermah',
+description =' what teacher asked',
+packages= setuptools.find_packages(),
+classifiers=[
+    "Programming Language :: Python :: 3",
+    " Operating System :: OS Independent ",
+    " License :: OSI Approved :: MIT License",
+]
+)
