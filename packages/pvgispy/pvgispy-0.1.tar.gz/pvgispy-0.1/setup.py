49ca9c720e8cc2512ab83da6b91ad35924d80d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:12:12 +0000

    Add package pvgispy/0.1/pvgispy-0.1.tar.gz

---
+++ packages/pvgispy/pvgispy-0.1.tar.gz/pvgispy-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pvgispy",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests"
+    ],
+    license='MIT',
+)
