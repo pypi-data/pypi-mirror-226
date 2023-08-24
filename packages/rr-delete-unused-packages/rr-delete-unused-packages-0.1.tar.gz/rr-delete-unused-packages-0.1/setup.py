efedff33ecd19c1695a1b3dad8cf875960920c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:35 +0000

    Add package rr-delete-unused-packages/0.1/rr-delete-unused-packages-0.1.tar.gz

---
+++ packages/rr-delete-unused-packages/rr-delete-unused-packages-0.1.tar.gz/rr-delete-unused-packages-0.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_file = 'requirements.txt'
+full_path = '/'.join([lib_folder, requirement_file])
+
+install_requires = []
+if os.path.isfile(full_path):
+    with open(full_path) as f:
+        install_requires = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='rr-delete-unused-packages',
+      version='0.1',
+      description='rr-delete-unused-packages',
+      url='https://upload.pypi.org/legacy/',
+      author='Yossi Arar',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author_email='yossiarar@gmail.com',
+      install_requires=install_requires,
+      packages=find_packages(),
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent"],
+      python_requires='>=3.10',
+      )
