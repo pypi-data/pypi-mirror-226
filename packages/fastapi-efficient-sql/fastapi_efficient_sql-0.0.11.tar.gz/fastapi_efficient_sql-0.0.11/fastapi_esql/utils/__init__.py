538cf8897b0e8c3da18fcf79f78990d82972c0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 15:09:52 +0000

    Add package fastapi-efficient-sql/0.0.11/fastapi_efficient_sql-0.0.11.tar.gz

---
+++ packages/fastapi-efficient-sql/fastapi_efficient_sql-0.0.11.tar.gz/fastapi_efficient_sql-0.0.11/fastapi_esql/utils/__init__.py
@@ -0,0 +1,5 @@
+from .converter import convert_dicts
+from .cursor_handler import CursorHandler
+from .decorator import timing
+from .metaclass import Singleton
+from .sqlizer import Cases, RawSQL, SQLizer
