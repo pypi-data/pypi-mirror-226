969f1bf1bc7a8489ad9a38917931c892dde281f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 14:09:33 +0000

    Add package python-carrier-infinity/0.0.3/python_carrier_infinity-0.0.3-py3-none-any.whl

---
+++ packages/python-carrier-infinity/python_carrier_infinity-0.0.3-py3-none-any.whl/python_carrier_infinity/gql_schemas/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2022-present @mileswu <mileswu@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+
+"""Module containing all the GraphQL queries"""
+from .get_user import get_user_query
+from .get_status import get_status_query
+from .get_config import get_config_query
+from .update_config import update_config_mode_query
+from .update_zone_config import update_zone_config_query
+from .update_activity import (
+    update_activity_fan_query,
+    update_activity_temp_query,
+)
