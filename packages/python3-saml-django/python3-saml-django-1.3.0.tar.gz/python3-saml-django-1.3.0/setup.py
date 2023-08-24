dbe741405aeee26edffb3fd116f766fb079d3c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 05:07:49 +0000

    Add package python3-saml-django/1.3.0/python3-saml-django-1.3.0.tar.gz

---
+++ packages/python3-saml-django/python3-saml-django-1.3.0.tar.gz/python3-saml-django-1.3.0/setup.py
@@ -0,0 +1,50 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    # Start the description after the badges
+    start = long_description.find('#')
+    long_description = long_description[start:]
+
+setuptools.setup(
+    name="python3-saml-django",
+    version="1.3.0",
+    author="THON Technology",
+    author_email="technology@thon.org",
+    description="Implement SAML Single Sign-On in your Django project quickly and easily.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/penn-state-dance-marathon/python3-saml-django",
+    packages=['django_saml'],
+    install_requires=['python3-saml'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    python_requires='>=3',
+    extras_require={
+        'test': (
+            'django',
+            'coverage',
+            'pylint',
+            'flake8',
+            'flake8-docstrings',
+            'isort',
+            'codecov'
+        ),
+    },
+)
