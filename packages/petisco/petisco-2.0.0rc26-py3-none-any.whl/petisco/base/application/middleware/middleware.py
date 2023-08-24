cf2f098f5b8adf6be9e84508013ac6dba458ced6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:09:22 +0000

    Add package petisco/2.0.0rc26/petisco-2.0.0rc26-py3-none-any.whl

---
+++ packages/petisco/petisco-2.0.0rc26-py3-none-any.whl/petisco/base/application/middleware/middleware.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Any, Union
+
+from meiga import AnyResult
+
+
+class Middleware(ABC):
+    """
+    A base class to model your Middleware.
+    A Middleware works before some operations (Controller and Subscriber) and also before returning the final result.
+    """
+
+    wrapped_class_name: Union[str, None] = None
+    wrapped_class_input_arguments: Union[str, None] = None
+
+    def set_data(
+        self, wrapped_class_name: str, wrapped_class_input_arguments: Any
+    ) -> None:
+        self.wrapped_class_name = wrapped_class_name
+        self.wrapped_class_input_arguments = wrapped_class_input_arguments
+
+    @abstractmethod
+    def before(self) -> None:
+        pass
+
+    @abstractmethod
+    def after(self, result: AnyResult) -> None:
+        pass
