480f60d2ceacc33bf10c8bc25575e1f31c91a916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-24 03:11:56 +0000

    Add package zetascale/0.2.7/zetascale-0.2.7-py3-none-any.whl

---
+++ packages/zetascale/zetascale-0.2.7-py3-none-any.whl/zeta/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2022 Agora
+# Licensed under The MIT License [see LICENSE for details]
+
+#architecture
+from zeta.nn.architecture.transformer import (
+    AttentionLayers,
+    Decoder,
+    Encoder,
+    Transformer,
+    ViTransformerWrapper,
+)
+
+
+#attentions
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+from zeta.nn.attention.flash_attention import FlashAttention
+from zeta.nn.attention.dilated_attention import DilatedAttention
+
+#models
+from zeta.models.gpt4 import GPT4, GPT4MultiModal
+
+
+#######
+from zeta.nn import *
+from zeta.models import *
+from zeta.training import *
+
+
+
+
+
+#tokenizers
+from zeta.training.multi_modal_tokenizer import MultiModalTokenizer
+from zeta.training.language_tokenizer import LanguageTokenizerGPTX
