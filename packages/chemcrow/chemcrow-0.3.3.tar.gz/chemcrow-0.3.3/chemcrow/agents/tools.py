12d3e3d3296d8c218da6c920db3150f087708971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:50 +0000

    Add package chemcrow/0.3.3/chemcrow-0.3.3.tar.gz

---
+++ packages/chemcrow/chemcrow-0.3.3.tar.gz/chemcrow-0.3.3/chemcrow/agents/tools.py
@@ -0,0 +1,25 @@
+import os
+
+from langchain import agents
+from langchain.base_language import BaseLanguageModel
+
+from chemcrow.tools import *
+
+
+def make_tools(llm: BaseLanguageModel, verbose=True):
+    serp_key = os.getenv("SERP_API_KEY")
+
+    all_tools = agents.load_tools(["python_repl"]) #, "human"])
+
+    all_tools += [
+        Query2SMILES(),
+        Query2CAS(),
+        PatentCheck(),
+        MolSimilarity(),
+        SMILES2Weight(),
+        FuncGroups(),
+        #LitSearch(llm=llm, verbose=verbose),
+    ]
+    if serp_key:
+        all_tools.append(WebSearch())
+    return all_tools
