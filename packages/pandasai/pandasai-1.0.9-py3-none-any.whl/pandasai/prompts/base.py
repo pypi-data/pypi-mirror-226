86267ec5f316666404ed88d2b2afc82287d9fb19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:11:05 +0000

    Add package pandasai/1.0.9/pandasai-1.0.9-py3-none-any.whl

---
+++ packages/pandasai/pandasai-1.0.9-py3-none-any.whl/pandasai/prompts/base.py
@@ -0,0 +1,45 @@
+""" Base class to implement a new Prompt
+In order to better handle the instructions, this prompt module is written.
+"""
+
+from pandasai.exceptions import MethodNotImplementedError
+from string import Formatter
+
+
+class Prompt:
+    """Base class to implement a new Prompt"""
+
+    text = None
+    _args = {}
+
+    def __init__(self, **kwargs):
+        """
+        __init__ method of Base class of Prompt Module
+        Args:
+            **kwargs: Inferred Keyword Arguments
+        """
+        if kwargs:
+            self._args = kwargs
+
+        """Set all the variables with underscore prefix with default value as DEFAULT
+        This will prevent any possible key errors if anyone tries to print 
+        prompt before running .run method"""
+        if self.text:
+            vars_ = [
+                fn for _, fn, _, _ in Formatter().parse(self.text) if fn is not None
+            ]
+            for var in vars_:
+                if var[0] == "_" and var not in self._args:
+                    self._args[var] = var
+
+    def override_var(self, var, value):
+        self._args[var] = value
+
+    def to_string(self):
+        if self.text is None:
+            raise MethodNotImplementedError
+
+        return self.text.format(**self._args)
+
+    def __str__(self):
+        return self.to_string()
