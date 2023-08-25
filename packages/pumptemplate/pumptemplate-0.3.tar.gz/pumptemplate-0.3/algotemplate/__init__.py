a41d2ca8a22462064c5109d124a05eb93b77f3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:11:57 +0000

    Add package pumptemplate/0.3/pumptemplate-0.3.tar.gz

---
+++ packages/pumptemplate/pumptemplate-0.3.tar.gz/pumptemplate-0.3/algotemplate/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return (u'Hello world We are Dhanapal/Kiran/Ramya/Prasanna working hard for Digital Twin platform on 24 Aug 2023.')
---
+++ packages/pumptemplate/pumptemplate-0.3.tar.gz/pumptemplate-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pumptemplate',
+      version='0.3',
+      description='The funniest joke in the world',
+      url='http://github.com/dhanapalschandran/algotemplate',
+      author='Dhanapal Chandran',
+      author_email='dhanapal@gmail.com',
+      license='MIT',
+      packages=['algotemplate'],
+      zip_safe=False)
