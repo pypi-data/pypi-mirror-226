812da9720cbe2a844571aaad24163512f6fda5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:11:47 +0000

    Add package hljm/1.1/hljm-1.1.tar.gz

---
+++ packages/hljm/hljm-1.1.tar.gz/hljm-1.1/setup.py
@@ -0,0 +1,12 @@
+
+import setuptools
+
+setuptools.setup(
+    name='hljm',
+    version='1.1',
+    description='A description of your library',
+    author='黑龙',
+    author_email='2076293320@qq.com',
+    packages=setuptools.find_packages(),
+    long_description_content_type="hljm"
+)
