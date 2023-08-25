32c274857cc64b8bfe9da240f9f12c0684c8665c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:11:58 +0000

    Add package sen2nbar/2023.8.1/sen2nbar-2023.8.1.tar.gz

---
+++ packages/sen2nbar/sen2nbar-2023.8.1.tar.gz/sen2nbar-2023.8.1/sen2nbar/__init__.py
@@ -0,0 +1,7 @@
+"""sen2nbar - Nadir BRDF Adjusted Reflectance (NBAR) for Sentinel-2 in Python"""
+
+__version__ = "2023.8.1"
+__author__ = "David Montero Loaiza <dml.mont@gmail.com>"
+__all__ = []
+
+from . import *
