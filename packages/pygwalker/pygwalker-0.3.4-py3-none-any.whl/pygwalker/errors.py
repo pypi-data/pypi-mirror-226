331e7d7e1343f19b53c307e56280a128ef94b58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:10:26 +0000

    Add package pygwalker/0.3.4/pygwalker-0.3.4-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.3.4-py3-none-any.whl/pygwalker/errors.py
@@ -0,0 +1,23 @@
+"""
+    This module contains all the custom errors used by pygwalker.
+"""
+
+
+class BaseError(Exception):
+    """Base class for all exceptions raised by pygwalker."""
+    pass
+
+
+class InvalidConfigIdError(BaseError):
+    """Raised when the config is invalid."""
+    pass
+
+
+class PrivacyError(BaseError):
+    """Raised when the privacy setting is invalid."""
+    pass
+
+
+class CloudFunctionError(BaseError):
+    """Raised when the cloud function is invalid."""
+    pass
