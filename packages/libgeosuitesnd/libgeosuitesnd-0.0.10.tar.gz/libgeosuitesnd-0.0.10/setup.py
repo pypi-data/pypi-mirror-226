e84682ff9aeb29d2826be90331c852850c3015c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:11:18 +0000

    Add package libgeosuitesnd/0.0.10/libgeosuitesnd-0.0.10.tar.gz

---
+++ packages/libgeosuitesnd/libgeosuitesnd-0.0.10.tar.gz/libgeosuitesnd-0.0.10/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import setuptools
+import os
+
+setuptools.setup(
+    name='libgeosuitesnd',
+    version='0.0.10',
+    description='Parser for the GeoSuite<tm> SND export format',
+    long_description="""Parser for the GeoSuite<tm> SND export format""",
+    long_description_content_type="text/markdown",
+    author='Egil Moeller, Craig William Christensen and others ',
+    author_email='em@emeraldgeo.no, cch@emeraldgeo.no',
+    url='https://github.com/emerald-geomodelling/libgeosuitesnd',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={'libgeosuitesnd': ['*/*.csv']},
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    entry_points = {
+        'libsgfdata.parsers': ['snd=libgeosuitesnd:parse'],
+    }
+)
