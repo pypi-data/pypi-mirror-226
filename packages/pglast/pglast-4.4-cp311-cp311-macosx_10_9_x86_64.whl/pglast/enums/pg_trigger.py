a2178e942dee0caec8887e50a9ebdda6ab73bdcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:10:40 +0000

    Add package pglast/4.4/pglast-4.4-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/pglast/pglast-4.4-cp311-cp311-macosx_10_9_x86_64.whl/pglast/enums/pg_trigger.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# :Project:   pglast -- DO NOT EDIT: automatically extracted from pg_trigger.h @ 14-3.0.0-0-g397cbb9
+# :Author:    Lele Gaifax <lele@metapensiero.it>
+# :License:   GNU General Public License version 3 or later
+# :Copyright: © 2017-2023 Lele Gaifax
+#
+
+from enum import Enum, IntEnum, IntFlag, auto
+
+try:
+    from enum import StrEnum
+except ImportError:  # pragma: no cover
+    # Python < 3.10
+    class StrEnum(str, Enum):
+        pass
+
+
+
+# #define-ed constants
+
+TriggerConstraintIndexId = 2699
+
+TriggerRelidNameIndexId = 2701
+
+TriggerOidIndexId = 2702
+
+TRIGGER_TYPE_ROW = 1 << 0
+
+TRIGGER_TYPE_BEFORE = 1 << 1
+
+TRIGGER_TYPE_INSERT = 1 << 2
+
+TRIGGER_TYPE_DELETE = 1 << 3
+
+TRIGGER_TYPE_UPDATE = 1 << 4
+
+TRIGGER_TYPE_TRUNCATE = 1 << 5
+
+TRIGGER_TYPE_INSTEAD = 1 << 6
+
+TRIGGER_TYPE_STATEMENT = 0
+
+TRIGGER_TYPE_AFTER = 0
