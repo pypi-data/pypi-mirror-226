4e9578bc42c26ae16b2633d5e2a56a4e3207ff33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:10:34 +0000

    Add package cca-zoo/2.3.2/cca_zoo-2.3.2.tar.gz

---
+++ packages/cca-zoo/cca_zoo-2.3.2.tar.gz/cca_zoo-2.3.2/cca_zoo/linear/_gradient/__init__.py
@@ -0,0 +1,14 @@
+from ._ey import CCAEY, PLSEY
+from ._gh import CCAGH
+
+from ._stochasticpls import PLSStochasticPower
+from ._svd import CCASVD, PLSSVD
+
+__all__ = [
+    "CCAEY",
+    "PLSEY",
+    "CCAGH",
+    "CCASVD",
+    "PLSSVD",
+    "PLSStochasticPower",
+]
