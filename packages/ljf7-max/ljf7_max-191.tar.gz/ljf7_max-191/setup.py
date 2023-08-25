3a564ce2eb859e3b844eef34353d8a6fbe724f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:11:30 +0000

    Add package ljf7-max/191/ljf7_max-191.tar.gz

---
+++ packages/ljf7-max/ljf7_max-191.tar.gz/ljf7_max-191/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "ljf7_max",
+    version = "191",
+    description = "Count max frequency in a string",
+    author = "Andy Woz",
+    py_modules = ["max.max"],
+    long_description = "I am Andy Woz, it is nice to meet pypi.org. I can write scripts"
+)
