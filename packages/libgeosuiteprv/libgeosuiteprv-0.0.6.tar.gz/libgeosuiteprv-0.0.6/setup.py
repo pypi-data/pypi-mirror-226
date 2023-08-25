59115ec45435a2f1633689d6ec9ec141df2795cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:11:17 +0000

    Add package libgeosuiteprv/0.0.6/libgeosuiteprv-0.0.6.tar.gz

---
+++ packages/libgeosuiteprv/libgeosuiteprv-0.0.6.tar.gz/libgeosuiteprv-0.0.6/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import setuptools
+import os
+
+setuptools.setup(
+    name='libgeosuiteprv',
+    version='0.0.6',
+    description='Parser for the GeoSuite<tm> PRV export format',
+    long_description="""Parser for the GeoSuite<tm> PRV export format""",
+    long_description_content_type="text/markdown",
+    author='Egil Moeller, Craig William Christensen and others ',
+    author_email='em@emeraldgeo.no, cch@emeraldgeo.no',
+    url='https://github.com/emerald-geomodelling/libgeosuiteprv',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+    entry_points = {
+        'libsgfdata.parsers': ['prv=libgeosuiteprv:parse'],
+    }
+)
