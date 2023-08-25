77d6a3c4c6b9ba343c599d6f8c81102765893a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:11:30 +0000

    Add package OverDose/0.0.1/OverDose-0.0.1.tar.gz

---
+++ packages/OverDose/OverDose-0.0.1.tar.gz/OverDose-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r",encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OverDose",
+    version="0.0.1",
+    author="Izuru Inose",
+    author_email="i.inose0304@gmail.com",
+    description="Overdose of drugs in the US",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/i-inose/OverDose",
+    project_urls={
+        "Bug Tracker":
+            "https://github.com/i-inose/OverDose",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['OverDose'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'OverDose = OverDose:main'
+        ]
+    },
+)
