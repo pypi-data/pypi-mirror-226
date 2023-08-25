9131966f70e70312003ed6c29fb09ec3317d8e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:10:34 +0000

    Add package isTrue/0.0.19/isTrue-0.0.19.tar.gz

---
+++ packages/isTrue/isTrue-0.0.19.tar.gz/isTrue-0.0.19/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+setuptools.setup(
+    name="isTrue",
+    version="0.0.19",
+    description="IDK???",
+    keywords="IDK???",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.4',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "License :: Free To Use But Restricted",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+
+    ],
+    package_data={"isTrue": ["LICENSE.txt", "README.md"]},
+)
