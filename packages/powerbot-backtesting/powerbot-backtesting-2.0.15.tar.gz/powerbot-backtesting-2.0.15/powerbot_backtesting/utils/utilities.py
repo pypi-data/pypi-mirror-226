9259f61cc04be0c57d9fac47ccb2b7dcd554fda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:10:38 +0000

    Add package powerbot-backtesting/2.0.15/powerbot-backtesting-2.0.15.tar.gz

---
+++ packages/powerbot-backtesting/powerbot-backtesting-2.0.15.tar.gz/powerbot-backtesting-2.0.15/powerbot_backtesting/utils/utilities.py
@@ -0,0 +1,42 @@
+import certifi
+import pandas as pd
+from powerbot_client import ApiClient, Configuration
+from pathlib import Path
+
+
+def init_client(api_key: str, host: str) -> ApiClient:
+    """
+    Initializes PowerBot Client to enable data requests by the API.
+
+    Args:
+        api_key (str): API Key for PowerBot
+        host (str): Host URL for PowerBot
+
+    Returns:
+        PowerBot ApiClient Object
+    """
+    config = Configuration(api_key={'api_key_security': api_key}, host=host, ssl_ca_cert=certifi.where())
+    return ApiClient(config)
+
+
+def generate_input_file(cache_path: Path):
+    """
+    Generates a csv file to put positions and signals into to use with the BacktestingAlgo
+
+    Args:
+        cache_path (Path): The cache_path
+
+    Returns:
+        csv file
+    """
+    # File creation
+    input_file = pd.DataFrame(columns=["delivery_start", "delivery_end", "position", "price", "side"])
+
+    # Caching
+    cache_path.mkdir(parents=True, exist_ok=True)
+
+    # File name
+    f_count = 1
+    while cache_path.joinpath(f"backtesting_input_{f_count}.csv").exists():
+        f_count += 1
+    input_file.to_csv(cache_path.joinpath(f"backtesting_input_{f_count}.csv"), sep=";", index=False)
